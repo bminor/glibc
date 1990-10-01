bf08d6fe6323acc49fe0fe188aae16ab82830fd2
Roland McGrath <roland@gnu.org>
1990-10-01 20:47:23 +0000

    Initial revision

---
+++ locale/C-monetary.c
@@ -0,0 +1,25 @@
+/* Built-in monetary information for `C' locale,
+   generated Thu Jan  1 00:00:00 1970 by li2c.  */
+
+#include <ansidecl.h>
+#include <localeinfo.h>
+#include <stddef.h>
+
+
+struct monetary_info __monetary_C =
+  {
+    (char *) "", (char *) "",
+    (char *) "", (char *) "",
+    (char *) "",
+    (char *) "", (char *) "",
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX,
+    CHAR_MAX
+  };
+
+CONST struct monetary_info *_monetary_info = &__monetary_C;
---
+++ sysdeps/unix/siglist.c
@@ -0,0 +1,43 @@
+#include <ansidecl.h>
+#include <stddef.h>
+
+
+/* This is a list of all known signal numbers.  */
+
+CONST char *CONST _sys_siglist[] =
+  {
+    "Signal 0",
+    "Hangup",
+    "Interrupt",
+    "Quit",
+    "Illegal instruction",
+    "Trace/BPT trap",
+    "IOT trap",
+    "EMT trap",
+    "Floating point exception",
+    "Killed",
+    "Bus error",
+    "Segmentation fault",
+    "Bad system call",
+    "Broken pipe",
+    "Alarm clock",
+    "Terminated",
+    "Urgent I/O condition",
+    "Stopped (signal)",
+    "Stopped",
+    "Continued",
+    "Child exited",
+    "Stopped (tty input)",
+    "Stopped (tty output)",
+    "I/O possible",
+    "Cputime limit exceeded",
+    "Filesize limit exceeded",
+    "Virtual timer expired",
+    "Profiling timer expired",
+    "Window changed",
+    "Resource lost",
+    "User defined signal 1",
+    "User defined signal 2",
+    NULL
+  };
+
