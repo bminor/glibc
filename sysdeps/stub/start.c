2c4b6dff111cb19f5c6be389b16b2e69ffc4cf1c
Roland McGrath <roland@gnu.org>
1990-10-05 05:43:14 +0000

    Initial revision

---
+++ sysdeps/stub/start.c
@@ -0,0 +1,7 @@
+/* This file should define the low-level program entry point,
+   which should set up `__environ', and then do:
+     __libc_init();
+     exit(main(argc, argv, __environ));
+
+   This file should be prepared to be the first thing in the text section (on
+   Unix systems), or otherwise appropriately special.  */
