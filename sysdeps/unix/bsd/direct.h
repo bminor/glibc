38cfb705fea2ece1ade0e5c8525f910e2282abfa
Roland McGrath <roland@gnu.org>
1990-10-31 09:21:51 +0000

    Initial revision

---
+++ sysdeps/unix/bsd/direct.h
@@ -0,0 +1,11 @@
+#ifndef	MAXNAMLEN
+#define	MAXNAMLEN	255
+#endif
+
+struct direct
+  {
+    unsigned long int d_fileno;
+    unsigned short int d_reclen;
+    unsigned short int d_namlen;
+    char d_name[MAXNAMLEN + 1];
+  };
