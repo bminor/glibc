b20a6e1e45a2e5f37f7e97441a81a665fec4857b
Roland McGrath <roland@gnu.org>
1991-01-30 09:59:51 +0000

    Formerly generic/varargs.h.~2~

--- sysdeps/generic/varargs.h
+++ sysdeps/generic/varargs.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1990 Free Software Foundation, Inc.
+/* Copyright (C) 1991 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or modify
@@ -20,6 +20,30 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define	_VARARGS_H	1
 #include <features.h>
 
+#ifdef	__GNUC__
+
+#define va_alist  __builtin_va_alist
+#define va_dcl    int __builtin_va_alist;
+#define va_list   char *
+
+#ifdef __sparc__
+#define va_start(AP) 						\
+ (__builtin_saveregs (),					\
+  AP = ((void *) &__builtin_va_alist))
+#else
+#define va_start(AP)  AP=(char *) &__builtin_va_alist
+#endif
+#define va_end(AP)
+
+#define __va_rounded_size(TYPE)  \
+  (((sizeof (TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
+
+#define va_arg(AP, TYPE)					\
+ (*((TYPE *) (AP += __va_rounded_size (TYPE),			\
+	      AP - __va_rounded_size (TYPE))))
+
+#else	/* Not GCC.  */
+
 /* Implement varargs on top of our stdarg implementation.  */
 
 #include <stdarg.h>
@@ -30,5 +54,7 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #undef	va_start
 #define	va_start(ap)	(__va_start((ap), __va_fakearg), \
 			 (ap) -= sizeof(__va_fakearg))
+
+#endif	/* GCC.  */
 
 #endif	/* varargs.h  */
