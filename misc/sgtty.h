153337929b2ca581747a6f566e61b85c4ca8f83e
Roland McGrath <roland@gnu.org>
1991-04-11 00:08:14 +0000

    Initial revision

---
+++ misc/sgtty.h
@@ -0,0 +1,32 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the GNU C Library; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifndef	_SGTTY_H
+#define	_SGTYY_H	1
+#include <features.h>
+
+#include <sys/ioctl.h>
+
+
+/* Fill in *PARAMS with terminal parameters associated with FD.  */
+extern int EXFUN(gtty, (int __fd, struct sgttyb *__params));
+
+/* Set the terminal parameters associated with FD to *PARAMS.  */
+extern int EXFUN(stty, (int __fd, CONST struct sgttyb *__params));
+
+
+#endif	/* sgtty.h  */
