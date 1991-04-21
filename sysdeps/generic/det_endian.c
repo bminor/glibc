acb2acf3894b11f96a5bc5126d0df331322a5754
Roland McGrath <roland@gnu.org>
1991-04-21 20:54:55 +0000

    Initial revision

---
+++ sysdeps/generic/det_endian.c
@@ -0,0 +1,44 @@
+/* Determine the "endianess" of the CPU.
+   Copyright (C) 1991 Free Software Foundation, Inc.
+   Contributed by Torbjorn Granlund (tege@sics.se).
+
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the GNU C Library; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include <stdio.h>
+
+main()
+{
+  unsigned long int i;
+
+  puts("#ifndef\t_ENDIAN_H\n#define\t_ENDIAN_H\t1");
+
+  if (sizeof(i) != 4)
+    puts("#error \"Not a 32-bit machine!\"");
+
+  i = (((((('A' << 8) + 'B') << 8) + 'C') << 8) + 'D');
+
+  if (!strncmp ("ABCD", (char *) &i, 4))
+    puts ("#define __BIG_ENDIAN");
+  else if (!strncmp ("DCBA", (char *) &i, 4))
+    puts ("#define __LITTLE_ENDIAN");
+  else
+    puts ("#define __MIXED_ENDIAN");
+
+  puts("#endif\t/* endian.h */");
+
+  exit (0);
+}
