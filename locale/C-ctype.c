1500be090d2d33c456c45eed74569fb64cfb29ca
Roland McGrath <roland@gnu.org>
1991-07-17 18:56:25 +0000

    entered into RCS

--- locale/C-ctype.c
+++ locale/C-ctype.c
@@ -7,8 +7,8 @@ extern CONST struct ctype_ctype_info __ctype_ctype_C;
 extern CONST struct ctype_mbchar_info __ctype_mbchar_C;
 CONST struct ctype_info __ctype_C =
   {
-    &__ctype_ctype_C,
-    &__ctype_mbchar_C
+    (struct ctype_ctype_info*)&__ctype_ctype_C,
+    (struct ctype_mbchar_info*) &__ctype_mbchar_C
   };
 
 CONST struct ctype_info *_ctype_info = &__ctype_C;
