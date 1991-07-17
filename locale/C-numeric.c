c46604f6fe811ba0c3ddd4a67290ea4fe54abedc
Roland McGrath <roland@gnu.org>
1991-07-17 02:57:08 +0000

    Formerly ../locale/C-numeric.c.~3~

--- locale/C-numeric.c
+++ locale/C-numeric.c
@@ -1,12 +1,9 @@
-/* Built-in numeric information for `C' locale,
-   generated Thu Jan  1 00:00:00 1970 by li2c.  */
-
 #include <ansidecl.h>
 #include <localeinfo.h>
 #include <stddef.h>
 
 
-struct numeric_info __numeric_C =
+CONST struct numeric_info __numeric_C =
   {
     (char *) ".", (char *) "",
     (char *) "\177"
