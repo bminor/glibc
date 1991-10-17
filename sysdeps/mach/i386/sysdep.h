c4ca8144ae2ac5750c6d8d0b414697e0c1ea07b2
Roland McGrath <roland@gnu.org>
1991-10-17 23:57:32 +0000

    Initial revision

---
+++ sysdeps/mach/i386/sysdep.h
@@ -0,0 +1,50 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <sysdeps/mach/sysdep.h>
+
+#ifdef	__STDC__
+#define	ENTRY(name)							      \
+  .globl _##name;							      \
+  .align 4;								      \
+  _##name##:
+#else
+#define	ENTRY(name)							      \
+  .globl _/**/name;							      \
+  .align 4;								      \
+  _/**/name/**/:
+#endif
+
+#ifdef	__STDC__
+#define	PSEUDO(name, syscall_name)					      \
+  .text;								      \
+  .align 4								      \
+  ENTRY (name)								      \
+  lea SYS_##syscall_name, %eax;						      \
+  lcall $7, $0								      \
+#else
+#define	PSEUDO(name, syscall_name)					      \
+  .text;								      \
+  .align 4								      \
+  ENTRY (name)								      \
+  lea SYS_/**/syscall_name, %eax;					      \
+  lcall $7, $0								      \
+#endif
+
+#define	r0	%eax
+#define	r1	%edx
