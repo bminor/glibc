ec3819a0ad6fcc720a9027b911d5230e2b182be7
Roland McGrath <roland@gnu.org>
1991-10-23 14:44:15 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/fsync.c
@@ -0,0 +1,42 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <hurd.h>
+
+/* Make all changes done to FD actually appear on disk.  */
+int
+DEFUN(fsync, (fd), int fd)
+{
+  error_t err;
+  __mutex_lock (&_hurd_dtable.lock);
+  if (fd < 0 || fd >= _hurd_dtable.size ||
+      _hurd_dtable.d[fd].server == MACH_PORT_NULL)
+    {
+      __mutex_unlock (&_hurd_dtable.lock);
+      errno = EBADF;
+      return -1;
+    }
+  err = __file_sync (_hurd_dtable.d[fd].server);
+  __mutex_unlock (&_hurd_dtable.lock);
+  if (err)
+    return __hurd_fail (err);
+  return 0;
+}
---
+++ sysdeps/mach/hurd/geteuid.c
@@ -0,0 +1,44 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <hurd.h>
+
+/* Get the effective user ID of the calling process.  */
+uid_t
+DEFUN_VOID(__geteuid)
+{
+  uid_t euid;
+  __mutex_lock (&_hurd_idlock);
+  if (!_hurd_id_valid)
+    {
+      error_t err = __auth_getids (_hurd_auth, &_hurd_id);
+      if (err)
+	{
+	  __mutex_unlock (&_hurd_idlock);
+	  errno = __hurd_errno (err);
+	  return -1;
+	}
+      _hurd_id_valid = 1;
+    }
+  euid = _hurd_id.euid;
+  __mutex_unlock (&_hurd_idlock);
+  return euid;
+}
