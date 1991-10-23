0379a943fe701b222da1566c09e84ce345e04a4c
Roland McGrath <roland@gnu.org>
1991-10-23 19:19:33 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/execve.c
@@ -0,0 +1,94 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <hurd.h>
+
+/* Replace the current process, executing PATH with arguments ARGV and
+   environment ENVP.  ARGV and ENVP are terminated by NULL pointers.  */
+int
+DEFUN(__execve, (path, argv, envp),
+      CONST char *path AND
+      char *CONST argv[] AND char *CONST envp[])
+{
+  error_t err;
+  file_t file;
+  char *args, *env, *ap;
+  size_t argslen, envlen;
+  int ints[INIT_INT_MAX];
+  mach_port_t ports[INIT_PORT_MAX];
+  file_t *dtable;
+  int dtablesize;
+  int i;
+  char *const *p;
+
+  file = __hurd_path_lookup (path, FS_LOOKUP_EXECUTE, 0);
+  if (file == MACH_PORT_NULL)
+    return -1;
+
+  argslen = 0;
+  p = argv;
+  while (*p != NULL)
+    argslen += strlen (*p++) + 1;
+  args = alloca (argslen);
+  ap = args;
+  for (p = argv; *p != NULL; ++p)
+    ap = __memccpy (ap, *p, '\0', UINT_MAX);
+
+  envlen = 0;
+  p = envp;
+  while (*p != NULL)
+    envlen += strlen (*p++) + 1;
+  env = alloca (envlen);
+  ap = env;
+  for (p = envp; *p != NULL; ++p)
+    ap = __memccpy (ap, *p, '\0', UINT_MAX);
+
+  ports[INIT_PORT_CCDIR] = _hurd_ccdir;
+  ports[INIT_PORT_CWDIR] = _hurd_cwdir;
+  ports[INIT_PORT_CRDIR] = _hurd_crdir;
+  ports[INIT_PORT_AUTH] = _hurd_auth;
+  ports[INIT_PORT_PROC] = _hurd_proc;
+  ints[INIT_UMASK] = _hurd_umask;
+  ints[INIT_CTTY_FSTYPE] = _hurd_ctty_fstype;
+  ints[INIT_CTTY_FSID1] = _hurd_ctty_fsid.val[0];
+  ints[INIT_CTTY_FSID2] = _hurd_ctty_fsid.val[1];
+  ints[INIT_CTTY_FILEID] = _hurd_ctty_fileid;
+  
+  __mutex_lock (&_hurd_dtable.lock);
+  dtablesize = _hurd_dtable.size;
+  dtable = alloca (dtablesize * sizeof (file_t));
+  for (i = 0; i < dtablesize; ++i)
+    dtable[i] = _hurd_dtable.d[i].server;
+  __mutex_unlock (&_hurd_dtable.lock);
+
+  err = __file_exec (file, __mach_task_self (),
+		     args, argslen, env, envlen,
+		     dtable, dtablesize,
+		     ints, INIT_MAX_INTS,
+		     ports, INIT_MAX_PORTS,
+		     0);
+  if (err)
+    return __hurd_fail (err);
+
+  /* That's interesting.  */
+  return 0;
+}
---
+++ sysdeps/mach/hurd/kill.c
@@ -0,0 +1,34 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <signal.h>
+#include <hurd.h>
+
+/* Send signal SIG to process number PID.  If PID is zero,
+   send SIG to all processes in the current process's process group.
+   If PID is < -1, send SIG to all processes in process group - PID.  */
+int
+DEFUN(__kill, (pid, sig), int pid AND int sig)
+{
+  error_t err = __proc_kill (_hurd_proc, pid, sig);
+  if (err)
+    return __hurd_fail (err);
+  return 0;
+}
