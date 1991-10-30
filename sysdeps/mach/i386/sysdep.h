5f643394f10d0ababcf4032fb1d48ae235fdd84e
Roland McGrath <roland@gnu.org>
1991-10-30 05:39:51 +0000

    Formerly mach/i386/sysdep.h.~3~

--- sysdeps/mach/i386/sysdep.h
+++ sysdeps/mach/i386/sysdep.h
@@ -16,8 +16,6 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <sysdeps/mach/sysdep.h>
-
 #define	ENTRY(name)							      \
   .globl _##name;							      \
   .align 4;								      \
