ed77f3477d56fadeb3399a048934508975f92704
Roland McGrath <roland@gnu.org>
1991-11-12 03:49:05 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/getgroups.c
@@ -0,0 +1,52 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <hurd.h>
+
+int
+getgroups (int n, gid_t *gidset)
+{
+  int ngids;
+
+  __mutex_lock (&_hurd_idlock);
+  if (!_hurd_id_valid)
+    {
+      error_t err = __auth_getids (_hurd_auth, &_hurd_id);
+      if (err)
+	{
+	  __mutex_unlock (&_hurd_idlock);
+	  return __hurd_fail (err);
+	}
+      _hurd_id_valid = 1;
+    }
+  ngids = _hurd_id.ngroups;
+  if (gidset != NULL)
+    {
+      gid_t gids[ngids];
+      memcpy (gids, _hurd_id.groups, sizeof (gids));
+      __mutex_unlock (&_hurd_idlock);
+      memcpy (gidset, gids, sizeof (gids));
+    }
+  else
+    __mutex_unlock (&_hurd_idlock);
+
+  return ngids;
+}
