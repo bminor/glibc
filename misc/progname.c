03176616c659062885ccdefb1272bd6286f0257d
Roland McGrath <roland@gnu.org>
1992-01-11 04:00:34 +0000

    Initial revision

---
+++ misc/progname.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 1992 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#ifdef	__GNU_STAB__
+
+#include <ansidecl.h>
+#include <string.h>
+#include <gnu-stabs.h>
+
+char *program_invocation_name;
+char *program_invocation_short_name;
+
+static void
+DEFUN(set_progname, (argc, argv, envp),
+      int argc AND char **argv AND char **envp)
+{
+  char *p;
+  program_invocation_name = argv[0];
+  p = strrchr (argv[0], '/');
+  if (p == NULL)
+    program_invocation_short_name = argv[0];
+  else
+    program_invocation_short_name = p + 1;
+}
+
+#endif
