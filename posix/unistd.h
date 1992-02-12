ef1b5b31841ff345ecc5469f2c4f1910eff3e372
Roland McGrath <roland@gnu.org>
1992-02-12 18:14:51 +0000

    Formerly ../posix/unistd.h.~17~

--- posix/unistd.h
+++ posix/unistd.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -79,7 +79,7 @@ Cambridge, MA 02139, USA.  */
 				effect associated with that element.
    */
 
-#include <posix_options.h>
+#include <posix_opt.h>
 
 
 /* Standard file descriptors.  */
