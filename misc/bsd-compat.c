8f3ed7f137397d6d0ab59c7835753b929b76efb9
Roland McGrath <roland@gnu.org>
1992-02-17 06:49:40 +0000

    Initial revision

---
+++ misc/bsd-compat.c
@@ -0,0 +1,43 @@
+/* BSD-compatible versions of functions where BSD and POSIX.1 conflict.
+
+Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#define	__FAVOR_BSD
+
+#include <ansidecl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <gnu-stabs.h>
+#include <limits.h>
+#include <setjmp.h>
+
+#undef	getpgrp
+function_alias(getpgrp, __getpgrp, pid_t, (pid),
+	       DEFUN(getpgrp, (pid), pid_t pid))
+
+#undef	setjmp
+int
+DEFUN(setjmp, (env), jmp_buf env)
+{
+  return sigsetjmp (env, 1);
+}
+
+#undef	longjmp
+function_alias(longjmp, siglongjmp, void, (env, val),
+	       DEFUN(longjmp, (env, val), jmp_buf env AND int val))
