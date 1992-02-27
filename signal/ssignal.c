cf9a85c954c9a458c1faa43024c16bae24ae7b4c
Roland McGrath <roland@gnu.org>
1992-02-27 19:49:29 +0000

    entered into RCS

--- signal/ssignal.c
+++ signal/ssignal.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -23,4 +23,4 @@ Cambridge, MA 02139, USA.  */
 #undef	ssignal
 
 function_alias(ssignal, signal, __sighandler_t, (sig, func),
-	       DEFUN(ssignal, (sig, handler), itn sig AND __sighandler_t func))
+	       DEFUN(ssignal, (sig, handler), int sig AND __sighandler_t func))
