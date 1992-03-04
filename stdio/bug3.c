7335afa1b3cea699c552229ab7dc0b1ea8f1e5cc
Roland McGrath <roland@gnu.org>
1992-03-04 22:56:23 +0000

    Initial revision

---
+++ stdio/bug3.c
@@ -0,0 +1,15 @@
+#include <ansidecl.h>
+#include <stdio.h>
+
+main () {
+  FILE *f;
+  int i;
+
+  f = fopen("bugtest", "w+");
+  for (i=0; i<9000; i++) {
+    putc('x', f);
+  }
+  fseek(f, 8180L, 0);
+  fwrite("Where does this text go?", 1, 24, f);
+  fclose(f);
+}
