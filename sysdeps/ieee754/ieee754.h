55f0f5fa9b47cf60b34a06e4343f6e71c767bfde
Roland McGrath <roland@gnu.org>
1992-03-05 22:43:10 +0000

    Initial revision

---
+++ sysdeps/ieee754/ieee754.h
@@ -0,0 +1,43 @@
+/* Copyright (C) 1992 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <endian.h>
+
+union
+  {
+    double d;
+    
+    /* This is the IEEE 754 double-precision format.  */
+    struct
+      {
+#ifdef	__BIG_ENDIAN
+	unsigned int negative:1;
+	int exponent:11;
+	/* Together these comprise the signed mantissa.  */
+	int mantissa0:20;
+	unsigned int mantissa1:32;
+#endif
+#ifdef	__LITTLE_ENDIAN
+	/* Together these comprise the signed mantissa.  */
+	unsigned int mantissa1:32;
+	int mantissa0:20;
+	int exponent:11;
+	unsigned int negative:1;
+#endif
+      } ieee;
+  };
