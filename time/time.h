71a5bf5a83f000a1800707a44a66856aa43d9c31
Roland McGrath <roland@gnu.org>
1992-03-08 01:10:15 +0000

    Formerly time/time.h.~8~

--- time/time.h
+++ time/time.h
@@ -74,13 +74,13 @@ typedef __time_t time_t;
 /* Used by other time functions.  */
 struct tm
   {
-    int tm_sec;		/* Seconds.	[0-60]	*/
-    int tm_min;		/* Minutes.	[0-59]	*/
-    int tm_hour;	/* Hours.	[0-23]	*/
-    int tm_mday;	/* Day.		[1-31]	*/
-    int tm_mon;		/* Month.	[0-11]	*/
-    int tm_year;	/* Year	- 1900.		*/
-    int tm_wday;	/* Day of week.	[0-6]	*/
+    int tm_sec;		/* Seconds.	[0-61] (2 leap seconds) */
+    int tm_min;		/* Minutes.	[0-59] */
+    int tm_hour;	/* Hours.	[0-23] */
+    int tm_mday;	/* Day.		[1-31] */
+    int tm_mon;		/* Month.	[0-11] */
+    int tm_year;	/* Year	- 1900.  */
+    int tm_wday;	/* Day of week.	[0-6] */
     int tm_yday;	/* Days in year.[0-365]	*/
     int tm_isdst;	/* DST.		[-1/0/1]*/
   };
