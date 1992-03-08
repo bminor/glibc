7a979900a167207c96c1890cbe03a0b6815d34c8
Roland McGrath <roland@gnu.org>
1992-03-08 20:46:04 +0000

    Initial revision

---
+++ gnu-stabs.h
@@ -0,0 +1,83 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#ifndef	__GNU_STABS_H
+
+#define	__GNU_STABS_H	1
+
+#ifdef	HAVE_GNU_LD
+
+/* Alias a function:
+   	function_alias(creat, _creat, int, (file, mode),
+		       DEFUN(creat, (file, mode),
+		             CONST char *file AND int mode))
+   Yes, this is very repetitive.  Nothing you can do about it, so shut up.  */
+#define	function_alias(name, _name, type, args, defun) \
+  symbol_alias(_name, name);
+
+/* Make references to ALIAS refer to SYMBOL.  */
+#ifdef	__STDC__
+#define	symbol_alias(symbol, alias)	\
+  asm(".stabs \"" "_" #alias "\",11,0,0,0\n"\
+      ".stabs \"" "_" #symbol "\",1,0,0,0")
+#else
+/* Your assembler better grok this right!  */
+#define	symbol_alias(symbol, alias)	\
+  asm(".stabs \"_/**/alias\",11,0,0,0\n.stabs \"_/**/symbol\",1,0,0,0")
+#endif
+
+/* Issue a warning message from the linker whenever SYMBOL is referenced.  */
+#ifdef	__STDC__
+#define	warn_references(symbol, msg)	\
+  asm(".stabs \"" msg "\",30,0,0,0\n"	\
+      ".stabs \"_" #symbol "\",1,0,0,0")
+#else
+#define	warn_references(symbol, msg)	\
+  asm(".stabs msg,30,0,0,0\n.stabs \"_/**/symbol\",1,0,0,0")
+#endif
+
+#ifdef	__STDC__
+#define	stub_warning(name) \
+  warn_references(name, \
+		  "warning: " #name " is not implemented and will always fail")
+#else
+#define	stub_warning(name) \
+  warn_references(name, \
+		  "warning: name is not implemented and will always fail")
+#endif
+
+#ifdef	__STDC__
+#define	text_set_element(set, symbol)	\
+  asm(".stabs \"_" #set "\",23,0,0,_" #symbol)
+#define	data_set_element(set, symbol)	\
+  asm(".stabs \"_" #set "\",25,0,0,_" #symbol)
+#else
+#define	text_set_element(set, symbol)	\
+  asm(".stabs \"_/**/set\",23,0,0,_/**/symbol")
+#define	data_set_element(set, symbol)	\
+  asm(".stabs \"_/**/set\",25,0,0,_/**/symbol")
+#endif
+
+#else	/* No GNU stabs.  */
+
+#define	function_alias(name, _name, type, args, defun) \
+  type defun { return name args; }
+
+#endif	/* GNU stabs.  */
+
+#endif	/* gnu-stabs.h  */
---
+++ sysdeps/stub/sysd-stdio.c
@@ -0,0 +1,117 @@
+/* Copyright (C) 1991 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <stdio.h>
+
+
+/* Read up to N chars into BUF from COOKIE.
+   Return how many chars were read, 0 for EOF or -1 for error.  */
+int
+DEFUN(__stdio_read, (cookie, buf, n),
+      PTR cookie AND register char *buf AND register size_t n)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+/* Write up to N chars from BUF to COOKIE.
+   Return how many chars were written or -1 for error.  */
+int
+DEFUN(__stdio_write, (cookie, buf, n),
+      PTR cookie AND register CONST char *buf AND register size_t n)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+/* Move COOKIE's file position *POS bytes, according to WHENCE.
+   The old file position is stored in *POS.
+   Returns zero if successful, nonzero if not.  */
+int
+DEFUN(__stdio_seek, (cookie, pos, whence),
+      PTR cookie AND fpos_t *pos AND int whence)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+/* Close the file associated with COOKIE.
+   Return 0 for success or -1 for failure.  */
+int
+DEFUN(__stdio_close, (cookie), PTR cookie)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+
+/* Open FILENAME with the mode in M.
+   Return the magic cookie associated with the opened file
+   or NULL which specifies that an integral descriptor may be
+   found in *FDPTR.  This descriptor is negative for errors.  */
+PTR
+DEFUN(__stdio_open, (filename, m, fdptr),
+      CONST char *filename AND __io_mode m AND int *fdptr)
+{
+  errno = ENOSYS;
+  *fdptr = -1;
+  return NULL;
+}
+
+
+/* Write a message to the error output.
+   Try hard to make it really get out.  */
+void
+DEFUN(__stdio_errmsg, (msg, len), CONST char *msg AND size_t len)
+{
+}
+
+
+/* Generate a (hopefully) unique temporary filename
+   in DIR (if applicable), using prefix PFX.
+   If DIR_SEARCH is nonzero, perform directory searching
+   malarky as per the SVID for tempnam.
+   Return the generated filename or NULL if one could not
+   be generated, putting the length of the string in *LENPTR.  */
+char *
+DEFUN(__stdio_gen_tempname, (dir, pfx, dir_search, lenptr),
+      CONST char *dir AND CONST char *pfx AND
+      int dir_search AND size_t *lenptr)
+{
+  *lenptr = 0;
+  errno = ENOSYS;
+  return NULL;
+}
+
+
+#ifdef	 HAVE_GNU_LD
+
+#include <gnu-stabs.h>
+
+stub_warning(__stdio_read);
+stub_warning(__stdio_write);
+stub_warning(__stdio_seek);
+stub_warning(__stdio_close);
+stub_warning(__stdio_open);
+stub_warning(__stdio_linebf);
+stub_warning(__stdio_errmsg);
+stub_warning(__stdio_gen_tempname);
+
+#endif	/* GNU stabs.  */
