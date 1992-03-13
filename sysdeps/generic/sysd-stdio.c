f4a846fd5738914d44580a4567cd91d081b517c6
Roland McGrath <roland@gnu.org>
1992-03-13 23:16:50 +0000

    Formerly posix/sysd-stdio.c.~3~

--- sysdeps/generic/sysd-stdio.c
+++ sysdeps/generic/sysd-stdio.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -291,6 +291,6 @@ DEFUN(__stdio_gen_tempname, (dir, pfx, dir_search, lenptr),
     }
 
   if (lenptr != NULL)
-    *lenptr = len;
+    *lenptr = len + 1;
   return buf;
 }
