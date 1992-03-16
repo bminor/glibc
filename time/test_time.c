7b8550c0ee96b67e6707f4f61dcce3f56635c4cf
Roland McGrath <roland@gnu.org>
1992-03-16 12:13:57 +0000

    Formerly ../time/test_time.c.~5~

--- time/test_time.c
+++ time/test_time.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -42,9 +42,11 @@ DEFUN(main, (argc, argv), int argc AND char **argv)
 	  sprintf(buf, "TZ=%s", *argv);
 	  if (putenv(buf))
 	    {
-	      puts("putenv() failed.");
+	      puts("putenv failed.");
 	      lose = 1;
 	    }
+	  else
+	    puts (buf);
 	}
       tzset();
       tbuf.tm_year = 72;
