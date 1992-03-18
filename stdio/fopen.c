e36a533c2d880325d27a1c7a13188b18b999ee57
Roland McGrath <roland@gnu.org>
1992-03-18 00:18:37 +0000

    Formerly ../stdio/fopen.c.~9~

--- stdio/fopen.c
+++ stdio/fopen.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -41,6 +41,7 @@ DEFUN(__getmode, (mode, mptr), CONST char *mode AND __io_mode *mptr)
     {
     case 'a':
       mptr->__write = mptr->__create = mptr->__append = 1;
+      break;
     case 'w':
       mptr->__write = mptr->__create = mptr->__truncate = 1;
       break;
