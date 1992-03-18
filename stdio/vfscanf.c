4e860bc79fce33bbdee83efbf7864fae1b2bcfec
Roland McGrath <roland@gnu.org>
1992-03-18 01:05:49 +0000

    Formerly ../stdio/__vfscanf.c.~10~

--- stdio/vfscanf.c
+++ stdio/vfscanf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -256,9 +256,6 @@ DEFUN(__vfscanf, (s, format, arg),
 	      *str = '\0';
 	      ++done;
 	    }
-
-	  if (c == EOF)
-	    input_error();
 	  break;
 
 	case 'x':	/* Hexadecimal integer.  */
