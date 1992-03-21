7d7a6ee72365d01b49961c78f6bd9f4e2cb0b8e8
Roland McGrath <roland@gnu.org>
1992-03-21 22:31:29 +0000

    entered into RCS

--- stdio/fopncook.c
+++ stdio/fopncook.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,10 +31,10 @@ DEFUN(fopencookie, (cookie, mode, functions),
   __io_mode m;
   FILE *f;
 
-  if (!__getmode(mode, &m))
+  if (!__getmode (mode, &m))
     return NULL;
 
-  f = __newstream();
+  f = __newstream ();
   if (f == NULL)
     return NULL;
 
