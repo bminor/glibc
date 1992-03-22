daad67fabc9cf24cf8f53a40b5e927c9fcb26385
Roland McGrath <roland@gnu.org>
1992-03-22 02:03:31 +0000

    Formerly mach/hurd/__access.c.~3~

--- sysdeps/mach/hurd/access.c
+++ sysdeps/mach/hurd/access.c
@@ -17,22 +17,17 @@ not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #include <ansidecl.h>
-#include <errno.h>
-#include <stddef.h>
 #include <unistd.h>
+#include <hurd.h>
 
 /* Test for access to FILE by our real user and group IDs.  */
 int
 DEFUN(__access, (file, type), CONST char *file AND int type)
 {
   error_t err;
-  const char *name;
-  file_t rdir, dir, file;
+  file_t crdir, cwdir, rcrdir, rcwdir, io;
+  int dealloc_crdir, dealloc_cwdir;
   int flags;
-
-  dir = __hurd_path_split (file, &name);
-  if (dir == MACH_PORT_NULL)
-    return -1;
 
   /* Set up _hurd_rid_auth.  */
   __mutex_lock (&_hurd_idlock);
@@ -60,12 +55,23 @@ DEFUN(__access, (file, type), CONST char *file AND int type)
 	goto lose;
     }
 
-  if (err = __io_reauthenticate (dir))
-    goto lose;
-
-  err = __auth_user_authenticate (_hurd_rid_auth, dir, &rdir);
+  crdir = _hurd_port_get (&_hurd_crdir, &dealloc_crdir);
+  err = __io_reauthenticate (crdir);
+  if (!err)
+    {
+      err = __auth_user_authenticate (_hurd_rid_auth, crdir, &rcrdir);
+      __mach_port_deallocate (__mach_task_self (), crdir);
+    }
+  _hurd_port_free (crdir, &dealloc_crdir);
+  cwdir = _hurd_port_get (&_hurd_cwdir, &dealloc_cwdir);
+  err = __io_reauthenticate (cwdir);
+  if (!err)
+    {
+      err = __auth_user_authenticate (_hurd_rid_auth, cwdir, &rcwdir);
+      __mach_port_deallocate (__mach_task_self (), cwdir);
+    }
   __mutex_unlock (&_hurd_idlock);
-  __mach_port_deallocate (__mach_task_self (), dir);
+  _hurd_port_free (cwdir, &dealloc_cwdir);
   if (err)
     return __hurd_fail (err);
 
@@ -77,17 +83,16 @@ DEFUN(__access, (file, type), CONST char *file AND int type)
   if (type & X_OK)
     flags |= FS_LOOKUP_EXECUTE;
 
-  /* XXX auth for crdir on retries from /? */
-  err = __dir_lookup (rdir, name, flags, &file);
-  __mach_port_deallocate (__mach_task_self (), rdir);
+  err = __hurd_path_lookup (rcrdir, rcwdir, file, flags, 0, &io);
+  __mach_port_deallocate (__mach_task_self (), rcrdir);
+  __mach_port_deallocate (__mach_task_self (), rcwdir);
   if (err)
     return __hurd_fail (err);
 
-  __mach_port_deallocate (__mach_task_self (), file);
+  __mach_port_deallocate (__mach_task_self (), io);
   return 0;
 
  lose:
   __mutex_unlock (&_hurd_idlock);
-  __mach_port_deallocate (__mach_task_self (), dir);
   return __hurd_fail (err);
 }
