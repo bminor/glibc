4d6dee133327fde88f792aac67a57e45fbdcb8ac
Roland McGrath <roland@gnu.org>
1992-03-26 00:59:36 +0000

    Formerly mach/hurd/__setreuid.c.~2~

--- sysdeps/mach/hurd/setreuid.c
+++ sysdeps/mach/hurd/setreuid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -29,7 +29,8 @@ __setreuid (uid_t ruid, uid_t euid)
   __mutex_lock (&_hurd_idlock);
   if (!_hurd_id_valid)
     {
-      error_t err = __auth_getids (_hurd_auth, &_hurd_id);
+      error_t err = _HURD_PORT_USE (&_hurd_auth,
+				    __auth_getids (port, &_hurd_id));
       if (err)
 	{
 	  __mutex_unlock (&_hurd_idlock);
@@ -52,7 +53,7 @@ __setreuid (uid_t ruid, uid_t euid)
       if (_hurd_id.nuids == sizeof (_hurd_id.uids) / sizeof (_hurd_id.uids[0]))
 	{
 	  __mutex_unlock (&_hurd_idlock);
-	  errno = ENOMEM;	/* ? */
+	  errno = ENOMEM;	/* XXX ? */
 	  return -1;
 	}
       else
@@ -64,7 +65,8 @@ __setreuid (uid_t ruid, uid_t euid)
 
   _hurd_id.ruid = ruid;
 
-  if (err = auth_makeauth (_hurd_auth, &_hurd_id, &newauth))
+  if (err = _HURD_PORT_USE (&_hurd_auth,
+			    __auth_makeauth (port, &_hurd_id, &newauth)))
     {
       _hurd_id_valid = 0;
       __mutex_unlock (&_hurd_idlock);
