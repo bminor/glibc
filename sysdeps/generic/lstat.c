0e727824bd8d1952a92a2d3817cc6a2da8d0e2c4
Roland McGrath <roland@gnu.org>
1992-03-27 21:50:07 +0000

    entered into RCS

--- sysdeps/generic/lstat.c
+++ sysdeps/generic/lstat.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <stddef.h>
 #include <sys/stat.h>
+#include <gnu-stabs.h>
 
 function_alias (__lstat, __stat, int, (file, buf),
 		DEFUN(__lstat, (file, buf),
