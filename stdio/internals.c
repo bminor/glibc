28f30dd7d891eb622709ca6bc07633c4db367bee
Roland McGrath <roland@gnu.org>
1992-03-27 21:42:14 +0000

    Formerly ../stdio/internals.c.~37~

--- stdio/internals.c
+++ stdio/internals.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -293,8 +293,11 @@ DEFUN(flushbuf, (fp, c),
 	  if ((*fp->__io_funcs.__write)(fp->__cookie, &cc, 1) < 1)
 	    fp->__error = 1;
 	  else
-	    /* Record that we've moved forward in the file.  */
-	    ++fp->__offset;
+	    {
+	      /* Record that we've moved forward in the file.  */
+	      ++fp->__offset;
+	      ++fp->__target;
+	    }
 	}
       else
 	/* Just put C in the buffer.  */
