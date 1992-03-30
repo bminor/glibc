7351bef1ed5f35590270d043d02c40baddd41783
Roland McGrath <roland@gnu.org>
1992-03-30 22:44:44 +0000

    Formerly ../posix/sys/wait.h.~15~

--- posix/sys/wait.h
+++ posix/sys/wait.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -34,6 +34,8 @@ Cambridge, MA 02139, USA.  */
 
 
 /* Encoding of the status word.  */
+
+#include <endian.h>
 
 #ifdef	__USE_BSD
 union __wait
