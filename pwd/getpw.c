bd9ed4fe6409589b301effd000a17f5f8b1227df
Roland McGrath <roland@gnu.org>
1992-04-01 06:22:44 +0000

    Formerly ../pwd/getpw.c.~3~

--- pwd/getpw.c
+++ pwd/getpw.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@ Cambridge, MA 02139, USA.  */
    in the given buffer.  This knows the format that the caller
    will expect, but this need not be the format of the password file.  */
 int
-DEFUN(getpw, (uid, buf), int uid AND register char *buf)
+DEFUN(getpw, (uid, buf), __uid_t uid AND register char *buf)
 {
   register struct passwd *p;
 
