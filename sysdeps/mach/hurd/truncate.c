b4a84aabf9cfa8cb886c6999f8a30a9cba9d7d4b
Roland McGrath <roland@gnu.org>
1992-04-02 06:15:22 +0000

    Formerly mach/hurd/truncate.c.~2~

--- sysdeps/mach/hurd/truncate.c
+++ sysdeps/mach/hurd/truncate.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -27,7 +27,7 @@ DEFUN(truncate, (path, length),
       CONST char *path AND off_t length)
 {
   error_t err;
-  file_t file = __hurd_path_lookup (path, FS_LOOKUP_WRITE, 0);
+  file_t file = __path_lookup (path, FS_LOOKUP_WRITE, 0);
 
   if (file == MACH_PORT_NULL)
     return -1;
