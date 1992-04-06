a18fa3f69aa8118b5e38ba038e6263cc4f3050d6
Roland McGrath <roland@gnu.org>
1992-04-06 05:45:47 +0000

    Formerly ../time/localtime.c.~4~

--- time/localtime.c
+++ time/localtime.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,6 +25,12 @@ Cambridge, MA 02139, USA.  */
 #include <string.h>
 #include <time.h>
 
+
+#ifndef	HAVE_GNU_LD
+#define	__tzname	tzname
+#define	__daylight	daylight
+#define	__timezone	timezone
+#endif
 
 /* Return the `struct tm' representation of *TIMER in the local timezone.  */
 struct tm *
