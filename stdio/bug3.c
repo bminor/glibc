ec373422009539c80572978918c3c7043858b776
Roland McGrath <roland@gnu.org>
1992-04-10 02:43:48 +0000

    Formerly ../stdio/bug3.c.~3~

--- stdio/bug3.c
+++ stdio/bug3.c
@@ -1,5 +1,6 @@
 #include <ansidecl.h>
 #include <stdio.h>
+#include <string.h>
 
 int
 main ()
@@ -8,11 +9,44 @@ main ()
   int i;
 
   f = fopen("bugtest", "w+");
-  for (i=0; i<9000; i++) {
-    putc('x', f);
-  }
-  fseek(f, 8180L, 0);
-  fwrite("Where does this text go?", 1, 24, f);
+  for (i=0; i<9000; i++)
+    putc ('x', f);
+  fseek (f, 8180L, 0);
+  fwrite ("Where does this text go?", 1, 24, f);
+  fflush (f);
+
+  rewind (f);
+  for (i=0; i<9000; i++)
+    {
+      int j;
+
+      if ((j = getc(f)) != 'x')
+	{
+	  if (i != 8180)
+	    {
+	      printf ("Test FAILED!");
+	      return 1;
+	    }
+	  else
+	    {
+	      char buf[25];
+
+	      buf[0] = j;
+	      fread (buf + 1, 1, 23, f);
+	      buf[24] = '\0';
+	      if (strcmp (buf, "Where does this text go?") != 0)
+		{
+		  printf ("%s\nTest FAILED!\n", buf);
+		  return 1;
+		}
+	      i += 23;
+	    }
+	}
+    }
+
   fclose(f);
+
+  puts ("Test succeeded.");
+
   return 0;
 }
