4acfffe0b25858153ecf37f44c37726a780a144e
Roland McGrath <roland@gnu.org>
1992-04-10 23:17:54 +0000

    Formerly ../stdio/bug6.c.~2~

--- stdio/bug6.c
+++ stdio/bug6.c
@@ -6,12 +6,12 @@ DEFUN_VOID(main)
 {
   char buf[80];
   int i;
-  FILE *f = fopen ("scanf.dat", "r");
+  FILE *f = stdin;
   if (f == NULL) perror ("");
   fscanf (f, "%2s", buf);
   if (buf[0] != 'X' || buf[1] != 'Y' || buf[2] != '\0')
     printf ("test of %%2s failed.\n");
-  fscanf (f, " ", buf);
+  fscanf (f, " ");
   fscanf (f, "%d", &i);
   if (i != 1234) printf ("test of %%d failed.\n");
   fscanf (f, "%c", buf);
