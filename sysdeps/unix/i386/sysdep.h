d46ce436f0f325bfa8e2655ea41de0dd3defecd6
Roland McGrath <roland@gnu.org>
1992-04-15 05:52:28 +0000

    Formerly unix/i386/sysdep.h.~12~

--- sysdeps/unix/i386/sysdep.h
+++ sysdeps/unix/i386/sysdep.h
@@ -16,7 +16,9 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+#ifndef HAVE_SYSCALLS
 #include <sysdeps/unix/sysdep.h>
+#endif
 
 #ifdef	__STDC__
 #define	ENTRY(name)							      \
