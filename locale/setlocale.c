84115bb1b918778c3c68cf2dab0f92761379100f
Roland McGrath <roland@gnu.org>
1992-04-15 06:05:30 +0000

    Formerly ../locale/setlocale.c.~6~

--- locale/setlocale.c
+++ locale/setlocale.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -34,7 +34,7 @@ DEFUN(setlocale, (category, name), int category AND CONST char *name)
 {
   /* Braindead implementation until I finish the fancy one.  */
 
-  if (name == NULL)
+  if (name == NULL || name[0] = '\0')
     return (char *) "C";
 
   if (!strcmp(name, "C") || !strcmp(name, "POSIX"))
