d437d07bfcaca49bae9a7b4edee27c9db76c3986
Roland McGrath <roland@gnu.org>
1992-04-21 05:30:28 +0000

    Formerly ../stdio/test-fseek.c.~2~

--- stdio/test-fseek.c
+++ stdio/test-fseek.c
@@ -10,53 +10,58 @@ main ()
   int i, j;
 
   puts ("\nFile seek test");
-  if ((fp = fopen (TESTFILE, "w")) != NULL)
+  fp = fopen (TESTFILE, "w");
+  if (fp == NULL)
     {
-      for (i = 0; i < 256; i++)
-	putc (i, fp);
-      if (freopen (TESTFILE, "r", fp) != fp)
-	puts ("Cannot open file for reading");
-      else
+      perror (TESTFILE);
+      return 1;
+    }
+
+  for (i = 0; i < 256; i++)
+    putc (i, fp);
+  if (freopen (TESTFILE, "r", fp) != fp)
+    {
+      perror ("Cannot open file for reading");
+      return 1;
+    }
+
+  for (i = 1; i <= 255; i++)
+    {
+      printf ("%3d\n", i);
+      fseek (fp, (long) -i, SEEK_END);
+      if ((j = getc (fp)) != 256 - i)
 	{
-	  for (i = 1; i <= 255; i++)
-	    {
-	      printf ("\r%3d ", i);
-	      fflush (stdout);
-	      fseek (fp, (long) -i, SEEK_END);
-	      if ((j = getc (fp)) != 256 - i)
-		{
-		  printf ("SEEK_END failed %d\n", j);
-		  break;
-		}
-	      if (fseek (fp, (long) i, SEEK_SET))
-		{
-		  puts ("Cannot SEEK_SET");
-		  break;
-		}
-	      if ((j = getc (fp)) != i)
-		{
-		  printf ("SEEK_SET failed %d\n", j);
-		  break;
-		}
-	      if (fseek (fp, (long) i, SEEK_SET))
-		{
-		  puts ("Cannot SEEK_SET");
-		  break;
-		}
-	      if (fseek (fp, (long) (i >= 128 ? -128 : 128), SEEK_CUR))
-		{
-		  puts ("Cannot SEEK_CUR");
-		  break;
-		}
-	      if ((j = getc (fp)) != (i >= 128 ? i - 128 : i + 128))
-		{
-		  printf ("SEEK_CUR failed %d\n", j);
-		  break;
-		}
-	    }
-	  fclose (fp);
-	  if (i > 255)
-	    puts ("ok");
+	  printf ("SEEK_END failed %d\n", j);
+	  break;
+	}
+      if (fseek (fp, (long) i, SEEK_SET))
+	{
+	  puts ("Cannot SEEK_SET");
+	  break;
+	}
+      if ((j = getc (fp)) != i)
+	{
+	  printf ("SEEK_SET failed %d\n", j);
+	  break;
+	}
+      if (fseek (fp, (long) i, SEEK_SET))
+	{
+	  puts ("Cannot SEEK_SET");
+	  break;
+	}
+      if (fseek (fp, (long) (i >= 128 ? -128 : 128), SEEK_CUR))
+	{
+	  puts ("Cannot SEEK_CUR");
+	  break;
+	}
+      if ((j = getc (fp)) != (i >= 128 ? i - 128 : i + 128))
+	{
+	  printf ("SEEK_CUR failed %d\n", j);
+	  break;
 	}
     }
+  fclose (fp);
+
+  puts ((i > 255) ? "Test succeeded." : "Test FAILED!");
+  return (i > 255) ? 0 : 1;
 }
