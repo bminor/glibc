9f8df1d29fd9747ea9e284758722df1f1c3211ed
Roland McGrath <roland@gnu.org>
1992-04-21 05:11:07 +0000

    Formerly ../stdio/fseek.c.~19~

--- stdio/fseek.c
+++ stdio/fseek.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -82,7 +82,7 @@ DEFUN(fseek, (stream, offset, whence),
 	  stream->__offset = pos;
 	  /* Make O be absolute, rather than
 	     relative to the end of the file.  */
-	  o += pos;
+	  o = pos;
 	}
 
       /* Fall through to try an absolute seek.  */
