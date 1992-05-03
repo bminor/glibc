51da9639cf8e952ac47dfc613fd9a1c651f7c363
Roland McGrath <roland@gnu.org>
1992-05-03 18:00:40 +0000

    Formerly unix/bsd/ulimit.c.~4~

--- sysdeps/unix/bsd/ulimit.c
+++ sysdeps/unix/bsd/ulimit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -21,6 +21,10 @@ Cambridge, MA 02139, USA.  */
 #include <sys/resource.h>
 #include <unistd.h>
 #include <errno.h>
+
+#ifndef	 HAVE_GNU_LD
+#define	 _etext	etext
+#endif
 
 extern char _etext;
 
