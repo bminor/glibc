740c5fe85bdb389a243b026c4c446716dcf81c60
Roland McGrath <roland@gnu.org>
1992-05-03 20:41:23 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/reboot.c
@@ -0,0 +1,43 @@
+/* Copyright (C) 1992 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <hurd.h>
+#include <hurd/startup.h>
+
+/* Reboot the system.  */
+int
+DEFUN(reboot, (howto), int howto)
+{
+  error_t err;
+  mach_port_t init;
+
+  err = _HURD_PORT_USE (&_hurd_proc, __proc_getmsgport (1, &init));
+  if (!err)
+    {
+      err = __startup_reboot (init, howto);
+      __mach_port_deallocate (__mach_task_self (), init);
+    }
+
+  if (err)
+    return __hurd_fail (err);
+
+  return 0;
+}
