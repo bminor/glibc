eeacd2248d70d94ac1f0b0ef584a70e501cc5945
Roland McGrath <roland@gnu.org>
1992-05-03 19:53:24 +0000

    Formerly mach/hurd/getsocknam.c.~2~

--- sysdeps/mach/hurd/getsockname.c
+++ sysdeps/mach/hurd/getsockname.c
@@ -37,6 +37,6 @@ DEFUN(getsockname, (fd, addr, len),
 	   err = __socket_whatis_address (aport, addr, len);
 	   __mach_port_deallocate (__mach_task_self (), aport);
 	 }
-       err ? __hurd_fail (err) : 0;
+       err ? __hurd_dfail (fd, err) : 0;
      }));
 }
