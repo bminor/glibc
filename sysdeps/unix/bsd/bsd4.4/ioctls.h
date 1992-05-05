4789d179c98cc747671e5ece9c831101f4e85f96
Roland McGrath <roland@gnu.org>
1992-05-05 23:27:40 +0000

    Formerly unix/bsd/bsd4.4/ioctls.h.~2~

--- sysdeps/unix/bsd/bsd4.4/ioctls.h
+++ sysdeps/unix/bsd/bsd4.4/ioctls.h
@@ -198,7 +198,8 @@ struct ttysize {
 #define	SIOCDARP	_IOW('i', 32, struct arpreq)	/* delete arp entry */
 
 
-/* Compatibility with 4.3 BSD terminal driver.  */
+/* Compatibility with 4.3 BSD terminal driver.
+   From 4.4 <sys/ioctl_compat.h>.  */
 
 #ifdef USE_OLD_TTY
 # undef  TIOCGETD
@@ -287,5 +288,25 @@ struct ttysize {
 #define	OTTYDISC	0
 #define	NETLDISC	1
 #define	NTTYDISC	2
+
+/* From 4.4 <sys/ttydev.h>.   */
+#ifdef USE_OLD_TTY
+#define B0	0
+#define B50	1
+#define B75	2
+#define B110	3
+#define B134	4
+#define B150	5
+#define B200	6
+#define B300	7
+#define B600	8
+#define B1200	9
+#define	B1800	10
+#define B2400	11
+#define B4800	12
+#define B9600	13
+#define EXTA	14
+#define EXTB	15
+#endif /* USE_OLD_TTY */
 
 #endif /* !_IOCTLS_H_ */
