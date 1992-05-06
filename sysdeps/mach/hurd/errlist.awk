1cf46471ee5dfd6fc18f6f6bba1928a85d2ce9f9
Roland McGrath <roland@gnu.org>
1992-05-06 03:51:06 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/errlist.awk
@@ -0,0 +1,56 @@
+# Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+# This file is part of the GNU C Library.
+
+# The GNU C Library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public License
+# as published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+
+# The GNU C Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+
+# You should have received a copy of the GNU Library General Public
+# License along with the GNU C Library; see the file COPYING.LIB.  If
+# not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+# Cambridge, MA 02139, USA.
+
+# errno.texinfo contains lines like:
+# @comment errno.h
+# @comment POSIX.1: Function not implemented
+# @deftypevr Macro int ENOSYS
+
+BEGIN {
+    print "/* This file is generated from errno.texinfo by sys_errlist.awk. */"
+    print "";
+    print "const char *_sys_errlist[] =";
+    print "  {";
+    errno = 0;
+    print "    \"Success,\""
+  }
+$1 == "@comment" && $2 == "errno.h" { errnoh=1; next }
+errnoh == 1 && $1 == "@comment" \
+  {
+    ++errnoh;
+    etext = $3;
+    for (i = 4; i <= NF; ++i)
+      etext = etext " " $i;
+    next;
+  }
+errnoh == 2 && $1 == "@deftypevr"  && $2 == "Macro" && $3 == "int" \
+  {
+    e = $4;
+    s = "    \"" etext "\",";
+    l = 40 - length (s);
+    while (l-- > 0)
+      s = s " ";
+    printf "%s/* %d = %s */\n", s, ++errno, e;
+    next;
+  }
+{ errnoh=0 }
+END {
+  print "  };";
+  print "";
+  printf "const int _sys_nerr = %d;\n", errno;
+  }
---
+++ sysdeps/mach/hurd/seteuid.c
@@ -0,0 +1,79 @@
+/* Copyright (C) 1992 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <hurd.h>
+
+/* Set the effective user ID of the calling process to UID.  */
+int
+DEFUN(seteuid, (uid), int uid)
+{
+  auth_t newauth;
+  int i;
+
+  __mutex_lock (&_hurd_idlock);
+  if (!_hurd_id_valid)
+    {
+      error_t err = _HURD_PORT_USE (&_hurd_auth,
+				    __auth_getids (port, &_hurd_id));
+      if (err)
+	{
+	  __mutex_unlock (&_hurd_idlock);
+	  return __hurd_fail (err);
+	}
+      _hurd_id_valid = 1;
+    }
+
+  for (i = 0; i < _hurd_id.nuids; ++i)
+    if (_hurd_id.uids[i] == uid)
+      {
+	/* We already have this uid.  Swap it with uids[0]
+	   so geteuid will return it.  */
+	_hurd_id.uids[i] = _hurd_id.uids[0];
+	break;
+      }
+
+  if (i == _hurd_id.nuids)
+    {
+      if (_hurd_id.nuids == sizeof (_hurd_id.uids) / sizeof (_hurd_id.uids[0]))
+	{
+	  __mutex_unlock (&_hurd_idlock);
+	  errno = ENOMEM;	/* ? */
+	  return -1;
+	}
+      else
+	{
+	  _hurd_id.uids[_hurd_id.nuids++] = _hurd_id.uids[0];
+	  _hurd_id.uids[0] = uid;
+	}
+    }
+
+  err = _HURD_PORT_USE (&_hurd_auth,
+			__auth_makeauth (port, &_hurd_id, &newauth));
+  _hurd_id_valid = 0;
+  __mutex_unlock (&_hurd_idlock);
+
+  if (err)
+    return __hurd_fail (err);
+
+  err = __setauth (newauth);
+  __mach_port_deallocate (__mach_task_self (), newauth);
+  return err;
+}
