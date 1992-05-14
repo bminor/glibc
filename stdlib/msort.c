a9fef8674a53c8c838b1a42efa5a8633e8c5b8da
Roland McGrath <roland@gnu.org>
1992-05-14 07:40:00 +0000

    entered into RCS

--- stdlib/msort.c
+++ stdlib/msort.c
@@ -1,6 +1,23 @@
 /* msort -- an alternative to qsort, with an identical interface.
-   Copyright (C) 1988 Mike Haertel
-   Written by Mike Haertel, September 1988.  */
+   Copyright (C) 1992 Free Software Foundation, Inc.
+   Written by Mike Haertel, September 1988.
+
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
 
 #include <ansidecl.h>
 #include <stdlib.h>
@@ -54,7 +71,7 @@ DEFUN(msort_with_tmp, (b, n, s, cmp, t),
 }
 
 void
-DEFUN(msort, (b, n, s, cmp),
+DEFUN(qsort, (b, n, s, cmp),
       PTR b AND size_t n AND size_t s AND __compar_fn_t cmp)
 {
   CONST size_t size = n * s;
@@ -68,9 +85,14 @@ DEFUN(msort, (b, n, s, cmp),
       int save = errno;
       char *tmp = malloc (size);
       if (tmp == NULL)
-	/* Couldn't get space, so use the slower algorithm
-	   that doesn't need a temporary array.  */
-	qsort (b, n, s, cmp);
+	{
+	  /* Couldn't get space, so use the slower algorithm
+	     that doesn't need a temporary array.  */
+	  extern void EXFUN(_quicksort, (PTR __base,
+					 size_t __nmemb, size_t __size,
+					 __compar_fn_t __compar));
+	  _quicksort (b, n, s, cmp);
+	}
       else
 	{
 	  msort_with_tmp (b, n, s, cmp, tmp);
