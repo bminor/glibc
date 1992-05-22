a0a72cd8f87348637fd976e10c99ff925a91fd38
Roland McGrath <roland@gnu.org>
1992-05-22 09:04:12 +0000

    Formerly io/fcntl.h.~5~

--- io/fcntl.h
+++ io/fcntl.h
@@ -13,7 +13,7 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with the GNU C Library; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+not, write to the, 1992 Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 /*
@@ -24,6 +24,8 @@ Cambridge, MA 02139, USA.  */
 
 #define	_FCNTL_H	1
 #include <features.h>
+
+__BEGIN_DECLS
 
 #include <fcntlbits.h>
 #include <filebits.h>
@@ -72,25 +74,25 @@ Cambridge, MA 02139, USA.  */
 
 #ifdef	__USE_MISC
 /* Unix-style flags for `fcntl' F_GETFL and F_SETFL.  */
-#define	FREAD		0x1		/* Read access.  */
-#define	FWRITE		0x2		/* Write access.  */
+#define	FREAD		0x1	/* Read access.  */
+#define	FWRITE		0x2	/* Write access.  */
 #define	FNDELAY		O_NONBLOCK
 #define	FAPPEND		O_APPEND
-#define	FASYNC		0x40		/* Send SIGIO when data is ready.  */
+#define	FASYNC		0x40	/* Send SIGIO when data is ready.  */
 #define	FCREAT		O_CREAT
 #define	FTRUNC		O_TRUNC
 #define	FEXCL		O_EXCL
-#define	FSYNC		0x2000		/* Synchronous writes.  */
+#define	FSYNC		0x2000	/* Synchronous writes.  */
 
-#ifndef	R_OK	/* Verbatim from <unistd.h>.  Ugh.  */
+#ifndef	R_OK			/* Verbatim from <unistd.h>.  Ugh.  */
 /* Values for the second argument to access.
    These may be OR'd together.  */
-#define	R_OK	4	/* Test for read permission.  */
-#define	W_OK	2	/* Test for write permission.  */
-#define	X_OK	1	/* Test for execute permission.  */
-#define	F_OK	0	/* Test for existence.  */
+#define	R_OK	4		/* Test for read permission.  */
+#define	W_OK	2		/* Test for write permission.  */
+#define	X_OK	1		/* Test for execute permission.  */
+#define	F_OK	0		/* Test for existence.  */
 #endif
-#endif	/* Use misc.  */
+#endif /* Use misc.  */
 
 
 /* Mask for file access modes.  */
@@ -99,12 +101,12 @@ Cambridge, MA 02139, USA.  */
 
 /* Do the file control operation described by CMD on FD.
    The remaining arguments are interpreted depending on CMD.  */
-extern int EXFUN(__fcntl, (int __fd, int __cmd, ...));
+extern int __fcntl __P ((int __fd, int __cmd,...));
 
 /* Open FILE and return a new file descriptor for it, or -1 on error.
    OFLAG determines the type of access used.  If O_CREAT is on OFLAG,
    the third argument is taken as a `mode_t', the mode of the created file.  */
-extern int EXFUN(__open, (CONST char *__file, int __oflag, ...));
+extern int __open __P ((__const char *__file, int __oflag,...));
 
 #define	fcntl	__fcntl
 #define	open	__open
@@ -112,16 +114,17 @@ extern int EXFUN(__open, (CONST char *__file, int __oflag, ...));
 /* Create and open FILE, with mode MODE.
    This takes an `int' MODE argument because that is
    what `mode_t' will be widened to.  */
-extern int EXFUN(creat, (CONST char *__file, __mode_t __mode));
+extern int creat __P ((__const char *__file, __mode_t __mode));
 
 #ifdef	__OPTIMIZE__
 #define	creat(file, m)	__open((file), O_WRONLY|O_CREAT|O_TRUNC, (m))
-#endif	/* Optimizing.  */
+#endif /* Optimizing.  */
 
 #define	flock	__flock
 #define	F_RDLCK	__F_RDLCK
 #define	F_WRLCK	__F_WRLCK
 #define	F_UNLCK	__F_UNLCK
 
+__END_DECLS
 
-#endif	/* fcntl.h  */
+#endif /* fcntl.h  */
