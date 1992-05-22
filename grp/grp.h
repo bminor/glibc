e5fb1813dbc08a35e3a2562f5929542b307dad04
Roland McGrath <roland@gnu.org>
1992-05-22 07:50:42 +0000

    Formerly ../grp/grp.h.~9~

--- grp/grp.h
+++ grp/grp.h
@@ -13,7 +13,7 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with the GNU C Library; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+not, write to the, 1992 Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 /*
@@ -25,21 +25,19 @@ Cambridge, MA 02139, USA.  */
 #define	_GRP_H	1
 #include <features.h>
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+__BEGIN_DECLS
 
 #include <gnu/types.h>
 
 
 /* The group structure.	 */
 struct group
-  {
-    char *gr_name;	/* Group name.	*/
-    char *gr_passwd;	/* Password.	*/
-    __gid_t gr_gid;	/* Group ID.	*/
-    char **gr_mem;	/* Member list.	*/
-  };
+{
+  char *gr_name;		/* Group name.	*/
+  char *gr_passwd;		/* Password.	*/
+  __gid_t gr_gid;		/* Group ID.	*/
+  char **gr_mem;		/* Member list.	*/
+};
 
 
 #if defined(__USE_SVID) || defined(__USE_GNU)
@@ -49,38 +47,38 @@ struct group
 
 #ifdef	__USE_GNU
 /* Return a new stream open on the group file.  */
-extern FILE *EXFUN(__grpopen, (NOARGS));
+extern FILE *__grpopen __P ((void));
 
 /* Read a group entry from STREAM, filling in G.
    Return the `struct group' of G if successful, NULL on failure.  */
-extern struct group *EXFUN(__grpread, (FILE *__stream, PTR __g));
+extern struct group *__grpread __P ((FILE * __stream, __ptr_t __g));
 
 /* Return a chunk of memory containing pre-initialized data for __grpread.  */
-extern PTR EXFUN(__grpalloc, (NOARGS));
+extern __ptr_t __grpalloc __P ((void));
 #endif
 
 
-#if defined(__USE_SVID) || defined(__USE_MISC)
+#if defined(__USE_SVID) || defined(__USE_MISC) || defined (__USE_BSD)
 /* Rewind the group-file stream.  */
-extern void EXFUN(setgrent, (NOARGS));
+extern void setgrent __P ((void));
 
 /* Close the group-file stream.  */
-extern void EXFUN(endgrent, (NOARGS));
+extern void endgrent __P ((void));
 
 /* Read an entry from the group-file stream, opening it if necessary.  */
-extern struct group *EXFUN(getgrent, (NOARGS));
+extern struct group *getgrent __P ((void));
 #endif
 
 #ifdef	__USE_SVID
 /* Read a group entry from STREAM.  */
-extern struct group *EXFUN(fgetgrent, (FILE *__stream));
+extern struct group *fgetgrent __P ((FILE * __stream));
 #endif
 
 /* Search for an entry with a matching group ID.  */
-extern struct group *EXFUN(getgrgid, (__gid_t __gid));
+extern struct group *getgrgid __P ((__gid_t __gid));
 
 /* Search for an entry with a matching group name.  */
-extern struct group *EXFUN(getgrnam, (CONST char *__name));
+extern struct group *getgrnam __P ((__const char *__name));
 
 
 #ifdef	__USE_BSD
@@ -89,17 +87,15 @@ extern struct group *EXFUN(getgrnam, (CONST char *__name));
 #include <stddef.h>
 
 /* Set the group set for the current user to GROUPS (N of them).  */
-extern int EXFUN(setgroups, (size_t __n, CONST __gid_t *groups));
+extern int setgroups __P ((size_t __n, __const __gid_t * groups));
 
 /* Initialize the group set for the current user
    by reading the group database and using all groups
    of which USER is a member.  Also include GROUP.  */
-extern int EXFUN(initgroups, (CONST char *user, __gid_t group));
+extern int initgroups __P ((__const char *user, __gid_t group));
 
-#endif	/* Use BSD.  */
+#endif /* Use BSD.  */
 
-#ifdef	__cplusplus
-}
-#endif
+__END_DECLS
 
-#endif	/* grp.h  */
+#endif /* grp.h  */
