7a558b56f71803d99744afd705508d1b6aecb555
Roland McGrath <roland@gnu.org>
1992-05-22 10:04:42 +0000

    Formerly features.h.~6~

--- features.h
+++ features.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -136,5 +136,8 @@ Cambridge, MA 02139, USA.  */
 #define	__extension__
 #endif
 
+
+/* This is here only because every header file already includes this one.  */
+#include <sys/cdefs.h>
 
 #endif	/* __features.h  */
