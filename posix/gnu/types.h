3528e943522bf00e01f22db919eac6857c31c9c3
Roland McGrath <roland@gnu.org>
1992-05-22 07:51:11 +0000

    Formerly ../posix/gnu/types.h.~8~

--- posix/gnu/types.h
+++ posix/gnu/types.h
@@ -13,7 +13,7 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with the GNU C Library; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+not, write to the, 1992 Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #ifndef	_GNU_TYPES_H
@@ -26,27 +26,33 @@ typedef unsigned char __u_char;
 typedef unsigned short __u_short;
 typedef unsigned int __u_int;
 typedef unsigned long __u_long;
-typedef struct { long val[2]; } __quad;
-typedef struct { __u_long val[2]; } __u_quad;
+typedef struct
+{
+  long val[2];
+} __quad;
+typedef struct
+{
+  __u_long val[2];
+} __u_quad;
 
-typedef int __dev_t;			/* Type of device numbers.  */
-typedef unsigned int __uid_t;		/* Type of user identifications.  */
-typedef unsigned int __gid_t;		/* Type of group identifications.  */
+typedef int __dev_t;		/* Type of device numbers.  */
+typedef unsigned int __uid_t;	/* Type of user identifications.  */
+typedef unsigned int __gid_t;	/* Type of group identifications.  */
 typedef unsigned long int __ino_t;	/* Type of file serial numbers.  */
-typedef unsigned int __mode_t;		/* Type of file attribute bitmasks.  */
+typedef unsigned int __mode_t;	/* Type of file attribute bitmasks.  */
 typedef unsigned short int __nlink_t;	/* Type of file link counts.  */
-typedef long int __off_t;		/* Type of file sizes and offsets.  */
-typedef int __pid_t;			/* Type of process identifications.  */
-typedef int __ssize_t;			/* Type of a byte count, or error.  */
-
+typedef long int __off_t;	/* Type of file sizes and offsets.  */
+typedef int __pid_t;		/* Type of process identifications.  */
+typedef int __ssize_t;		/* Type of a byte count, or error.  */
+typedef __quad __fsid_t;	/* Type of file system IDs.  */
 
 /* Everythin' else.  */
 typedef long int __daddr_t;	/* The type of a disk address.  */
 typedef char *__caddr_t;
 typedef long int __time_t;
 typedef long int __swblk_t;	/* Type of a swap block maybe?  */
-
 
+
 
 /* fd_set for select.  */
 
@@ -59,16 +65,16 @@ typedef long int __swblk_t;	/* Type of a swap block maybe?  */
 #define	__FDMASK(d)	(1 << ((d) % __NFDBITS))
 
 typedef struct
-  {
-    unsigned long int __bits[(__FD_SETSIZE + (__NFDBITS - 1)) / __NFDBITS];
-  } __fd_set;
+{
+  unsigned long int __bits[(__FD_SETSIZE + (__NFDBITS - 1)) / __NFDBITS];
+} __fd_set;
 
 /* This line MUST be split!  Otherwise m4 will not change it.  */
 #define	__FD_ZERO(set)	\
-  ((void) memset((PTR) (set), 0, sizeof(fd_set)))
+  ((void) memset((__ptr_t) (set), 0, sizeof(fd_set)))
 #define	__FD_SET(d, set)	((set)->__bits[__FDELT(d)] |= __FDMASK(d))
 #define	__FD_CLR(d, set)	((set)->__bits[__FDELT(d)] &= ~__FDMASK(d))
 #define	__FD_ISSET(d, set)	((set)->__bits[__FDELT(d)] & __FDMASK(d))
 
 
-#endif	/* gnu/types.h */
+#endif /* gnu/types.h */
