e45aacc8c1236d121e1a8a9a252c03b84f16d279
Roland McGrath <roland@gnu.org>
1992-06-10 00:29:46 +0000

    entered into RCS

--- sysdeps/posix/rename.c
+++ sysdeps/posix/rename.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -36,6 +36,8 @@ DEFUN(rename, (old, new), CONST char *old AND CONST char *new)
 	      __link(old, new) < 0)
 	    return -1;
 	}
+      else
+	return -1;
     }
   if (__unlink(old) < 0)
     {
