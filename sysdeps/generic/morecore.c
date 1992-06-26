5bad3ddad7fd43bc56615664b420cdd0ee3abf97
Roland McGrath <roland@gnu.org>
1992-06-26 23:56:56 +0000

    Formerly unix/morecore.c.~5~

--- sysdeps/generic/morecore.c
+++ sysdeps/generic/morecore.c
@@ -15,16 +15,20 @@ You should have received a copy of the GNU General Public License
 along with the GNU C Library; see the file COPYING.  If not, write to
 the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include <stdio.h>
-
+#ifndef	_MALLOC_INTERNAL
 #define	_MALLOC_INTERNAL
 #include <malloc.h>
+#endif
 
 #ifndef	__GNU_LIBRARY__
 #define	__sbrk	sbrk
 #endif
 
 extern __ptr_t __sbrk __P ((int increment));
+
+#ifndef NULL
+#define NULL 0
+#endif
 
 /* Allocate INCREMENT more bytes of data space,
    and return the start of data space, or NULL on errors.
