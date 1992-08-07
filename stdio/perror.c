7aba9e710cae5a3f997705e13276442c29824f38
Roland McGrath <roland@gnu.org>
1992-08-07 21:39:06 +0000

    Formerly ../stdio/perror.c.~7~

--- stdio/perror.c
+++ stdio/perror.c
@@ -23,9 +23,6 @@ Cambridge, MA 02139, USA.  */
 #ifndef HAVE_GNU_LD
 #define _sys_errlist sys_errlist
 #define _sys_nerr sys_nerr
-#else
-extern CONST char *CONST _sys_errlist[];
-extern CONST int _sys_nerr;
 #endif
 
 /* Print a line on stderr consisting of the text in S, a colon, a space,
