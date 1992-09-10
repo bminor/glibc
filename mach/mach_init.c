081ab0c9a1141c3ba3ec94ce2d2bcac337ae3727
Roland McGrath <roland@gnu.org>
1992-09-10 23:55:32 +0000

    Formerly ../mach/mach_init.c.~3~

--- mach/mach_init.c
+++ mach/mach_init.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1992 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@ vm_size_t __vm_page_size;
 void
 __mach_init (void)
 {
-  error_t err;
+  kern_return_t err;
   vm_statistics_data_t stats;
 
   __mach_task_self_ = (__mach_task_self) ();
