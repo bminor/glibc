1b5e5338d66d4b6302134289ed985e6aab48f29f
Roland McGrath <roland@gnu.org>
1992-09-17 23:01:28 +0000

    Formerly mach/hurd/statbuf.h.~2~

--- sysdeps/mach/hurd/statbuf.h
+++ sysdeps/mach/hurd/statbuf.h
@@ -24,7 +24,7 @@ Cambridge, MA 02139, USA.  */
 
 struct stat
   {
-    int st_ftype;		/* File system type.  */
+    int st_fstype;		/* File system type.  */
     __fsid_t st_fsid;		/* File system ID.  */
     __ino_t st_ino;		/* File number.  */
     unsigned int st_gen;	/* To detect reuse of file numbers.  */
@@ -40,8 +40,33 @@ struct stat
     unsigned long int st_mtime_usec; /* and microseconds.  */
     __time_t st_ctime;		/* Status change time, seconds */
     unsigned long int st_ctime_usec; /* and microseconds.  */
+    unsigned int st_blksize;	/* Optimal size for I/O.  */
+    unsigned int st_blocks;	/* Number of 512-byte blocks allocated.
+				   Not particular related to `st_blksize'.  */
     __uid_t st_author;		/* File author.  */
     int st_spare[8];		/* Reserved for future use.  */
   };
+
+/* Encoding of the file mode.  */
+
+#define	__S_IFMT	0170000	/* These bits determine file type.  */
+
+/* File types.  */
+#define	__S_IFDIR	0040000	/* Directory.  */
+#define	__S_IFCHR	0020000	/* Character device.  */
+#define	__S_IFBLK	0060000	/* Block device.  */
+#define	__S_IFREG	0100000	/* Regular file.  */
+#define	__S_IFLNK	0120000	/* Symbolic link.  */
+#define	__S_IFSOCK	0140000	/* Socket.  */
+#define	__S_IFIFO	0010000	/* FIFO.  */
+
+/* Protection bits.  */
+
+#define	__S_ISUID	04000	/* Set user ID on execution.  */
+#define	__S_ISGID	02000	/* Set group ID on execution.  */
+#define	__S_ISVTX	01000	/* Save swapped text after use (sticky).  */
+#define	__S_IREAD	0400	/* Read by owner.  */
+#define	__S_IWRITE	0200	/* Write by owner.  */
+#define	__S_IEXEC	0100	/* Execute by owner.  */
 
 #endif /* statbuf.h */
