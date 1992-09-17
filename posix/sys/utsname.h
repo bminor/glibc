1d236200a083a9007413a47844afe800cfd78f94
Roland McGrath <roland@gnu.org>
1992-09-17 04:43:36 +0000

    Formerly posix/sys/utsname.h.~4~

--- posix/sys/utsname.h
+++ posix/sys/utsname.h
@@ -31,21 +31,21 @@ __BEGIN_DECLS
 
 /* Structure describing the system and machine.  */
 struct utsname
-{
-  /* Name of the implementation of the operating system.  */
-  char sysname[_UTSNAME_LENGTH];
+  {
+    /* Name of the implementation of the operating system.  */
+    char sysname[_UTSNAME_LENGTH];
 
-  /* Name of this node on the network.  */
-  char nodename[_UTSNAME_LENGTH];
+    /* Name of this node on the network.  */
+    char nodename[_UTSNAME_LENGTH];
 
-  /* Current release level of this implementation.  */
-  char release[_UTSNAME_LENGTH];
-  /* Current version level of this release.  */
-  char version[_UTSNAME_LENGTH];
+    /* Current release level of this implementation.  */
+    char release[_UTSNAME_LENGTH];
+    /* Current version level of this release.  */
+    char version[_UTSNAME_LENGTH];
 
-  /* Name of the hardware type the system is running on.  */
-  char machine[_UTSNAME_LENGTH];
-};
+    /* Name of the hardware type the system is running on.  */
+    char machine[_UTSNAME_LENGTH];
+  };
 
 
 /* Put information about the system in NAME.  */
