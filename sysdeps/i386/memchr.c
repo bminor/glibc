90962a87e2c90bd8d2235d83ea828df28136f640
Roland McGrath <roland@gnu.org>
1992-10-02 22:20:07 +0000

    Formerly i386/memchr.c.~10~

--- sysdeps/i386/memchr.c
+++ sysdeps/i386/memchr.c
@@ -33,10 +33,10 @@ DEFUN(memchr, (str, c, len),
       "testl %1,%1\n"		/* Clear Z flag, to handle LEN == 0.  */
       "repne\n"			/* Search for C in al.  */
       "scasb\n"
-      "movl %2,%0\n"		/* Set %0 to 0 (without affecting Z flag) */
-      "jnz 1f\n"		/* Jump if we found nothing equal to C */
+      "movl %2,%0\n"		/* Set %0 to 0 (without affecting Z flag).  */
+      "jnz done\n"		/* Jump if we found nothing equal to C.  */
       "leal -1(%1),%0\n"	/* edi has been incremented.  Return edi-1.  */
-      "1:" :
+      "done:" :
       "=a" (retval), "=D" (str), "=c" (len) :
       "0" (c), "1" (str), "2" (len));
   return retval;
