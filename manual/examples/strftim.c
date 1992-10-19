fdde6bbe2af46d51b65297a6880148ec1f6e48dd
Roland McGrath <roland@gnu.org>
1992-10-19 23:24:19 +0000

    Add @group...@end group.

--- manual/examples/strftim.c
+++ manual/examples/strftim.c
@@ -19,6 +19,7 @@ main (void)
   /* Print out the date and time in the standard format. */
   fputs (asctime (loctime), stdout);
 
+/*@group*/
   /* Print it out in a nice format. */
   strftime (buffer, SIZE, "Today is %A, %B %d.\n", loctime);
   fputs (buffer, stdout);
@@ -27,3 +28,4 @@ main (void)
 
   return 0;
 }
+/*@end group*/
