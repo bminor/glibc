baaf33134ff9eb0e8d5a3d65c4a1897fc08d2a98
Roland McGrath <roland@gnu.org>
1992-11-06 21:09:56 +0000

    Formerly ../resource/sys/resource.h.~9~

--- resource/sys/resource.h
+++ resource/sys/resource.h
@@ -28,39 +28,39 @@ __BEGIN_DECLS
 
 /* Kinds of resource limit.  */
 enum __rlimit_resource
-{
-  /* Per-process CPU limit, in seconds.  */
-  RLIMIT_CPU,
-  /* Largest file that can be created, in bytes.  */
-  RLIMIT_FSIZE,
-  /* Maximum size of data segment, in bytes.  */
-  RLIMIT_DATA,
-  /* Maximum size of stack segment, in bytes.  */
-  RLIMIT_STACK,
-  /* Largest core file that can be created, in bytes.  */
-  RLIMIT_CORE,
-  /* Largest resident set size, in bytes.
-     This affects swapping; processes that are exceeding their
-     resident set size will be more likely to have physical memory
-     taken from them.  */
-  RLIMIT_RSS,
-  /* Locked-in-memory address space.  */
-  RLIMIT_MEMLOCK,
-  /* Number of processes,  */
-  RLIMIT_NPROC,
-  /* Number of open files.  */
-  RLIMIT_OFILE,
+  {
+    /* Per-process CPU limit, in seconds.  */
+    RLIMIT_CPU,
+    /* Largest file that can be created, in bytes.  */
+    RLIMIT_FSIZE,
+    /* Maximum size of data segment, in bytes.  */
+    RLIMIT_DATA,
+    /* Maximum size of stack segment, in bytes.  */
+    RLIMIT_STACK,
+    /* Largest core file that can be created, in bytes.  */
+    RLIMIT_CORE,
+    /* Largest resident set size, in bytes.
+       This affects swapping; processes that are exceeding their
+       resident set size will be more likely to have physical memory
+       taken from them.  */
+    RLIMIT_RSS,
+    /* Locked-in-memory address space.  */
+    RLIMIT_MEMLOCK,
+    /* Number of processes,  */
+    RLIMIT_NPROC,
+    /* Number of open files.  */
+    RLIMIT_OFILE,
 
-  RLIM_NLIMITS
-};
+    RLIM_NLIMITS
+    };
 
 struct rlimit
-{
-  /* The current (soft) limit.  */
-  int rlim_cur;
-  /* The hard limit.  */
-  int rlim_max;
-};
+  {
+    /* The current (soft) limit.  */
+    int rlim_cur;
+    /* The hard limit.  */
+    int rlim_max;
+  };
 
 /* Value used to indicate that there is no limit.  */
 #define RLIM_INFINITY 0x7fffffff
@@ -79,64 +79,62 @@ int setrlimit __P ((enum __rlimit_resource __resource,
 
 /* Whose usage statistics do you want?  */
 enum __rusage_who
-{
-  /* The calling process.  */
-  RUSAGE_SELF = 0,
-  /* All of its terminated child processes.  */
-  RUSAGE_CHILDREN = -1,
-};
+  {
+    /* The calling process.  */
+    RUSAGE_SELF = 0,
+    /* All of its terminated child processes.  */
+    RUSAGE_CHILDREN = -1,
+  };
+
+#include <sys/time.h>		/* For `struct timeval'.  */
 
 /* Structure which says how much of each resource has been used.  */
 struct rusage
-{
-  /* Total amount of user time used.  */
-  struct __timeval ru_utime;
-  /* Total amount of system time used.  */
-  struct __timeval ru_stime;
-  /* Maximum resident set size (in kilobytes).  */
-  int ru_maxrss;
-  /* Amount of sharing of text segment memory
-     with other processes (kilobyte-seconds).  */
-  int ru_ixrss;
-  /* Amount of data segment memory used (kilobyte-seconds).  */
-  int ru_idrss;
-  /* Amount of stack memory used (kilobyte-seconds).  */
-  int ru_isrss;
-  /* Number of soft page faults (i.e. those serviced by reclaiming
-     a page from the list of pages awaiting reallocation.  */
-  int ru_minflt;
-  /* Number of hard page faults (i.e. those that required I/O).  */
-  int ru_majflt;
-  /* Number of times a process was swapped out of physical memory.  */
-  int ru_nswap;
-  /* Number of input operations via the file system.  Note: This
-     and `ru_oublock' do not include operations with the cache.  */
-  int ru_inblock;
-  /* Number of output operations via the file system.  */
-  int ru_oublock;
-  /* Number of IPC messages sent.  */
-  int ru_msgsnd;
-  /* Number of IPC messages received.  */
-  int ru_msgrcv;
-  /* Number of signals delivered.  */
-  int ru_nsignals;
-  /* Number of voluntary context switches, i.e. because the process
-     gave up the process before it had to (usually to wait for some
-     resource to be available).  */
-  int ru_nvcsw;
-  /* Number of involuntary context switches, i.e. a higher priority process
-     became runnable or the current process used up its time slice.  */
-  int ru_nivcsw;
-};
+  {
+    /* Total amount of user time used.  */
+    struct timeval ru_utime;
+    /* Total amount of system time used.  */
+    struct timeval ru_stime;
+    /* Maximum resident set size (in kilobytes).  */
+    int ru_maxrss;
+    /* Amount of sharing of text segment memory
+       with other processes (kilobyte-seconds).  */
+    int ru_ixrss;
+    /* Amount of data segment memory used (kilobyte-seconds).  */
+    int ru_idrss;
+    /* Amount of stack memory used (kilobyte-seconds).  */
+    int ru_isrss;
+    /* Number of soft page faults (i.e. those serviced by reclaiming
+       a page from the list of pages awaiting reallocation.  */
+    int ru_minflt;
+    /* Number of hard page faults (i.e. those that required I/O).  */
+    int ru_majflt;
+    /* Number of times a process was swapped out of physical memory.  */
+    int ru_nswap;
+    /* Number of input operations via the file system.  Note: This
+       and `ru_oublock' do not include operations with the cache.  */
+    int ru_inblock;
+    /* Number of output operations via the file system.  */
+    int ru_oublock;
+    /* Number of IPC messages sent.  */
+    int ru_msgsnd;
+    /* Number of IPC messages received.  */
+    int ru_msgrcv;
+    /* Number of signals delivered.  */
+    int ru_nsignals;
+    /* Number of voluntary context switches, i.e. because the process
+       gave up the process before it had to (usually to wait for some
+       resource to be available).  */
+    int ru_nvcsw;
+    /* Number of involuntary context switches, i.e. a higher priority process
+       became runnable or the current process used up its time slice.  */
+    int ru_nivcsw;
+  };
 
 /* Return resource usage information on process indicated by WHO
    and put it in *USAGE.  Returns 0 for success, -1 for failure.  */
 int __getrusage __P ((enum __rusage_who __who, struct rusage * __usage));
 int getrusage __P ((enum __rusage_who __who, struct rusage * __usage));
-
-#ifdef	__OPTIMIZE__
-#define	getrusage(who, usage)	__getrusage((who), (usage))
-#endif /* Optimizing.  */
 
 /* Function depends on CMD:
    1 = Return the limit on the size of a file, in units of 512 bytes.
@@ -148,10 +146,6 @@ int getrusage __P ((enum __rusage_who __who, struct rusage * __usage));
 long int __ulimit __P ((int __cmd, long int __newlimit));
 long int ulimit __P ((int __cmd, long int __newlimit));
 
-#ifdef	__OPTIMIZE__
-#define	ulimit(cmd, newlimit)	__ulimit((cmd), (newlimit))
-#endif /* Optimizing.  */
-
 
 /* Priority limits.  */
 #define	PRIO_MIN	-20	/* Minimum priority a process can have.  */
@@ -160,11 +154,11 @@ long int ulimit __P ((int __cmd, long int __newlimit));
 /* The type of the WHICH argument to `getpriority' and `setpriority',
    indicating what flavor of entity the WHO argument specifies.  */
 enum __priority_which
-{
-  PRIO_PROCESS = 0,		/* WHO is a process ID.  */
-  PRIO_PGRP = 1,		/* WHO is a process group ID.  */
-  PRIO_USER = 2,		/* WHO is a user ID.  */
-};
+  {
+    PRIO_PROCESS = 0,		/* WHO is a process ID.  */
+    PRIO_PGRP = 1,		/* WHO is a process group ID.  */
+    PRIO_USER = 2,		/* WHO is a user ID.  */
+  };
 
 /* Return the highest priority of any process specified by WHICH and WHO
    (see above); if WHO is zero, the current process, process group, or user
