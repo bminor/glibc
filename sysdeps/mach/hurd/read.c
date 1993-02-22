f0e7d50b3661c7e0b85f5a65425596367187cec3
Roland McGrath <roland@gnu.org>
1993-02-22 21:29:45 +0000

    Formerly mach/hurd/__read.c.~7~

--- sysdeps/mach/hurd/read.c
+++ sysdeps/mach/hurd/read.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,7 +31,7 @@ DEFUN(__read, (fd, buf, nbytes),
   size_t nread;
 
   data = buf;
-!!  _HURD_DPORT_USE
+  _HURD_DPORT_USE
     (fd,
      ({
      call:
