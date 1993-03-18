4b7dba3c7a1711791aefdcfb480ed1c74f2a2944
Roland McGrath <roland@gnu.org>
1993-03-18 00:19:32 +0000

    entered into RCS

--- mach/mach_init.h
+++ mach/mach_init.h
@@ -1,3 +1,22 @@
+/* Declarations and macros for the basic Mach things set at startup.
+Copyright (C) 1993 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
 #ifndef	_MACH_INIT_H
 
 #define	_MACH_INIT_H	1
@@ -10,8 +29,8 @@ extern mach_port_t mach_task_self (void);
 
 /* This cache is initialized at startup.  */
 extern mach_port_t __mach_task_self_;
-#define __mach_task_self()	__mach_task_self_
-#define mach_task_self()	__mach_task_self ()
+#define __mach_task_self()	(__mach_task_self_ + 0)	/* Not an lvalue.  */
+#define mach_task_self()	(__mach_task_self ())
 
 /* Kernel page size.  */
 extern vm_size_t __vm_page_size;
