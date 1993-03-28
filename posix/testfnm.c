8f5b1e149cc9b58cb1b515718c0f45376775a626
Roland McGrath <roland@gnu.org>
1993-03-28 21:12:05 +0000

    Initial revision

---
+++ posix/testfnm.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "fnmatch.h"
+
+main (c, v)
+int c;
+char **v;
+{
+printf ("%d\n", fnmatch (v[1], v[2], FNM_PERIOD));
+printf ("%d\n", fnmatch (v[1], v[2], FNM_CASEFOLD|FNM_PERIOD));
+exit (0);
+}
