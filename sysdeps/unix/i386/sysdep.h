1473404cda77cc4b79f5d70d39e4b7fd766d7619
Roland McGrath <roland@gnu.org>
1993-04-05 00:19:49 +0000

    Formerly unix/i386/sysdep.h.~18~

--- sysdeps/unix/i386/sysdep.h
+++ sysdeps/unix/i386/sysdep.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -72,6 +72,7 @@ Cambridge, MA 02139, USA.  */
   jb syscall_error
 #endif
 
-#define	r0	%eax
-#define	r1	%edx
-#define MOVE(x,y)	movl x , y
+#define	r0		%eax	/* Normal return-value register.  */
+#define	r1		%edx	/* Secondary return-value register.  */
+#define scratch 	%ecx	/* Call-clobbered register for random use.  */
+#define MOVE(x,y)	movl x, y
