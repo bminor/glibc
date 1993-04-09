438f32369f57c2bd8871bef09077ebb19b54cc8f
Roland McGrath <roland@gnu.org>
1993-04-09 00:31:42 +0000

    Formerly ../stdio/vfprintf.c.~10~

--- stdio/vfprintf.c
+++ stdio/vfprintf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -426,15 +426,15 @@ DEFUN(vfprintf, (s, format, args),
 	      else if (space)
 		outchar(' ');
 
-	      if (!left && pad == '0')
-		while (width-- > 0)
-		  outchar('0');
-
 	      if (alt && base == 16)
 		{
 		  outchar ('0');
 		  outchar (fc);
 		}
+
+	      if (!left && pad == '0')
+		while (width-- > 0)
+		  outchar('0');
 
 	      /* Write the number.  */
 	      while (++w <= workend)
