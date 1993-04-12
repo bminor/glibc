44e6f6b00f4e108de88c64355252dffac07e023b
Roland McGrath <roland@gnu.org>
1993-04-12 03:17:03 +0000

    Formerly stdio/stdio.h.~44~

--- stdio/stdio.h
+++ stdio/stdio.h
@@ -147,7 +147,8 @@ extern __NORETURN void __libc_fatal __P ((__const char *__message));
 struct __stdio_file
 {
   /* Magic number for validation.  Must be negative in open streams
-     for the glue to Unix stdio getc/putc to work.  */
+     for the glue to Unix stdio getc/putc to work.
+     NOTE: stdio/glue.c has special knowledge of these first four members.  */
   int __magic;
 #define	_IOMAGIC	0xfedabeeb	/* Magic number to fill `__magic'.  */
 #define	_GLUEMAGIC	0xfeedbabe	/* Magic for glued Unix streams.  */
@@ -155,25 +156,27 @@ struct __stdio_file
   char *__bufp;			/* Pointer into the buffer.  */
   char *__get_limit;		/* Reading limit.  */
   char *__put_limit;		/* Writing limit.  */
+
   char *__buffer;		/* Base of buffer.  */
   size_t __bufsize;		/* Size of the buffer.  */
-  FILE *__next;			/* Next FILE in the linked list.  */
   __ptr_t __cookie;		/* Magic cookie.  */
   int __fileno;			/* System file descriptor.  */
-  unsigned char __pushback;	/* Pushed-back character.  */
-  char *__pushback_bufp;	/* Old bufp if char pushed back.  */
-  unsigned int __pushed_back:1;	/* A char has been pushed back.  */
-  unsigned int __eof:1;		/* End of file encountered.  */
-  unsigned int __error:1;	/* Error encountered.  */
-  unsigned int __userbuf:1;	/* Buffer is from user.  */
-  unsigned int __linebuf:1;	/* Flush on newline.  */
-  unsigned int __seen:1;	/* This stream has been seen.  */
-  unsigned int __ispipe:1;	/* Nonzero if opened by popen.  */
   __io_mode __mode;		/* File access mode.  */
   __io_functions __io_funcs;	/* I/O functions.  */
   __room_functions __room_funcs;/* I/O buffer room functions.  */
   fpos_t __offset;		/* Current file position.  */
   fpos_t __target;		/* Target file position.  */
+  FILE *__next;			/* Next FILE in the linked list.  */
+  char *__pushback_bufp;	/* Old bufp if char pushed back.  */
+  unsigned char __pushback;	/* Pushed-back character.  */
+  unsigned int __pushed_back:1;	/* A char has been pushed back.  */
+  unsigned int __eof:1;		/* End of file encountered.  */
+  unsigned int __error:1;	/* Error encountered.  */
+  unsigned int __userbuf:1;	/* Buffer from user (should not be freed).  */
+  unsigned int __linebuf:1;	/* Flush on newline.  */
+  unsigned int __linebuf_active:1; /* put_limit is not really in use.  */
+  unsigned int __seen:1;	/* This stream has been seen.  */
+  unsigned int __ispipe:1;	/* Nonzero if opened by popen.  */
 };
 
 
@@ -448,23 +451,23 @@ extern int putc __P ((int __c, FILE *__stream));
 /* Write a character to stdout.  */
 extern int putchar __P ((int __c));
 
+
 /* The C standard explicitly says this can
    re-evaluate its arguments, so it does.  */
 #define	__putc(c, stream)						      \
-  ((stream)->__bufp < (stream)->__put_limit ? 				      \
+  ((stream)->__bufp < (stream)->__put_limit ?				      \
    (int) (unsigned char) (*(stream)->__bufp++ = (unsigned char) (c)) :	      \
-   ((stream)->__linebuf && (c) != '\n' &&				      \
-    (stream)->__bufp - (stream)->__buffer < (stream)->__bufsize) ?	      \
-   (*(stream)->__bufp++ = (unsigned char) (c)) :			      \
-   __flshfp((stream), (unsigned char) (c)))
+   __flshfp ((stream), (unsigned char) (c)))
 
 /* The C standard explicitly says this can be a macro,
    so we always do the optimization for it.  */
-#define	putc(c, stream)	__putc((c), (stream))
+#define	putc(c, stream)	__putc ((c), (stream))
 
-#ifdef	__OPTIMIZE__
-#define	putchar(c)	__putc((c), stdout)
-#endif /* Optimizing.  */
+#ifdef __OPTIMIZE__
+/* Note that putchar is NOT allowed to evaluate its arguments more than once.
+   This macro is valid only because __putc evaluates C exactly once.  */
+#define putchar(c)	__putc ((c), stdout)
+#endif
 
 
 #if defined(__USE_SVID) || defined(__USE_MISC)
