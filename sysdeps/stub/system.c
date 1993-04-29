1469e41861dfab32911ead28e1ef3e2c485d6d57
Roland McGrath <roland@gnu.org>
1993-04-29 01:50:06 +0000

    entered into RCS

--- sysdeps/stub/system.c
+++ sysdeps/stub/system.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,8 +25,11 @@ Cambridge, MA 02139, USA.  */
 int
 DEFUN(system, (line), CONST char *line)
 {
+  if (line == NULL)
+    return 0;			/* This indicates no command processor.  */
+
   errno = ENOSYS;
-  return(-1);
+  return -1;
 }
 
 
