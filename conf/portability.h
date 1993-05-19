269b84f79d8f1a680ab551942c33c26ba0becbdd
Roland McGrath <roland@gnu.org>
1993-05-19 20:01:06 +0000

    Formerly ../conf/portability.h.~2~

--- conf/portability.h
+++ conf/portability.h
@@ -1,1 +1,6 @@
-/* fnord */
+/* This file is used by some of the resolver code in inet/ that
+   comes from BIND 4.9.  I have written this file instead of modifying
+   those things not to use it so that I can later drop in replacement
+   files from future BIND distributions without change.  */
+
+#include <unistd.h>
