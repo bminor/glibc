1e0106ebb3cbe5e5a2e078c7f067bb857f9741f0
Roland McGrath <roland@gnu.org>
1993-05-20 22:03:08 +0000

    entered into RCS

--- sysdeps/unix/common/direct.h
+++ sysdeps/unix/common/direct.h
@@ -33,7 +33,11 @@ struct direct
 
 #include <stddef.h>
 
+/* We calculate the length of the name by taking the length of the whole
+   `struct direct' record, subtracting the size of everything before the
+   name, and subtracting one for the terminating null.  */
+
 #define D_NAMLEN(d) \
-  ((d)->d_reclen - offsetof (struct direct, d_name))
+  ((d)->d_reclen - offsetof (struct direct, d_name) - 1)
 
 #endif
