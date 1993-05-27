db33ffa4c1f7f423c92e7d5f42de46cdfabce668
Roland McGrath <roland@gnu.org>
1993-05-27 00:13:43 +0000

    Formerly ../stdio/internals.c.~43~

--- stdio/internals.c
+++ stdio/internals.c
@@ -458,7 +458,7 @@ extern __io_seek __stdio_seek;
 extern __io_close __stdio_close;
 CONST __io_functions __default_io_functions =
   {
-    __stdio_read, __stdio_write, __stdio_seek, __stdio_close
+    __stdio_read, __stdio_write, __stdio_seek, __stdio_close, __stdio_fileno
   };
 
 CONST __room_functions __default_room_functions =
