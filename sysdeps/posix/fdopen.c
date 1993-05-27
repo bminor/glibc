7c827bc7ab99ccac86ea49c05e50502479a4c64b
Roland McGrath <roland@gnu.org>
1993-05-27 00:09:56 +0000

    Formerly posix/fdopen.c.~3~

--- sysdeps/posix/fdopen.c
+++ sysdeps/posix/fdopen.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -65,8 +65,7 @@ DEFUN(fdopen, (fd, mode), int fd AND CONST char *mode)
   if (stream == NULL)
     return NULL;
 
-  stream->__fileno = fd;
-  stream->__cookie = (PTR) &stream->__fileno;
+  stream->__cookie = (PTR) fd;
   stream->__mode = m;
 
   return stream;
