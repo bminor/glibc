5fe0886555767c9c5c1ad09627ba4340d7c4de60
Roland McGrath <roland@gnu.org>
1993-06-23 21:49:00 +0000

    Formerly ../hurd/__task2pid.c.~4~

--- hurd/task2pid.c
+++ hurd/task2pid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -23,7 +23,7 @@ __task2pid (task_t task)
 {
   error_t err;
   pid_t pid;
-  err = _HURD_PORT_USE (&_hurd_proc,
+  err = _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC],
 			__proc_task2pid (proc, task, &pid));
   if (err)
     return __hurd_fail (err);
