6e84c87660db36d8f082f300995ad7ced65a315d
Roland McGrath <roland@gnu.org>
1993-06-29 05:10:39 +0000

    Formerly ../hurd/hurdinit.c.~6~

--- hurd/hurdinit.c
+++ hurd/hurdinit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,51 +31,36 @@ _hurd_init (char **argv,
 {
   int i;
 
+  _hurd_ports = malloc (portarraysize * sizeof (*_hurd_ports));
+  if (_hurd_ports == NULL)
+    __libc_fatal ("Can't allocate _hurd_ports\n");
+  _hurd_nports = portarraysize;
+
   /* See what ports we were passed.  */
   for (i = 0; i < portarraysize; ++i)
-    switch (i)
-      {
-#define	initport(upper, lower) \
-      case INIT_PORT_##upper: \
-	_hurd_port_init (&_hurd_##lower, portarray[i]); \
-	break
+    {
+      _hurd_port_init (&_hurd_ports[i], portarray[i]);
 
-	/* Install the standard ports in their cells.  */
-	initport (CCDIR, ccdir);
-	initport (CWDIR, cwdir);
-	initport (CRDIR, crdir);
-	initport (AUTH, auth);
+      switch (i)
+	{
+	case INIT_PORT_PROC:
+	  /* Tell the proc server we exist.  */
+	  _hurd_proc_init (argv);
+	  break;
 
-      case INIT_PORT_PROC:
-	/* Install the proc port and tell the proc server we exist.  */
-	_hurd_proc_init (portarray[i], argv);
-	break;
-
-      case INIT_PORT_BOOTSTRAP:
-	/* When the user asks for the bootstrap port,
-	   he will get the one the exec server passed us.  */
-	__task_set_special_port (__mach_task_self (),
-				 TASK_BOOTSTRAP_PORT, portarray[i]);
-	/* FALLTHROUGH */
-
-      default:
-	/* Wonder what that could be.  */
-	__mach_port_deallocate (__mach_task_self (), portarray[i]);
-	break;
-      }
+	case INIT_PORT_BOOTSTRAP:
+	  /* When the user asks for the bootstrap port,
+	     he will get the one the exec server passed us.  */
+	  __task_set_special_port (__mach_task_self (),
+				   TASK_BOOTSTRAP_PORT, portarray[i]);
+	  break;
+	}
+    }
 
   if (intarraysize > INIT_UMASK)
     _hurd_umask = intarray[INIT_UMASK] & 0777;
   else
     _hurd_umask = 0022;		/* XXX */
-  if (intarraysize > INIT_CTTY_FILEID) /* Knows that these are sequential.  */
-    {
-      _hurd_ctty_fstype = intarray[INIT_CTTY_FSTYPE];
-      _hurd_ctty_fsid.val[0] = intarray[INIT_CTTY_FSID1];
-      _hurd_ctty_fsid.val[1] = intarray[INIT_CTTY_FSID2];
-      _hurd_ctty_fileid = intarray[INIT_CTTY_FILEID];
-    }
-
 
   /* All done with init ints and ports.  */
   __vm_deallocate (__mach_task_self (), intarray, nints * sizeof (int));
@@ -83,33 +68,47 @@ _hurd_init (char **argv,
 		   portarray, nports * sizeof (mach_port_t));
 }
 
-/* The user can do "int _hide_arguments = 1;" to make sure the
-   arguments are never visible with `ps'.  */
-int _hide_arguments;
+/* The user can do "int _hide_arguments = 1;" to make
+   sure the arguments are never visible with `ps'.  */
+int _hide_arguments, _hide_environment;
 
-/* Make PROCSERVER be our proc server port.
-   Tell the proc server that we exist.  */
+/* Do startup handshaking with the proc server just installed in _hurd_ports.
+   Call _hurdsig_init to set up signal processing.  */
 
 void
-_hurd_proc_init (process_t procserver, char **argv)
+_hurd_proc_init (char **argv)
 {
-  mach_port_t oldsig, oldtask;
-
-  _hurd_port_init (&_hurd_proc, procserver);
-
-  /* Tell the proc server where our args and environment are.  */
-  __proc_setprocargs (procserver, _hide_arguments ? 0 : argv, __environ);
+  mach_port_t oldmsg, oldtask;
+  int dealloc;
+  process_t procserver;
 
   /* Initialize the signal code; Mach exceptions will become signals.
-     This sets _hurd_msgport; it must be run before _hurd_proc_init.  */
+     This function will be a no-op on calls after the first.
+     On the first call, it sets up the message port and the signal thread.  */
   _hurdsig_init ();
 
-  /* Give the proc server our task and signal ports.  */
+  /* The signal thread is now prepared to receive messages.
+     It is safe to give the port to the proc server.  */
+
+  procserver = _hurd_port_get (&_hurd_ports[INIT_PORT_PROC], &dealloc);
+
+  /* Give the proc server our task and message ports.  */
   __proc_setports (procserver,
 		   _hurd_msgport, __mach_task_self (),
-		   &oldsig, &oldtask);
-  if (oldsig != MACH_PORT_NULL)
-    __mach_port_deallocate (__mach_task_self (), oldsig);
+		   &oldmsg, &oldtask);
+
+  /* Tell the proc server where our args and environment are.  */
+  __proc_setprocargs (procserver,
+		      _hide_arguments ? 0 : argv,
+		      _hide_environment ? 0 : __environ);
+
+  _hurd_port_free (&_hurd_ports[INIT_PORT_PROC], &dealloc, procserver);
+
+  /* Initialize proc server-assisted fault recovery for the signal thread.  */
+  _hurdsig_fault_init ();
+
+  if (oldmsg != MACH_PORT_NULL)
+    __mach_port_deallocate (__mach_task_self (), oldmsg);
   if (oldtask != MACH_PORT_NULL)
     __mach_port_deallocate (__mach_task_self (), oldtask);
 }
