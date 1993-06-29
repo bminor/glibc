af35ec563508d5ad01a09c081933a63a0ed5c712
Roland McGrath <roland@gnu.org>
1993-06-29 06:29:47 +0000

    Formerly mach/hurd/getlogin.c.~4~

--- sysdeps/mach/hurd/getlogin.c
+++ sysdeps/mach/hurd/getlogin.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,7 +30,8 @@ DEFUN_VOID(getlogin)
   static char login[1024];
   error_t err;
 
-  if (err = _HURD_PORT_USE (&_hurd_proc, __proc_getlogin (port, login)))
+  if (err = _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC],
+			    __proc_getlogin (port, login)))
     {
       errno = err;
       return NULL;
