bb0b192c6cbcb1ce28fcc5ac4ea74a5dc18e6879
Roland McGrath <roland@gnu.org>
1993-06-29 06:29:29 +0000

    Formerly mach/hurd/gethostid.c.~3~

--- sysdeps/mach/hurd/gethostid.c
+++ sysdeps/mach/hurd/gethostid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,7 +26,8 @@ DEFUN_VOID(gethostid)
 {
   long int hostid;
   error_t err;
-  if (err = _HURD_PORT_USE (&_hurd_proc, __proc_gethostid (port, &hostid)))
+  if (err = _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC],
+			    __proc_gethostid (port, &hostid)))
     return __hurd_fail (err);
   return hostid;
 }
