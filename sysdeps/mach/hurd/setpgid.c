508c82ebf838ab0d5b7586b924253a5daee66e3e
Roland McGrath <roland@gnu.org>
1993-06-29 06:20:09 +0000

    Formerly mach/hurd/__setpgrp.c.~3~

--- sysdeps/mach/hurd/setpgid.c
+++ sysdeps/mach/hurd/setpgid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -26,9 +26,7 @@ Cambridge, MA 02139, USA.  */
 int
 DEFUN(__setpgrp, (pid, pgid), pid_t pid AND pid_t pgid)
 {
-  error_t err = _HURD_PORT_USE (&_hurd_proc,
+  error_t err = _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC],
 				__proc_setpgrp (port, pid, pgid));
-  if (err)
-    return __hurd_fail (err);
-  return 0;
+  return err ? __hurd_fail (err) : 0;
 }
