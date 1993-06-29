c8bedb978cacc1ffb2580354da0b37b0a6ae0154
Roland McGrath <roland@gnu.org>
1993-06-29 06:03:48 +0000

    Formerly mach/hurd/__gethstnm.c.~3~

--- sysdeps/mach/hurd/gethostname.c
+++ sysdeps/mach/hurd/gethostname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -29,7 +29,7 @@ DEFUN(__gethostname, (name, len),
       char *name AND size_t len)
 {
   error_t err;
-  if (err = _HURD_PORT_USE (&_hurd_proc,
+  if (err = _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC],
 			    __proc_gethostname (port, name, len)))
     return __hurd_fail (err);
   return 0;
