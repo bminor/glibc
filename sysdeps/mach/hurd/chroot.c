435f1266ee249c2c8c03351a8b70b3a5ed25feb6
Roland McGrath <roland@gnu.org>
1993-06-29 06:26:35 +0000

    Formerly mach/hurd/chroot.c.~5~

--- sysdeps/mach/hurd/chroot.c
+++ sysdeps/mach/hurd/chroot.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -33,7 +33,7 @@ DEFUN(chroot, (path), CONST char *path)
   if (crdir == MACH_PORT_NULL)
     return -1;
 
-  _hurd_port_set (&_hurd_crdir, crdir);
+  _hurd_port_set (&_hurd_ports[INIT_PORT_CRDIR], crdir);
 
   return 0;
 }
