63035cc6d3a438e3421d92941b6030c8e8098c9d
Roland McGrath <roland@gnu.org>
1993-06-29 06:28:31 +0000

    Formerly mach/hurd/fchdir.c.~6~

--- sysdeps/mach/hurd/fchdir.c
+++ sysdeps/mach/hurd/fchdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -29,13 +29,13 @@ DEFUN(fchdir, (fd), int fd)
   error_t err;
   file_t cwdir;
 
-!!  if (err = _HURD_DPORT_USE (fd,
+  if (err = _HURD_DPORT_USE (fd,
 			     __mach_port_mod_refs (__mach_task_self (),
 						   (cwdir = port),
 						   MACH_PORT_RIGHT_SEND, 1)))
     return err;
 
-  _hurd_port_set (&_hurd_cwdir, cwdir);
+  _hurd_port_set (&_hurd_ports[INIT_PORT_CWDIR], cwdir);
 
   return 0;
 }
