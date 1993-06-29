a84559d93f1e4ed987f45d24f64570b0a2fac2e4
Roland McGrath <roland@gnu.org>
1993-06-29 04:56:07 +0000

    Formerly mach/hurd/__chdir.c.~7~

--- sysdeps/mach/hurd/chdir.c
+++ sysdeps/mach/hurd/chdir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,7 +30,7 @@ DEFUN(__chdir, (path), CONST char *path)
   if (cwdir == MACH_PORT_NULL)
     return -1;
 
-  _hurd_port_set (&_hurd_cwdir, cwdir);
+  _hurd_port_set (&_hurd_ports[INIT_PORT_CWDIR], cwdir);
 
   return 0;
 }
