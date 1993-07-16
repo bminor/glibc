03a1f47c850fc92c66635183eba0858d550141e2
Roland McGrath <roland@gnu.org>
1993-07-16 20:34:37 +0000

    Initial revision

---
+++ stdio/temptest.c
@@ -0,0 +1,18 @@
+main ()
+{
+  char *fn;
+  FILE *fp;
+  int i;
+
+  for (i = 0; i < 500; i++) {
+    fn = __stdio_gen_tempname((CONST char *) NULL,
+	"file", 0, (size_t *) NULL);
+    if (fn == NULL) {
+      printf ("__stdio_gen_tempname failed\n");
+      exit (1);
+    }
+    printf ("file: %s\n", fn);
+    fp = fopen (fn, "w");
+    fclose (fp);
+  }
+}
