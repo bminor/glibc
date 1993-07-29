42dc2e2f6c71d51eafd57e65543c3d03faf06455
Roland McGrath <roland@gnu.org>
1993-07-29 21:46:01 +0000

    entered into RCS

--- ctype/ctype.h
+++ ctype/ctype.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -64,7 +64,7 @@ extern __const short int *__ctype_tolower;	/* Case conversions.  */
 extern __const short int *__ctype_toupper;	/* Case conversions.  */
 
 #define	__isctype(c, type) \
-  ((__ctype_b[(int) (c)] & (unsigned short int) type) != 0)
+  (__ctype_b[(int) (c)] & (unsigned short int) type)
 
 #define	__isascii(c)	(((c) & (1 << 7)) == 0)	/* If high bit is set.  */
 #define	__toascii(c)	((c) & 0x7f) /* Mask off high bit.  */
