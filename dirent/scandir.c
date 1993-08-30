c2eed6aef18f4878e78a9f5c4d38b973374beee5
Roland McGrath <roland@gnu.org>
1993-08-30 17:44:09 +0000

    entered into RCS

--- dirent/scandir.c
+++ dirent/scandir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -73,10 +73,13 @@ DEFUN(scandir, (dir, namelist, select, cmp),
 
   if (errno != 0)
     {
+      save = errno;
       (void) closedir (dp);
+      errno = save;
       return -1;
     }
 
+  (void) closedir (dp);
   errno = save;
 
   /* Sort the list if we have a comparison function to sort with.  */
