496998b3e53ea9f25933c3472c7d4d1258d67ea7
Roland McGrath <roland@gnu.org>
1993-10-15 22:32:21 +0000

    Formerly posix/mk-stdiolim.c.~5~

--- sysdeps/posix/mk-stdiolim.c
+++ sysdeps/posix/mk-stdiolim.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -27,8 +27,42 @@ main()
   printf("#define L_ctermid %u\n", sizeof("/dev/tty"));
   printf("#define L_cuserid 9\n");
   puts  ("#endif");
-  printf("#define FOPEN_MAX %u\n", OPEN_MAX);
-  printf("#define FILENAME_MAX %u\n", PATH_MAX);
+
+  /* POSIX does not require that OPEN_MAX and PATH_MAX be defined, so
+     <local_lim.h> will not define them if they are run-time variant (which
+     is the case in the Hurd).  ANSI still requires that FOPEN_MAX and
+     FILENAME_MAX be defined, however.  */
+
+  printf("#define FOPEN_MAX %u\n", 
+#ifdef	OPEN_MAX
+
+	 OPEN_MAX
+#else
+	 /* This is the minimum number of files that the implementation
+	    guarantees can be open simultaneously.  OPEN_MAX not being
+	    defined means the maximum is run-time variant; but POSIX.1
+	    requires that it never be less than _POSIX_OPEN_MAX, so that is
+	    a good minimum to use.  */
+	 _POSIX_OPEN_MAX
+#endif
+
+	 );
+
+  printf("#define FILENAME_MAX %u\n", 
+#ifdef	PATH_MAX
+	 PATH_MAX
+#else
+	 /* This is supposed to be the size needed to hold the longest file
+	    name string the implementation guarantees can be opened.
+	    PATH_MAX not being defined means the actual limit on the length
+	    of a file name is runtime-variant (or it is unlimited).  ANSI
+	    says in such a case FILENAME_MAX should be a good size to
+	    allocate for a file name string.  POSIX.1 guarantees that a
+	    file name up to _POSIX_PATH_MAX chars long can be opened, so
+	    this value must be at least that.  */
+	    1024		/* _POSIX_PATH_MAX is 255.  */
+#endif
+	 );
 
   exit(0);
 }
