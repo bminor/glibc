ce373a1d1298481b265a9c3ee43a9b698d60c5c8
Roland McGrath <roland@gnu.org>
1993-10-20 07:26:33 +0000

    Formerly ../hurd/hurdpath.c.~7~

--- hurd/hurdlookup.c
+++ hurd/hurdlookup.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -70,8 +70,10 @@ __hurd_path_lookup (file_t crdir, file_t cwdir,
 	  /* Fall through.  */
 
 	case FS_RETRY_NORMAL:
-	  if (nloops++ >= MAXSYMLINKS)
+#ifdef SYMLINK_MAX
+	  if (nloops++ >= SYMLINK_MAX)
 	    return ELOOP;
+#endif
 
 	  if (retryname[0] == '/')
 	    {
