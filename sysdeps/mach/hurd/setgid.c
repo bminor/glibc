f245449f3213b5b67523419d85603eb537163c1e
Roland McGrath <roland@gnu.org>
1993-10-27 20:53:46 +0000

    Formerly mach/hurd/__setgid.c.~7~

--- sysdeps/mach/hurd/setgid.c
+++ sysdeps/mach/hurd/setgid.c
@@ -19,6 +19,8 @@ Cambridge, MA 02139, USA.  */
 #include <ansidecl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <hurd.h>
 
 /* Set the group ID of the calling process to UID.
    If the calling process is the super-user, the real
@@ -28,33 +30,39 @@ int
 DEFUN(__setgid, (gid), gid_t gid)
 {
   auth_t newauth;
-  int i;
   error_t err;
 
   __mutex_lock (&_hurd_idlock);
   err = _hurd_check_ids ();
 
-  for (i = 0; i < _hurd_ngids - 2; ++i)
-    if (_hurd_gid->ids[i] == gid)
-      {
-	/* We already have this gid.
-	   Swap it with gids[0] so getegid will return it.  */
-	_hurd_gid->ids[i] = _hurd_gid->ids[0];
-	break;
-      }
+  if (!err)
+    {
+      /* Make a new auth handle which has GID as the real gid,
+	 and as the first element in the list of effective gids.  */
 
-  /* XXX unfinished */
+      gid_t newgen[_hurd_id.gen.ngids + 1];
+      gid_t newaux[_hurd_id.aux.ngids];
+      
+      newgen[0] = gid;
+      memcpy (&newgen[1], _hurd_id.gen.gids,
+	      _hurd_id.gen.ngids * sizeof (gid_t));
+      newaux[0] = gid;
+      memcpy (&newaux[1], _hurd_id.aux.gids,
+	      (_hurd_id.aux.ngids - 1) * sizeof (gid_t));
 
-  _hurd_gid->rid = gid;
-
-  err = _HURD_PORT_USE (&_hurd_auth,
-			__auth_makeauth (port, &_hurd_id, &newauth));
-  _hurd_id_valid = 0;
-  __mutex_unlock (&_hurd_idlock);
+      err = __USEPORT (AUTH, __auth_makeauth
+		       (port,
+			_hurd_id.gen.uids, _hurd_id.gen.nuids,
+			_hurd_id.aux.uids, _hurd_id.aux.nuids,
+			newgen, 1 + _hurd_id.gen.ngids,
+			newaux, _hurd_id.aux.ngids));
+    }
+  __mutex_unlock (&_hurd_id.lock);
 
   if (err)
     return __hurd_fail (err);
 
+  /* Install the new handle and reauthenticate everything.  */
   err = __setauth (newauth);
   __mach_port_deallocate (__mach_task_self (), newauth);
   return err;
