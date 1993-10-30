df9aba02285e56aed3cfe02334219d7875778ce1
Roland McGrath <roland@gnu.org>
1993-10-30 02:00:16 +0000

    Formerly mach/hurd/__fchown.c.~5~

--- sysdeps/mach/hurd/fchown.c
+++ sysdeps/mach/hurd/fchown.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <hurd.h>
 
 /* Change the owner and group of the file referred to by FD.  */
 int
