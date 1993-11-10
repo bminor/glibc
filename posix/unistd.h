aa7e20d37a591a984b472e8034adb1ae9ae94f29
Roland McGrath <roland@gnu.org>
1993-11-10 11:26:47 +0000

    Formerly ../posix/unistd.h.~38~

--- posix/unistd.h
+++ posix/unistd.h
@@ -567,6 +567,19 @@ extern char *mktemp __P ((char *__template));
    or -1 if it cannot create a uniquely-named file.  */
 extern int mkstemp __P ((char *__template));
 
+
+/* Invoke `system call' number SYSNO, passing it the remaining arguments.
+   This is completely system-dependent, and not often useful.
+
+   In Unix, `syscall' sets `errno' for all errors and most calls return -1
+   for errors; in many systems you cannot pass arguments or get return
+   values for all system calls (`pipe', `fork', and `getppid' typically
+   among them).
+
+   In Mach, all system calls take normal arguments and always return an
+   error code (zero for success).  */
+extern int syscall __P ((int __sysno, ...));
+
 #endif	/* Use misc.  */
 
 
