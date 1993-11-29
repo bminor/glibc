e82d3e0348bc6f9cbf003e0c594c37c2856d9f29
Thomas Bushnell, BSG <thomas@gnu.org>
1993-11-29 21:39:20 +0000

    Formerly ../hurd/setuids.c.~3~

--- hurd/setuids.c
+++ hurd/setuids.c
@@ -16,6 +16,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+#ifndef MIB_HACKS
 #include <hurd.h>
 
 /* Set the uid set for the current user to UIDS (N of them).  */
@@ -54,3 +55,4 @@ setuids (int n, const uid_t *uids)
   __mach_port_deallocate (__mach_task_self (), newauth);
   return err;
 }
+#endif
