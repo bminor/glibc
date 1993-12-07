2a519cfcf5d424e2a0d5505bb49e48b8a6a03215
Roland McGrath <roland@gnu.org>
1993-12-07 01:31:30 +0000

    Formerly mach/hurd/__kill.c.~10~

--- sysdeps/mach/hurd/kill.c
+++ sysdeps/mach/hurd/kill.c
@@ -16,8 +16,8 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <signal.h>
 #include <hurd.h>
 
@@ -25,12 +25,12 @@ Cambridge, MA 02139, USA.  */
    send SIG to all processes in the current process's process group.
    If PID is < -1, send SIG to all processes in process group - PID.  */
 int
-DEFUN(__kill, (pid, sig), int pid AND int sig)
+__kill (pid_t pid, int sig)
 {
   error_t err;
   mach_port_t oneport, *ports;
-  mach_port_t refport;		/* XXX */
-  mach_msg_type_number_t nports, i;
+  pid_t *pids;
+  mach_msg_type_number_t npids, nports, i;
   mach_port_t proc;
   int dealloc_proc;
 
@@ -43,34 +43,86 @@ DEFUN(__kill, (pid, sig), int pid AND int sig)
       err = __proc_pgrp_pcoll (proc, - pid, &pcoll);
       if (!err)
 	{
-	  err = __proc_get_collports (proc, pcoll, &ports, &nports);
+	  err = __proc_get_collports (proc, pcoll,
+				      &pids, &npids,
+				      &ports, &nports);
 	  __mach_port_deallocate (__mach_task_self (), pcoll);
 	}
     }
   else
     {
-      err = __proc_getmsgport (proc, pid, &oneport);
+      if (sig == SIGKILL)
+	{
+	  err = 0;
+	  oneport = MACH_PORT_NULL;
+	}
+      else
+	err = __proc_getmsgport (proc, pid, &oneport);
+      npids = 1;
+      pids = &pid;
       ports = &oneport;
       nports = 1;
     }
 
-  if (!err)
-    {
-      err = __proc_pid2task (proc, pid, &refport);
-      if (err)
-	err = __proc_getsidport (proc, &refport);
-    }
+  if (npids != nports)
+    err = EGRATUITOUS;
 
-  _hurd_port_free (&_hurd_ports[INIT_PORT_PROC], &dealloc_proc, proc);
+  if (sig == SIGKILL)
+    {
+      /* SIGKILL is not delivered as a normal signal.
+	 Sending SIGKILL to a process means to terminate its task.  */
+      for (i = 0; i < npids; ++i)
+	{
+	  /* We don't care about the message port.  Deallocate it.  */
+	  if (ports[i])
+	    __mach_port_deallocate (__mach_task_self (), ports[i]);
+	  if (!err)
+	    /* XXX The above if makes kill fail as soon as it cannot kill
+               one process, returning that one's error and ignoring the
+               rest.  Is that right?  */
+
+	    /* Fetch the process's task port and terminate the task.  We
+	       loop in case the process execs and changes its task port.
+	       If the old task port dies after we fetch it but before we
+	       send the RPC, we get MACH_SEND_INVALID_DEST; if it dies
+	       after we send the RPC request but before it is serviced, we
+	       get MIG_SERVER_DIED.  */
+	    do
+	      {
+		task_t refport;
+		err = __proc_pid2task (proc, pids[i], &refport);
+		if (!err)
+		  {
+		    err = __task_terminate (refport);
+		    __mach_port_deallocate (__mach_task_self (), refport);
+		  }
+	      } while (err != MACH_SEND_INVALID_DEST &&
+		       err != MIG_SERVER_DIED);
+	}
+    }
 
   for (i = 0; i < nports; ++i)
     {
-      if (!err && sig == SIGKILL)
-	err = __task_terminate (refport); /* XXX */
-      if (!err)
-	err = __sig_post (ports[i], sig, refport); /* XXX */
-      __mach_port_deallocate (__mach_task_self (), ports[i]);
+      int tried = 0;
+      if (err)
+	/* We have already lost.  Just deallocate the port.  */
+	    /* XXX The above if makes kill fail as soon as it cannot kill
+               one process, returning that one's error and ignoring the
+               rest.  Is that right?  */
+	__mach_port_deallocate (__mach_task_self (), ports[i]);
+      else
+	err = _HURD_MSGPORT_RPC (tried ?
+				 __proc_getmsgport (proc, pids[i], &msgport) :
+				 (tried = 1, ports[i]),
+				 ({ err = __proc_pid2task (proc, pids[i],
+							   &refport);
+				    if (err)
+				      err = __proc_getsidport (proc, &refport);
+				  }),
+				 __sig_post (msgport, sig, refport));
     }
+
+  _hurd_port_free (&_hurd_ports[INIT_PORT_PROC], &dealloc_proc, proc);
 
   if (refport != MACH_PORT_NULL)
     __mach_port_deallocate (__mach_task_self (), refport); /* XXX */
