c4ba723b7736236400878de0d1a47a5a30f8ae26
Roland McGrath <roland@gnu.org>
1993-12-14 22:55:13 +0000

    Formerly mach/hurd/__unlink.c.~5~

--- sysdeps/mach/hurd/unlink.c
+++ sysdeps/mach/hurd/unlink.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,7 +31,7 @@ DEFUN(__unlink, (name), CONST char *name)
   file_t dir;
   CONST char *file;
 
-  dir = __path_split (name, &file);
+  dir = __path_split (name, (char *)&file);
   if (dir == MACH_PORT_NULL)
     return -1;
 
