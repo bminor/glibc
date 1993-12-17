6a1536eb44ff0c58847cba4eef955078f5f7fb10
Roland McGrath <roland@gnu.org>
1993-12-17 22:58:29 +0000

    Formerly mach/hurd/sigpending.c.~3~

--- sysdeps/mach/hurd/sigpending.c
+++ sysdeps/mach/hurd/sigpending.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -19,6 +19,7 @@ Cambridge, MA 02139, USA.  */
 #include <ansidecl.h>
 #include <errno.h>
 #include <stddef.h>
+#include <hurd.h>
 #include <signal.h>
 
 
