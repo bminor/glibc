92b99b7043d1776d1f97a844bcffb9e49b6aa03d
Roland McGrath <roland@gnu.org>
1993-12-17 23:29:13 +0000

    entered into RCS

--- sysdeps/mach/hurd/getpriority.c
+++ sysdeps/mach/hurd/getpriority.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -29,10 +29,5 @@ int
 DEFUN(getpriority, (which, who),
       enum __priority_which which AND int who)
 {
-  int prio;
-  error_t err = _HURD_PORT_USE (&_hurd_proc,
-				__proc_getpriority (port, which, who, &prio));
-  if (err)
-    return __hurd_fail (err);
-  return prio;
+  return ENOSYS;		/* XXX */
 }
