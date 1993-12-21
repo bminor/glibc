d6f739bf0c872a059b33bf41fc30ad1da3df7c58
Roland McGrath <roland@gnu.org>
1993-12-21 07:14:26 +0000

    Formerly ../hurd/hurdsock.c.~5~

--- hurd/hurdsock.c
+++ hurd/hurdsock.c
@@ -1,6 +1,6 @@
 /* _hurd_socket_server - Find the server for a socket domain.
 
-Copyright (C) 1991, 1992 Free Software Foundation, Inc.
+Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -30,7 +30,6 @@ init_sock (void)
 }
 text_set_element (__libc_subinit, init_sock);
 
-static file_t sockdir = MACH_PORT_NULL;
 static file_t *servers;
 static int max_domain;
 
@@ -51,16 +50,6 @@ _hurd_socket_server (int domain)
 
   __mutex_lock (&lock);
 
-  if (sockdir == MACH_PORT_NULL)
-    {
-      sockdir = __path_lookup (_SERVERS_SOCKET, FS_LOOKUP_EXEC, 0);
-      if (sockdir == MACH_PORT_NULL)
-	{
-	  __mutex_unlock (&lock);
-	  return MACH_PORT_NULL;
-	}
-    }
-
   if (domain > max_domain)
     {
       file_t *new = realloc (servers, (domain + 1) * sizeof (file_t));
@@ -75,13 +64,9 @@ _hurd_socket_server (int domain)
     }
 
   {
-    char name[100];
-    sprintf (name, "%d", domain);
-    if (err = _HURD_PORT_USE (&_hurd_crdir,
-			      __hurd_path_lookup (port, sockdir,
-						  name, 0, 0,
-						  &servers[domain])))
-      errno = err;
+    char name[sizeof (_SERVERS_SOCKET) + 100];
+    sprintf (name, "%s/%d", _SERVERS_SOCKET, domain);
+    servers[domain] = __path_lookup (name, 0, 0);
   }
 
   __mutex_unlock (&lock);
