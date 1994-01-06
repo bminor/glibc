143f6d88d1c89abe15b2f4140175b561cf03eaa8
Roland McGrath <roland@gnu.org>
1994-01-06 06:35:25 +0000

    Formerly mach/hurd/__fork.c.~5~

--- sysdeps/mach/hurd/fork.c
+++ sysdeps/mach/hurd/fork.c
@@ -16,6 +16,8 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+#ifdef notevenclose
+
 #include <ansidecl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -290,3 +292,5 @@ DEFUN_VOID(__fork)
 
   return pid;
 }
+
+#endif
