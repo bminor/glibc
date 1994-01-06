807f74bd0668cb197b7a71abd17a494558fc9f9c
Roland McGrath <roland@gnu.org>
1994-01-06 06:34:11 +0000

    Formerly mach/hurd/dirstream.h.~3~

--- sysdeps/mach/hurd/dirstream.h
+++ sysdeps/mach/hurd/dirstream.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,8 @@ Cambridge, MA 02139, USA.  */
 
 #define	_DIRSTREAM_H	1
 
+#include <gnu/types.h>
+
 /* Directory stream type.
 
    The Hurd directory format is the same as `struct dirent', so `readdir'
@@ -30,11 +32,11 @@ typedef struct
     /* XXX we need a namespace-clean name for mach_port_t! */
     unsigned int __port;	/* Port to the directory.  */
     __off_t __filepos;		/* Current file position.  */
-    size_t __block_size;	/* Size to read chunks in (st_blksize).  */
+    __off_t __block_size;	/* Size to read chunks in (st_blksize).  */
     char *__data;		/* Directory block.  */
     unsigned int __allocation;	/* Space allocated for the block.  */
     unsigned int __size;	/* Total valid data in the block.  */
-    size_t __offset;		/* Current offset into the block.  */
+    __off_t __offset;		/* Current offset into the block.  */
   } DIR;
 
 #endif	/* dirstream.h */
