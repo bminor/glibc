6c40bd572534abee0fb322523d4c4ddea28b6c57
Roland McGrath <roland@gnu.org>
1994-01-20 17:45:30 +0000

    Formerly stub/syscall.c.~3~

--- sysdeps/stub/syscall.c
+++ sysdeps/stub/syscall.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -23,7 +23,7 @@ Cambridge, MA 02139, USA.  */
    This only makes sense in certain operating systems.  */
 
 int
-DEFUN(syscall, (callno), callno DOTS)
+DEFUN(syscall, (callno), int callno DOTS)
 {
   errno = ENOSYS;
   return -1;
