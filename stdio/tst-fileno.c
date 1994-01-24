0ea9d6057d03d60927b8d0d0799e9b1fde9d85cf
Roland McGrath <roland@gnu.org>
1994-01-24 21:51:38 +0000

    Formerly ../stdio/tst-fileno.c.~2~

--- stdio/tst-fileno.c
+++ stdio/tst-fileno.c
@@ -16,6 +16,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+#include <ansidecl.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -28,7 +29,7 @@ DEFUN(check, (name, stream, fd), CONST char *name FILE *stream AND int fd)
 }
 
 int
-main ()
+DEFUN_VOID(main)
 {
   exit (check ("stdin", stdin, STDIN_FILENO) ||
 	check ("stdout", stdout, STDOUT_FILENO) ||
