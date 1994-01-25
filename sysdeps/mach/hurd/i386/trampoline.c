04e3b796291ce02dd9987eb251dd912ac637138e
Roland McGrath <roland@gnu.org>
1994-01-25 20:53:00 +0000

    Initial revision

---
+++ sysdeps/mach/hurd/i386/trampoline.c
@@ -0,0 +1,97 @@
+/* Set thread_state for sighandler, and sigcontext to recover.  i386 version.
+Copyright (C) 1994 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <hurd.h>
+#include <mach/thread_state.h>
+
+static void
+trampoline (__sighandler_t *handler,
+	    int signo, int sigcode, struct sigcontext *scp)
+{
+  (*handler) (signo, sigcode, scp);
+  (void) __sigreturn (scp);	/* Does not return.  */
+  while (1)
+    asm volatile ("hlt");		/* Firewall.  */
+}
+
+struct sigcontext *
+_hurd_setup_sighandler (int flags,
+			__sighandler_t handler,
+			struct sigaltstack *sigaltstack,
+			int signo, int sigcode,
+			void *state)
+{
+  struct i386_thread_state *ts;
+  void *sigsp;
+  struct sigcontext *scp;
+  struct 
+    {
+      void *retaddr;		/* Never used.  */
+      __sighandler_t handler;
+      int signo;
+      int sigcode;
+      struct sigcontext *scp;	/* Points to ctx, below.  */
+      struct sigcontext ctx;
+    } *stackframe;
+
+  ts = state;
+
+  if ((flags & SA_ONSTACK) &&
+      !(sigaltstack->ss_flags & (SA_DISABLE|SA_ONSTACK)))
+    {
+      sigsp = sigaltstack->ss_sp + sigaltstack->ss_size;
+      sigaltstack->ss_flags |= SA_ONSTACK;
+      scp->sc_onstack = 1;
+    }
+  else
+    {
+      sigsp = (char *) ts->uesp;
+      scp->sc_onstack = 0;
+    }
+
+  /* Push the arguments to call `trampoline' on the stack.  */
+  sigsp -= sizeof (*stackframe);
+  stackframe = sigsp;
+  stackframe->handler = handler;
+  stackframe->signo = signo;
+  stackframe->sigcode = sigcode;
+  stackframe->scp = scp = &stackframe->ctx;
+
+  /* Set up the sigcontext from the current state of the thread.  */
+
+  scp->sc_edi = ts->edi;
+  scp->sc_esi = ts->esi;
+  scp->sc_ebp = ts->ebp;
+  /* Segment registers??? XXX */
+
+  scp->sc_ebx = ts->ebx;
+  scp->sc_edx = ts->edx;
+  scp->sc_ecx = ts->ecx;
+  scp->sc_eax = ts->eax;
+  
+  scp->sc_eip = ts->eip;
+  scp->sc_uesp = ts->uesp;
+  scp->sc_efl = ts->efl;
+
+  /* Modify the thread state to call `trampoline' on the new stack.  */
+  ts->uesp = (int) sigsp;
+  ts->eip = (int) trampoline;
+
+  return scp;
+}
---
+++ sysdeps/mach/i386/thread_state.h
@@ -0,0 +1,25 @@
+/* Mach thread state definitions for Hurd code.  i386 version.
+Copyright (C) 1994 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <mach/thread_state.h>
+
+#define HURD_THREAD_STATE_FLAVOR	i386_THREAD_STATE
+#define HURD_THREAD_STATE_COUNT		i386_THREAD_STATE_COUNT
+
+#define hurd_thread_state i386_thread_state
