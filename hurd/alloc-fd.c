634722d83214a594f328febc47010519a94b0691
Roland McGrath <roland@gnu.org>
1994-02-01 03:20:02 +0000

    Formerly ../hurd/alloc-fd.c.~3~

--- hurd/alloc-fd.c
+++ hurd/alloc-fd.c
@@ -17,19 +17,37 @@ not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #include <hurd/fd.h>
+#include <stdlib.h>
 
 /* Allocate a new file descriptor and return it, locked.
+   The new descriptor number will be no less than FIRST_FD.
    If the table is full, set errno and return NULL.  */
 struct hurd_fd *
 _hurd_alloc_fd (int *fd, const int first_fd)
 {
   int i;
 
-  __mutex_lock (&hurd_dtable_lock);
+  __mutex_lock (&_hurd_dtable_lock);
 
   for (i = first_fd; i < _hurd_dtable.size; ++i)
     {
       struct hurd_fd *d = &_hurd_dtable.d[i];
+      if (d == NULL)
+	{
+	  /* Allocate a new descriptor structure for this slot.  */
+	  d = malloc (sizeof (struct hurd_fd));
+	  if (d == NULL)
+	    {
+	      __mutex_unlock (&hurd_dtable_lock);
+	      return NULL;
+	    }
+
+	  /* Initialize the port cells.  The test below will catch and
+	     return this descriptor cell after locking it.  */
+	  _hurd_port_init (&d->port, MACH_PORT_NULL);
+	  _hurd_port_init (&d->ctty, MACH_PORT_NULL);
+	}
+
       __spin_lock (&d->port.lock);
       if (d->port.port == MACH_PORT_NULL)
 	{
