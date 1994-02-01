6521c93356a961564d18f4febe4c37384d6f519f
Roland McGrath <roland@gnu.org>
1994-02-01 03:54:35 +0000

    Formerly ../hurd/hurdinit.c.~19~

--- hurd/hurdinit.c
+++ hurd/hurdinit.c
@@ -16,11 +16,12 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <hurd.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <hurd.h>
+#include <hurd/port.h>
 
 
 struct _hurd_port *_hurd_ports;
