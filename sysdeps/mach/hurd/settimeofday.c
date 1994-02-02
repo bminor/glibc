e89482ae6aa090dd39e6e4d1a9cdc99bf52e0300
Roland McGrath <roland@gnu.org>
1994-02-02 02:44:55 +0000

    entered into RCS

--- sysdeps/mach/hurd/settimeofday.c
+++ sysdeps/mach/hurd/settimeofday.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <sys/time.h>
 #include <hurd.h>
+#include <hurd/port.h>
 
 /* Set the current time of day and timezone information.
    This call is restricted to the super-user.  */
