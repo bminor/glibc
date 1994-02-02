9ea5bf4fd3e05eb7e2a4a9780d0260103aba576f
Roland McGrath <roland@gnu.org>
1994-02-02 03:41:56 +0000

    Formerly mach/hurd/setegid.c.~4~

--- sysdeps/mach/hurd/setegid.c
+++ sysdeps/mach/hurd/setegid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -21,6 +21,7 @@ Cambridge, MA 02139, USA.  */
 #include <unistd.h>
 #include <hurd.h>
 #include <sys/types.h>
+#include <hurd/id.h>
 
 /* Set the effective user ID of the calling process to GID.  */
 int
