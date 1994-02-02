c43645703bf7b15c1967ce81854d5f8ed15d0769
Roland McGrath <roland@gnu.org>
1994-02-02 01:01:53 +0000

    Formerly mach/hurd/__getegid.c.~8~

--- sysdeps/mach/hurd/getegid.c
+++ sysdeps/mach/hurd/getegid.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <errno.h>
 #include <unistd.h>
 #include <hurd.h>
+#include <hurd/id.h>
 
 /* Get the effective group ID of the calling process.  */
 gid_t
