2634c590a8c9cfc6736b83501cbba26f86fc5749
Roland McGrath <roland@gnu.org>
1994-02-02 03:07:06 +0000

    Formerly mach/hurd/_exit.c.~12~

--- sysdeps/mach/hurd/_exit.c
+++ sysdeps/mach/hurd/_exit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -19,6 +19,7 @@ Cambridge, MA 02139, USA.  */
 #include <ansidecl.h>
 #include <unistd.h>
 #include <hurd.h>
+#include <hurd/port.h>
 #include <sysdep.h>
 #include <sys/wait.h>
 
@@ -27,7 +28,7 @@ _hurd_exit (int status)
 {
 #ifdef notyet
   /* Give the proc server our exit status.  */
-  _HURD_PORT_USE (&_hurd_ports[INIT_PORT_PROC], __proc_exit (port, status));
+  __USEPORT (PROC, __proc_exit (port, status));
 #endif
 
   /* Commit suicide.  */
