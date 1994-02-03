f9fb38a09a3f9b98e95669eff633f72a5df0ceef
Roland McGrath <roland@gnu.org>
1994-02-03 08:35:14 +0000

    Formerly ../hurd/hurd-raise.c.~3~

--- hurd/hurd-raise.c
+++ hurd/hurd-raise.c
@@ -16,18 +16,17 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+#include <hurd.h>
 #include <hurd/signal.h>
+#include <setjmp.h>
 
 /* Handle signal SIGNO in the calling thread.
    If SS is not NULL it is the sigstate for the calling thread;
    SS->lock is held on entry and released before return.  */
 
 void
-_hurd_raise_signal (struct _hurd_sigstate *ss, int signo, int sigcode)
+_hurd_raise_signal (struct hurd_sigstate *ss, int signo, int sigcode)
 {
-  jmp_buf env;
-  struct sigcontext sc;
-
   if (ss == NULL)
     ss = _hurd_thread_sigstate (__mach_thread_self ());
 
