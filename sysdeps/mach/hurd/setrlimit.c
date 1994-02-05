838219142867e38489a79c7b28620218760a4ac0
Roland McGrath <roland@gnu.org>
1994-02-05 20:39:01 +0000

    Formerly mach/hurd/setrlimit.c.~6~

--- sysdeps/mach/hurd/setrlimit.c
+++ sysdeps/mach/hurd/setrlimit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -21,6 +21,8 @@ Cambridge, MA 02139, USA.  */
 #include <hurd.h>
 #include <errno.h>
 
+
+extern _hurd_core_limit, _hurd_dtable_rlimit; /* XXX */
 
 /* Set the soft and hard limits for RESOURCE to *RLIMITS.
    Only the super-user can increase hard limits.
