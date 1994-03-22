05a2383063a7fcc58f519b56f4048e13d6620108
Roland McGrath <roland@gnu.org>
1994-03-22 06:20:31 +0000

    Formerly ../hurd/msgportdemux.c.~4~

--- hurd/msgportdemux.c
+++ hurd/msgportdemux.c
@@ -18,7 +18,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <mach.h>
+#include <hurd.h>
 #include <hurd/signal.h>
 #include <stddef.h>
 
