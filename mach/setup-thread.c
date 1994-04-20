200dd2a5585b44b75bfabf0da484b82ae99bdb3f
Roland McGrath <roland@gnu.org>
1994-04-20 20:18:22 +0000

    Formerly ../mach/setup-thread.c.~8~

--- mach/setup-thread.c
+++ mach/setup-thread.c
@@ -20,50 +20,74 @@ Cambridge, MA 02139, USA.  */
 #include "thread_state.h"
 #include <string.h>
 #include <mach/machine/vm_param.h>
+#include "sysdep.h"		/* Defines stack direction.  */
 
 #define	STACK_SIZE	(16 * 1024 * 1024) /* 16MB, arbitrary.  */
 
-/* Give THREAD a stack and set it to run at PC when resumed.  */
+/* Give THREAD a stack and set it to run at PC when resumed.
+   If *STACK_SIZE is nonzero, that size of stack is allocated.
+   If *STACK_BASE is nonzero, that stack location is used.
+   If STACK_BASE is not null it is filled in with the chosen stack base.
+   If STACK_SIZE is not null it is filled in with the chosen stack size.
+   Regardless, an extra page of red zone is allocated off the end; this
+   is not included in *STACK_SIZE.  */
+
 kern_return_t
-__mach_setup_thread (task_t task, thread_t thread, void *pc)
+__mach_setup_thread (task_t task, thread_t thread, void *pc,
+		     vm_address_t *stack_base, vm_size_t *stack_size)
 {
   kern_return_t error;
   struct machine_thread_state ts;
   mach_msg_type_number_t tssize = MACHINE_THREAD_STATE_COUNT;
   vm_address_t stack;
+  vm_size_t size;
 
-  /* Cthreads has a bug that makes its stack-probing code fail if
-     the stack is too low in memory.  It's bad to try and fix it there
-     until cthreads is integrated into libc, so we'll just do it here
-     by requesting a high address.  When the cthreads bug is fixed,
-     this assignment to STACK should be changed to 0, and the ANYWHERE
-     argument to vm_allocate should be changed to 0.  This comment should
-     be left, however, in order to confuse people who wonder why its
-     here.  (Though perhaps that last sentence (and this one) should
-     be deleted to maximize the effect.)  */
+  if (stack_base && *stack_base)
+    stack = *stack_base;
+  else
+    {
+      /* Cthreads has a bug that makes its stack-probing code fail if
+	 the stack is too low in memory.  It's bad to try and fix it there
+	 until cthreads is integrated into libc, so we'll just do it here
+	 by requesting a high address.  When the cthreads bug is fixed,
+	 this assignment to STACK should be changed to 0, and the ANYWHERE
+	 argument to vm_allocate should be changed to 0.  This comment should
+	 be left, however, in order to confuse people who wonder why its
+	 here.  (Though perhaps that last sentence (and this one) should
+	 be deleted to maximize the effect.)  */
 #ifdef STACK_GROWTH_DOWN
-  stack = VM_MAX_ADDRESS - STACK_SIZE - __vm_page_size;
+      stack = VM_MAX_ADDRESS - STACK_SIZE - __vm_page_size;
 #else
-  stack = VM_MIN_ADDRESS;
+      stack = VM_MIN_ADDRESS;
 #endif
+    }
 
-  if (error = __vm_allocate (task, &stack, STACK_SIZE + __vm_page_size, 0))
+  size = stack_size ? *stack_size ? : STACK_SIZE : STACK_SIZE;
+
+  if (error = __vm_allocate (task, &stack, size + __vm_page_size, 0))
     return error;
 
-  /* Create a red zone */
-  if (error = __vm_protect (task, stack, __vm_page_size, 0, 0))
-    return error;
-  
+  if (stack_base)
+    *stack_base = stack;
+  if (stack_size)
+    *stack_size = size;
+
   memset (&ts, 0, sizeof (ts));
   ts.PC = (int) pc;
 #ifdef STACK_GROWTH_DOWN
-  ts.SP = stack + STACK_SIZE;
+  ts.SP = stack + size;
+  stack -= __vm_page_size;
 #elif defined (STACK_GROWTH_UP)
   ts.SP = stack;
+  stack += size;
 #else
   #error stack direction unknown
 #endif
 
+  /* Create the red zone.  */
+  if (error = __vm_protect (task, stack, __vm_page_size, 0, VM_PROT_NONE))
+    return error;
+  
   return __thread_set_state (thread, MACHINE_THREAD_STATE_FLAVOR,
 			     (int *) &ts, tssize);
 }
