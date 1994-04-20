5aef170e0d6f157eb71748216df262968978ca88
Roland McGrath <roland@gnu.org>
1994-04-20 03:36:44 +0000

    Formerly mach/hurd/getrlimit.c.~3~

--- sysdeps/mach/hurd/getrlimit.c
+++ sysdeps/mach/hurd/getrlimit.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -20,6 +20,7 @@ Cambridge, MA 02139, USA.  */
 #include <sys/resource.h>
 #include <errno.h>
 #include <hurd.h>
+#include <hurd/fd.h>
 
 extern int _hurd_core_limit, _hurd_dtable_rlimit; /* XXX */
 
