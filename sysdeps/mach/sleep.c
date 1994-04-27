d9b4730506b23db6e14f9a4af336ee40828d55f9
Roland McGrath <roland@gnu.org>
1994-04-27 00:07:02 +0000

    Formerly mach/sleep.c.~3~

--- sysdeps/mach/sleep.c
+++ sysdeps/mach/sleep.c
@@ -36,7 +36,7 @@ DEFUN(sleep, (seconds), unsigned int seconds)
   recv = __mach_reply_port ();
 
   before = time ((time_t *) NULL);
-  (void) __mach_msg (NULL, MACH_RCV_TIMEOUT|MACH_RCV_INTERRUPT|MACH_RCV_MSG,
+  (void) __mach_msg (NULL, MACH_RCV_MSG|MACH_RCV_TIMEOUT|MACH_RCV_INTERRUPT,
 		     0, 0, recv, seconds * 1000000, MACH_PORT_NULL);
   after = time ((time_t *) NULL);
   __mach_port_destroy (__mach_task_self (), recv);
