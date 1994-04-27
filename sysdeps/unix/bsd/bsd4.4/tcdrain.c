e91537e70cd0a2fd6f7afe4b8686d66ae95cae97
Roland McGrath <roland@gnu.org>
1994-04-27 00:09:13 +0000

    Formerly unix/bsd/bsd4.4/tcdrain.c.~2~

--- sysdeps/unix/bsd/bsd4.4/tcdrain.c
+++ sysdeps/unix/bsd/bsd4.4/tcdrain.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -17,10 +17,7 @@ not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
 #include <ansidecl.h>
-#include <errno.h>
 #include <stddef.h>
-#include <termios.h>
-#include <unistd.h>
 #include <sys/ioctl.h>
 
 /* Wait for pending output to be written on FD.  */
