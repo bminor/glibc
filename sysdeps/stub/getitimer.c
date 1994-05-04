9564aa12c0bc3234c211da5502f72da085de1534
Roland McGrath <roland@gnu.org>
1994-05-04 10:18:03 +0000

    entered into RCS

--- sysdeps/stub/getitimer.c
+++ sysdeps/stub/getitimer.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -25,7 +25,7 @@ Cambridge, MA 02139, USA.  */
    Return 0 on success, -1 on errors.  */
 int
 DEFUN(__getitimer, (which, value),
-      enum __itimer_which __which AND struct itimerval *value)
+      enum __itimer_which which AND struct itimerval *value)
 {
   if (value == NULL)
     {
