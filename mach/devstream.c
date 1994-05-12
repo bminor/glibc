35abae0dd65fcd8da32d89b81d3c0935584813b7
Roland McGrath <roland@gnu.org>
1994-05-12 08:36:04 +0000

    Formerly ../mach/devstream.c.~16~

--- mach/devstream.c
+++ mach/devstream.c
@@ -76,9 +76,9 @@ output (FILE *f, int c)
       int wrote;
       while (to_write > 0)
 	{
-	  if (err = device_write_inband ((device_t) f->__cookie, 0,
-					 f->__target, (char *)p, 
-					 to_write, &wrote))
+	  if (err = device_write ((device_t) f->__cookie, 0,
+				  f->__target, (char *)p, 
+				  to_write, &wrote))
 	    {
 	      errno = err;
 	      f->__error = 1;
