c823a93738f6183b50a272af665dc0c75e68fb33
Roland McGrath <roland@gnu.org>
1994-05-17 03:26:35 +0000

    Initial revision

---
+++ sysdeps/stub/sys/param.h
@@ -0,0 +1,6 @@
+/* This file should contain various parameter macros appropriate for the
+   machine and operating system.  There is no standard set of macros; this
+   file is just for compatibility with programs written for Unix that
+   expect it to define things.  */
+
+#include <limits.h>
