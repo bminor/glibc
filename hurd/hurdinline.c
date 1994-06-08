ae8747ed148ad4c0a4ad375b7582998a9613a350
Roland McGrath <roland@gnu.org>
1994-06-08 16:21:24 +0000

    Initial revision

---
+++ hurd/hurdinline.c
@@ -0,0 +1,7 @@
+#define _EXTERN_INLINE /* Define the real function. */
+
+#include "hurd/fd.h"
+#include "hurd/signal.h"
+#include "hurd/userlink.h"
+#include "hurd/threadvar.h"
+#include "hurd/port.h"
