9165a09ef1a06019c2ec4e23c93ae0b951514aff
Roland McGrath <roland@gnu.org>
1994-06-11 10:06:24 +0000

    entered into RCS

--- hurd/hurdinline.c
+++ hurd/hurdinline.c
@@ -1,3 +1,7 @@
+/* Include this first to avoid defining its inline functions.  */
+#include <lock-intern.h>
+
+#undef _EXTERN_INLINE
 #define _EXTERN_INLINE /* Define the real function. */
 
 #include "hurd/fd.h"
