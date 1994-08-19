2f0499b62ac853e6a09f74f1253fa827c2872f0f
Roland McGrath <roland@gnu.org>
1994-08-19 04:54:43 +0000

    (ENOMEM, EACCES): New macros.

--- sysdeps/stub/errnos.h
+++ sysdeps/stub/errnos.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -31,5 +31,7 @@ Cambridge, MA 02139, USA.  */
 #define	EINVAL	4
 #define	ESPIPE	5
 #define	EBADF	6
+#define	ENOMEM	7
+#define	EACCES	8
 #endif
 
