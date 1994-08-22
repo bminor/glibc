d11aad089607487e54a9e607de15491a1f9bd4ca
Roland McGrath <roland@gnu.org>
1994-08-22 05:12:22 +0000

    (sp): New global register variable. (__sigreturn): Arg is not const. After restoring SCP->sc_mask, check for pending signals (newly unblocked); if any, set SS->context to SCP, clear SS->intr_port, and send sig_post to the signal thread to deliver the pending signals. Point SP directly at &SCP->sc_gs and used popa;iret to restore. (This does not actually work; iret is unhelpful.)

--- sysdeps/mach/hurd/i386/sigreturn.c
+++ sysdeps/mach/hurd/i386/sigreturn.c
@@ -16,29 +16,57 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
+register int *sp asm ("%esp");
+
 #include <hurd.h>
 #include <hurd/signal.h>
 #include <hurd/threadvar.h>
+#include <hurd/msg.h>
 
 int
-__sigreturn (register const struct sigcontext *scp)
+__sigreturn (struct sigcontext *scp)
 {
   struct hurd_sigstate *ss;
-  register int *usp asm ("%eax"); /* Force it into a register.  */
   mach_port_t *reply_port;
 
-  if (scp == NULL)
+  if (scp == NULL || (scp->sc_mask & _SIG_CANT_MASK))
     {
       errno = EINVAL;
       return -1;
     }
 
-  ss = _hurd_self_sigstate ();
+  ss = _hurd_self_sigstate ();	/* SS->lock now locked.  */
+
+  /* Restore the set of blocked signals, and the intr_port slot.  */
   ss->blocked = scp->sc_mask;
   ss->intr_port = scp->sc_intr_port;
+
+  /* Check for pending signals that were blocked by the old set.  */
+  if (ss->pending & ~ss->blocked)
+    {
+      /* There are pending signals that just became unblocked.  Wake up the
+	 signal thread to deliver them.  But first, squirrel away SCP where
+	 the signal thread will notice it if it runs another handler, and
+	 arrange to have us called over again in the new reality.  */
+      ss->context = scp;
+      /* Clear the intr_port slot, since we are not in fact doing
+	 an interruptible RPC right now.  If SS->intr_port is not null,
+	 the SCP context is doing an interruptible RPC, but the signal
+	 thread will examine us while we are blocked in the sig_post RPC.  */
+      ss->intr_port = MACH_PORT_NULL;
+      __mutex_unlock (&ss->lock);
+      __sig_post (_hurd_msgport, 0, __mach_task_self ());
+      /* If a pending signal was handled, sig_post never returned.  */
+      __mutex_lock (&ss->lock);
+    }
+
   if (scp->sc_onstack)
-    ss->sigaltstack.ss_flags &= ~SA_ONSTACK; /* XXX threadvars */
-  __mutex_unlock (&ss->lock);
+    {
+      ss->sigaltstack.ss_flags &= ~SA_ONSTACK; /* XXX threadvars */
+      /* XXX cannot unlock until off sigstack */
+    }
+  else
+    __mutex_unlock (&ss->lock);
 
   /* Destroy the MiG reply port used by the signal handler, and restore the
      reply port in use by the thread when interrupted.  */
@@ -48,34 +76,40 @@ __sigreturn (register const struct sigcontext *scp)
     __mach_port_destroy (__mach_task_self (), *reply_port);
   *reply_port = scp->sc_reply_port;
 
-  /* Push the flags and registers onto the stack we're returning to.  */
-  usp = (int *) scp->sc_uesp;
+  if (scp->sc_fpused)
+    {
+  /* XXX should restore FPU state here XXX roland needs 387 manual */
+    }
 
-  /* The last thing popped will be the PC being restored, so push it first.  */
-  *--usp = scp->sc_eip;
-  *--usp = scp->sc_efl;		/* Second to last, processor flags.  */
+  scp->sc_uesp += 128;		/* XXX bogosity XXX iret doesn't do SP */
 
-  /* Segment registers??? XXX */
+  /* There are convenient instructions to pop state off the stack,
+     so we switch our stack pointer to point into SCP.  */
+  sp = &scp->sc_gs;
 
-  /* Now push the general registers in the reverse of the order they will
-     be popped off by the `popa' instruction.  The instruction passes over
-     a word corresponding to %esp, but does not use the value.  */
-  *--usp = scp->sc_eax;
-  *--usp = scp->sc_ecx;
-  *--usp = scp->sc_edx;
-  *--usp = scp->sc_ebx;
-  *--usp = 0;			/* Ignored by `popa'.  */
-  *--usp = scp->sc_ebp;
-  *--usp = scp->sc_esi;
-  *--usp = scp->sc_edi;
+#define A(line) asm volatile (#line)
+  /* The members in the sigcontext are arranged in this order
+     so we can pop them easily.  */
 
-  asm volatile
-    ("movl %0, %%esp\n"		/* Switch to the target stack.  */
-     "popa\n"			/* Pop all the general registers.  */
-     "popf\n"			/* Pop the flags.  */
-     "ret\n"			/* Pop the target PC and jump to it.  */
-     "hlt" : :			/* Firewall.  */
-     "g" (usp));
+  /* Pop the segment registers (except %cs and %ss, done last).  */
+  A (popl %gs);
+  A (popl %fs);
+  A (popl %es);
+  A (popl %ds);
+  /* Pop the general registers.  */
+  A (popa);
+  /* Now at the top of the stack:
+     	sc_eip
+	sc_cs
+	sc_efl
+	sc_uesp	XXX DOES NOT WORK
+	sc_ss	XXX it does not restore %esp and %ss in user mode!!!
+     The `iret' instruction does just what we need.  It pops %eip, %cs, the
+     processor flag word, %esp, and %ss; then resumes execution at the new
+     %eip value in the new code segment.  */
+  A (iret);
+
+#undef A
 
   /* NOTREACHED */
   return -1;
