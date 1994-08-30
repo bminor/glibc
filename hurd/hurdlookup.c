68ae262b2d4c4d2718d5bcc205719edabdd926b6
Roland McGrath <roland@gnu.org>
1994-08-30 08:33:09 +0000

    Globally replace `pathtrans' with `lookup' and `path' with `file_name'. (__hurd_file_name_split): Don't bother skipping leading slashes.

--- hurd/hurdlookup.c
+++ hurd/hurdlookup.c
@@ -24,15 +24,15 @@ Cambridge, MA 02139, USA.  */
 #include <hurd/term.h>
 
 
-/* Translate the error from dir_pathtrans into the error the user sees.  */
+/* Translate the error from dir_lookup into the error the user sees.  */
 static inline error_t
-pathtrans_error (error_t error)
+lookup_error (error_t error)
 {
   switch (error)
     {
     case EOPNOTSUPP:
     case MIG_BAD_ID:
-      /* These indicate that the server does not understand dir_pathtrans
+      /* These indicate that the server does not understand dir_lookup
 	 at all.  If it were a directory, it would, by definition.  */
       return ENOTDIR;
     default:
@@ -41,39 +41,39 @@ pathtrans_error (error_t error)
 }
 
 error_t
-__hurd_path_lookup (file_t crdir, file_t cwdir,
-		    const char *path, int flags, mode_t mode,
-		    file_t *result)
+__hurd_file_name_lookup (file_t crdir, file_t cwdir,
+			 const char *file_name, int flags, mode_t mode,
+			 file_t *result)
 {
   error_t err;
   enum retry_type doretry;
   char retryname[1024];		/* XXX string_t LOSES! */
   file_t startdir;
 
-  startdir = path[0] == '/' ? crdir : cwdir;
+  startdir = file_name[0] == '/' ? crdir : cwdir;
 
-  while (path[0] == '/')
-    path++;
+  while (file_name[0] == '/')
+    file_name++;
 
-  if (err = __dir_pathtrans (startdir, path, flags, mode,
-			     &doretry, retryname, result))
-    return pathtrans_error (err);
+  if (err = __dir_lookup (startdir, file_name, flags, mode,
+			  &doretry, retryname, result))
+    return lookup_error (err);
 
-  return __hurd_path_lookup_retry (crdir, doretry, retryname, flags, mode,
-				   result);
+  return __hurd_file_name_lookup_retry (crdir, doretry, retryname, flags, mode,
+					result);
 }
 
 error_t
-__hurd_path_lookup_retry (file_t crdir,
-			  enum retry_type doretry,
-			  char retryname[1024],
-			  int flags, mode_t mode,
-			  file_t *result)
+__hurd_file_name_lookup_retry (file_t crdir,
+			       enum retry_type doretry,
+			       char retryname[1024],
+			       int flags, mode_t mode,
+			       file_t *result)
 {
   error_t err;
   file_t startdir;
   file_t newpt;
-  char *path;
+  char *file_name;
   int dealloc_dir;
   int nloops;
 
@@ -86,7 +86,7 @@ __hurd_path_lookup_retry (file_t crdir,
       if (dealloc_dir)
 	__mach_port_deallocate (__mach_task_self (), startdir);
       if (err)
-	return pathtrans_error (err);
+	return lookup_error (err);
 
       switch (doretry)
 	{
@@ -134,7 +134,7 @@ __hurd_path_lookup_retry (file_t crdir,
 
 	  startdir = *result;
 	  dealloc_dir = 1;
-	  path = retryname;
+	  file_name = retryname;
 	  break;
 
 	case FS_RETRY_MAGICAL:
@@ -145,7 +145,7 @@ __hurd_path_lookup_retry (file_t crdir,
 	      dealloc_dir = 0;
 	      if (*result != MACH_PORT_NULL)
 		__mach_port_deallocate (__mach_task_self (), *result);
-	      path = &retryname[1];
+	      file_name = &retryname[1];
 	      break;
 
 	    case 'f':
@@ -183,7 +183,7 @@ __hurd_path_lookup_retry (file_t crdir,
 		      /* Do a normal retry on the remaining components.  */
 		      startdir = *result;
 		      dealloc_dir = 1;
-		      path = end + 1; /* Skip the slash.  */
+		      file_name = end + 1; /* Skip the slash.  */
 		      break;
 		    }
 		}
@@ -277,35 +277,26 @@ __hurd_path_lookup_retry (file_t crdir,
 	  return EGRATUITOUS;
 	}
 
-      err = __dir_pathtrans (startdir, path, flags, mode,
-			     &doretry, retryname, result);
+      err = __dir_lookup (startdir, file_name, flags, mode,
+			  &doretry, retryname, result);
     }
 }
 
 error_t
-__hurd_path_split (file_t crdir, file_t cwdir,
-		   const char *path,
-		   file_t *dir, char **name)
+__hurd_file_name_split (file_t crdir, file_t cwdir,
+			const char *file_name,
+			file_t *dir, char **name)
 {
   const char *lastslash;
   error_t err;
-  
-  /* Skip leading slashes in the pathname.  */
-  if (*path == '/')
-    {
-      while (*path == '/')
-	++path;
-      --path;			/* Leave on one slash.  */
-    }
-  
-  lastslash = strrchr (path, '/');
-  
+
+  lastslash = strrchr (file_name, '/');
   if (lastslash != NULL)
     {
-      if (lastslash == path)
+      if (lastslash == file_name)
 	{
 	  /* "/foobar" => crdir + "foobar".  */
-	  *name = (char *) path + 1;
+	  *name = (char *) file_name + 1;
 	  if (err = __mach_port_mod_refs (__mach_task_self (), 
 					  crdir, MACH_PORT_RIGHT_SEND, +1))
 	    return err;
@@ -315,17 +306,17 @@ __hurd_path_split (file_t crdir, file_t cwdir,
       else
 	{
 	  /* "/dir1/dir2/.../file".  */
-	  char dirname[lastslash - path + 1];
-	  memcpy (dirname, path, lastslash - path);
-	  dirname[lastslash - path] = '\0';
+	  char dirname[lastslash - file_name + 1];
+	  memcpy (dirname, file_name, lastslash - file_name);
+	  dirname[lastslash - file_name] = '\0';
 	  *name = (char *) lastslash + 1;
-	  return __hurd_path_lookup (crdir, cwdir, dirname, 0, 0, dir);
+	  return __hurd_file_name_lookup (crdir, cwdir, dirname, 0, 0, dir);
 	}
     }
   else
     {
       /* "foobar" => cwdir + "foobar".  */
-      *name = (char *) path;
+      *name = (char *) file_name;
       if (err = __mach_port_mod_refs (__mach_task_self (),
 				      cwdir, MACH_PORT_RIGHT_SEND, +1))
 	return err;
@@ -335,7 +326,7 @@ __hurd_path_split (file_t crdir, file_t cwdir,
 }
 
 file_t
-__path_lookup (const char *path, int flags, mode_t mode)
+__file_name_lookup (const char *file_name, int flags, mode_t mode)
 {
   error_t err;
   file_t result, crdir, cwdir;
@@ -344,22 +335,20 @@ __path_lookup (const char *path, int flags, mode_t mode)
   crdir = _hurd_port_get (&_hurd_ports[INIT_PORT_CRDIR], &crdir_ulink);
   cwdir = _hurd_port_get (&_hurd_ports[INIT_PORT_CWDIR], &cwdir_ulink);
 
-  err = __hurd_path_lookup (crdir, cwdir, path, flags, mode, &result);
+  err = __hurd_file_name_lookup (crdir, cwdir, file_name, flags, mode,
+				 &result);
 
   _hurd_port_free (&_hurd_ports[INIT_PORT_CRDIR], &crdir_ulink, crdir);
   _hurd_port_free (&_hurd_ports[INIT_PORT_CWDIR], &cwdir_ulink, cwdir);
 
   if (err)
-    {
-      errno = err;
-      return MACH_PORT_NULL;
-    }
+    return __hurd_fail (err), MACH_PORT_NULL;
   else
     return result;
 }
 
 file_t
-__path_split (const char *path, char **name)
+__file_name_split (const char *file_name, char **name)
 {
   error_t err;
   file_t dir, crdir, cwdir;
@@ -368,7 +357,7 @@ __path_split (const char *path, char **name)
   crdir = _hurd_port_get (&_hurd_ports[INIT_PORT_CRDIR], &crdir_ulink);
   cwdir = _hurd_port_get (&_hurd_ports[INIT_PORT_CWDIR], &cwdir_ulink);
 
-  err = __hurd_path_split (crdir, cwdir, path, &dir, name);
+  err = __hurd_file_name_split (crdir, cwdir, file_name, &dir, name);
 
   _hurd_port_free (&_hurd_ports[INIT_PORT_CRDIR], &crdir_ulink, crdir);
   _hurd_port_free (&_hurd_ports[INIT_PORT_CWDIR], &cwdir_ulink, cwdir);
