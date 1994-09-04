9fc0a759387906996565859dcc8698b61b29ddc9
Roland McGrath <roland@gnu.org>
1994-09-04 18:57:37 +0000

    Include "hurdfault.h" instead of <hurd/fault.h>.

--- hurd/hurdfault.c
+++ hurd/hurdfault.c
@@ -17,7 +17,7 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <hurd/fault.h>
+#include "hurdfault.h"
 #include <string.h>
 #include <setjmp.h>
 #include "thread_state.h"
