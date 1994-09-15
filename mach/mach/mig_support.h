d0591d0a4393bba295729d0a5278c5f8a3691e46
Roland McGrath <roland@gnu.org>
1994-09-15 18:24:07 +0000

    (__mig_put_reply_port): Declare. (__mig_dealloc_reply_port): Take arg.

--- mach/mach/mig_support.h
+++ mach/mach/mig_support.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -39,10 +39,13 @@ extern void __mig_deallocate (vm_address_t __addr, vm_size_t __size);
 extern void mig_deallocate (vm_address_t __addr, vm_size_t __size);
 
 /* Reply-port management support functions.  */
-extern void __mig_dealloc_reply_port (void);
-extern void mig_dealloc_reply_port (void);
+extern void __mig_dealloc_reply_port (mach_port_t);
+extern void mig_dealloc_reply_port (mach_port_t);
 extern mach_port_t __mig_get_reply_port (void);
 extern mach_port_t mig_get_reply_port (void);
+extern void __mig_put_reply_port (mach_port_t);
+extern void mig_put_reply_port (mach_port_t);
+
 extern void __mig_reply_setup (const mach_msg_header_t *__request,
 			       mach_msg_header_t *__reply);
 extern void mig_reply_setup (const mach_msg_header_t *__request,
