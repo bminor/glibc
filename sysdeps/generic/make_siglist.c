2d3974d8f3c32c1e13897f2212d333410cb87b67
Roland McGrath <roland@gnu.org>
1994-09-15 20:56:18 +0000

    (sys_siglist): Define as macro to my_siglist.

--- sysdeps/generic/make_siglist.c
+++ sysdeps/generic/make_siglist.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or modify
@@ -23,6 +23,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 
 #undef	HAVE_SYS_SIGLIST
+#define sys_siglist my_siglist	/* Avoid clash with signal.h.  */
+
 #include "signame.c"
 
 
