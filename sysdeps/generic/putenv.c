58599df51f1127ce6e7d440514bb6aef5d8a89c9
Roland McGrath <roland@gnu.org>
1994-10-17 04:05:55 +0000

    Deansideclized, added portability cruft.

--- sysdeps/generic/putenv.c
+++ sysdeps/generic/putenv.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 1991 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1994 Free Software Foundation, Inc.
+
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -16,30 +17,49 @@ License along with the GNU C Library; see the file COPYING.LIB.  If
 not, write to the Free Software Foundation, Inc., 675 Mass Ave,
 Cambridge, MA 02139, USA.  */
 
-#include <ansidecl.h>
 #include <errno.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#if defined (__GNU_LIBRARY__) || defined (HAVE_STDLIB_H)
 #include <stdlib.h>
+#endif
+#if defined (__GNU_LIBRARY__) || defined (HAVE_STRING_H)
 #include <string.h>
+#endif
+#if defined (__GNU_LIBRARY__) || defined (HAVE_UNISTD_H)
 #include <unistd.h>
+#endif
+
+#if !defined (__GNU_LIBRARY__) && !defined (HAVE_STRCHR)
+#define strchr index
+#endif
+#if !defined (__GNU_LIBRARY__) && !defined (HAVE_MEMCPY)
+#define memcpy(d,s,n) bcopy ((s), (d), (n))
+#endif
 
 #ifndef	HAVE_GNU_LD
 #define	__environ	environ
 #endif
 
+
 /* Put STRING, which is of the form "NAME=VALUE", in the environment.  */
 int
-DEFUN(putenv, (string), CONST char *string)
+putenv (string)
+     const char *string;
 {
-  CONST char *CONST name_end = strchr(string, '=');
+  const char *const name_end = strchr (string, '=');
   register size_t size;
   register char **ep;
 
   if (name_end == NULL)
     {
       /* Remove the variable from the environment.  */
-      size = strlen(string);
+      size = strlen (string);
       for (ep = __environ; *ep != NULL; ++ep)
-	if (!strncmp(*ep, string, size) && (*ep)[size] == '=')
+	if (!strncmp (*ep, string, size) && (*ep)[size] == '=')
 	  {
 	    while (ep[1] != NULL)
 	      {
@@ -53,7 +73,7 @@ DEFUN(putenv, (string), CONST char *string)
 
   size = 0;
   for (ep = __environ; *ep != NULL; ++ep)
-    if (!strncmp(*ep, string, name_end - string) &&
+    if (!strncmp (*ep, string, name_end - string) &&
 	(*ep)[name_end - string] == '=')
       break;
     else
@@ -62,14 +82,15 @@ DEFUN(putenv, (string), CONST char *string)
   if (*ep == NULL)
     {
       static char **last_environ = NULL;
-      char **new_environ = (char **) malloc((size + 2) * sizeof(char *));
+      char **new_environ = (char **) malloc ((size + 2) * sizeof (char *));
       if (new_environ == NULL)
 	return -1;
-      (void) memcpy((PTR) new_environ, (PTR) __environ, size * sizeof(char *));
+      (void) memcpy ((void *) new_environ, (void *) __environ,
+		     size * sizeof (char *));
       new_environ[size] = (char *) string;
       new_environ[size + 1] = NULL;
       if (last_environ != NULL)
-	free((PTR) last_environ);
+	free ((void *) last_environ);
       last_environ = new_environ;
       __environ = new_environ;
     }
