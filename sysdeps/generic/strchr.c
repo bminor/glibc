789469e03c7c6be0829632355136d3efd45e15be
Roland McGrath <roland@gnu.org>
1994-10-27 00:44:00 +0000

    Increment CP properly in check for 5th char of quadword hit.

--- sysdeps/generic/strchr.c
+++ sysdeps/generic/strchr.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1993, 1994 Free Software Foundation, Inc.
    Based on strlen implemention by Torbjorn Granlund (tege@sics.se),
    with help from Dan Sahlin (dan@sics.se) and
    bug fix and commentary by Jim Blandy (jimb@ai.mit.edu);
@@ -154,7 +154,7 @@ DEFUN(strchr, (s, c), CONST char *s AND int c)
 	    return NULL;
 	  if (sizeof (longword) > 4)
 	    {
-	      if (*cp == c)
+	      if (*++cp == c)
 		return (char *) cp;
 	      else if (*cp == '\0')
 		return NULL;
