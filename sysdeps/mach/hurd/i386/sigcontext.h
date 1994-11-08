b316d2c3a031e8f0c47089e0466297a05518b38c
Roland McGrath <roland@gnu.org>
1994-11-08 22:44:38 +0000

    Add descriptive comment at top.

--- sysdeps/mach/hurd/i386/sigcontext.h
+++ sysdeps/mach/hurd/i386/sigcontext.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 1991, 1992, 1994 Free Software Foundation, Inc.
+/* Machine-dependent signal context structure for GNU Hurd.  i386 version.
+Copyright (C) 1991, 1992, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
