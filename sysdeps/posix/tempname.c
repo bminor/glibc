02482c4712ca53620df06f0388d4c33bdd8c3391
Roland McGrath <roland@gnu.org>
1994-12-09 20:09:03 +0000

    (__stdio_gen_tempname): Cast FD to long int before casting to pointer.

--- sysdeps/posix/tempname.c
+++ sysdeps/posix/tempname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -181,7 +181,7 @@ DEFUN(__stdio_gen_tempname, (dir, pfx, dir_search, lenptr, streamptr),
 		  errno = save;
 		  return NULL;
 		}
-	      (*streamptr)->__cookie = (PTR) fd;
+	      (*streamptr)->__cookie = (PTR) (long int) fd;
 	      (*streamptr)->__mode.__write = 1;
 	      (*streamptr)->__mode.__read = 1;
 	      (*streamptr)->__mode.__binary = 1;
