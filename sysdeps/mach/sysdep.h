bfb46a67a4b6ee26197e62ac3988d4204c18e999
Roland McGrath <roland@gnu.org>
1994-12-09 01:34:15 +0000

    (ENTRY): Don't use #.

--- sysdeps/mach/sysdep.h
+++ sysdeps/mach/sysdep.h
@@ -32,7 +32,7 @@ Cambridge, MA 02139, USA.  */
 
 /* Produce a text assembler label for the C global symbol NAME.  */
 #ifndef ENTRY
-#define ENTRY(name) #error ENTRY not defined by sysdeps/mach/MACHINE/sysdep.h
+#define ENTRY(name) .error ENTRY not defined by sysdeps/mach/MACHINE/sysdep.h
 /* This is not used on all machines.  */
 #endif
 
