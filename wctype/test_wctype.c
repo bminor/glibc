d963ca0bce4621185c3576874b11c86727757080
Ulrich Drepper <drepper@redhat.com>
1997-12-09 23:59:04 +0000

    Add test for isw* functions.

--- wctype/test_wctype.c
+++ wctype/test_wctype.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -48,12 +48,20 @@ main (int argc, char *argv[])
     {
 #define TEST(test)							      \
       do								      \
-	if ((is##test (ch) == 0) != (iswctype (ch, bit_##test)) == 0)	      \
-	  {								      \
-	    printf ("class `%s' test for character \\%o failed\n",	      \
-		    #test, ch);						      \
-	    result = 1;							      \
-	  }								      \
+	{								      \
+	  if ((is##test (ch) == 0) != (iswctype (ch, bit_##test)) == 0)	      \
+	    {								      \
+	      printf ("class `%s' test for character \\%o failed\n",	      \
+		      #test, ch);					      \
+	      result = 1;						      \
+	    }								      \
+	  if ((is##test (ch) == 0) != (isw##test (ch) == 0))		      \
+	    {								      \
+	      printf ("`isw%s' test for character \\%o failed\n",	      \
+		      #test, ch);					      \
+	      result = 1;						      \
+	    }								      \
+	}								      \
       while (0)
 
       TEST (alnum);
