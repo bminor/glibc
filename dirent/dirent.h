a8059443501bf70442f0a91d499cabb0f6b10063
Ulrich Drepper <drepper@redhat.com>
1998-01-30 16:54:02 +0000

    Add defines for DT_*.

--- dirent/dirent.h
+++ dirent/dirent.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 92, 93, 94, 95, 96, 97 Free Software Foundation, Inc.
+/* Copyright (C) 1991,92,93,94,95,96,97,98 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -83,13 +83,21 @@ __BEGIN_DECLS
 enum
   {
     DT_UNKNOWN = 0,
+#define DT_UNKNOWN	DT_UNKNOWN
     DT_FIFO = 1,
+#define DT_FIFO		DT_FIFO
     DT_CHR = 2,
+#define DT_CHR		DT_CHR
     DT_DIR = 4,
+#define DT_DIR		DT_DIR
     DT_BLK = 6,
+#define DT_BLK		DT_BLK
     DT_REG = 8,
+#define DT_REG		DT_REG
     DT_LNK = 10,
+#define DT_LNK		DT_LNK
     DT_SOCK = 12
+#define DT_SOCK		DT_SOCK
   };
 
 /* Convert between stat structure types and directory types.  */
