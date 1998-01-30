ebc9ab8be40d3999c6d0c13b1339191de3c334b9
Ulrich Drepper <drepper@redhat.com>
1998-01-30 17:13:41 +0000

    (__nss_configure_lookup): Correct test for early exit of loop.

--- nss/nsswitch.c
+++ nss/nsswitch.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
@@ -202,7 +202,7 @@ __nss_configure_lookup (const char *dbname, const char *service_line)
       int cmp = strcmp (dbname, databases[cnt].name);
       if (cmp == 0)
 	break;
-      if (cmp > 0)
+      if (cmp < 0)
 	{
 	  __set_errno (EINVAL);
 	  return -1;
