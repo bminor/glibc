722b8f3c78699e400ebf425c6076e4d1f9a20831
Ulrich Drepper <drepper@redhat.com>
1998-02-01 17:13:20 +0000

    Include <errno.h>.

--- nss/getXXbyYY_r.c
+++ nss/getXXbyYY_r.c
@@ -18,6 +18,7 @@
    Boston, MA 02111-1307, USA.  */
 
 #include "nsswitch.h"
+#include <errno.h>
 
 /*******************************************************************\
 |* Here we assume several symbols to be defined:		   *|
--- nss/getXXent_r.c
+++ nss/getXXent_r.c
@@ -18,7 +18,7 @@
    Boston, MA 02111-1307, USA.  */
 
 #include <libc-lock.h>
-
+#include <errno.h>
 #include "nsswitch.h"
 
 /*******************************************************************\
