d916e8b37d390c3931acb34101e6872ed17110bf
Ulrich Drepper <drepper@redhat.com>
1998-03-02 13:41:20 +0000

    Include <gnu/types.h> for __fsid_t.

--- sysdeps/unix/sysv/linux/statfsbuf.h
+++ sysdeps/unix/sysv/linux/statfsbuf.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -18,6 +18,8 @@
 
 #ifndef _STATFSBUF_H
 #define _STATFSBUF_H
+
+#include <gnu/types.h>  /* for __fsid_t */
 
 struct statfs
   {
