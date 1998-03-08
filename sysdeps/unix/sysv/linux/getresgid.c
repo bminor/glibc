d3c7d47dd61c2c23a10dda087b7160f45dee88ba
Ulrich Drepper <drepper@redhat.com>
1998-03-08 21:39:03 +0000

    Add getresuid and getresgid.

---
+++ sysdeps/unix/sysv/linux/getresgid.c
@@ -0,0 +1,47 @@
+/* Copyright (C) 1998 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <unistd.h>
+#include <sys/types.h>
+
+#include <linux/posix_types.h>
+#include <linux/unistd.h>
+
+#ifndef __NR_getresuid
+/* The kernel does not define the getresgid syscall.  */
+#include <sysdeps/generic/getresgid.c>
+#else
+
+extern int __syscall_getresgid (__kernel_gid_t *rgid, __kernel_gid_t *egid,
+				__kernel_gid_t *sgid);
+
+int
+getresgid (gid_t *rgid, gid_t *egid, gid_t *sgid)
+{
+  __kernel_gid_t k_rgid, k_egid, k_sgid;
+
+  if (__syscall_getresgid (&k_rgid, &k_egid, &k_sgid) < 0)
+    return -1;
+
+  *rgid = (gid_t) k_rgid;
+  *egid = (gid_t) k_egid;
+  *sgid = (gid_t) k_sgid;
+  return 0;
+}
+
+#endif
---
+++ sysdeps/unix/sysv/linux/getresuid.c
@@ -0,0 +1,47 @@
+/* Copyright (C) 1998 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <unistd.h>
+#include <sys/types.h>
+
+#include <linux/posix_types.h>
+#include <linux/unistd.h>
+
+#ifndef __NR_getresuid
+/* The kernel does not define the getresuid syscall.  */
+#include <sysdeps/generic/getresuid.c>
+#else
+
+extern int __syscall_getresuid (__kernel_uid_t *ruid, __kernel_uid_t *euid,
+				__kernel_uid_t *suid);
+
+int
+getresuid (uid_t *ruid, uid_t *euid, uid_t *suid)
+{
+  __kernel_uid_t k_ruid, k_euid, k_suid;
+
+  if (__syscall_getresuid (&k_ruid, &k_euid, &k_suid) < 0)
+    return -1;
+
+  *ruid = (uid_t) k_ruid;
+  *euid = (uid_t) k_euid;
+  *suid = (uid_t) k_suid;
+  return 0;
+}
+
+#endif
