9f5b279d98811da2bf17caa741a2662c709b9290
Ulrich Drepper <drepper@redhat.com>
1998-03-18 12:21:34 +0000

    (__opendir): Don't block on FIFOs etc.

--- sysdeps/unix/opendir.c
+++ sysdeps/unix/opendir.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 92, 93, 94, 95, 96 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 92, 93, 94, 95, 96, 98 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -45,7 +45,7 @@ __opendir (const char *name)
       return NULL;
     }
 
-  fd = __open (name, O_RDONLY);
+  fd = __open (name, O_RDONLY|O_NDELAY);
   if (fd < 0)
     return NULL;
 
