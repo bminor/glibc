1348a38bde1d01db221f38cd07f8d5e07e5dc078
Ulrich Drepper <drepper@redhat.com>
1998-03-30 12:46:51 +0000

    glibc 2.1 compatible startup code.

---
+++ sysdeps/generic/libc-start.c
@@ -0,0 +1,59 @@
+/* Copyright (C) 1998 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+extern void __libc_init_first (int argc, char **argv, char **envp);
+
+extern int _dl_starting_up;
+weak_extern (_dl_starting_up)
+extern int __libc_multiple_libcs;
+
+int
+__libc_start_main (int (*main) (int, char **, char **), int argc,
+		   char **argv, void (*init) (void), void (*fini) (void),
+		   void (*rtld_fini) (void))
+{
+#ifndef PIC
+  /* The next variable is only here to work around a bug in gcc <= 2.7.2.2.
+     If the address would be taken inside the expression the optimizer
+     would try to be too smart and throws it away.  Grrr.  */
+  int *dummy_addr = &_dl_starting_up;
+
+  __libc_multiple_libcs = dummy_addr && !_dl_starting_up;
+#endif
+
+  /* Register the destructor of the dynamic linker if there is any.  */
+  if (rtld_fini != NULL)
+    atexit (rtld_fini);
+
+  /* Set the global _environ variable correctly.  */
+  __environ = &argv[argc + 1];
+
+  /* Call the initializer of the libc.  */
+  __libc_init_first (argc, argv, __environ);
+
+  /* Call the initializer of the program.  */
+  (*init) ();
+
+  /* Register the destructor of the program.  */
+  atexit (fini);
+
+  exit ((*main) (argc, argv, __environ));
+}
