6420ca4fa5b5a810dbf9c2267e8fd563ca098989
Ulrich Drepper <drepper@redhat.com>
1998-03-31 21:39:22 +0000

    Wrapper arpund syscall.

---
+++ sysdeps/unix/sysv/linux/sigpending.c
@@ -0,0 +1,31 @@
+/* Copyright (C) 1998 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <signal.h>
+#include <memory.h>
+
+extern int __syscall_sigpending __P((sigset_t *));
+
+int
+sigpending (set)
+     sigset_t *set;
+{
+  if (set)
+    bzero (set, sizeof (*set));
+  return __syscall_sigpending (set);
+}
