93ff7f9cf48bd95fcda55537b9070bd7bcb9c987
Ulrich Drepper <drepper@redhat.com>
1998-04-04 18:17:30 +0000

    (locfile_read): Fix typo.

--- locale/programs/locfile.c
+++ locale/programs/locfile.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, 1996.
 
@@ -612,7 +612,7 @@ from-value of `collating-element' must be a string"));
 		  now = lr_token (ldfile, charset);
 		}
 	      while (now->tok == tok_comma
-		     && (now == lr_token (ldfile, charset) != tok_none));
+		     && ((now = lr_token (ldfile, charset)) != tok_none));
 
 	      /* Check for consistency: forward and backwards are
 		 mutually exclusive.  */
