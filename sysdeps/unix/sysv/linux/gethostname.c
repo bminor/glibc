cb5993c2e67aa267643fc3b352fc7eb1f52836f4
Ulrich Drepper <drepper@redhat.com>
1998-07-21 12:17:51 +0000

    Return ENAMETOOLONG if buffer is too small.

--- sysdeps/unix/sysv/linux/gethostname.c
+++ sysdeps/unix/sysv/linux/gethostname.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992, 1995, 1996 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1995, 1996, 1998 Free Software Foundation, Inc.
 This file is part of the GNU C Library.
 
 The GNU C Library is free software; you can redistribute it and/or
@@ -42,7 +42,7 @@ __gethostname (name, len)
 
   if (strlen (buf.nodename) + 1 > len)
     {
-      __set_errno (EINVAL);
+      __set_errno (ENAMETOOLONG);
       return -1;
     }
 
