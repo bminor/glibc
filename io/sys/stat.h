2d64b4c0a424ff6ba86d5777daa0898a6f54412d
Ulrich Drepper <drepper@redhat.com>
1998-08-31 11:09:42 +0000

    (ALLPERMS): Fix type (S_ISTXT -> S_ISVTX).

--- io/sys/stat.h
+++ io/sys/stat.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1992, 1995, 1996 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1995, 1996, 1998 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -108,7 +108,7 @@ __BEGIN_DECLS
 #ifdef	__USE_BSD
 /* Macros for common mode bit masks.  */
 #define	ACCESSPERMS (S_IRWXU|S_IRWXG|S_IRWXO) /* 0777 */
-#define	ALLPERMS (S_ISUID|S_ISGID|S_ISTXT|S_IRWXU|S_IRWXG|S_IRWXO)/* 07777 */
+#define	ALLPERMS (S_ISUID|S_ISGID|S_ISVTX|S_IRWXU|S_IRWXG|S_IRWXO)/* 07777 */
 #define	DEFFILEMODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)/* 0666 */
 
 #define S_BLKSIZE	512	/* Block size for `st_blocks'.  */
