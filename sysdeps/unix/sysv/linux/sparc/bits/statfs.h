0e96cac4944fc66d373a7ffbfd9afa53603c9b44
Ulrich Drepper <drepper@redhat.com>
1999-05-25 12:28:09 +0000

    Update from main branch.

--- sysdeps/unix/sysv/linux/sparc/bits/statfs.h
+++ sysdeps/unix/sysv/linux/sparc/bits/statfs.h
@@ -1,5 +1,4 @@
-/* Definition of `struct statfs', information about a filesystem.
-   Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1997, 1998, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -21,50 +20,42 @@
 # error "Never include <bits/statfs.h> directly; use <sys/statfs.h> instead."
 #endif
 
-#include <bits/types.h>
-
-/* GNU Hurd NOTE: The size of this structure (16 ints) is known in
-   <hurd/hurd_types.defs>, since it is used in the `file_statfs' RPC.  MiG
-   does not cope at all well with the passed C structure not being of the
-   expected size.  There are some filler words at the end to allow for
-   future expansion.  To increase the size of the structure used in the RPC
-   and retain binary compatibility, we would need to assign a new message
-   number.  */
+#include <bits/types.h>  /* for __fsid_t and __fsblkcnt_t*/
 
 struct statfs
   {
-    unsigned int f_type;
-    unsigned int f_bsize;
+    long int f_type;
+    long int f_bsize;
 #ifndef __USE_FILE_OFFSET64
     __fsblkcnt_t f_blocks;
     __fsblkcnt_t f_bfree;
     __fsblkcnt_t f_bavail;
-    __fsblkcnt_t f_files;
-    __fsblkcnt_t f_ffree;
+    __fsfilcnt_t f_files;
+    __fsfilcnt_t f_ffree;
 #else
     __fsblkcnt64_t f_blocks;
     __fsblkcnt64_t f_bfree;
     __fsblkcnt64_t f_bavail;
-    __fsblkcnt64_t f_files;
-    __fsblkcnt64_t f_ffree;
+    __fsfilcnt64_t f_files;
+    __fsfilcnt64_t f_ffree;
 #endif
     __fsid_t f_fsid;
-    unsigned int f_namelen;
-    unsigned int f_spare[6];
+    long int f_namelen;
+    long int f_spare[6];
   };
 
 #ifdef __USE_LARGEFILE64
 struct statfs64
   {
-    unsigned int f_type;
-    unsigned int f_bsize;
+    long int f_type;
+    long int f_bsize;
     __fsblkcnt64_t f_blocks;
     __fsblkcnt64_t f_bfree;
     __fsblkcnt64_t f_bavail;
-    __fsblkcnt64_t f_files;
-    __fsblkcnt64_t f_ffree;
+    __fsfilcnt64_t f_files;
+    __fsfilcnt64_t f_ffree;
     __fsid_t f_fsid;
-    unsigned int f_namelen;
-    unsigned int f_spare[6];
+    long int f_namelen;
+    long int f_spare[6];
   };
 #endif
