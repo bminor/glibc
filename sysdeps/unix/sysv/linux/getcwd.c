c7eecb53b20149a5ee437252a80cd24d9cb558d6
Ulrich Drepper <drepper@redhat.com>
1999-08-20 23:45:31 +0000

    Undo last change.

--- sysdeps/unix/sysv/linux/getcwd.c
+++ sysdeps/unix/sysv/linux/getcwd.c
@@ -58,7 +58,7 @@ __getcwd (char *buf, size_t size)
   if (no_syscall_getcwd && !have_new_dcache)
     return generic_getcwd (buf, size);
 
-  if (size <= 0)
+  if (size == 0)
     {
       if (buf != NULL)
 	{
@@ -150,7 +150,7 @@ __getcwd (char *buf, size_t size)
   __set_errno (save_errno);
 
   /* Don't put restrictions on the length of the path unless the user does.  */
-  if (size <= 0)
+  if (size == 0)
     {
       free (path);
       path = NULL;
@@ -158,7 +158,7 @@ __getcwd (char *buf, size_t size)
 
   result = generic_getcwd (path, size);
 
-  if (result == NULL && buf == NULL && size > 0)
+  if (result == NULL && buf == NULL && size != 0)
     free (path);
 
   return result;
