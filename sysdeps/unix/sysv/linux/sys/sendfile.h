813e69352adbe688df170eb712406cc4e9265580
Ulrich Drepper <drepper@redhat.com>
1999-08-20 15:50:15 +0000

    Don't allow use with _FILE_OFFSET_BITS=64.

--- sysdeps/unix/sysv/linux/sys/sendfile.h
+++ sysdeps/unix/sysv/linux/sys/sendfile.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1998, 1999 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -21,6 +21,10 @@
 
 #include <features.h>
 #include <sys/types.h>
+
+#ifdef __USE_FILE_OFFSET64
+# error "<sendfile.h> cannot be used with _FILE_OFFSET_BITS=64"
+#endif
 
 __BEGIN_DECLS
 
