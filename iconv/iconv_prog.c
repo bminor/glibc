bf7346e744b3ef950e4f182ed66aa61af61082cc
Ulrich Drepper <drepper@redhat.com>
2000-01-05 17:26:33 +0000

    (process_block): Add cast to avoid warning.

--- iconv/iconv_prog.c
+++ iconv/iconv_prog.c
@@ -368,7 +368,7 @@ conversion stopped due to problem in writing the output"));
 	    {
 	    case EILSEQ:
 	      error (0, 0, _("illegal input sequence at position %ld"),
-		     addr - start);
+		     (long) (addr - start));
 	      break;
 	    case EINVAL:
 	      error (0, 0, _("\
