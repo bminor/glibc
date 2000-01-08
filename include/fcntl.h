820aa71c428c2a1f4c08767d15f87002ab4a8fc2
Ulrich Drepper <drepper@redhat.com>
2000-01-08 05:13:37 +0000

    Declare __libc_fcntl. Move __libc_open and __libc_open64 to here.

--- include/fcntl.h
+++ include/fcntl.h
@@ -3,4 +3,7 @@
 
 /* Now define the internal interfaces.  */
 extern int __open64 __P ((__const char *__file, int __oflag, ...));
+extern int __libc_open64 __P ((const char *file, int oflag, ...));
+extern int __libc_open __P ((const char *file, int oflag, ...));
+extern int __libc_fcntl __P ((int fd, int cmd, ...));
 #endif
