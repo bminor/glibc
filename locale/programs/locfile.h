d7a5e01348628a93b176af39c8111c9383d36038
Ulrich Drepper <drepper@redhat.com>
2000-02-13 07:29:49 +0000

    (struct localedef_t): Add repertoire element.

--- locale/programs/locfile.h
+++ locale/programs/locfile.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1996, 1997, 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, 1996.
 
@@ -51,6 +51,8 @@ struct localedef_t
   } categories[6];
 
   size_t len[6];
+
+  struct repertoire_t *repertoire;
 };
 
 /* Declared in localedef.c.  */
