800be20593b033e46c4672f7feb556d6c697ca99
Ulrich Drepper <drepper@redhat.com>
2000-03-17 03:25:57 +0000

    Mark second parameter as const.

--- sysdeps/generic/msgsnd.c
+++ sysdeps/generic/msgsnd.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995, 1996, 1997 Free Software Foundation, Inc.
+/* Copyright (C) 1995, 1996, 1997, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, August 1995.
 
@@ -29,7 +29,7 @@
 int
 msgsnd (msqid, msgp, msgsz, msgflg)
      int msqid;
-     void *msgp;
+     const void *msgp;
      size_t msgsz;
      int msgflg;
 {
--- sysdeps/unix/sysv/linux/msgsnd.c
+++ sysdeps/unix/sysv/linux/msgsnd.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1995, 1997, 1998 Free Software Foundation, Inc.
+/* Copyright (C) 1995, 1997, 1998, 2000 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Ulrich Drepper <drepper@gnu.ai.mit.edu>, August 1995.
 
@@ -26,7 +26,7 @@
 int
 msgsnd (msqid, msgp, msgsz, msgflg)
      int msqid;
-     void *msgp;
+     const void *msgp;
      size_t msgsz;
      int msgflg;
 {
