18c5126084749b1c6b80c325aaa329a9b50c43b2
Andreas Jaeger <aj@suse.de>
2001-01-10 16:52:19 +0000

    * elf/rtld.c (process_envvars): Place output files for profiling 	in SUID binaries in /var/profile.
    	* elf/dl-load.c (_dl_map_object): Don't look in cache for
    	preloading in SUID binaries.
    
    	* elf/dl-profile.c (_dl_start_profile): Open the output file with
    	O_NOFOLLOW if possible.
    
    	* sysdeps/generic/segfault.c (install_handler): Check output file
    	name with access().

--- elf/dl-load.c
+++ elf/dl-load.c
@@ -1,5 +1,5 @@
 /* Map in a shared object's segments from the file.
-   Copyright (C) 1995, 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+   Copyright (C) 1995, 1996, 1997, 1998, 1999, 2001 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -1338,7 +1338,7 @@ _dl_map_object (struct link_map *loader, const char *name, int preloaded,
       if (fd == -1 && env_path_list != NULL)
 	fd = open_path (name, namelen, preloaded, env_path_list, &realname);
 
-      if (fd == -1)
+      if (fd == -1 && (! preloaded || ! __libc_enable_secure))
 	{
 	  /* Check the list of libraries in the file /etc/ld.so.cache,
 	     for compatibility with Linux's ldconfig program.  */
