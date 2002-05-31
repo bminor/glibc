aa928f79d282d358e533b72c4175938a5f28e6c0
Ulrich Drepper <drepper@redhat.com>
2002-05-31 00:08:20 +0000

    Avoid using INTUSE.

--- sysdeps/powerpc/libgcc-compat.c
+++ sysdeps/powerpc/libgcc-compat.c
@@ -24,90 +24,90 @@
 #if SHLIB_COMPAT(libc, GLIBC_2_0, GLIBC_2_2_6)
 
 extern int64_t __ashldi3 (int64_t, int32_t);
-int64_t INTUSE (__ashldi3) (int64_t u, int32_t b)
+int64_t __ashldi3 (int64_t u, int32_t b)
 {
   return __ashldi3 (u, b);
 }
-symbol_version (INTUSE (__ashldi3), __ashldi3, GLIBC_2.0);
+symbol_version (__ashldi3, __ashldi3, GLIBC_2.0);
 
 
 extern int64_t __ashrdi3 (int64_t, int32_t);
-int64_t INTUSE (__ashrdi3) (int64_t u, int32_t b)
+int64_t __ashrdi3 (int64_t u, int32_t b)
 {
   return __ashrdi3 (u, b);
 }
-symbol_version (INTUSE (__ashrdi3), __ashrdi3, GLIBC_2.0);
+symbol_version (__ashrdi3, __ashrdi3, GLIBC_2.0);
 
 
 extern int64_t __lshrdi3 (int64_t, int32_t);
-int64_t INTUSE (__lshrdi3) (int64_t u, int32_t b)
+int64_t __lshrdi3 (int64_t u, int32_t b)
 {
   return __lshrdi3 (u, b);
 }
-symbol_version (INTUSE (__lshrdi3), __lshrdi3, GLIBC_2.0);
+symbol_version (__lshrdi3, __lshrdi3, GLIBC_2.0);
 
 
 extern int32_t __cmpdi2 (int64_t, int64_t);
-int32_t INTUSE (__cmpdi2) (int64_t u, int64_t v)
+int32_t __cmpdi2 (int64_t u, int64_t v)
 {
   return __cmpdi2 (u, v);
 }
-symbol_version (INTUSE (__cmpdi2), __cmpdi2, GLIBC_2.0);
+symbol_version (__cmpdi2, __cmpdi2, GLIBC_2.0);
 
 
 extern int32_t __ucmpdi2 (int64_t, int64_t);
-int32_t INTUSE (__ucmpdi2) (int64_t u, int64_t v)
+int32_t __ucmpdi2 (int64_t u, int64_t v)
 {
   return __ucmpdi2 (u, v);
 }
-symbol_version (INTUSE (__ucmpdi2), __ucmpdi2, GLIBC_2.0);
+symbol_version (__ucmpdi2, __ucmpdi2, GLIBC_2.0);
 
 
 extern int64_t __fixdfdi (double);
-int64_t INTUSE (__fixdfdi) (double d)
+int64_t __fixdfdi (double d)
 {
   return __fixdfdi (d);
 }
-symbol_version (INTUSE (__fixdfdi), __fixdfdi, GLIBC_2.0);
+symbol_version (__fixdfdi, __fixdfdi, GLIBC_2.0);
 
 
 extern int64_t __fixunsdfdi (double);
-int64_t INTUSE (__fixunsdfdi) (double d)
+int64_t __fixunsdfdi (double d)
 {
   return __fixunsdfdi (d);
 }
-symbol_version (INTUSE (__fixunsdfdi), __fixunsdfdi, GLIBC_2.0);
+symbol_version (__fixunsdfdi, __fixunsdfdi, GLIBC_2.0);
 
 
 extern int64_t __fixsfdi (float);
-int64_t INTUSE (__fixsfdi) (float d)
+int64_t __fixsfdi (float d)
 {
   return __fixsfdi (d);
 }
-symbol_version (INTUSE (__fixsfdi), __fixsfdi, GLIBC_2.0);
+symbol_version (__fixsfdi, __fixsfdi, GLIBC_2.0);
 
 
 extern int64_t __fixunssfdi (float);
-int64_t INTUSE (__fixunssfdi) (float d)
+int64_t __fixunssfdi (float d)
 {
   return __fixunssfdi (d);
 }
-symbol_version (INTUSE (__fixunssfdi), __fixunssfdi, GLIBC_2.0);
+symbol_version (__fixunssfdi, __fixunssfdi, GLIBC_2.0);
 
 
 extern double __floatdidf (int64_t);
-double INTUSE (__floatdidf) (int64_t u)
+double __floatdidf (int64_t u)
 {
   return __floatdidf (u);
 }
-symbol_version (INTUSE (__floatdidf), __floatdidf, GLIBC_2.0);
+symbol_version (__floatdidf, __floatdidf, GLIBC_2.0);
 
 
 extern float __floatdisf (int64_t);
-float INTUSE (__floatdisf) (int64_t u)
+float __floatdisf (int64_t u)
 {
   return __floatdisf (u);
 }
-symbol_version (INTUSE (__floatdisf), __floatdisf, GLIBC_2.0);
+symbol_version (__floatdisf, __floatdisf, GLIBC_2.0);
 
 #endif
