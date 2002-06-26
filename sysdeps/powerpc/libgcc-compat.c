c51accf4f24c0bdd1c5c632241ca3c574869c12b
Ulrich Drepper <drepper@redhat.com>
2002-06-26 15:31:30 +0000

    Fix function names.

--- sysdeps/powerpc/libgcc-compat.c
+++ sysdeps/powerpc/libgcc-compat.c
@@ -24,90 +24,90 @@
 #if SHLIB_COMPAT(libc, GLIBC_2_0, GLIBC_2_2_6)
 
 extern int64_t __ashldi3 (int64_t, int32_t);
-int64_t __ashldi3 (int64_t u, int32_t b)
+int64_t __ashldi3_internal (int64_t u, int32_t b)
 {
   return __ashldi3 (u, b);
 }
-symbol_version (__ashldi3, __ashldi3, GLIBC_2.0);
+symbol_version (__ashldi3_internal, __ashldi3, GLIBC_2.0);
 
 
 extern int64_t __ashrdi3 (int64_t, int32_t);
-int64_t __ashrdi3 (int64_t u, int32_t b)
+int64_t __ashrdi3_internal (int64_t u, int32_t b)
 {
   return __ashrdi3 (u, b);
 }
-symbol_version (__ashrdi3, __ashrdi3, GLIBC_2.0);
+symbol_version (__ashrdi3_internal, __ashrdi3, GLIBC_2.0);
 
 
 extern int64_t __lshrdi3 (int64_t, int32_t);
-int64_t __lshrdi3 (int64_t u, int32_t b)
+int64_t __lshrdi3_internal (int64_t u, int32_t b)
 {
   return __lshrdi3 (u, b);
 }
-symbol_version (__lshrdi3, __lshrdi3, GLIBC_2.0);
+symbol_version (__lshrdi3_internal, __lshrdi3, GLIBC_2.0);
 
 
 extern int32_t __cmpdi2 (int64_t, int64_t);
-int32_t __cmpdi2 (int64_t u, int64_t v)
+int32_t __cmpdi2_internal (int64_t u, int64_t v)
 {
   return __cmpdi2 (u, v);
 }
-symbol_version (__cmpdi2, __cmpdi2, GLIBC_2.0);
+symbol_version (__cmpdi2_internal, __cmpdi2, GLIBC_2.0);
 
 
 extern int32_t __ucmpdi2 (int64_t, int64_t);
-int32_t __ucmpdi2 (int64_t u, int64_t v)
+int32_t __ucmpdi2_internal (int64_t u, int64_t v)
 {
   return __ucmpdi2 (u, v);
 }
-symbol_version (__ucmpdi2, __ucmpdi2, GLIBC_2.0);
+symbol_version (__ucmpdi2_internal, __ucmpdi2, GLIBC_2.0);
 
 
 extern int64_t __fixdfdi (double);
-int64_t __fixdfdi (double d)
+int64_t __fixdfdi_internal (double d)
 {
   return __fixdfdi (d);
 }
-symbol_version (__fixdfdi, __fixdfdi, GLIBC_2.0);
+symbol_version (__fixdfdi_internal, __fixdfdi, GLIBC_2.0);
 
 
 extern int64_t __fixunsdfdi (double);
-int64_t __fixunsdfdi (double d)
+int64_t __fixunsdfdi_internal (double d)
 {
   return __fixunsdfdi (d);
 }
-symbol_version (__fixunsdfdi, __fixunsdfdi, GLIBC_2.0);
+symbol_version (__fixunsdfdi_internal, __fixunsdfdi, GLIBC_2.0);
 
 
 extern int64_t __fixsfdi (float);
-int64_t __fixsfdi (float d)
+int64_t __fixsfdi_internal (float d)
 {
   return __fixsfdi (d);
 }
-symbol_version (__fixsfdi, __fixsfdi, GLIBC_2.0);
+symbol_version (__fixsfdi_internal, __fixsfdi, GLIBC_2.0);
 
 
 extern int64_t __fixunssfdi (float);
-int64_t __fixunssfdi (float d)
+int64_t __fixunssfdi_internal (float d)
 {
   return __fixunssfdi (d);
 }
-symbol_version (__fixunssfdi, __fixunssfdi, GLIBC_2.0);
+symbol_version (__fixunssfdi_internal, __fixunssfdi, GLIBC_2.0);
 
 
 extern double __floatdidf (int64_t);
-double __floatdidf (int64_t u)
+double __floatdidf_internal (int64_t u)
 {
   return __floatdidf (u);
 }
-symbol_version (__floatdidf, __floatdidf, GLIBC_2.0);
+symbol_version (__floatdidf_internal, __floatdidf, GLIBC_2.0);
 
 
 extern float __floatdisf (int64_t);
-float __floatdisf (int64_t u)
+float __floatdisf_internal (int64_t u)
 {
   return __floatdisf (u);
 }
-symbol_version (__floatdisf, __floatdisf, GLIBC_2.0);
+symbol_version (__floatdisf_internal, __floatdisf, GLIBC_2.0);
 
 #endif
