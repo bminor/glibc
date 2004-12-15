0d60d7de5db10af7b1c1574520d3ab3c0f64d530
Jakub Jelinek <jakub@redhat.com>
2004-12-15 09:16:13 +0000

    Updated to fedora-glibc-20041215T0847

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -1,5 +1,5 @@
 # This file is updated automatically by Makefile.
 glibc-branch := fedora
 glibc-base := HEAD
-fedora-sync-date := 2004-12-14 10:58 UTC
-fedora-sync-tag := fedora-glibc-20041214T1058
+fedora-sync-date := 2004-12-15 08:47 UTC
+fedora-sync-tag := fedora-glibc-20041215T0847
