53bce6f6fda2bfbfd277072c77a77eaa5aed3baf
Roland McGrath <roland@gnu.org>
2005-04-07 08:39:43 +0000

    Write upstream var into branch.mk when doing make update upstream=...

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -6,3 +6,4 @@ COLLECTION := dist-fc3-updates-candidate
 releases-url := ftp://sources.redhat.com/pub/glibc/releases
 fedora-2_3-sync-date := 2005-04-07 02:42 UTC
 fedora-2_3-sync-tag := glibc-2_3_5
+upstream := 2.3.5
