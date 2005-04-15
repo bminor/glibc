2c0b1b931ef708e235654a23b12b4cb237df3e67
Roland McGrath <roland@gnu.org>
2005-04-15 05:46:43 +0000

    2005-04-14  Ulrich Drepper  <drepper@redhat.com>
    	[BZ #851]
    	* posix/execvp.c (execvp): Use file name including path when
    	trying to run it with shell.
    	* posix/Makefile: Add rules to build and run tst-execvp3.
    	* posix/tst-execvp3.c: New file.

--- posix/execvp.c
+++ posix/execvp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991,92,1995-99,2002,2004 Free Software Foundation, Inc.
+/* Copyright (C) 1991,92,1995-99,2002,2004,2005 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -133,14 +133,14 @@ execvp (file, argv)
 	  else
 	    startp = (char *) memcpy (name - (p - path), path, p - path);
 
-	  /* Try to execute this name.  If it works, execv will not return.  */
+	  /* Try to execute this name.  If it works, execve will not return. */
 	  __execve (startp, argv, __environ);
 
 	  if (errno == ENOEXEC)
 	    {
 	      if (script_argv == NULL)
 		{
-		  script_argv = allocate_scripts_argv (file, argv);
+		  script_argv = allocate_scripts_argv (startp, argv);
 		  if (script_argv == NULL)
 		    {
 		      /* A possible EACCES error is not as important as
