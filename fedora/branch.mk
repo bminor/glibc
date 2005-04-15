6183d906dd822337dec18dd1f48209dfd17127bd
Roland McGrath <roland@gnu.org>
2005-04-15 09:33:04 +0000

    Updated to fedora-glibc-20050415T0909

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -1,5 +1,5 @@
 # This file is updated automatically by Makefile.
 glibc-branch := fedora
 glibc-base := HEAD
-fedora-sync-date := 2005-04-05 21:14 UTC
-fedora-sync-tag := fedora-glibc-20050405T2114
+fedora-sync-date := 2005-04-15 09:09 UTC
+fedora-sync-tag := fedora-glibc-20050415T0909
