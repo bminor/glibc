ae9e937241bed564572618e5affe9af4b6a87d27
Roland McGrath <roland@gnu.org>
2005-07-29 23:40:08 +0000

    Updated to fedora-glibc-2_3-20050729T2336

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora-2_3
 glibc-base := glibc-2_3-branch
 DIST_BRANCH := FC-3
 COLLECTION := dist-fc3-updates-candidate
-fedora-2_3-sync-date := 2005-07-18 08:04 UTC
-fedora-2_3-sync-tag := fedora-glibc-2_3-20050718T0804
+fedora-2_3-sync-date := 2005-07-29 23:36 UTC
+fedora-2_3-sync-tag := fedora-glibc-2_3-20050729T2336
