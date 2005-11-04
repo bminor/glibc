75129816de8f1da6cb9e94de4f739bca1fb41347
Roland McGrath <roland@gnu.org>
2005-11-04 01:13:13 +0000

    2.3.6-0.fc3.1

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -6,3 +6,4 @@ COLLECTION := dist-fc3-updates-candidate
 upstream := 2.3.6
 fedora-2_3-sync-date := 2005-11-04 01:01 UTC
 fedora-2_3-sync-tag := glibc-2_3_6
+releases-url := ftp://sources.redhat.com/pub/glibc/releases
