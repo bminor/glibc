ab5823b4b6e760345d347b98830ccc75aa81bff6
Roland McGrath <roland@gnu.org>
2006-07-29 23:12:43 +0000

    Updated to fedora-glibc-20060729T2255

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora
 glibc-base := HEAD
 DIST_BRANCH := devel
 COLLECTION := dist-fc4
-fedora-sync-date := 2006-07-10 22:06 UTC
-fedora-sync-tag := fedora-glibc-20060710T2206
+fedora-sync-date := 2006-07-29 22:55 UTC
+fedora-sync-tag := fedora-glibc-20060729T2255
