b75a89e7de5530eec2527cb9a8995e48d339a703
Jakub Jelinek <jakub@redhat.com>
2006-10-07 19:06:46 +0000

    Adjust for fedora-2_5-branch.

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -1,7 +1,7 @@
 # This file is updated automatically by Makefile.
-glibc-branch := fedora
-glibc-base := HEAD
-DIST_BRANCH := devel
-COLLECTION := dist-fc4
+glibc-branch := fedora-2_5
+glibc-base := glibc-2_5-branch
+DIST_BRANCH := FC-6
+COLLECTION := dist-fc6
 fedora-sync-date := 2006-10-02 18:58 UTC
 fedora-sync-tag := fedora-glibc-20061002T1858
