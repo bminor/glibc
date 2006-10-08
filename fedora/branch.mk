70c9744ed64bd4f9a1b8e62ee4c3eb9fb716ba62
Jakub Jelinek <jakub@redhat.com>
2006-10-08 13:04:41 +0000

    Updated to fedora-glibc-2_5-20061008T1257

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora-2_5
 glibc-base := glibc-2_5-branch
 DIST_BRANCH := FC-6
 COLLECTION := dist-fc6
-fedora-sync-date := 2006-10-02 18:58 UTC
-fedora-sync-tag := fedora-glibc-20061002T1858
+fedora-2_5-sync-date := 2006-10-08 12:57 UTC
+fedora-2_5-sync-tag := fedora-glibc-2_5-20061008T1257
