e6042ff1eea1f5259df14cf42961898e6c9e6cb1
Jakub Jelinek <jakub@redhat.com>
2008-06-13 16:16:18 +0000

    Updated to fedora-glibc-20080613T1601

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora
 glibc-base := HEAD
 DIST_BRANCH := devel
 COLLECTION := dist-f8
-fedora-sync-date := 2008-06-12 16:19 UTC
-fedora-sync-tag := fedora-glibc-20080612T1619
+fedora-sync-date := 2008-06-13 16:01 UTC
+fedora-sync-tag := fedora-glibc-20080613T1601
