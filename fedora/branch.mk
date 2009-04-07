d2aa96e13c2f7a3066d53e4711aff63e313c4c0b
Jakub Jelinek <jakub@redhat.com>
2009-04-07 07:04:24 +0000

    Updated to fedora-glibc-20090407T0657

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora
 glibc-base := HEAD
 DIST_BRANCH := devel
 COLLECTION := dist-f8
-fedora-sync-date := 2009-04-07 05:45 UTC
-fedora-sync-tag := fedora-glibc-20090407T0545
+fedora-sync-date := 2009-04-07 06:57 UTC
+fedora-sync-tag := fedora-glibc-20090407T0657
