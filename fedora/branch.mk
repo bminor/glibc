1ad9ccd18a9d2ca18c12143a2a0d6dfe78a382a5
Jakub Jelinek <jakub@redhat.com>
2009-04-08 16:24:32 +0000

    Updated to fedora-glibc-20090408T1602

--- fedora/branch.mk
+++ fedora/branch.mk
@@ -3,5 +3,5 @@ glibc-branch := fedora
 glibc-base := HEAD
 DIST_BRANCH := devel
 COLLECTION := dist-f8
-fedora-sync-date := 2009-04-07 06:57 UTC
-fedora-sync-tag := fedora-glibc-20090407T0657
+fedora-sync-date := 2009-04-08 16:02 UTC
+fedora-sync-tag := fedora-glibc-20090408T1602
--- resolv/res_hconf.c
+++ resolv/res_hconf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1993, 1995-2006, 2007 Free Software Foundation, Inc.
+/* Copyright (C) 1993, 1995-2006, 2007, 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by David Mosberger (davidm@azstarnet.com).
 
