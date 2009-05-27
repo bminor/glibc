dfbbe67270efa9c03f9444d50d2f98a7a64622b4
Roland McGrath <roland@redhat.com>
2009-05-27 03:17:43 -0700

    Fedora makefile magic using GIT.

--- fedora/branch.mk
+++
@@ -1,7 +0,0 @@
-# This file is updated automatically by Makefile.
-glibc-branch := fedora
-glibc-base := HEAD
-DIST_BRANCH := devel
-COLLECTION := dist-f8
-fedora-sync-date := 2009-05-10 18:42 UTC
-fedora-sync-tag := fedora-glibc-20090510T1842
