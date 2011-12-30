e73988fd47a8ab815e4a4427683ac1098c8ff31f
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2011-12-29 19:39:22 -0500

    Fix compilation due to __nan defines
    Backport of commit #ee2aafe08e68c88412b7dcfa4b1e0d11789348f8

--- sysdeps/ieee754/ldbl-128ibm/s_atanl.c
+++ sysdeps/ieee754/ldbl-128ibm/s_atanl.c
@@ -58,7 +58,7 @@
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA */
 
-
+#include <math.h>
 #include "math_private.h"
 #include <math_ldbl_opt.h>
 
