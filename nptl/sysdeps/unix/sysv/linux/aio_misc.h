9aaaf1363c43f84c47ba6e35586a2ae8f8d44cba
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2011-12-30 15:41:20 -0500

    Fix for AIO stack size (backported from 'master' branch)

--- nptl/sysdeps/unix/sysv/linux/aio_misc.h
+++ nptl/sysdeps/unix/sysv/linux/aio_misc.h
@@ -46,8 +46,8 @@ __aio_create_helper_thread (pthread_t *threadp, void *(*tf) (void *),
   pthread_attr_init (&attr);
   pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
 
-  /* Don't set the stacksize to PTHREAD_STACK_MIN.  Let it default, otherwise
-     the stack may be too small if TLS is in use.  */
+  /* The helper thread needs only very little resources.  */
+  (void) pthread_attr_setstacksize (&attr, __pthread_get_minstack (&attr));
 
   /* Block all signals in the helper thread.  To do this thoroughly we
      temporarily have to block all signals here.  */
