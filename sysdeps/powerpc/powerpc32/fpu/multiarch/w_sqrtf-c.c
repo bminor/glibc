852da90a7c8d3d2333aff246ea52c58e8af58469
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-10 15:42:10 -0500

    PowerPC: sqrtf multilib for PowerPC32

--- sysdeps/powerpc/powerpc32/fpu/multiarch/w_sqrtf-c.c
+++ sysdeps/powerpc/powerpc32/fpu/multiarch/w_sqrtf-c.c
@@ -3,11 +3,11 @@
 /* The PPC32 default implementation will fallback to __ieee754_sqrt symbol
    from sysdeps/powerpc/fpu/e_sqrt.c  */
 
-#define __sqrtf __sqrtf_ppc32
+#define __sqrt __sqrt_ppc32
 #undef weak_alias
 #define weak_alias(a, b)
 #undef strong_alias
 #define strong_alias(a, b)
 
 
-#include <math/w_sqrtf.c>
+#include <math/w_sqrt.c>
