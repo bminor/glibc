dc65929f124c88babc3dd49b85818ca7bc0ea653
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-04-14 09:38:31 -0500

    PowerPC: fixing missing sqrtl libm symbol

--- sysdeps/powerpc/powerpc32/fpu/multiarch/w_sqrt.c
+++ sysdeps/powerpc/powerpc32/fpu/multiarch/w_sqrt.c
@@ -38,3 +38,6 @@ weak_alias (__sqrt, sqrt)
 strong_alias (__sqrt, __sqrtl)
 weak_alias (__sqrt, sqrtl)
 #endif
+#if LONG_DOUBLE_COMPAT(libm, GLIBC_2_0)
+compat_symbol (libm, __sqrt, sqrtl, GLIBC_2_0);
+#endif
