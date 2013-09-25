dfc25d72984eb5a3354e104612d0ca0129af3f98
Adhemerval Zanella <azanella@linux.vnet.ibm.com>
2013-09-25 13:43:04 -0500

    PowerPC: Fix POINTER_CHK_GUARD thread register for PPC64

--- sysdeps/powerpc/powerpc32/stackguard-macros.h
+++ sysdeps/powerpc/powerpc32/stackguard-macros.h
@@ -1,7 +1,14 @@
 #include <stdint.h>
 
 #define STACK_CHK_GUARD \
-  ({ uintptr_t x; asm ("lwz %0,-28680(2)" : "=r" (x)); x; })
+  ({												\
+     uintptr_t x;										\
+     asm ("lwz %0,%1(2)"									\
+	  : "=r" (x)										\
+	  : "i" (offsetof (tcbhead_t, stack_guard) - TLS_TCB_OFFSET - sizeof (tcbhead_t))	\
+         );											\
+     x;												\
+   })
 
 #define POINTER_CHK_GUARD \
   ({												\
--- sysdeps/powerpc/powerpc64/stackguard-macros.h
+++ sysdeps/powerpc/powerpc64/stackguard-macros.h
@@ -1,12 +1,19 @@
 #include <stdint.h>
 
 #define STACK_CHK_GUARD \
-  ({ uintptr_t x; asm ("ld %0,-28688(13)" : "=r" (x)); x; })
+  ({												\
+     uintptr_t x;										\
+     asm ("ld %0,%1(13)"										\
+	  : "=r" (x)										\
+	  : "i" (offsetof (tcbhead_t, stack_guard) - TLS_TCB_OFFSET - sizeof (tcbhead_t))	\
+         );											\
+     x;												\
+   })
 
 #define POINTER_CHK_GUARD \
   ({												\
      uintptr_t x;										\
-     asm ("ld %0,%1(2)"										\
+     asm ("ld %0,%1(13)"										\
 	  : "=r" (x)										\
 	  : "i" (offsetof (tcbhead_t, pointer_guard) - TLS_TCB_OFFSET - sizeof (tcbhead_t))	\
          );											\
