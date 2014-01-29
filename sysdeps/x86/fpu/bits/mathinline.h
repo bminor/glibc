cec24099fb06c785b89119aab93940312c2949ba
H.J. Lu <hjl.tools@gmail.com>
2014-01-29 07:51:41 -0800

    Disable x87 inline functions for SSE2 math
    When i386 and x86-64 mathinline.h was merged into a single mathinline.h,
    "gcc -m32" enables x87 inline functions on x86-64 even when -mfpmath=sse
    and SSE2 is enabled.  It is a regression on x86-64.  We should check
    __SSE2_MATH__ instead of __x86_64__ when disabling x87 inline functions.
    
    (cherry picked from commit 409e00bd69b8d8dd74d7327085351d26769ea6fc)
    
    Conflicts:
    	ChangeLog
    	NEWS
    	sysdeps/x86/fpu/bits/mathinline.h

--- sysdeps/x86/fpu/bits/mathinline.h
+++ sysdeps/x86/fpu/bits/mathinline.h
@@ -1,5 +1,5 @@
 /* Inline math functions for i387 and SSE.
-   Copyright (C) 1995-2012 Free Software Foundation, Inc.
+   Copyright (C) 1995-2014 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -382,7 +382,7 @@ __END_NAMESPACE_C99
 # endif
 #endif
 
-#ifndef __x86_64__
+#ifndef __SSE2_MATH__
 # if ((!defined __NO_MATH_INLINES || defined __LIBC_INTERNAL_MATH_INLINES) \
      && defined __OPTIMIZE__)
 
@@ -965,4 +965,4 @@ __inline_mathcode2 (__ieee754_atan2, __y, __x,
 		    return __value;)
 # endif
 
-#endif /* !__x86_64__ */
+#endif /* !__SSE2_MATH__ */
