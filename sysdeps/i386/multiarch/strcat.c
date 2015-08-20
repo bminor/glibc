6df87253b289e5d51e01f1a45353294f3dd65a6e
H.J. Lu <hjl.tools@gmail.com>
2015-08-20 15:20:58 -0700

    Add i386 strcat multiarch functions

---
+++ sysdeps/i386/multiarch/strcat.c
@@ -0,0 +1,51 @@
+/* Multiple versions of strcat.
+   All versions must be listed in ifunc-impl-list.c.
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+/* Define multiple versions only for the definition in libc.  */
+#if IS_IN (libc)
+/* Redefine strcat so that the compiler won't complain about the type
+   mismatch with the IFUNC selector in strong_alias, below.  */
+# undef strcat
+# define strcat __redirect_strcat
+# include <string.h>
+# undef strcat
+
+# include <init-arch.h>
+
+extern __typeof (__redirect_strcat) __strcat_i486 attribute_hidden;
+extern __typeof (__redirect_strcat) __strcat_sse2 attribute_hidden;
+extern __typeof (__redirect_strcat) __strcat_ssse3 attribute_hidden;
+
+/* Avoid DWARF definition DIE on ifunc symbol so that GDB can handle
+   ifunc symbol properly.  */
+extern __typeof (__redirect_strcat) strcat;
+extern void *strcat_ifunc (void) __asm__ ("strcat");
+
+void *
+strcat_ifunc (void)
+{
+  if (HAS_CPU_FEATURE (SSSE3))
+    return __strcat_ssse3;
+  else if (HAS_CPU_FEATURE (SSE2))
+    return __strcat_sse2;
+
+  return __strcat_i486;
+}
+__asm__ (".type strcat, %gnu_indirect_function");
+#endif
--- sysdeps/i386/multiarch/strncat-i386.c
+++ sysdeps/i386/multiarch/strncat-i386.c
@@ -1,8 +1,8 @@
-#define STRNCAT __strncat_ia32
+#define STRNCAT __strncat_i386
 #ifdef SHARED
 #undef libc_hidden_def
 #define libc_hidden_def(name) \
-  __hidden_ver1 (__strncat_ia32, __GI___strncat, __strncat_ia32);
+  __hidden_ver1 (__strncat_i386, __GI___strncat, __strncat_i386);
 #endif
 
 #include "string/strncat.c"
