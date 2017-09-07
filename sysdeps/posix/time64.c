8ba84c8a86bb0baa44c4311569257b30777e322c
Albert ARIBAUD (3ADEV) <albert.aribaud@3adev.fr>
2017-09-08 00:41:55 +0200

    Y2038: add function __time64
    This provides a generic Posix implementation based on calling
    __gettimeofday64().

--- sysdeps/posix/time64.c
+++ sysdeps/posix/time64.c
@@ -1,4 +1,6 @@
-/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+/* Get the 64-bit current time
+ * 
+   Copyright (C) 1991-2018 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -15,32 +17,27 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
+#include <stddef.h>		/* For NULL.  */
 #include <time.h>
+#include <sys/time.h>
 
-/* Return the time now, and store it in *TIMER if not NULL.  */
-time_t
-time (time_t *timer)
-{
-  __set_errno (ENOSYS);
 
-  if (timer != NULL)
-    *timer = (time_t) -1;
-  return (time_t) -1;
-}
-libc_hidden_def (time)
-
-stub_warning (time)
-
-/* 64-bit time version */
+/* Return the current time as a `time_t' and also put it in *T if T is
+   not NULL.  Time is represented as seconds from Jan 1 00:00:00 1970.  */
 
 __time64_t
-__time64 (__time64_ *timer)
+__time64 (__time64_t *t)
 {
-  __set_errno (ENOSYS);
+  struct __timeval64 tv;
+  __time64_t result;
 
-  if (timer != NULL)
-    *timer = (__time64_t) -1;
-  return (__time64_t) -1;
+  if (__gettimeofday64 (&tv, (struct timezone *) NULL))
+    result = (__time64_t) -1;
+  else
+    result = tv.tv_sec;
+
+  if (t != NULL)
+    *t = result;
+
+  return result;
 }
-libc_hidden_def (__time64)
