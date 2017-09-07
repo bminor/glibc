f4f353b27f23d54bfb2e6481ef9e985d57300d7f
Albert ARIBAUD (3ADEV) <albert.aribaud@3adev.fr>
2017-09-08 00:42:12 +0200

    Y2038: add struct __ntp_timeval64

--- sysdeps/unix/sysv/linux/sys/timex.h
+++ sysdeps/unix/sysv/linux/sys/timex.h
@@ -40,6 +40,19 @@ struct ntptimeval
   long int __glibc_reserved4;
 };
 
+struct __ntptimeval64
+{
+  struct __timeval64 time;	/* current time (ro) */
+  long int maxerror;	/* maximum error (us) (ro) */
+  long int esterror;	/* estimated error (us) (ro) */
+  long int tai;		/* TAI offset (ro) */
+
+  long int __glibc_reserved1;
+  long int __glibc_reserved2;
+  long int __glibc_reserved3;
+  long int __glibc_reserved4;
+};
+
 /* Clock states (time_state) */
 #define TIME_OK		0	/* clock synchronized, no leap second */
 #define TIME_INS	1	/* insert leap second */
