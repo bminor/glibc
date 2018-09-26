61d8b5feeed36e242a043befe9b11f7f8c294f58
Joseph Myers <joseph@codesourcery.com>
2018-09-26 12:33:14 +0000

    Share MAP_* flags between more architectures.
    Continuing bits/mman.h unification between architectures using the
    Linux kernel, this patch arranges for the common set of MAP_* flags to
    be used by two more architectures.  That common set is moved to
    bits/mman-map-flags-generic.h, which is included by bits/mman.h, to
    allow architectures to use that common set even if they also have
    architecture-specific additions to it.  As well as the generic
    bits/mman.h, the versions for x86 and ia64 are also then made to
    include bits/mman-map-flags-generic.h, so while they still need
    architecture-specific bits/mman.h (for MAP_32BIT and MAP_GROWSUP
    respectively), they do not need to duplicate the generic flag
    definitions in there.
    
    Tested for x86_64 and x86, and with build-many-glibcs.py.
    
    	* sysdeps/unix/sysv/linux/bits/mman-map-flags-generic.h: New
    	file.  Most contents moved from ....
    	* sysdeps/unix/sysv/linux/bits/mman.h: ... here.  Move contents to
    	and include <bits/mman-map-flags-generic.h>.
    	* sysdeps/unix/sysv/linux/Makefile [$(subdir) = misc]
    	(sysdep_headers): Add bits/mman-map-flags-generic.h.
    	* sysdeps/unix/sysv/linux/ia64/bits/mman.h: Include
    	<bits/mman-map-flags-generic.h>.
    	[__USE_MISC] (MAP_GROWSUP): Only define this macro, not other
    	macros defined in <bits/mman-map-flags-generic.h>.
    	* sysdeps/unix/sysv/linux/x86/bits/mman.h: Include
    	<bits/mman-map-flags-generic.h>.
    	[__USE_MISC] (MAP_32BIT): Only define this macro, not other macros
    	defined in <bits/mman-map-flags-generic.h>.

--- sysdeps/unix/sysv/linux/bits/mman-map-flags-generic.h
+++ sysdeps/unix/sysv/linux/bits/mman-map-flags-generic.h
@@ -17,16 +17,26 @@
    <http://www.gnu.org/licenses/>.  */
 
 #ifndef _SYS_MMAN_H
-# error "Never use <bits/mman.h> directly; include <sys/mman.h> instead."
+# error "Never use <bits/mman-map-flags-generic.h> directly; include <sys/mman.h> instead."
 #endif
 
 /* These definitions are appropriate for architectures that, in the
    Linux kernel, either have no uapi/asm/mman.h, or have one that
-   includes asm-generic/mman.h without any changes or additions
-   relevant to glibc.  If there are additions relevant to glibc, an
-   architecture-specific bits/mman.h is needed.  */
+   includes asm-generic/mman.h without any changes to the values of
+   the MAP_* flags defined in that header.  */
 
-#include <bits/mman-map-flags-generic.h>
-
-/* Include generic Linux declarations.  */
-#include <bits/mman-linux.h>
+#ifdef __USE_MISC
+# define MAP_GROWSDOWN	0x00100		/* Stack-like segment.  */
+# define MAP_DENYWRITE	0x00800		/* ETXTBSY.  */
+# define MAP_EXECUTABLE	0x01000		/* Mark it as an executable.  */
+# define MAP_LOCKED	0x02000		/* Lock the mapping.  */
+# define MAP_NORESERVE	0x04000		/* Don't check for reservations.  */
+# define MAP_POPULATE	0x08000		/* Populate (prefault) pagetables.  */
+# define MAP_NONBLOCK	0x10000		/* Do not block on IO.  */
+# define MAP_STACK	0x20000		/* Allocation is for a stack.  */
+# define MAP_HUGETLB	0x40000		/* Create huge page mapping.  */
+# define MAP_SYNC	0x80000		/* Perform synchronous page
+					   faults for the mapping.  */
+# define MAP_FIXED_NOREPLACE 0x100000	/* MAP_FIXED but do not unmap
+					   underlying mapping.  */
+#endif
