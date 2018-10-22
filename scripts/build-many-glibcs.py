8190520f2a518bec6e6d54d27ed6b6d985cacb59
Joseph Myers <joseph@codesourcery.com>
2018-10-22 16:49:37 +0000

    Use Linux 4.19 in build-many-glibcs.py.
    	* scripts/build-many-glibcs.py (Context.checkout): Default Linux
    	version to 4.19.

--- scripts/build-many-glibcs.py
+++ scripts/build-many-glibcs.py
@@ -718,7 +718,7 @@ class Context(object):
                             'gcc': 'vcs-8',
                             'glibc': 'vcs-mainline',
                             'gmp': '6.1.2',
-                            'linux': '4.18',
+                            'linux': '4.19',
                             'mpc': '1.1.0',
                             'mpfr': '4.0.1',
                             'mig': 'vcs-mainline',
