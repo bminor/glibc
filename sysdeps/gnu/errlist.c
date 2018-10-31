3ed7c33fa2915008ec861f25b2d54a0ccc59ca21
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-10-31 09:17:05 +0100

    hurd: Fix build
    	* sysdeps/gnu/errlist.c (EIEIO): Fix comment marker.

--- sysdeps/gnu/errlist.c
+++ sysdeps/gnu/errlist.c
@@ -974,7 +974,7 @@ TRANS You did @strong{what}? */
 #endif
 #ifdef EIEIO
 /*
-TRANS Go home and have a glass of warm, dairy-fresh milk. */
+TRANS Go home and have a glass of warm, dairy-fresh milk.
 TRANS @c Okay.  Since you are dying to know, I'll tell you.
 TRANS @c This is a joke, obviously.  There is a children's song which begins,
 TRANS @c "Old McDonald had a farm, e-i-e-i-o."  Every time I see the (real)
@@ -987,7 +987,7 @@ TRANS @c
 TRANS @c "bought the farm" means "died".  -jtobey
 TRANS @c
 TRANS @c Translators, please do not translate this litteraly, translate it into
-TRANS @c an idiomatic funny way of saying that the computer died.
+TRANS @c an idiomatic funny way of saying that the computer died. */
     [ERR_REMAP (EIEIO)] = N_("Computer bought the farm"),
 # if EIEIO > ERR_MAX
 # undef ERR_MAX
