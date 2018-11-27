c74a91deaa5de416237c02bbb3e41bda76ca4c7b
Florian Weimer <fweimer@redhat.com>
2018-11-27 21:35:56 +0100

    support: Implement support_quote_string
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>

--- support/support.h
+++ support/support.h
@@ -69,6 +69,11 @@ void support_write_file_string (const char *path, const char *contents);
    the result).  */
 char *support_quote_blob (const void *blob, size_t length);
 
+/* Quote the contents of the at STR, in such a way that the result
+   string can be included in a C literal (in single/double quotes,
+   without putting the quotes into the result).  */
+char *support_quote_string (const char *str);
+
 /* Returns non-zero if the file descriptor is a regular file on a file
    system which supports holes (that is, seeking and writing does not
    allocate storage for the range of zeros).  FD must refer to a
