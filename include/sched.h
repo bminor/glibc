37caec3a11eb329f333bc70cf772cb9a9734325a
H.J. Lu <hjl.tools@gmail.com>
2018-12-07 12:47:30 -0800

    Don't use __typeof__ (getcpu)
    We can't use "__typeof__ (getcpu)" since getcpu is Linux specific and
    Hurd doesn't have it.
    
    Tested with build-many-glibcs.py.
    
    	* include/sched.h (__getcpu): Don't use __typeof__ (getcpu).

--- include/sched.h
+++ include/sched.h
@@ -26,7 +26,9 @@ libc_hidden_proto (__clone)
 extern int __clone2 (int (*__fn) (void *__arg), void *__child_stack_base,
 		     size_t __child_stack_size, int __flags, void *__arg, ...);
 libc_hidden_proto (__clone2)
-extern __typeof__ (getcpu) __getcpu;
+/* NB: Can't use "__typeof__ (getcpu)" since getcpu is Linux specific
+   and Hurd doesn't have it.  */
+extern int __getcpu (unsigned int *, unsigned int *);
 libc_hidden_proto (__getcpu)
 #endif
 #endif
