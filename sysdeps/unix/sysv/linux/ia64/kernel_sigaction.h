8b1d5da56601ba7e59340dda235a6f3dbaa98ec9
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2018-12-11 17:15:07 -0200

    ia64: Remove kernel_sigaction.h
    IA64 kernel_sigaction.h definition is the sama as the Linux generic
    one.
    
    Checked on ia64-linux-gnu.
    
    	* sysdeps/unix/sysv/linux/ia64/kernel_sigaction.h: Remove file.

--- sysdeps/unix/sysv/linux/ia64/kernel_sigaction.h
+++
@@ -1,7 +0,0 @@
-/* This is the sigaction structure from the Linux 3.2 kernel.  */
-struct kernel_sigaction
-{
-  __sighandler_t k_sa_handler;
-  unsigned long sa_flags;
-  sigset_t sa_mask;               /* mask last for extensibility */
-};
