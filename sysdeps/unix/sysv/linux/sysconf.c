77b6f5534778b5403c87fa5415625aeb4c3cbf44
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2019-01-16 17:30:07 +0000

    linux: Assume clock_getres CLOCK_{PROCESS,THREAD}_CPUTIME_ID
    The Linux 3.2 clock_getres kernel code (kernel/posix-cpu-timers.c)
    issued for clock_getres CLOCK_PROCESS_CPUTIME_ID (process_cpu_clock_getres)
    and CLOCK_THREAD_CPUTIME_ID (thread_cpu_clock_getres) call
    posix_cpu_clock_getres. And it fails on check_clock only if an invalid
    clock is used (not the case) or if we pass an invalid the pid/tid in
    29 msb of clock_id (not the case either).
    
    This patch assumes that clock_getres syscall always support
    CLOCK_PROCESS_CPUTIME_ID and CLOCK_THREAD_CPUTIME_ID, so there is no need
    to fallback to hp-timing support for _SC_MONOTONIC_CLOCK neither to issue
    the syscall to certify the clock_id is supported bt the kernel.  This
    allows simplify the sysconf support to always use the syscall.
    
    it also removes ia64 itc drift check and assume kernel handles it correctly.
    
    Checked on aarch64-linux-gnu, x86_64-linux-gnu, and i686-linux-gnu.
    
    	* sysdeps/unix/sysv/linux/ia64/has_cpuclock.c: Remove file.
    	* sysdeps/unix/sysv/linux/ia64/sysconf.c: Likewise.
    	* sysdeps/unix/sysv/linux/sysconf.c (has_cpuclock): Remove function.
    	(__sysconf): Assume kernel support for _SC_MONOTONIC_CLOCK,
    	_SC_CPUTIME, and _SC_THREAD_CPUTIME.

--- sysdeps/unix/sysv/linux/sysconf.c
+++ sysdeps/unix/sysv/linux/sysconf.c
@@ -35,34 +35,6 @@
 static long int posix_sysconf (int name);
 
 
-#ifndef HAS_CPUCLOCK
-static long int
-has_cpuclock (int name)
-{
-# if defined __NR_clock_getres || HP_TIMING_AVAIL
-  /* If we have HP_TIMING, we will fall back on that if the system
-     call does not work, so we support it either way.  */
-#  if !HP_TIMING_AVAIL
-  /* Check using the clock_getres system call.  */
-  struct timespec ts;
-  INTERNAL_SYSCALL_DECL (err);
-  int r = INTERNAL_SYSCALL (clock_getres, err, 2,
-			    (name == _SC_CPUTIME
-			     ? CLOCK_PROCESS_CPUTIME_ID
-			     : CLOCK_THREAD_CPUTIME_ID),
-			    &ts);
-  if (INTERNAL_SYSCALL_ERROR_P (r, err))
-    return -1;
-#  endif
-  return _POSIX_VERSION;
-# else
-  return -1;
-# endif
-}
-# define HAS_CPUCLOCK(name) has_cpuclock (name)
-#endif
-
-
 /* Get the value of the system variable NAME.  */
 long int
 __sysconf (int name)
@@ -71,29 +43,20 @@ __sysconf (int name)
 
   switch (name)
     {
-      struct rlimit rlimit;
-#ifdef __NR_clock_getres
     case _SC_MONOTONIC_CLOCK:
-      /* Check using the clock_getres system call.  */
-      {
-	struct timespec ts;
-	INTERNAL_SYSCALL_DECL (err);
-	int r;
-	r = INTERNAL_SYSCALL (clock_getres, err, 2, CLOCK_MONOTONIC, &ts);
-	return INTERNAL_SYSCALL_ERROR_P (r, err) ? -1 : _POSIX_VERSION;
-      }
-#endif
-
     case _SC_CPUTIME:
     case _SC_THREAD_CPUTIME:
-      return HAS_CPUCLOCK (name);
+      return _POSIX_VERSION;
 
     case _SC_ARG_MAX:
-      /* Use getrlimit to get the stack limit.  */
-      if (__getrlimit (RLIMIT_STACK, &rlimit) == 0)
-	return MAX (legacy_ARG_MAX, rlimit.rlim_cur / 4);
+      {
+        struct rlimit rlimit;
+        /* Use getrlimit to get the stack limit.  */
+        if (__getrlimit (RLIMIT_STACK, &rlimit) == 0)
+	  return MAX (legacy_ARG_MAX, rlimit.rlim_cur / 4);
 
-      return legacy_ARG_MAX;
+        return legacy_ARG_MAX;
+      }
 
     case _SC_NGROUPS_MAX:
       /* Try to read the information from the /proc/sys/kernel/ngroups_max
@@ -102,11 +65,14 @@ __sysconf (int name)
       break;
 
     case _SC_SIGQUEUE_MAX:
-      if (__getrlimit (RLIMIT_SIGPENDING, &rlimit) == 0)
-	return rlimit.rlim_cur;
+      {
+        struct rlimit rlimit;
+        if (__getrlimit (RLIMIT_SIGPENDING, &rlimit) == 0)
+	  return rlimit.rlim_cur;
 
-      /* The /proc/sys/kernel/rtsig-max file contains the answer.  */
-      procfname = "/proc/sys/kernel/rtsig-max";
+        /* The /proc/sys/kernel/rtsig-max file contains the answer.  */
+        procfname = "/proc/sys/kernel/rtsig-max";
+      }
       break;
 
     default:
