525478a25143fa1d040fd857600415fb2e4c0fe1
Florian Weimer <fweimer@redhat.com>
2019-01-21 21:26:03 +0100

    CVE-2016-10739: getaddrinfo: Fully parse IPv4 address strings [BZ #20018]
    The IPv4 address parser in the getaddrinfo function is changed so that
    it does not ignore trailing whitespace and all characters after it.
    For backwards compatibility, the getaddrinfo function still recognizes
    legacy name syntax, such as 192.000.002.010 interpreted as 192.0.2.8
    (octal).
    
    This commit does not change the behavior of inet_addr and inet_aton.
    gethostbyname already had additional sanity checks (but is switched
    over to the new __inet_aton_exact function for completeness as well).
    
    To avoid sending the problematic query names over DNS, commit
    6ca53a2453598804a2559a548a08424fca96434a ("resolv: Do not send queries
    for non-host-names in nss_dns [BZ #24112]") is needed.
    
    (cherry picked from commit 108bc4049f8ae82710aec26a92ffdb4b439c83fd)

--- sysdeps/posix/getaddrinfo.c
+++ sysdeps/posix/getaddrinfo.c
@@ -488,7 +488,7 @@ gaih_inet (const char *name, const struct gaih_service *service,
 	  malloc_name = true;
 	}
 
-      if (__inet_aton (name, (struct in_addr *) at->addr) != 0)
+      if (__inet_aton_exact (name, (struct in_addr *) at->addr) != 0)
 	{
 	  if (req->ai_family == AF_UNSPEC || req->ai_family == AF_INET)
 	    at->family = AF_INET;
