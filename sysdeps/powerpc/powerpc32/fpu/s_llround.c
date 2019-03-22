9d5d214e86db30633e905907f7b3cf56b8fee16e
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2019-03-22 16:47:34 +0000

    powerpc: Refactor powerpc32 lround/lroundf/llround/llroundf
    This patches consolidates all the powerpc llround{f} implementations on
    the generic sysdeps/powerpc/powerpc32/fpu/s_llround{f}.
    
    Checked on powerpc-linux-gnu (built without --with-cpu, with
    --with-cpu=power4 and with --with-cpu=power5+ and --disable-multi-arch),
    powerpc64-linux-gnu (built without --with-cp and with --with-cpu=power5+
    and --disable-multi-arch).
    
    	* sysdeps/powerpc/powerpc32/fpu/Makefile
    	[$(subdir) == math] (CFLAGS-s_lround.c): New rule.
    	* sysdeps/powerpc/powerpc32/fpu/s_llround.c (__llround): Add power5+
    	and fctidz optimization.
    	* sysdeps/powerpc/powerpc32/fpu/s_lround.S: Remove file.
    	* sysdeps/powerpc/powerpc32/fpu/s_lround.c: New file.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/Makefile
    	(CFLAGS-s_llround-power6.c, CFLAGS-s_llround-power5+.c,
    	CFLAGS-s_llround-ppc32.c, CFLAGS-s_lround-ppc32.c,
    	CFLAGS-s_lround-power5+.c): New rule.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-power5+.c:
    	New file.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-power6.c:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-ppc32.c:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_lround-power5+.c:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_lround-ppc32.c:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-power5+.S:
    	Remove file.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-power6.S:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_llround-ppc32.S:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_lround-power5+.S:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/multiarch/s_lround-ppc32.S:
    	Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/s_llround.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power4/fpu/s_llroundf.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power5+/fpu/s_llround.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power5+/fpu/s_llroundf.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power5+/fpu/s_lround.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power6/fpu/s_llround.S: Likewise.
    	* sysdeps/powerpc/powerpc32/power6/fpu/s_llroundf.S: Likewise.
    
    Reviewed-by: Gabriel F. T. Gomes <gabriel@inconstante.eti.br>

--- sysdeps/powerpc/powerpc32/fpu/s_llround.c
+++ sysdeps/powerpc/powerpc32/fpu/s_llround.c
@@ -18,10 +18,10 @@
 
 #include <limits.h>
 #include <math.h>
-#include <math_ldbl_opt.h>
 #include <math_private.h>
 #include <stdint.h>
 #include <libm-alias-double.h>
+#include <math-barriers.h>
 
 /* Round to the nearest integer, with values exactly on a 0.5 boundary
    rounded away from zero, regardless of the current rounding mode.
@@ -31,9 +31,53 @@
 long long int
 __llround (double x)
 {
+#ifdef _ARCH_PWR5X
+  x = round (x);
+  /* The barrier prevents compiler from optimizing it to llround when
+     compiled with -fno-math-errno */
+  math_opt_barrier (x);
+  return x;
+#else
   long long xr;
   if (HAVE_PPC_FCTIDZ)
-    xr = (long long) x;
+    {
+       /* IEEE 1003.1 lround function.  IEEE specifies "round to the nearest
+	  integer value, rounding halfway cases away from zero, regardless of
+	  the current rounding mode."  However PowerPC Architecture defines
+	  "round to Nearest" as "Choose the best approximation. In case of a
+	  tie, choose the one that is even (least significant bit o).".
+	  So we can't use the PowerPC "round to Nearest" mode. Instead we set
+	  "round toward Zero" mode and round by adding +-0.5 before rounding
+	  to the integer value.
+
+	  It is necessary to detect when x is (+-)0x1.fffffffffffffp-2
+	  because adding +-0.5 in this case will cause an erroneous shift,
+	  carry and round.  We simply return 0 if 0.5 > x > -0.5.  Likewise
+	  if x is and odd number between +-(2^52 and 2^53-1) a shift and
+	  carry will erroneously round if biased with +-0.5.  Therefore if x
+	  is greater/less than +-2^52 we don't need to bias the number with
+	  +-0.5.  */
+      double ax = fabs (x);
+
+      if (ax < 0.5)
+	return 0;
+
+      if (ax < 0x1p+52)
+	{
+	  /* Test whether an integer to avoid spurious "inexact".  */
+	  double t = ax + 0x1p+52;
+	  t = t - 0x1p+52;
+	  if (ax != t)
+	    {
+	      ax = ax + 0.5;
+	      if (x < 0.0)
+		ax = -fabs (ax);
+	      x = ax;
+	    }
+        }
+
+      return x;
+    }
   else
     {
       /* Avoid incorrect exceptions from libgcc conversions (as of GCC
@@ -80,5 +124,8 @@ __llround (double x)
 	xr -= (long long) ((unsigned long long) xr - 1) < 0;
     }
   return xr;
+#endif
 }
+#ifndef __llround
 libm_alias_double (__llround, llround)
+#endif
