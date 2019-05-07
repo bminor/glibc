c2b11710fb4a2e8d337ae8f042724143c5ccf173
Joseph Myers <joseph@codesourcery.com>
2019-05-07 14:49:05 +0000

    Use Linux 5.1 in build-many-glibcs.py.
    	* scripts/build-many-glibcs.py (Context.checkout): Default Linux
    	version to 5.1.

--- scripts/build-many-glibcs.py
+++ scripts/build-many-glibcs.py
@@ -714,7 +714,7 @@ class Context(object):
                             'gcc': 'vcs-9',
                             'glibc': 'vcs-mainline',
                             'gmp': '6.1.2',
-                            'linux': '5.0',
+                            'linux': '5.1',
                             'mpc': '1.1.0',
                             'mpfr': '4.0.2',
                             'mig': 'vcs-mainline',
