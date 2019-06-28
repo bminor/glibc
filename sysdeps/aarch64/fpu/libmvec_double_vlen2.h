80792b61024d810c3d36e9a1dd8a166249d8b6e8
Szabolcs Nagy <szabolcs.nagy@arm.com>
2019-06-28 11:29:19 +0100

    Aarch64: Add simd exp/expf ABI symbols
    The implementation is in assembly and just calls the scalar math code.
    This ensures that old compiler without vector call abi support can
    build libmvec. The abi is supported since GCC 9.1, the specification is
    
    https://developer.arm.com/tools-and-software/server-and-hpc/arm-architecture-tools/arm-compiler-for-hpc/vector-function-abi
    
    Vector functions require a STO_AARCH64_VARIANT_PCS marking in the
    dynamic symbol table for lazy bound calls to work. This will be
    missing in libmvec, which works because the marking only affects
    the behaviour if there are calls to the symbols in the binary.
    
    Testing requires vector call abi support, which is detected.
    
    Header declarations are not added yet, so the symbols will not be used
    by the compiler: they are just added so the abi is in place which
    enables backporting later. Currently we cannot add correct declarations
    that only declare the specific symbols we provide: the OpenMP pragma
    mechanism would declare both AdvSIMD and SVE variants.
    
    2019-07-15  Steve Ellcey  <sellcey@marvell.com>
    	    Szabolcs Nagy  <szabolcs.nagy@arm.com>
    
    	* sysdeps/aarch64/configure.ac (build_mathvec): Enable.
    	(test-mathvec): Enable if ABI is supported.
    	* sysdeps/aarch64/configure: Regenerate.
    	* sysdeps/aarch64/fpu/Makefile
    	(libmvec-support): Add libmvec_double_vlen2_exp,
    	libmvec_float_vlen4_expf to list.
    	(libmvec_nonshared.a): Use make-dummy-lib.
    	(libmvec-tests): Add double-vlen2, float-vlen4 to list.
    	(double-vlen2-funcs): Add new vector function name.
    	(float-vlen4-funcs): Add new vector function name.
    	* sysdeps/aarch64/fpu/Versions: New file.
    	* sysdeps/aarch64/fpu/libmvec_double_vlen2.h: New file.
    	* sysdeps/aarch64/fpu/libmvec_double_vlen2_exp.S: New file.
    	* sysdeps/aarch64/fpu/libmvec_float_vlen4.h: New file.
    	* sysdeps/aarch64/fpu/libmvec_float_vlen4_expf.S: New file.
    	* sysdeps/aarch64/fpu/test-double-vlen2-wrappers.c: New file.
    	* sysdeps/aarch64/fpu/test-float-vlen4-wrappers.c: New file.
    	* sysdeps/aarch64/libm-test-ulps (exp_vlen2): New entry.
    	(exp_vlen4): Likewise.
    	* sysdeps/unix/sysv/linux/aarch64/libmvec.abilist: New file.

---
+++ sysdeps/aarch64/fpu/libmvec_double_vlen2.h
@@ -0,0 +1,59 @@
+/* Double-precision 2 element vector function template.
+   Copyright (C) 2019 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <sysdep.h>
+
+ENTRY (VECTOR_FUNCTION)
+	stp	x29, x30, [sp, -288]!
+	cfi_adjust_cfa_offset (288)
+	cfi_rel_offset (x29, 0)
+	cfi_rel_offset (x30, 8)
+	mov	x29, sp
+	stp	 q8,  q9, [sp, 16]
+	stp	q10, q11, [sp, 48]
+	stp	q12, q13, [sp, 80]
+	stp	q14, q15, [sp, 112]
+	stp	q16, q17, [sp, 144]
+	stp	q18, q19, [sp, 176]
+	stp	q20, q21, [sp, 208]
+	stp	q22, q23, [sp, 240]
+
+	// Use per lane load/store to avoid endianness issues.
+	str	q0, [sp, 272]
+	ldr	d0, [sp, 272]
+	bl SCALAR_FUNCTION
+	str	d0, [sp, 272]
+	ldr	d0, [sp, 280]
+	bl SCALAR_FUNCTION
+	str	d0, [sp, 280]
+	ldr	q0, [sp, 272]
+
+	ldp	q8, q9, [sp, 16]
+	ldp	q10, q11, [sp, 48]
+	ldp	q12, q13, [sp, 80]
+	ldp	q14, q15, [sp, 112]
+	ldp	q16, q17, [sp, 144]
+	ldp	q18, q19, [sp, 176]
+	ldp	q20, q21, [sp, 208]
+	ldp	q22, q23, [sp, 240]
+	ldp	x29, x30, [sp], 288
+	cfi_adjust_cfa_offset (288)
+	cfi_restore (x29)
+	cfi_restore (x30)
+	ret
+END (VECTOR_FUNCTION)
