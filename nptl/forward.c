eaad14b56aa0d18b3b6bbb1618de2ab5b242d434
Florian Weimer <fweimer@redhat.com>
2019-10-04 17:36:55 +0200

    nptl: Move pthread_attr_setinheritsched implementation into libc.
    This is part of the libpthread removal project:
    
      <https://sourceware.org/ml/libc-alpha/2019-10/msg00080.html>

--- nptl/forward.c
+++ nptl/forward.c
@@ -56,9 +56,6 @@ name decl								      \
   FORWARD2 (name, int, decl, params, return defretval)
 
 
-FORWARD (pthread_attr_setinheritsched, (pthread_attr_t *attr, int inherit),
-	 (attr, inherit), 0)
-
 FORWARD (pthread_attr_getschedparam,
 	 (const pthread_attr_t *attr, struct sched_param *param),
 	 (attr, param), 0)
