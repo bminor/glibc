fe75ee0ca65536352fcdaebfbf39aac9bee271f0
Andrew Eggenberger <andrew.eggenberger@gmail.com>
2019-10-29 23:19:32 -0500

    hurd: add getrandom and getentropy implementations
    * sysdeps/mach/hurd/getentropy.c: New file.
    * sysdeps/mach/hurd/getrandom.c: Likewise.

---
+++ sysdeps/mach/hurd/getrandom.c
@@ -0,0 +1,43 @@
+/* Hurdish implementation of getrandom
+   Copyright (C) 2016-2019 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sys/random.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <not-cancel.h>
+
+/* Write up to LENGTH bytes of randomness starting at BUFFER.
+   Return the number of bytes written, or -1 on error.  */
+ssize_t
+getrandom (void *buffer, size_t length, unsigned int flags)
+{
+  const char *random_source = "/dev/urandom";
+  size_t amount_read;
+  int fd;
+
+  if (flags & GRND_RANDOM)
+    random_source = "/dev/random";
+
+  fd = __open_nocancel(random_source, O_RDONLY | O_CLOEXEC);
+  if (fd == -1)
+    return -1;
+
+  amount_read = __read_nocancel(fd, buffer, length);
+  __close_nocancel_nostatus(fd);
+  return amount_read;
+}
