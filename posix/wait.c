c5cbdacb8acec54e140c879393c8c7dd658c3488
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2019-11-13 13:30:46 -0300

    Implement wait in terms of waitpid
    The POSIX implementation is used as default and both BSD and Linux
    version are removed.  It simplifies the implementation for
    architectures that do not provide either __NR_waitpid or
    __NR_wait4.
    
    Checked on x86_64-linux-gnu and powerpc64le-linux-gnu.

--- posix/wait.c
+++ posix/wait.c
@@ -16,16 +16,12 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include <sys/wait.h>
-#include <errno.h>
 
 /* Wait for a child to die.  When one does, put its status in *STAT_LOC
    and return its process ID.  For errors, return (pid_t) -1.  */
 __pid_t
 __wait (int *stat_loc)
 {
-  __set_errno (ENOSYS);
-  return -1;
+  return __waitpid (WAIT_ANY, stat_loc, 0);
 }
-stub_warning (wait)
-
 weak_alias (__wait, wait)
