5d39f37b260a98cfcc5ac312817ce8de534c00b1
Gabriel F. T. Gomes <gabrielftg@linux.ibm.com>
2019-11-27 18:14:14 -0300

    ldbl-128ibm-compat: Replace http with https in new files
    Several commits to the ldbl-128ibm-compat directory added new files
    where the URL in the copyright notice pointed to an http, rather than to
    an https, address.  This happened because I copied the notices before
    commit ID 5a82c74822d3.  This trivial patch fixes this issue.

--- sysdeps/ieee754/ldbl-128ibm-compat/ieee128-error.c
+++ sysdeps/ieee754/ldbl-128ibm-compat/ieee128-error.c
@@ -14,7 +14,7 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   <https://www.gnu.org/licenses/>.  */
 
 #include <error.h>
 #include <stdarg.h>
--- sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vsprintf_chk.c
+++ sysdeps/ieee754/ldbl-128ibm-compat/ieee128-vsprintf_chk.c
@@ -14,7 +14,7 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   <https://www.gnu.org/licenses/>.  */
 
 #include <libio/libioP.h>
 
