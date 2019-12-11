171d23d7cbce7b6f175a6690e625ccf80b647d23
Stefan Liebler <stli@linux.ibm.com>
2019-12-11 15:09:27 +0100

    Adjust s_ceilf.c and s_ceill.c regarding code style.
    This patch just adjusts the generic implementation regarding code style.
    No functional change.
    
    Reviewed-by: Adhemerval Zanella <adhemerval.zanella@linaro.org>

--- sysdeps/ieee754/flt-32/s_ceilf.c
+++ sysdeps/ieee754/flt-32/s_ceilf.c
@@ -20,34 +20,46 @@
 #include <math-use-builtins.h>
 
 float
-__ceilf(float x)
+__ceilf (float x)
 {
 #if USE_CEILF_BUILTIN
   return __builtin_ceilf (x);
 #else
   /* Use generic implementation.  */
-	int32_t i0,j0;
-	uint32_t i;
+  int32_t i0, j0;
+  uint32_t i;
 
-	GET_FLOAT_WORD(i0,x);
-	j0 = ((i0>>23)&0xff)-0x7f;
-	if(j0<23) {
-	    if(j0<0) {
-		/* return 0*sign(x) if |x|<1 */
-		if(i0<0) {i0=0x80000000;}
-		else if(i0!=0) { i0=0x3f800000;}
-	    } else {
-		i = (0x007fffff)>>j0;
-		if((i0&i)==0) return x; /* x is integral */
-		if(i0>0) i0 += (0x00800000)>>j0;
-		i0 &= (~i);
-	    }
-	} else {
-	    if(__builtin_expect(j0==0x80, 0)) return x+x; /* inf or NaN */
-	    else return x;		/* x is integral */
+  GET_FLOAT_WORD (i0, x);
+  j0 = ((i0 >> 23) & 0xff) - 0x7f;
+  if (j0 < 23)
+    {
+      if (j0 < 0)
+	{
+	  /* return 0 * sign (x) if |x| < 1  */
+	  if (i0 < 0)
+	    i0 = 0x80000000;
+	  else if (i0 != 0)
+	    i0 = 0x3f800000;
 	}
-	SET_FLOAT_WORD(x,i0);
-	return x;
+      else
+	{
+	  i = (0x007fffff) >> j0;
+	  if ((i0 & i) == 0)
+	    return x;		/* x is integral  */
+	  if (i0 > 0)
+	    i0 += (0x00800000) >> j0;
+	  i0 &= (~i);
+	}
+    }
+  else
+    {
+      if (__glibc_unlikely (j0 == 0x80))
+	return x + x;		/* inf or NaN  */
+      else
+	return x;		/* x is integral  */
+    }
+  SET_FLOAT_WORD (x, i0);
+  return x;
 #endif /* ! USE_CEILF_BUILTIN  */
 }
 #ifndef __ceilf
