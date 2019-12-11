cdae973b6a7eb95b1caf1e1ecfc93de720ac6b44
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2019-12-11 11:19:41 -0300

    linux: Enable vDSO clock_gettime64 for mips
    It was added on Linux 5.4 (commit 1f66c45db3302).
    
    Reviewed-by: Siddhesh Poyarekar <siddhesh@sourceware.org>

--- sysdeps/unix/sysv/linux/mips/sysdep.h
+++ sysdeps/unix/sysv/linux/mips/sysdep.h
@@ -16,11 +16,16 @@
    License along with the GNU C Library.  If not, see
    <http://www.gnu.org/licenses/>.  */
 
+#include <sgidefs.h>
+
 #define VDSO_NAME  "LINUX_2.6"
 #define VDSO_HASH  61765110
 
 /* List of system calls which are supported as vsyscalls.  */
 #define HAVE_CLOCK_GETTIME_VSYSCALL     "__vdso_clock_gettime"
+#if _MIPS_SIM != _ABI64
+# define HAVE_CLOCK_GETTIME64_VSYSCALL  "__vdso_clock_gettime64"
+#endif
 #define HAVE_GETTIMEOFDAY_VSYSCALL      "__vdso_gettimeofday"
 #define HAVE_CLOCK_GETRES_VSYSCALL      "__vdso_clock_getres"
 
