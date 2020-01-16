1959ed4b9e9f95c991abdf31b24fb8cdf98475e2
Lukasz Majewski <lukma@denx.de>
2020-01-16 14:27:27 +0100

    y2038: hurd: Provide __clock_gettime64 function
    For Linux glibc ports the __TIMESIZE == 64 ensures proper aliasing for
    __clock_gettime64 (to __clock_gettime).
    When __TIMESIZE != 64 (like ARM32, PPC) the glibc expects separate definition
    of the __clock_gettime64.
    
    The HURD port only provides __clock_gettime, so this patch adds
    __clock_gettime64 as a tiny wrapper on it.
    Acked-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- sysdeps/mach/clock_gettime.c
+++ sysdeps/mach/clock_gettime.c
@@ -49,3 +49,17 @@ versioned_symbol (libc, __clock_gettime, clock_gettime, GLIBC_2_17);
 strong_alias (__clock_gettime, __clock_gettime_2);
 compat_symbol (libc, __clock_gettime_2, clock_gettime, GLIBC_2_2);
 #endif
+
+int
+__clock_gettime64 (clockid_t clock_id, struct __timespec64 *ts64)
+{
+  struct timespec ts;
+  int ret;
+
+  ret = __clock_gettime (clock_id, &ts);
+  if (ret == 0)
+    *ts64 = valid_timespec_to_timespec64 (ts);
+
+  return ret;
+}
+libc_hidden_def (__clock_gettime64)
