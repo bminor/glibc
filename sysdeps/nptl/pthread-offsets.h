6d0d2eb1e73345abdcfa539787416dd63bffe0ab
Andreas Schwab <schwab@suse.de>
2020-02-05 17:33:30 +0100

    nptl: update default pthread-offsets.h
    Remove unused definitions, and correct __PTHREAD_RWLOCK_FLAGS_OFFSET for
    __WORDSIZE == 64.

--- sysdeps/nptl/pthread-offsets.h
+++ sysdeps/nptl/pthread-offsets.h
@@ -1,26 +1,16 @@
 #include <bits/wordsize.h>
 
 /* These values are based on internal generic types from
-   mutex-internal.h and rwlock-internal.h.  */
+   bits/struct_mutex.h and bits/struct_rwlock.h.  */
 
 #if __WORDSIZE == 64
-# define __PTHREAD_MUTEX_NUSERS_OFFSET          12
-# define __PTHREAD_MUTEX_KIND_OFFSET            16
-# define __PTHREAD_MUTEX_SPINS_OFFSET           20
-# define __PTHREAD_MUTEX_LIST_OFFSET            24
+# define __PTHREAD_MUTEX_KIND_OFFSET		16
 #else
-# define __PTHREAD_MUTEX_NUSERS_OFFSET          16
-# define __PTHREAD_MUTEX_KIND_OFFSET            12
-# define __PTHREAD_MUTEX_SPINS_OFFSET           20
-# define __PTHREAD_MUTEX_LIST_OFFSET            20
+# define __PTHREAD_MUTEX_KIND_OFFSET		12
 #endif
 
-#if __WORDSIZE == 64
-# define __PTHREAD_RWLOCK_FLAGS_OFFSET          48
+#if __BYTE_ORDER == __BIG_ENDIAN
+# define __PTHREAD_RWLOCK_FLAGS_OFFSET		27
 #else
-# if __BYTE_ORDER == __BIG_ENDIAN
-#  define __PTHREAD_RWLOCK_FLAGS_OFFSET         27
-# else
-#  define __PTHREAD_RWLOCK_FLAGS_OFFSET         24
-# endif
+# define __PTHREAD_RWLOCK_FLAGS_OFFSET		24
 #endif
