270e33ab0b4bbdc849f03e360d0fd0def0cd460e
Alistair Francis <alistair.francis@wdc.com>
2020-02-10 10:36:21 -0800

    RISC-V: nptl: update default pthread-offsets.h
    Update the RISC-V pthread-offsets.h values to support RV32.

--- sysdeps/riscv/nptl/pthread-offsets.h
+++ sysdeps/riscv/nptl/pthread-offsets.h
@@ -1,3 +1,12 @@
-#define __PTHREAD_MUTEX_KIND_OFFSET		16
+#if __WORDSIZE == 64
+# define __PTHREAD_MUTEX_KIND_OFFSET           16
+#else
+# define __PTHREAD_MUTEX_KIND_OFFSET           12
+#endif
 
-#define __PTHREAD_RWLOCK_FLAGS_OFFSET		48
+
+#if __WORDSIZE == 64
+# define __PTHREAD_RWLOCK_FLAGS_OFFSET		48
+#else
+# define __PTHREAD_RWLOCK_FLAGS_OFFSET          24
+#endif
