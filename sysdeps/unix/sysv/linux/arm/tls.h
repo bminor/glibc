9c7a58347b37065698106561f5c50010591f5194
Andreas Schwab <schwab@suse.de>
2020-02-15 10:21:20 +0100

    arm: fix use of INTERNAL_SYSCALL_CALL
    Remove extra argument from INTERNAL_SYSCALL_CALL macro call.  Fixes
    commit bc2eb9321e ("linux: Remove INTERNAL_SYSCALL_DECL").

--- sysdeps/unix/sysv/linux/arm/tls.h
+++ sysdeps/unix/sysv/linux/arm/tls.h
@@ -32,7 +32,7 @@
    operation can cause a failure 'errno' must not be touched.  */
 # define TLS_INIT_TP(tcbp) \
   ({ long int result_var;						\
-     result_var = INTERNAL_SYSCALL_CALL (set_tls, 1, (tcbp));		\
+     result_var = INTERNAL_SYSCALL_CALL (set_tls, (tcbp));		\
      INTERNAL_SYSCALL_ERROR_P (result_var)				\
        ? "unknown error" : NULL; })
 
