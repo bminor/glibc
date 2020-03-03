11ae9a185baa1dbe2b200659d043761206fe70e3
Florian Weimer <fweimer@redhat.com>
2020-03-03 12:17:24 +0100

    Linux: exit_group syscall number is always available
    Due to the built-in tables, __NR_exit_group is always defined.
    
    Reviewed-by: Adhemerval Zanella <adhemerval.zanella@linaro.org>

--- sysdeps/unix/sysv/linux/_exit.c
+++ sysdeps/unix/sysv/linux/_exit.c
@@ -27,9 +27,7 @@ _exit (int status)
 {
   while (1)
     {
-#ifdef __NR_exit_group
       INLINE_SYSCALL (exit_group, 1, status);
-#endif
       INLINE_SYSCALL (exit, 1, status);
 
 #ifdef ABORT_INSTRUCTION
