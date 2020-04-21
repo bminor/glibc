a308615f7c067371df8bd96cb233cd0574537863
Lukasz Majewski <lukma@denx.de>
2020-04-21 19:01:56 +0200

    y2038: include: Move struct __timeval64 definition to a separate file
    The struct __timeval64's definition has been moved from ./include/time.h to
    ./include/struct___timeval64.h.
    
    This change would prevent from polluting other glibc namespaces (when
    headers are modified to support 64 bit time on architectures with
    __WORDSIZE==32).
    
    Now it is possible to just include definition of this particular structure
    when needed.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- include/time.h
+++ include/time.h
@@ -4,6 +4,7 @@
 #ifndef _ISOMAC
 # include <bits/types/struct_timeval.h>
 # include <struct___timespec64.h>
+# include <struct___timeval64.h>
 # include <bits/types/locale_t.h>
 # include <stdbool.h>
 # include <time/mktime-internal.h>
@@ -70,20 +71,6 @@ struct __itimerspec64
 {
   struct __timespec64 it_interval;
   struct __timespec64 it_value;
-};
-#endif
-
-#if __TIMESIZE == 64
-# define __timeval64 timeval
-#else
-/* The glibc Y2038-proof struct __timeval64 structure for a time value.
-   This structure is NOT supposed to be passed to the Linux kernel.
-   Instead, it shall be converted to struct __timespec64 and time shall
-   be [sg]et via clock_[sg]ettime (which are now Y2038 safe).  */
-struct __timeval64
-{
-  __time64_t tv_sec;         /* Seconds */
-  __suseconds64_t tv_usec;       /* Microseconds */
 };
 #endif
 
