8a7923b57e70ea500815d666e82156d291aa11fd
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-06-05 03:33:51 +0000

    alpha: Use builtin sqrt{f}
    The generic implementation is simplified by removing the
    'optimization' for !_IEEE_FP_INEXACT (which does not handle
    inexact neither some values).
    
    Checked on alpha-linux-gnu.

--- sysdeps/alpha/fpu/e_sqrtf.c
+++ sysdeps/alpha/fpu/e_sqrtf.c
@@ -1,13 +1,8 @@
 #include <shlib-compat.h>
 
-#define __ieee754_sqrtf __ieee754_sqrtf
 #include <sysdeps/ieee754/flt-32/e_sqrtf.c>
 
-/* Work around forgotten symbol in alphaev6 build.  */
-#if SHLIB_COMPAT (libm, GLIBC_2_15, GLIBC_2_18)
-strong_alias(__ieee754_sqrtf, __sqrtf_dummy)
-compat_symbol (libm, __sqrtf_dummy, __sqrtf_finite, GLIBC_2_15);
-#endif
 #if SHLIB_COMPAT (libm, GLIBC_2_18, GLIBC_2_31)
-compat_symbol (libm, __ieee754_sqrtf, __sqrtf_finite, GLIBC_2_18);
+strong_alias (__ieee754_sqrtf, __sqrtf_finite_2_18)
+compat_symbol (libm, __sqrtf_finite_2_18, __sqrtf_finite, GLIBC_2_18);
 #endif
