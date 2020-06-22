605338745ba15930e9acac46c32ce6cada12a52b
Sudakshina Das <sudi.das@arm.com>
2020-06-22 10:57:20 +0100

    aarch64: enable BTI at runtime
    Binaries can opt-in to using BTI via an ELF object file marking.
    The dynamic linker has to then mprotect the executable segments
    with PROT_BTI. In case of static linked executables or in case
    of the dynamic linker itself, PROT_BTI protection is done by the
    operating system.
    
    On AArch64 glibc uses PT_GNU_PROPERTY instead of PT_NOTE to check
    the properties of a binary because PT_NOTE can be unreliable with
    old linkers (old linkers just append the notes of input objects
    together and add them to the output without checking them for
    consistency which means multiple incompatible GNU property notes
    can be present in PT_NOTE).
    
    BTI property is handled in the loader even if glibc is not built
    with BTI support, so in theory user code can be BTI protected
    independently of glibc. In practice though user binaries are not
    marked with the BTI property if glibc has no support because the
    static linked libc objects (crt files, libc_nonshared.a) are
    unmarked.
    
    This patch relies on Linux userspace API that is not yet in a
    linux release but in v5.8-rc1 so scheduled to be in Linux 5.8.
    
    Co-authored-by: Szabolcs Nagy <szabolcs.nagy@arm.com>
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

---
+++ sysdeps/aarch64/dl-bti.c
@@ -0,0 +1,54 @@
+/* AArch64 BTI functions.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <unistd.h>
+#include <errno.h>
+#include <libintl.h>
+#include <ldsodefs.h>
+
+static int
+enable_bti (struct link_map *map, const char *program)
+{
+  const ElfW(Phdr) *phdr;
+  unsigned prot = PROT_READ | PROT_EXEC | PROT_BTI;
+
+  for (phdr = map->l_phdr; phdr < &map->l_phdr[map->l_phnum]; ++phdr)
+    if (phdr->p_type == PT_LOAD && (phdr->p_flags & PF_X))
+      {
+	void *start = (void *) (phdr->p_vaddr + map->l_addr);
+	size_t len = phdr->p_memsz;
+	if (__mprotect (start, len, prot) < 0)
+	  {
+	    if (program)
+	      _dl_fatal_printf ("%s: mprotect failed to turn on BTI\n",
+				map->l_name);
+	    else
+	      _dl_signal_error (errno, map->l_name, "dlopen",
+				N_("mprotect failed to turn on BTI"));
+	  }
+      }
+  return 0;
+}
+
+/* Enable BTI for L if required.  */
+
+void
+_dl_bti_check (struct link_map *l, const char *program)
+{
+  if (GLRO(dl_aarch64_cpu_features).bti && l->l_mach.bti)
+    enable_bti (l, program);
+}
