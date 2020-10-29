1f1a4847a404e26b5f729dc14a92aa94c87a6063
Lukasz Majewski <lukma@denx.de>
2020-10-29 14:19:25 +0100

    y2038: Convert cnd_timedwait to support 64 bit time
    The cnd_timedwait function has been converted to support 64 bit time.
    It was also necessary to provide Linux specific copy of it to avoid
    problems on i686-gnu (i.e. HURD) port, which is not providing
    pthread_cond_timedwait() supporting 64 bit time.
    
    Moreover, a linux specific copy of thrd_priv.h header file has been
    added as well.
    
    The cnd_timedwait is a wrapper on POSIX threads to provide C11 standard
    threads interface. It directly calls __pthread_cond_timedwait64().
    
    Build tests:
    ./src/scripts/build-many-glibcs.py glibcs
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ sysdeps/unix/sysv/linux/cnd_timedwait.c
@@ -0,0 +1,44 @@
+/* C11 threads conditional timed wait implementation - Linux variant.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+#include "thrd_priv.h"
+
+int
+__cnd_timedwait64 (cnd_t *restrict cond, mtx_t *restrict mutex,
+                   const struct __timespec64 *restrict time_point)
+{
+  int err_code = __pthread_cond_timedwait64 ((pthread_cond_t *) cond,
+                                             (pthread_mutex_t *) mutex,
+                                             time_point);
+  return thrd_err_map (err_code);
+}
+
+#if __TIMESIZE != 64
+libpthread_hidden_def (__cnd_timedwait64)
+
+int
+__cnd_timedwait (cnd_t *restrict cond, mtx_t *restrict mutex,
+                 const struct timespec *restrict time_point)
+{
+  struct __timespec64 ts64 = valid_timespec_to_timespec64 (*time_point);
+
+  return __cnd_timedwait64(cond, mutex, &ts64);
+}
+#endif
+weak_alias (__cnd_timedwait, cnd_timedwait)
