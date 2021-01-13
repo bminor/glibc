e2c0794d131db03d56db0b9cbbe397d100d6f60d
Lukasz Majewski <lukma@denx.de>
2021-01-13 17:10:21 +0100

    tst: Provide test for ppoll
    This change adds new test to assess ppoll()'s timeout related
    functionality (the struct pollfd does not provide valid fd to wait
    for - just wait for timeout).
    
    To be more specific - two use cases are checked:
    - if ppoll() times out immediately when passed struct timespec has zero
    values of tv_nsec and tv_sec.
    - if ppoll() times out after timeout specified in passed argument

---
+++ sysdeps/unix/sysv/linux/tst-ppoll.c
@@ -0,0 +1,56 @@
+/* Test for ppoll timeout
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+#include <poll.h>
+#include <errno.h>
+#include <support/check.h>
+#include <support/xtime.h>
+#include <support/timespec.h>
+#include <stdbool.h>
+
+static int test_ppoll_timeout (bool zero_tmo)
+{
+  /* We wait for half a second.  */
+  struct timespec ts;
+  xclock_gettime (CLOCK_REALTIME, &ts);
+  struct timespec timeout = make_timespec (0, zero_tmo ? 0 : TIMESPEC_HZ/2);
+  ts = timespec_add (ts, timeout);
+
+  /* Ignore fds - just wait for timeout.  */
+  struct pollfd fds = { -1, 0, 0 };
+  TEST_COMPARE (ppoll (&fds, 1, &timeout, 0), 0);
+
+  TEST_TIMESPEC_NOW_OR_AFTER (CLOCK_REALTIME, ts);
+
+  return 0;
+}
+
+static int
+do_test (void)
+{
+  /* Check if ppoll exits immediately.  */
+  test_ppoll_timeout (true);
+
+  /* Check if ppoll exits after specified timeout.  */
+  test_ppoll_timeout (false);
+
+  return 0;
+}
+
+#include <support/test-driver.c>
