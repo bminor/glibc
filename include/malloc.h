b3c08077cf6d884f45013168cd67e6e5133badf6
Szabolcs Nagy <szabolcs.nagy@arm.com>
2021-01-28 17:34:36 +0000

    malloc: Move MTAG_MMAP_FLAGS definition
    This is only used internally in malloc.c, the extern declaration
    was wrong, __mtag_mmap_flags has internal linkage.
    
    Reviewed-by: DJ Delorie <dj@redhat.com>

--- include/malloc.h
+++ include/malloc.h
@@ -16,11 +16,4 @@ typedef struct malloc_state *mstate;
 
 # endif /* !_ISOMAC */
 
-#ifdef USE_MTAG
-extern int __mtag_mmap_flags;
-#define MTAG_MMAP_FLAGS __mtag_mmap_flags
-#else
-#define MTAG_MMAP_FLAGS 0
-#endif
-
 #endif
