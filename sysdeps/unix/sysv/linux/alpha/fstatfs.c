1bfbaf7130133ae740c09e12dfdd87df26e03d39
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-02-05 20:13:58 +0000

    linux: Consolidate fstatfs implementations
    The __NR_fstatfs64 syscall is supported on all architectures but
    aarch64, mips64, riscv64, and x86_64.  And newer ABIs also uses
    the new fstatfs64 interface (where the struct size is used as
    first argument).
    
    So the default implementation now uses:
    
      1. __NR_fstatfs64 for non-LFS call and handle overflow directly
         There is no need to handle __NR_fstatfs since all architectures
         that only support are LFS only.
    
      2. __NR_fstatfs if defined or __NR_fstatfs64 otherwise for LFS
         call.
    
    Alpha is the only outlier, it is a 64-bit architecture which
    provides non-LFS interface and only provides __NR_fstatfs64 on
    newer kernels (5.1+).
    
    Checked on x86_64-linux-gnu.
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>

--- sysdeps/unix/sysv/linux/alpha/fstatfs.c
+++ sysdeps/unix/sysv/linux/alpha/fstatfs.c
@@ -1,6 +1,6 @@
-/* Copyright (C) 2011-2021 Free Software Foundation, Inc.
+/* Get filesystem statistics.  Linux/alpha.
+   Copyright (C) 2011-2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Chris Metcalf <cmetcalf@tilera.com>, 2011.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -18,18 +18,13 @@
 
 #include <sys/statfs.h>
 #include <kernel_stat.h>
-#include <stddef.h>
 #include <sysdep.h>
-
-#if !STATFS_IS_STATFS64
-#include "overflow.h"
 
 /* Return information about the filesystem on which FD resides.  */
 int
 __fstatfs (int fd, struct statfs *buf)
 {
-  int rc = INLINE_SYSCALL (fstatfs64, 3, fd, sizeof (*buf), buf);
-  return rc ?: statfs_overflow (buf);
+  return INLINE_SYSCALL_CALL (fstatfs, fd, buf);
 }
+libc_hidden_def (__fstatfs)
 weak_alias (__fstatfs, fstatfs)
-#endif
--- sysdeps/unix/sysv/linux/fstatfs64.c
+++ sysdeps/unix/sysv/linux/fstatfs64.c
@@ -16,77 +16,28 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <string.h>
-#include <stddef.h>
+#define __fstatfs __fstatfs_disable
+#define fstatfs fstatfs_disable
+#include <sys/statfs.h>
 #include <sysdep.h>
 #include <kernel_stat.h>
-
-/* Hide the prototypes for __fstatfs and fstatfs so that GCC will not
-   complain about the different function signatures if they are aliased
-   to  __fstat64.  If STATFS_IS_STATFS64 is not zero then the statfs and
-   statfs64 structures have an identical layout but different type names.  */
-
-#if STATFS_IS_STATFS64
-# define __fstatfs __fstatfs_disable
-# define fstatfs fstatfs_disable
-#endif
-#include <sys/statfs.h>
-
-#include <kernel-features.h>
-
-/* Defined in statfs64.c.  */
-extern int __no_statfs64 attribute_hidden;
+#undef __fstatfs
+#undef fstatfs
 
 /* Return information about the filesystem on which FD resides.  */
 int
 __fstatfs64 (int fd, struct statfs64 *buf)
 {
 #ifdef __NR_fstatfs64
-# if __ASSUME_STATFS64 == 0
-  if (! __no_statfs64)
-# endif
-    {
-      int result = INLINE_SYSCALL (fstatfs64, 3, fd, sizeof (*buf), buf);
-
-# if __ASSUME_STATFS64 == 0
-      if (result == 0 || errno != ENOSYS)
-# endif
-	return result;
-
-# if __ASSUME_STATFS64 == 0
-      __no_statfs64 = 1;
-# endif
-    }
-#endif
-
-#if __ASSUME_STATFS64 == 0
-  struct statfs buf32;
-
-  if (__fstatfs (fd, &buf32) < 0)
-    return -1;
-
-  buf->f_type = buf32.f_type;
-  buf->f_bsize = buf32.f_bsize;
-  buf->f_blocks = buf32.f_blocks;
-  buf->f_bfree = buf32.f_bfree;
-  buf->f_bavail = buf32.f_bavail;
-  buf->f_files = buf32.f_files;
-  buf->f_ffree = buf32.f_ffree;
-  buf->f_fsid = buf32.f_fsid;
-  buf->f_namelen = buf32.f_namelen;
-  buf->f_frsize = buf32.f_frsize;
-  memcpy (buf->f_spare, buf32.f_spare, sizeof (buf32.f_spare));
-
-  return 0;
+  return INLINE_SYSCALL_CALL (fstatfs64, fd, sizeof (*buf), buf);
+#else
+  return INLINE_SYSCALL_CALL (fstatfs, fd, buf);
 #endif
 }
 weak_alias (__fstatfs64, fstatfs64)
 
-#undef __fstatfs
-#undef fstatfs
-
 #if STATFS_IS_STATFS64
 weak_alias (__fstatfs64, __fstatfs)
 weak_alias (__fstatfs64, fstatfs)
+libc_hidden_ver (__fstatfs64, __fstatfs)
 #endif
