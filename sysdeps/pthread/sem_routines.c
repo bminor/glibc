e99156816d5033ca9421c80f55c5c8e7fcd5082b
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-02-09 14:40:06 -0300

    linux: Fix __sem_check_add_mapping name length
    Take in consideration the trailling NULL since sem_search uses
    strcmp to compare entries.
    
    Checked on x86_64-linux-gnu and powerpc-linux-gnu (where it triggered
    a nptl/tst-sem7 regression).

--- sysdeps/pthread/sem_routines.c
+++ sysdeps/pthread/sem_routines.c
@@ -72,6 +72,7 @@ __sem_check_add_mapping (const char *name, int fd, sem_t *existing)
   size_t namelen = strlen (name);
   if (namelen > NAME_MAX)
     return SEM_FAILED;
+  namelen += 1;
 
   sem_t *result = SEM_FAILED;
 
