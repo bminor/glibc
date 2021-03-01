fd19b8464042084e461dec9a7ce97cd33d8bb4e0
Florian Weimer <fweimer@redhat.com>
2021-03-01 15:56:36 +0100

    i386: Implement backtrace on top of <unwind-link.h>
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>

--- sysdeps/i386/backtrace.c
+++ sysdeps/i386/backtrace.c
@@ -17,51 +17,17 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <libc-lock.h>
-#include <dlfcn.h>
 #include <execinfo.h>
 #include <stdlib.h>
-#include <unwind.h>
+#include <unwind-link.h>
 
 struct trace_arg
 {
   void **array;
+  struct unwind_link *unwind_link;
   int cnt, size;
   void *lastebp, *lastesp;
 };
-
-#ifdef SHARED
-static _Unwind_Reason_Code (*unwind_backtrace) (_Unwind_Trace_Fn, void *);
-static _Unwind_Ptr (*unwind_getip) (struct _Unwind_Context *);
-static _Unwind_Ptr (*unwind_getcfa) (struct _Unwind_Context *);
-static _Unwind_Ptr (*unwind_getgr) (struct _Unwind_Context *, int);
-static void *libgcc_handle;
-
-static void
-init (void)
-{
-  libgcc_handle = __libc_dlopen ("libgcc_s.so.1");
-
-  if (libgcc_handle == NULL)
-    return;
-
-  unwind_backtrace = __libc_dlsym (libgcc_handle, "_Unwind_Backtrace");
-  unwind_getip = __libc_dlsym (libgcc_handle, "_Unwind_GetIP");
-  unwind_getcfa = __libc_dlsym (libgcc_handle, "_Unwind_GetCFA");
-  unwind_getgr = __libc_dlsym (libgcc_handle, "_Unwind_GetGR");
-  if (unwind_getip == NULL || unwind_getgr == NULL || unwind_getcfa == NULL)
-    {
-      unwind_backtrace = NULL;
-      __libc_dlclose (libgcc_handle);
-      libgcc_handle = NULL;
-    }
-}
-#else
-# define unwind_backtrace _Unwind_Backtrace
-# define unwind_getip _Unwind_GetIP
-# define unwind_getcfa _Unwind_GetCFA
-# define unwind_getgr _Unwind_GetGR
-#endif
 
 static _Unwind_Reason_Code
 backtrace_helper (struct _Unwind_Context *ctx, void *a)
@@ -71,13 +37,16 @@ backtrace_helper (struct _Unwind_Context *ctx, void *a)
   /* We are first called with address in the __backtrace function.
      Skip it.  */
   if (arg->cnt != -1)
-    arg->array[arg->cnt] = (void *) unwind_getip (ctx);
+    arg->array[arg->cnt]
+      = (void *) UNWIND_LINK_PTR (arg->unwind_link, _Unwind_GetIP) (ctx);
   if (++arg->cnt == arg->size)
     return _URC_END_OF_STACK;
 
   /* %ebp is DWARF2 register 5 on IA-32.  */
-  arg->lastebp = (void *) unwind_getgr (ctx, 5);
-  arg->lastesp = (void *) unwind_getcfa (ctx);
+  arg->lastebp
+    = (void *) UNWIND_LINK_PTR (arg->unwind_link, _Unwind_GetGR) (ctx, 5);
+  arg->lastesp
+    = (void *) UNWIND_LINK_PTR (arg->unwind_link, _Unwind_GetCFA) (ctx);
   return _URC_NO_REASON;
 }
 
@@ -111,20 +80,19 @@ struct layout
 int
 __backtrace (void **array, int size)
 {
-  struct trace_arg arg = { .array = array, .size = size, .cnt = -1 };
+  struct trace_arg arg =
+    {
+     .array = array,
+     .unwind_link = __libc_unwind_link_get (),
+     .size = size,
+     .cnt = -1,
+    };
 
-  if (size <= 0)
+  if (size <= 0 || arg.unwind_link == NULL)
     return 0;
 
-#ifdef SHARED
-  __libc_once_define (static, once);
-
-  __libc_once (once, init);
-  if (unwind_backtrace == NULL)
-    return 0;
-#endif
-
-  unwind_backtrace (backtrace_helper, &arg);
+  UNWIND_LINK_PTR (arg.unwind_link, _Unwind_Backtrace)
+    (backtrace_helper, &arg);
 
   if (arg.cnt > 1 && arg.array[arg.cnt - 1] == NULL)
     --arg.cnt;
@@ -147,17 +115,3 @@ __backtrace (void **array, int size)
 }
 weak_alias (__backtrace, backtrace)
 libc_hidden_def (__backtrace)
-
-
-#ifdef SHARED
-/* Free all resources if necessary.  */
-libc_freeres_fn (free_mem)
-{
-  unwind_backtrace = NULL;
-  if (libgcc_handle != NULL)
-    {
-      __libc_dlclose (libgcc_handle);
-      libgcc_handle = NULL;
-    }
-}
-#endif
