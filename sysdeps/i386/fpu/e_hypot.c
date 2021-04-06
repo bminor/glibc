a1d3c9b64266d84736ba8eac96aa864165905b79
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-04-06 12:32:06 -0300

    i386: Move hypot implementation to C
    The generic hypotf is slight slower, mostly due the tricks the assembly
    does to optimize the isinf/isnan/issignaling.  The generic hypot is way
    slower, since the optimized implementation uses the i386 default
    excessive precision to issue the operation directly.  A similar
    implementation is provided instead of using the generic implementation:
    
    Checked on i686-linux-gnu.

---
+++ sysdeps/i386/fpu/e_hypot.c
@@ -0,0 +1,48 @@
+/* Euclidean distance function.  Double/Binary64 i386 version.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <math.h>
+#include <math_private.h>
+#include <math-narrow-eval.h>
+#include <math-underflow.h>
+#include <math-svid-compat.h>
+#include <libm-alias-finite.h>
+#include <libm-alias-double.h>
+#include <errno.h>
+
+/* The i386 allows to use the default excess of precision to optimize the
+   hypot implementation, since internal multiplication and sqrt is carried
+   with 80-bit FP type.  */
+double
+__ieee754_hypot (double x, double y)
+{
+  if (!isfinite (x) || !isfinite (y))
+    {
+      if ((isinf (x) || isinf (y))
+	  && !issignaling (x) && !issignaling (y))
+	return INFINITY;
+      return x + y;
+    }
+
+  long double lx = x;
+  long double ly = y;
+  double r = math_narrow_eval ((double) sqrtl (lx * lx + ly * ly));
+  math_check_force_underflow_nonneg (r);
+  return r;
+}
+libm_alias_finite (__ieee754_hypot, __hypot)
