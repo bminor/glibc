fd42022a10f2e54cbd1e51b561097105d8ef51c5
Florian Weimer <fweimer@redhat.com>
2021-04-23 09:44:49 +0200

    nptl: Move pthread_mutexattr_destroy into libc
    And __pthread_mutexattr_destroy as a compat symbol (so no
    GLIBC_2.34 symbol version is added for it).
    
    The symbols were moved using scripts/move-symbol-to-libc.py.

--- nptl/pthread_mutexattr_destroy.c
+++ nptl/pthread_mutexattr_destroy.c
@@ -17,11 +17,18 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include <pthreadP.h>
-
+#include <shlib-compat.h>
 
 int
-__pthread_mutexattr_destroy (pthread_mutexattr_t *attr)
+___pthread_mutexattr_destroy (pthread_mutexattr_t *attr)
 {
   return 0;
 }
-strong_alias (__pthread_mutexattr_destroy, pthread_mutexattr_destroy)
+versioned_symbol (libc, ___pthread_mutexattr_destroy,
+                  pthread_mutexattr_destroy, GLIBC_2_34);
+#if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_0, GLIBC_2_34)
+compat_symbol (libc, ___pthread_mutexattr_destroy,
+               pthread_mutexattr_destroy, GLIBC_2_0);
+compat_symbol (libc, ___pthread_mutexattr_destroy,
+               __pthread_mutexattr_destroy, GLIBC_2_0);
+#endif
