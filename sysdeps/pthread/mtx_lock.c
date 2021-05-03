b7863c732e387032a49b21e9800e1755beaf5e10
Florian Weimer <fweimer@redhat.com>
2021-05-03 08:12:11 +0200

    nptl: Move mtx_lock into libc
    The symbol was moved using scripts/move-symbol-to-libc.py.
    
    __pthread_mutex_lock@GLIBC_2.34 is not removed in this commit
    because it is still used from nptl/nptl-init.c.
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Tested-by: Carlos O'Donell <carlos@redhat.com>

--- sysdeps/pthread/mtx_lock.c
+++ sysdeps/pthread/mtx_lock.c
@@ -17,10 +17,19 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include "thrd_priv.h"
+#include <shlib-compat.h>
 
 int
-mtx_lock (mtx_t *mutex)
+__mtx_lock (mtx_t *mutex)
 {
   int err_code = __pthread_mutex_lock ((pthread_mutex_t *) mutex);
   return thrd_err_map (err_code);
 }
+#if PTHREAD_IN_LIBC
+versioned_symbol (libc, __mtx_lock, mtx_lock, GLIBC_2_34);
+# if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_28, GLIBC_2_34)
+compat_symbol (libpthread, __mtx_lock, mtx_lock, GLIBC_2_28);
+# endif
+#else /* !PTHREAD_IN_LIBC */
+strong_alias (__mtx_lock, mtx_lock)
+#endif
