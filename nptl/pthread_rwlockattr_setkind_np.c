b444409479e34c80f27742e7017ea5741fe548d7
Florian Weimer <fweimer@redhat.com>
2021-05-03 09:10:57 +0200

    nptl: Move pthread_rwlockattr_setkind_np into libc
    The symbol was moved using scripts/move-symbol-to-libc.py.
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Tested-by: Carlos O'Donell <carlos@redhat.com>

--- nptl/pthread_rwlockattr_setkind_np.c
+++ nptl/pthread_rwlockattr_setkind_np.c
@@ -18,10 +18,10 @@
 
 #include <errno.h>
 #include "pthreadP.h"
-
+#include <shlib-compat.h>
 
 int
-pthread_rwlockattr_setkind_np (pthread_rwlockattr_t *attr, int pref)
+__pthread_rwlockattr_setkind_np (pthread_rwlockattr_t *attr, int pref)
 {
   struct pthread_rwlockattr *iattr;
 
@@ -36,3 +36,10 @@ pthread_rwlockattr_setkind_np (pthread_rwlockattr_t *attr, int pref)
 
   return 0;
 }
+versioned_symbol (libc, __pthread_rwlockattr_setkind_np,
+                  pthread_rwlockattr_setkind_np, GLIBC_2_34);
+
+#if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_1, GLIBC_2_34)
+compat_symbol (libpthread, __pthread_rwlockattr_setkind_np,
+               pthread_rwlockattr_setkind_np, GLIBC_2_1);
+#endif
