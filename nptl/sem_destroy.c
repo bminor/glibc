4b729cca87ea2f14bcb526588f992cdd931ef362
Florian Weimer <fweimer@redhat.com>
2021-05-05 17:15:57 +0200

    nptl: Move sem_destroy into libc
    The symbol was moved using scripts/move-symbol-to-libc.py.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- nptl/sem_destroy.c
+++ nptl/sem_destroy.c
@@ -29,8 +29,11 @@ __new_sem_destroy (sem_t *sem)
   /* Nothing to do.  */
   return 0;
 }
-versioned_symbol (libpthread, __new_sem_destroy, sem_destroy, GLIBC_2_1);
-#if SHLIB_COMPAT(libpthread, GLIBC_2_0, GLIBC_2_1)
+versioned_symbol (libc, __new_sem_destroy, sem_destroy, GLIBC_2_34);
+#if OTHER_SHLIB_COMPAT(libpthread, GLIBC_2_0, GLIBC_2_1)
 strong_alias (__new_sem_destroy, __old_sem_destroy)
 compat_symbol (libpthread, __old_sem_destroy, sem_destroy, GLIBC_2_0);
+#endif
+#if OTHER_SHLIB_COMPAT(libpthread, GLIBC_2_1, GLIBC_2_34)
+compat_symbol (libpthread, __new_sem_destroy, sem_destroy, GLIBC_2_1);
 #endif
