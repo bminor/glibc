736c57c96cd1fd79905cf0f224ca6e3864eb71b4
Florian Weimer <fweimer@redhat.com>
2021-05-11 19:32:43 +0200

    nptl: Move pthread_attr_setstackaddr into libc
    The symbol was moved using scripts/move-symbol-to-libc.py.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- nptl/pthread_attr_setstackaddr.c
+++ nptl/pthread_attr_setstackaddr.c
@@ -18,7 +18,7 @@
 
 #include <errno.h>
 #include "pthreadP.h"
-
+#include <shlib-compat.h>
 
 int
 __pthread_attr_setstackaddr (pthread_attr_t *attr, void *stackaddr)
@@ -36,7 +36,12 @@ __pthread_attr_setstackaddr (pthread_attr_t *attr, void *stackaddr)
 
   return 0;
 }
-strong_alias (__pthread_attr_setstackaddr, pthread_attr_setstackaddr)
+versioned_symbol (libc, __pthread_attr_setstackaddr,
+                  pthread_attr_setstackaddr, GLIBC_2_34);
+#if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_1, GLIBC_2_34)
+compat_symbol (libpthread, __pthread_attr_setstackaddr,
+               pthread_attr_setstackaddr, GLIBC_2_1);
+#endif
 
 link_warning (pthread_attr_setstackaddr,
               "the use of `pthread_attr_setstackaddr' is deprecated, use `pthread_attr_setstack'")
