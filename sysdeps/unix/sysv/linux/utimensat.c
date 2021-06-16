b286eca5d4117b3e17c939e3df56e132ae623df1
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-06-16 10:43:57 -0300

    linux: Only use 64-bit syscall if required for utimensat family
    For !__ASSUME_TIME64_SYSCALLS there is no need to issue a 64-bit syscall
    if the provided timeout fits in a 32-bit one.  The 64-bit usage should
    be rare since the timeout is a relative one.
    
    The large timeout are already tests by io/tst-utimensat-skeleton.c.
    
    Checked on i686-linux-gnu on a 4.15 kernel and on a 5.11 kernel
    (with and without --enable-kernel=5.1) and on x86_64-linux-gnu.
    
    Reviewed-by: Lukasz Majewski <lukma@denx.de>

--- sysdeps/unix/sysv/linux/utimensat.c
+++ sysdeps/unix/sysv/linux/utimensat.c
@@ -31,34 +31,39 @@ __utimensat64_helper (int fd, const char *file,
 #ifndef __NR_utimensat_time64
 # define __NR_utimensat_time64 __NR_utimensat
 #endif
-  int ret = INLINE_SYSCALL_CALL (utimensat_time64, fd, file, &tsp64[0], flags);
-#ifndef __ASSUME_TIME64_SYSCALLS
-  if (ret == 0 || errno != ENOSYS)
-    return ret;
 
+#ifdef __ASSUME_TIME64_SYSCALLS
+  return INLINE_SYSCALL_CALL (utimensat_time64, fd, file, &tsp64[0], flags);
+#else
   /* For UTIME_NOW and UTIME_OMIT the value of tv_sec field is ignored.  */
-# define TS_VALID(ns) \
-  ((((ns).tv_nsec == UTIME_NOW || (ns).tv_nsec == UTIME_OMIT) \
-   || in_time_t_range ((ns).tv_sec)))
+# define TS_SPECIAL(ts) \
+  ((ts).tv_nsec == UTIME_NOW || (ts).tv_nsec == UTIME_OMIT)
 
-  if (tsp64 != NULL
-      && (!TS_VALID (tsp64[0]) || !TS_VALID (tsp64[1])))
+  bool need_time64 = tsp64 != NULL
+		     && ((!TS_SPECIAL (tsp64[0])
+			  && !in_time_t_range (tsp64[0].tv_sec))
+			 || (!TS_SPECIAL (tsp64[1])
+			     && !in_time_t_range (tsp64[1].tv_sec)));
+  if (need_time64)
     {
+      int r = INLINE_SYSCALL_CALL (utimensat_time64, fd, file, &tsp64[0],
+				   flags);
+      if (r == 0 || errno != ENOSYS)
+	return r;
       __set_errno (EOVERFLOW);
       return -1;
     }
 
-  struct timespec tsp32[2];
+  struct timespec tsp32[2], *ptsp32 = NULL;
   if (tsp64)
     {
       tsp32[0] = valid_timespec64_to_timespec (tsp64[0]);
       tsp32[1] = valid_timespec64_to_timespec (tsp64[1]);
+      ptsp32 = tsp32;
     }
 
-  ret = INLINE_SYSCALL_CALL (utimensat, fd, file, tsp64 ? &tsp32[0] : NULL,
-			     flags);
+  return INLINE_SYSCALL_CALL (utimensat, fd, file, ptsp32, flags);
 #endif
-  return ret;
 }
 libc_hidden_def (__utimensat64_helper)
 
