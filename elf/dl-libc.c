58b90461ae100c95e0bc53d29d2187bad290ecaf
Florian Weimer <fweimer@redhat.com>
2021-06-17 15:06:43 +0200

    elf: Use _dl_catch_error from base namespace in dl-libc.c [BZ #27646]
    dlerrror_run in elf/dl-libc.c needs to call GLRO (dl_catch_error)
    from the base namespace, just like the exported dlerror
    implementation.
    
    Fixes commit b2964eb1d9a6b8ab1250e8a881cf406182da5875 ("dlfcn:
    Failures after dlmopen should not terminate process [BZ #24772]").
    
    Backport notes: GLRO (dl_catch_error) is replaced with
    _dl_catch_error_ptr to preserve _rtld_global_ro layout.
    
    Reviewed-by: Siddhesh Poyarekar <siddhesh@sourceware.org>
    (cherry picked from commit c2059edce20c124d1a99f1a94cc52e83b77a917a)

--- elf/dl-libc.c
+++ elf/dl-libc.c
@@ -43,9 +43,15 @@ dlerror_run (void (*operate) (void *), void *args)
   const char *last_errstring = NULL;
   bool malloced;
 
-  int result = (_dl_catch_error (&objname, &last_errstring, &malloced,
-				operate, args)
+#ifdef SHARED
+  int result = (_dl_catch_error_ptr (&objname, &last_errstring, &malloced,
+				     operate, args)
 		?: last_errstring != NULL);
+#else
+  int result = (_dl_catch_error (&objname, &last_errstring, &malloced,
+				 operate, args)
+		?: last_errstring != NULL);
+#endif
 
   if (result && malloced)
     free ((char *) last_errstring);
