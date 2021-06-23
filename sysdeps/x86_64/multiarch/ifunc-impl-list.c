25999674499fc0b38a9c7f97a0f58f3c1df7fb2d
Noah Goldstein <goldstein.w.n@gmail.com>
2021-06-23 01:19:34 -0400

    x86-64: Add wcslen optimize for sse4.1
    No bug. This comment adds the ifunc / build infrastructure
    necessary for wcslen to prefer the sse4.1 implementation
    in strlen-vec.S. test-wcslen.c is passing.
    
    Signed-off-by: Noah Goldstein <goldstein.w.n@gmail.com>
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>
    (cherry picked from commit 6f573a27b6c8b4236445810a44660612323f5a73)

--- sysdeps/x86_64/multiarch/ifunc-impl-list.c
+++ sysdeps/x86_64/multiarch/ifunc-impl-list.c
@@ -657,6 +657,9 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 			       && HAS_ARCH_FEATURE (AVX512BW_Usable)
 			       && HAS_CPU_FEATURE (BMI2)),
 			      __wcslen_evex)
+	      IFUNC_IMPL_ADD (array, i, wcsnlen,
+			      CPU_FEATURE_USABLE (SSE4_1),
+			      __wcsnlen_sse4_1)
 	      IFUNC_IMPL_ADD (array, i, wcslen, 1, __wcslen_sse2))
 
   /* Support sysdeps/x86_64/multiarch/wcsnlen.c.  */
