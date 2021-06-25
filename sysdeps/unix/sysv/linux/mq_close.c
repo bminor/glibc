12028b50310e83ffa0cafbca8cfffc222bfd6ac8
Florian Weimer <fweimer@redhat.com>
2021-06-25 10:30:36 +0200

    Linux: Move mq_close from librt to libc
    The symbol was moved using scripts/move-symbol-to-libc.py.
    
    Reviewed-by: Adhemerva Zanella  <adhemerval.zanella@linaro.org>

--- sysdeps/unix/sysv/linux/mq_close.c
+++ sysdeps/unix/sysv/linux/mq_close.c
@@ -18,11 +18,16 @@
 #include <errno.h>
 #include <mqueue.h>
 #include <sysdep.h>
+#include <shlib-compat.h>
 
 /* Removes the association between message queue descriptor MQDES and its
    message queue.  */
 int
-mq_close (mqd_t mqdes)
+__mq_close (mqd_t mqdes)
 {
   return INLINE_SYSCALL (close, 1, mqdes);
 }
+versioned_symbol (libc, __mq_close, mq_close, GLIBC_2_34);
+#if OTHER_SHLIB_COMPAT (librt, GLIBC_2_3_4, GLIBC_2_34)
+compat_symbol (libc, __mq_close, mq_close, GLIBC_2_3_4);
+#endif
