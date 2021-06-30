b8d11f955108dc1d444538dc31ef64c18d0142fc
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-06-30 17:33:57 -0300

    elf: Do not fail for failed dlmopen on audit modules (BZ #28061)
    The dl_main sets the LM_ID_BASE to RT_ADD just before starting to
    add load new shared objects.  The state is set to RT_CONSISTENT just
    after all objects are loaded.
    
    However if a audit modules tries to dlmopen an inexistent module,
    the _dl_open will assert that the namespace is in an inconsistent
    state.
    
    This is different than dlopen, since first it will not use
    LM_ID_BASE and second _dl_map_object_from_fd is the sole responsible
    to set and reset the r_state value.
    
    So the assert on _dl_open can not really be seen if the state is
    consistent, since _dt_main resets it.  This patch removes the assert.
    
    Checked on x86_64-linux-gnu, i686-linux-gnu, and aarch64-linux-gnu.
    
    Reviewed-by: Florian Weimer <fweimer@redhat.com>
    (cherry picked from commit 484e672ddabe0a919a692520e6ac8f2580866235)
    
    Resolved conflicts:
    	elf/Makefile
    	elf/dl-open.c

---
+++ elf/tst-auditmod20.c
@@ -0,0 +1,57 @@
+/* Check dlopen failure on audit modules.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <dlfcn.h>
+#include <link.h>
+#include <stdlib.h>
+
+unsigned int
+la_version (unsigned int v)
+{
+  return LAV_CURRENT;
+}
+
+static void
+check (void)
+{
+  {
+    void *mod = dlopen ("nonexistent.so", RTLD_NOW);
+    if (mod != NULL)
+      abort ();
+  }
+
+  {
+    void *mod = dlmopen (LM_ID_BASE, "nonexistent.so", RTLD_NOW);
+    if (mod != NULL)
+      abort ();
+  }
+}
+
+void
+la_activity (uintptr_t *cookie, unsigned int flag)
+{
+  if (flag != LA_ACT_CONSISTENT)
+    return;
+  check ();
+}
+
+void
+la_preinit (uintptr_t *cookie)
+{
+  check ();
+}
