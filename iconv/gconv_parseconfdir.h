7f784fabcb186ffaa082ed0aeed52a56b7d96cee
Siddhesh Poyarekar <siddhesh@sourceware.org>
2021-07-02 16:53:25 +0530

    iconvconfig: Use the public feof_unlocked
    Build of iconvconfig failed with CFLAGS=-Os since __feof_unlocked is
    not a public symbol.  Replace with feof_unlocked (defined to
    __feof_unlocked when IS_IN (libc)) to fix this.
    
    Reported-by: Szabolcs Nagy <szabolcs.nagy@arm.com>
    Reviewed-by: Szabolcs Nagy <szabolcs.nagy@arm.com>

--- iconv/gconv_parseconfdir.h
+++ iconv/gconv_parseconfdir.h
@@ -33,6 +33,7 @@
 # define closedir __closedir
 # define mempcpy __mempcpy
 # define lstat64 __lstat64
+# define feof_unlocked __feof_unlocked
 #endif
 
 /* Name of the file containing the module information in the directories
@@ -64,7 +65,7 @@ read_conf_file (const char *filename, const char *directory, size_t dir_len)
 
   /* Process the known entries of the file.  Comments start with `#' and
      end with the end of the line.  Empty lines are ignored.  */
-  while (!__feof_unlocked (fp))
+  while (!feof_unlocked (fp))
     {
       char *rp, *endp, *word;
       ssize_t n = __getdelim (&line, &line_len, '\n', fp);
