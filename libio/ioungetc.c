771473a8c4f44d11e8e6521a96f4e92c20984eb3
Florian Weimer <fweimer@redhat.com>
2021-07-07 18:33:52 +0200

    libio: Add hidden prototype for ungetc
    And make ungetc the primary symbol, with _IO_ungetc as an alias.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- libio/ioungetc.c
+++ libio/ioungetc.c
@@ -27,7 +27,7 @@
 #include "libioP.h"
 
 int
-_IO_ungetc (int c, FILE *fp)
+ungetc (int c, FILE *fp)
 {
   int result;
   CHECK_FILE (fp, EOF);
@@ -40,5 +40,5 @@ _IO_ungetc (int c, FILE *fp)
   _IO_release_lock (fp);
   return result;
 }
-
-weak_alias (_IO_ungetc, ungetc)
+libc_hidden_def (ungetc)
+strong_alias (ungetc, _IO_ungetc)
