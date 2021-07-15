282bbfc364e9f0c5a0b006da377af6828e2048d8
Stan Shebs <stanshebs@google.com>
2021-07-15 12:57:50 -0700

    Extend elf/unload8 to test an additional load/unload pattern

--- elf/unload8.c
+++ elf/unload8.c
@@ -27,7 +27,48 @@ main (void)
     }
 
   mod1 ();
+
+  // Additional test to detect when the fastload hash table has bad pointers to
+  // names of unloaded libraries hanging around in it.
+
+  int (*mod1b) (void) = dlsym (h, "mod1b");
+  if (mod1b == NULL)
+    {
+      puts ("dlsym failed");
+      return 1;
+    }
+
+  mod1b ();
+
   dlclose (h);
+
+  void *h2x = dlopen ("$ORIGIN/unload8mod2.so", RTLD_LAZY);
+  if (h2x == NULL)
+    {
+      puts ("dlopen unload8mod2.so failed");
+      return 1;
+    }
+
+
+  void *h2xx = dlopen ("$ORIGIN/unload8mod1x.so", RTLD_LAZY);
+  if (h2xx == NULL)
+    {
+      puts ("dlopen unload8mod1x.so failed");
+      return 1;
+    }
+
+  dlclose (h);
+  dlclose (h2x);
+  dlclose (h2xx);
+
+  void *h3xx = dlopen ("$ORIGIN/unload8mod3.so", RTLD_LAZY);
+  if (h3xx == NULL)
+    {
+      puts ("dlopen unload8mod3.so failed");
+      return 1;
+    }
+
+  dlclose (h3xx);
 
   return 0;
 }
--- elf/unload8mod1.c
+++ elf/unload8mod1.c
@@ -1,7 +1,20 @@
+#include <dlfcn.h>
+
 extern void mod2 (void);
 
 void
 mod1 (void)
 {
   mod2 ();
+}
+
+void
+mod1b (void)
+{
+  void *h = dlopen ("$ORIGIN/unload8mod3.so", RTLD_LAZY);
+  if (h == NULL)
+    {
+      puts ("dlopen unload8mod3.so failed");
+      return 1;
+    }
 }
