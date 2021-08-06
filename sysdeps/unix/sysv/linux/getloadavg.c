b5c8a3aa82f66f49b731ca5204104cee48bccfa5
Cristian Rodríguez <crrodriguez@opensuse.org>
2021-08-06 15:17:48 -0400

    Linux: implement getloadavg(3) using sysinfo(2)
    Signed-off-by: Cristian Rodríguez <crrodriguez@opensuse.org>
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- sysdeps/unix/sysv/linux/getloadavg.c
+++ sysdeps/unix/sysv/linux/getloadavg.c
@@ -1,4 +1,4 @@
-/* Get system load averages.  Linux (/proc/loadavg) version.
+/* Get system load averages.  Linux version.
    Copyright (C) 1999-2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
@@ -16,53 +16,31 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <fcntl.h>
-#include <locale.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <not-cancel.h>
+#include <array_length.h>
+#include <sys/param.h>
+#include <sys/sysinfo.h>
 
 /* Put the 1 minute, 5 minute and 15 minute load averages
    into the first NELEM elements of LOADAVG.
    Return the number written (never more than 3, but may be less than NELEM),
    or -1 if an error occurred.  */
 
+#define CLAMP(v, lo, hi) MIN (MAX (v, lo), hi)
+
+#define SYSINFO_LOADS_SCALE (1 << SI_LOAD_SHIFT)
+
 int
 getloadavg (double loadavg[], int nelem)
 {
-  int fd;
+  struct sysinfo info;
 
-  fd = __open_nocancel ("/proc/loadavg", O_RDONLY);
-  if (fd < 0)
+  if (__sysinfo (&info) != 0)
     return -1;
-  else
-    {
-      char buf[65], *p;
-      ssize_t nread;
-      int i;
 
-      nread = __read_nocancel (fd, buf, sizeof buf - 1);
-      __close_nocancel_nostatus (fd);
-      if (nread <= 0)
-	return -1;
-      buf[nread - 1] = '\0';
+  nelem = CLAMP (nelem, 0, array_length (info.loads));
 
-      if (nelem > 3)
-	nelem = 3;
-      p = buf;
-      for (i = 0; i < nelem; ++i)
-	{
-	  char *endp;
-	  loadavg[i] = __strtod_l (p, &endp, _nl_C_locobj_ptr);
-	  if (endp == p)
-	    /* This should not happen.  The format of /proc/loadavg
-	       must have changed.  Don't return with what we have,
-	       signal an error.  */
-	    return -1;
-	  p = endp;
-	}
+  for (int i = 0; i < nelem; i++)
+    loadavg[i] = (double) info.loads[i] / SYSINFO_LOADS_SCALE;
 
-      return i;
-    }
+  return nelem;
 }
