e47c117d01082139562f7ce6ed7d28c5307f95cc
Noah Goldstein <goldstein.w.n@gmail.com>
2021-09-21 18:31:49 -0500

    x86: Modify ENTRY in sysdep.h so that p2align can be specified
    No bug.
    
    This change adds a new macro ENTRY_P2ALIGN which takes a second
    argument, log2 of the desired function alignment.
    
    The old ENTRY(name) macro is just ENTRY_P2ALIGN(name, 4) so this
    doesn't affect any existing functionality.
    
    Signed-off-by: Noah Goldstein <goldstein.w.n@gmail.com>
    (cherry picked from commit fc5bd179ef3a953dff8d1655bd530d0e230ffe71)

--- sysdeps/x86/sysdep.h
+++ sysdeps/x86/sysdep.h
@@ -78,14 +78,17 @@ enum cf_protection_level
 #define ASM_SIZE_DIRECTIVE(name) .size name,.-name;
 
 /* Define an entry point visible from C.  */
-#define	ENTRY(name)							      \
+#define	ENTRY_P2ALIGN(name, alignment)					      \
   .globl C_SYMBOL_NAME(name);						      \
   .type C_SYMBOL_NAME(name),@function;					      \
-  .align ALIGNARG(4);							      \
+  .align ALIGNARG(alignment);						      \
   C_LABEL(name)								      \
   cfi_startproc;							      \
   _CET_ENDBR;								      \
   CALL_MCOUNT
+
+/* Common entry 16 byte aligns.  */
+#define ENTRY(name) ENTRY_P2ALIGN (name, 4)
 
 #undef	END
 #define END(name)							      \
