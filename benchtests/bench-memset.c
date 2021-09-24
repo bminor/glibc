5d26d12f4ab59c67399c4d5ddcad91704fb737f9
Noah Goldstein <goldstein.w.n@gmail.com>
2021-09-24 18:09:47 -0500

    benchtests: Add medium cases and increase iters in bench-memset.c
    No bug.
    
    This commit adds new medium size cases for lengths in [512, 1024). As
    well it increase the iters to INNER_LOOP_ITERS_LARGE for more reliable
    results.
    
    Signed-off-by: Noah Goldstein <goldstein.w.n@gmail.com>

--- benchtests/bench-memset.c
+++ benchtests/bench-memset.c
@@ -43,7 +43,7 @@ static void
 do_one_test (json_ctx_t *json_ctx, impl_t *impl, CHAR *s,
 	     int c __attribute ((unused)), size_t n)
 {
-  size_t i, iters = INNER_LOOP_ITERS;
+  size_t i, iters = INNER_LOOP_ITERS_LARGE;
   timing_t start, stop, cur;
 
   TIMING_NOW (start);
@@ -118,7 +118,7 @@ test_main (void)
 	  if (i & (i - 1))
 	    do_test (&json_ctx, 0, c, i);
 	}
-      for (i = 32; i < 512; i+=32)
+      for (i = 32; i < 1024; i+=32)
 	{
 	  do_test (&json_ctx, 0, c, i);
 	  do_test (&json_ctx, i, c, i);
