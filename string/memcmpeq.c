3592ccd472a47ec8be977b0ff33f841f4c175caf
Noah Goldstein <goldstein.w.n@gmail.com>
2021-10-26 19:42:59 -0500

    String: Add __memcmpeq as build target
    No bug. This commit just adds __memcmpeq as a build target so that
    implementations for __memcmpeq that are not just aliases to memcmp can
    be supported.

---
+++ string/memcmpeq.c
@@ -0,0 +1,24 @@
+/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+/* This file is intentionally left empty.  It exists so that both
+   architectures which implement __memcmpeq seperately from memcmp and
+   architectures which implement __memcmpeq by having it alias memcmp will
+   build.
+
+   The alias for __memcmpeq to memcmp for the C implementation is in
+   memcmp.c.  */
