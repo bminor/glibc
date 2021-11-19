bc801b3a40f3a9bc8f24f1d0fdc39413874c3172
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-11-19 11:06:00 -0300

    setjmp: Replace jmp_buf-macros.h with jmp_buf-macros.sym
    It requires less boilerplate code for newer ports.  The _Static_assert
    checks from internal setjmp are moved to its own internal test since
    setjmp.h is included early by multiple headers (to generate
    rtld-sizes.sym).
    
    The riscv jmp_buf-macros.h check is also redundant, it is already
    done by riscv configure.ac.
    
    Checked with a build for the affected architectures.

--- include/setjmp.h
+++ include/setjmp.h
@@ -31,39 +31,6 @@ libc_hidden_proto (__sigsetjmp)
 extern __typeof (__sigsetjmp) __sigsetjmp attribute_hidden;
 # endif
 
-/* Check jmp_buf sizes, alignments and offsets.  */
-# include <stddef.h>
-# include <jmp_buf-macros.h>
-
-# define SJSTR_HELPER(x) #x
-# define SJSTR(x) SJSTR_HELPER(x)
-
-# define TEST_SIZE(type, size) \
-  _Static_assert (sizeof (type) == size, \
-		  "size of " #type " != " \
-		  SJSTR (size))
-# define TEST_ALIGN(type, align) \
-  _Static_assert (__alignof__ (type) == align , \
-		  "align of " #type " != " \
-		  SJSTR (align))
-# define TEST_OFFSET(type, member, offset) \
-  _Static_assert (offsetof (type, member) == offset, \
-		  "offset of " #member " field of " #type " != " \
-		  SJSTR (offset))
-
-/* Check if jmp_buf have the expected sizes.  */
-TEST_SIZE (jmp_buf, JMP_BUF_SIZE);
-TEST_SIZE (sigjmp_buf, SIGJMP_BUF_SIZE);
-
-/* Check if jmp_buf have the expected alignments.  */
-TEST_ALIGN (jmp_buf, JMP_BUF_ALIGN);
-TEST_ALIGN (sigjmp_buf, SIGJMP_BUF_ALIGN);
-
-/* Check if internal fields in jmp_buf have the expected offsets.  */
-TEST_OFFSET (struct __jmp_buf_tag, __mask_was_saved,
-	     MASK_WAS_SAVED_OFFSET);
-TEST_OFFSET (struct __jmp_buf_tag, __saved_mask,
-	     SAVED_MASK_OFFSET);
 #endif
 
 #endif
---
+++ setjmp/tst-setjmp-check.c
@@ -0,0 +1,57 @@
+/* Check jmp_buf sizes, alignments and offsets.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stddef.h>
+#include <setjmp.h>
+#include <jmp_buf-macros.h>
+
+#define SJSTR_HELPER(x) #x
+#define SJSTR(x) SJSTR_HELPER(x)
+
+#define TEST_SIZE(type, size) \
+  _Static_assert (sizeof (type) == size, \
+		  "size of " #type " != " \
+		  SJSTR (size))
+#define TEST_ALIGN(type, align) \
+  _Static_assert (__alignof__ (type) == align , \
+		  "align of " #type " != " \
+		  SJSTR (align))
+#define TEST_OFFSET(type, member, offset) \
+  _Static_assert (offsetof (type, member) == offset, \
+		  "offset of " #member " field of " #type " != " \
+		  SJSTR (offset))
+
+/* Check if jmp_buf have the expected sizes.  */
+TEST_SIZE (jmp_buf, JMP_BUF_SIZE);
+TEST_SIZE (sigjmp_buf, SIGJMP_BUF_SIZE);
+
+/* Check if jmp_buf have the expected alignments.  */
+TEST_ALIGN (jmp_buf, JMP_BUF_ALIGN);
+TEST_ALIGN (sigjmp_buf, SIGJMP_BUF_ALIGN);
+
+/* Check if internal fields in jmp_buf have the expected offsets.  */
+TEST_OFFSET (struct __jmp_buf_tag, __mask_was_saved,
+	     MASK_WAS_SAVED_OFFSET);
+TEST_OFFSET (struct __jmp_buf_tag, __saved_mask,
+	     SAVED_MASK_OFFSET);
+
+int
+main (int argc, char *argv[])
+{
+  return 0;
+}
