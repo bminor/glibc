a329f68f2ef0db37cb39d6fa6de487b005feee40
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-11-19 16:58:52 -0300

    linux: Add generic ioctl implementation
    The powerpc is refactor to use the default implementation.

---
+++ sysdeps/unix/sysv/linux/ioctl.c
@@ -0,0 +1,49 @@
+/* Control device.  Linux generic implementation.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdarg.h>
+#include <sys/ioctl.h>
+#include <sysdep.h>
+#include <internal-ioctl.h>
+
+int
+__ioctl (int fd, unsigned long int request, ...)
+{
+  va_list args;
+  va_start (args, request);
+  void *arg = va_arg (args, void *);
+  va_end (args);
+
+  int r;
+  if (!__ioctl_arch (&r, fd, request, arg))
+    {
+      r = INTERNAL_SYSCALL_CALL (ioctl, fd, request, arg);
+      if (__glibc_unlikely (INTERNAL_SYSCALL_ERROR_P (r)))
+	{
+	  __set_errno (-r);
+	  return -1;
+	}
+    }
+  return r;
+}
+libc_hidden_def (__ioctl)
+weak_alias (__ioctl, ioctl)
+
+#if __TIMESIZE != 64
+strong_alias (__ioctl, __ioctl_time64)
+#endif
--- sysdeps/unix/sysv/linux/powerpc/internal-ioctl.h
+++ sysdeps/unix/sysv/linux/powerpc/internal-ioctl.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 1998-2021 Free Software Foundation, Inc.
+/* Linux internal definitions for ioctl.
+   Copyright (C) 2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -12,57 +13,36 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
+   License along with the GNU C Library.  If not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <stdarg.h>
 #include <termios.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <sysdep.h>
 
 /* The user-visible size of struct termios has changed.  Catch ioctl calls
    using the new-style struct termios, and translate them to old-style.  */
-
-int
-__ioctl (int fd, unsigned long int request, ...)
+static inline bool
+__ioctl_arch (int *r, int fd, unsigned long request, void *arg)
 {
-  void *arg;
-  va_list ap;
-  int result;
-
-  va_start (ap, request);
-  arg = va_arg (ap, void *);
-
   switch (request)
     {
     case TCGETS:
-      result = __tcgetattr (fd, (struct termios *) arg);
+      *r = __tcgetattr (fd, (struct termios *) arg);
       break;
 
     case TCSETS:
-      result = __tcsetattr (fd, TCSANOW, (struct termios *) arg);
+      *r = __tcsetattr (fd, TCSANOW, (struct termios *) arg);
       break;
 
     case TCSETSW:
-      result = __tcsetattr (fd, TCSADRAIN, (struct termios *) arg);
+      *r = __tcsetattr (fd, TCSADRAIN, (struct termios *) arg);
       break;
 
     case TCSETSF:
-      result = __tcsetattr (fd, TCSAFLUSH, (struct termios *) arg);
+      *r = __tcsetattr (fd, TCSAFLUSH, (struct termios *) arg);
       break;
 
     default:
-      result = INLINE_SYSCALL (ioctl, 3, fd, request, arg);
-      break;
+      return false;
     }
-
-  va_end (ap);
-
-  return result;
+  return true;
 }
-libc_hidden_def (__ioctl)
-weak_alias (__ioctl, ioctl)
-#if __TIMESIZE != 64
-weak_alias (__ioctl, __ioctl_time64)
-#endif
