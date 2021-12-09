1d350aa06091211863e41169729cee1bca39f72f
Florian Weimer <fweimer@redhat.com>
2021-12-09 09:49:32 +0100

    Linux: Use rseq to accelerate sched_getcpu
    Co-Authored-By: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Reviewed-by: Szabolcs Nagy <szabolcs.nagy@arm.com>

--- sysdeps/unix/sysv/linux/sched_getcpu.c
+++ sysdeps/unix/sysv/linux/sched_getcpu.c
@@ -20,8 +20,8 @@
 #include <sysdep.h>
 #include <sysdep-vdso.h>
 
-int
-sched_getcpu (void)
+static int
+vsyscall_sched_getcpu (void)
 {
   unsigned int cpu;
   int r = -1;
@@ -32,3 +32,18 @@ sched_getcpu (void)
 #endif
   return r == -1 ? r : cpu;
 }
+
+#ifdef RSEQ_SIG
+int
+sched_getcpu (void)
+{
+  int cpu_id = THREAD_GETMEM_VOLATILE (THREAD_SELF, rseq_area.cpu_id);
+  return __glibc_likely (cpu_id >= 0) ? cpu_id : vsyscall_sched_getcpu ();
+}
+#else /* RSEQ_SIG */
+int
+sched_getcpu (void)
+{
+  return vsyscall_sched_getcpu ();
+}
+#endif /* RSEQ_SIG */
