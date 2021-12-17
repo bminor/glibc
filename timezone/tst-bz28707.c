d5ba02f67dd62a63e29c29eebd6c543722aa6b5b
Hans-Peter Nilsson <hp@axis.com>
2021-12-17 21:45:54 +0100

    timezone: test-case for BZ #28707
    This test-case is the tzfile for Asuncion generated by
    tzlib-2021e as follows, using the tzlib-2021e zic: "zic -d
    DEST -r @1546300800 -L /dev/null -b slim
    SOURCE/southamerica".  Note that in its type 2 header, it
    has two entries in its "time-types" array (types), but only
    one entry in its "transition types" array (type_idxs).
    
    	* timezone/Makefile, timezone/tst-pr28707.c,
    	timezone/testdata/gen-XT5.sh: New test.
    
    Co-authored-by: Christopher Wong <Christopher.Wong@axis.com>
    (cherry picked from commit ebe899af0dc3215159a9c896ac6f35b72a18cb6e)

---
+++ timezone/tst-bz28707.c
@@ -0,0 +1,46 @@
+/* Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Test that we can use a truncated timezone-file, where the time-type
+   at index 0 is not indexed by the transition-types array (and the
+   transition-types array does not contain at least both one DST and one
+   normal time members).  */
+
+static int
+do_test (void)
+{
+  if (setenv ("TZ", "XT5", 1))
+    {
+      puts ("setenv failed.");
+      return 1;
+    }
+
+  tzset ();
+
+  return
+    /* Sanity-check that we got the right timezone-name for DST.  For
+       normal time, we're likely to get "-00" (the "unspecified" marker),
+       even though the POSIX timezone string says "-04".  Let's not test
+       that.  */
+    !(strcmp (tzname[1], "-03") == 0);
+}
+#include <support/test-driver.c>
