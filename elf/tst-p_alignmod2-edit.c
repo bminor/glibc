e4c9268d154ff11c070eba4791d33bd3a5875bca
H.J. Lu <hjl.tools@gmail.com>
2021-12-21 12:25:20 -0800

    elf: Add a test for PT_LOAD segments with p_align == 1 [BZ #28688]
    Add tst-p_alignmod2-edit to edit the copy of tst-p_alignmod-base.so to
    set p_align of the first PT_LOAD segment to 1 and verify that the shared
    library can be loaded normally.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

---
+++ elf/tst-p_alignmod2-edit.c
@@ -0,0 +1,27 @@
+/* Set p_align of the first PT_LOAD segment to 1.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include "tst-elf-edit.h"
+
+/* Set p_align to 1.  */
+
+static size_t
+update_p_align (size_t p_align __attribute__ ((unused)))
+{
+  return 1;
+}
