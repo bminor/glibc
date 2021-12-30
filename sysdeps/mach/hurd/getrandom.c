33e8e95cbd0d6724553f390142d4b11352d59dd0
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-12-30 21:25:24 +0100

    hurd: Make getrandom a stub inside the random translator
    glibc uses /dev/urandom for getrandom(), and from version 2.34 malloc
    initialization uses it. We have to detect when we are running the random
    translator itself, in which case we can't read ourself.

--- sysdeps/mach/hurd/getrandom.c
+++ sysdeps/mach/hurd/getrandom.c
@@ -21,6 +21,8 @@
 #include <unistd.h>
 #include <not-cancel.h>
 
+extern char *trivfs_server_name __attribute__((weak));
+
 /* Write up to LENGTH bytes of randomness starting at BUFFER.
    Return the number of bytes written, or -1 on error.  */
 ssize_t
@@ -30,6 +32,17 @@ __getrandom (void *buffer, size_t length, unsigned int flags)
   int open_flags = O_RDONLY | O_CLOEXEC;
   size_t amount_read;
   int fd;
+
+  if (&trivfs_server_name && trivfs_server_name
+      && trivfs_server_name[0] == 'r'
+      && trivfs_server_name[1] == 'a'
+      && trivfs_server_name[2] == 'n'
+      && trivfs_server_name[3] == 'd'
+      && trivfs_server_name[4] == 'o'
+      && trivfs_server_name[5] == 'm'
+      && trivfs_server_name[6] == '\0')
+    /* We are random, don't try to read ourselves!  */
+    return length;
 
   if (flags & GRND_RANDOM)
     random_source = "/dev/random";
