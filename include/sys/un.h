e368b12f6c16b6888dda99ba641e999b9c9643c8
Florian Weimer <fweimer@redhat.com>
2022-01-17 10:21:34 +0100

    socket: Add the __sockaddr_un_set function
    Reviewed-by: Siddhesh Poyarekar <siddhesh@sourceware.org>

--- include/sys/un.h
+++ include/sys/un.h
@@ -1,1 +1,13 @@
 #include <socket/sys/un.h>
+
+#ifndef _ISOMAC
+
+/* Set ADDR->sun_family to AF_UNIX and ADDR->sun_path to PATHNAME.
+   Return 0 on success or -1 on failure (due to overlong PATHNAME).
+   The caller should always use sizeof (struct sockaddr_un) as the
+   socket address length, disregaring the length of PATHNAME.
+   Only concrete (non-abstract) pathnames are supported.  */
+int __sockaddr_un_set (struct sockaddr_un *addr, const char *pathname)
+  attribute_hidden;
+
+#endif /* _ISOMAC */
