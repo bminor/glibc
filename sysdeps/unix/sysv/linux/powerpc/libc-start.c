1a85970f41ea1e5abe6da2298a5e8fedcea26b70
Alan Modra <amodra@gmail.com>
2022-01-24 10:25:49 +1030

    powerpc: Relocate stinfo->main
    start_addresses in sysdeps/powerpc/powerpc64/start.S is historical
    baggage that should disappear.  Until someone does that, relocating
    stinfo->main by hand is one solution to the fact that the field may be
    unrelocated at the time it is accessed.  This is similar to what is
    done for dynamic tags via the D_PTR macro.  stinfo->init and
    stinfo->fini are zero in both powerpc64/start.S and powerpc32/start.S,
    so make it a little more obvious they are unused by passing NULLs to
    LIBC_START_MAIN.  The makefile change is needed to pick up
    elf/dl-static-tls.h from dl-machine.h.
    
    Reviewed-by: Tulio Magno Quites Machado Filho <tuliom@linux.ibm.com>

--- sysdeps/unix/sysv/linux/powerpc/libc-start.c
+++ sysdeps/unix/sysv/linux/powerpc/libc-start.c
@@ -23,6 +23,10 @@
 #ifndef SHARED
 #include <hwcapinfo.h>
 #endif
+#if ENABLE_STATIC_PIE && !defined SHARED
+/* For elf_machine_load_address.  */
+#include <dl-machine.h>
+#endif
 
 /* The main work is done in the generic function.  */
 #define LIBC_START_MAIN generic_start_main
@@ -95,8 +99,15 @@ __libc_start_main_impl (int argc, char **argv,
   __tcb_parse_hwcap_and_convert_at_platform ();
 #endif
 
-  return generic_start_main (stinfo->main, argc, argv, auxvec,
-			     stinfo->init, stinfo->fini, rtld_fini,
+  void *stmain = stinfo->main;
+#if ENABLE_STATIC_PIE && !defined SHARED
+  struct link_map *map = _dl_get_dl_main_map ();
+  if (!map->l_relocated)
+    stmain = (char *) stmain + elf_machine_load_address ();
+#endif
+
+  return generic_start_main (stmain, argc, argv, auxvec,
+			     NULL, NULL, rtld_fini,
 			     stack_on_entry);
 }
 DEFINE_LIBC_START_MAIN_VERSION
