b118bce87a7c581cb2d6d7698eb582e45aed3481
Ben Woodard <woodard@redhat.com>
2022-01-24 10:46:18 -0300

    elf: Fix runtime linker auditing on aarch64 (BZ #26643)
    The rtld audit support show two problems on aarch64:
    
      1. _dl_runtime_resolve does not preserve x8, the indirect result
          location register, which might generate wrong result calls
          depending of the function signature.
    
      2. The NEON Q registers pushed onto the stack by _dl_runtime_resolve
         were twice the size of D registers extracted from the stack frame by
         _dl_runtime_profile.
    
    While 2. might result in wrong information passed on the PLT tracing,
    1. generates wrong runtime behaviour.
    
    The aarch64 rtld audit support is changed to:
    
      * Both La_aarch64_regs and La_aarch64_retval are expanded to include
        both x8 and the full sized NEON V registers, as defined by the
        ABI.
    
      * dl_runtime_profile needed to extract registers saved by
        _dl_runtime_resolve and put them into the new correctly sized
        La_aarch64_regs structure.
    
      * The LAV_CURRENT check is change to only accept new audit modules
        to avoid the undefined behavior of not save/restore x8.
    
      * Different than other architectures, audit modules older than
        LAV_CURRENT are rejected (both La_aarch64_regs and La_aarch64_retval
        changed their layout and there are no requirements to support multiple
        audit interface with the inherent aarch64 issues).
    
      * A new field is also reserved on both La_aarch64_regs and
        La_aarch64_retval to support variant pcs symbols.
    
    Similar to x86, a new La_aarch64_vector type to represent the NEON
    register is added on the La_aarch64_regs (so each type can be accessed
    directly).
    
    Since LAV_CURRENT was already bumped to support bind-now, there is
    no need to increase it again.
    
    Checked on aarch64-linux-gnu.
    
    Co-authored-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>
    Reviewed-by: Szabolcs Nagy <szabolcs.nagy@arm.com>
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Tested-by: Carlos O'Donell <carlos@redhat.com>
    (cherry picked from commit ce9a68c57c260c8417afc93972849ac9ad243ec4)
    
    Resolved conflicts:
    	NEWS
    	elf/rtld.c

---
+++ sysdeps/aarch64/tst-audit26.c
@@ -0,0 +1,37 @@
+/* Check LD_AUDIT for aarch64 ABI specifics.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <array_length.h>
+#include <string.h>
+#include <support/check.h>
+#include "tst-audit26mod.h"
+
+int
+do_test (void)
+{
+  /* Returning a large struct uses 'x8' as indirect result location.  */
+  struct large_struct r = tst_audit26_func (ARG1, ARG2, ARG3);
+
+  struct large_struct e = set_large_struct (ARG1, ARG2, ARG3);
+
+  TEST_COMPARE_BLOB (r.a, sizeof (r.a), e.a, sizeof (e.a));
+
+  return 0;
+}
+
+#include <support/test-driver.c>
---
+++ sysdeps/aarch64/tst-audit26mod.h
@@ -0,0 +1,50 @@
+/* Check LD_AUDIT for aarch64 specific ABI.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _TST_AUDIT27MOD_H
+#define _TST_AUDIT27MOD_H 1
+
+#include <array_length.h>
+
+struct large_struct
+{
+  char a[16];
+  short b[8];
+  long int c[4];
+};
+
+static inline struct large_struct
+set_large_struct (char a, short b, long int c)
+{
+  struct large_struct r;
+  for (int i = 0; i < array_length (r.a); i++)
+    r.a[i] = a;
+  for (int i = 0; i < array_length (r.b); i++)
+    r.b[i] = b;
+  for (int i = 0; i < array_length (r.c); i++)
+    r.c[i] = c;
+  return r;
+}
+
+#define ARG1 0x12
+#define ARG2 0x1234
+#define ARG3 0x12345678
+
+struct large_struct tst_audit26_func (char a, short b, long int c);
+
+#endif
