948ebc098ed3cd928ea10997f990115e7770bda3
Florian Weimer <fweimer@redhat.com>
2022-01-27 16:03:58 +0100

    Fix glibc 2.34 ABI omission (missing GLIBC_2.34 in dynamic loader)
    The glibc 2.34 release really should have added a GLIBC_2.34
    symbol to the dynamic loader. With it, we could move functions such
    as dlopen or pthread_key_create that work on process-global state
    into the dynamic loader (once we have fixed a longstanding issue
    with static linking).  Without the GLIBC_2.34 symbol, yet another
    new symbol version would be needed because old glibc will fail to
    load binaries due to the missing symbol version in ld.so that newly
    linked programs will require.
    
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>
    (cherry picked from commit af121ae3e7cd12628c91ecfc46a9d65313a6e972)

---
+++ elf/dl-compat.c
@@ -0,0 +1,32 @@
+/* Placeholder compatibility symbols.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <shlib-compat.h>
+#include <sys/cdefs.h>
+
+/* GLIBC_2.34 placeholder for future symbol moves.  */
+
+void
+attribute_compat_text_section
+__attribute_used__
+__rtld_version_placeholder_1 (void)
+{
+}
+
+compat_symbol (ld, __rtld_version_placeholder_1,
+               __rtld_version_placeholder, GLIBC_2_34);
