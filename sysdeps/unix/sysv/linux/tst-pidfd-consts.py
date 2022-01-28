97f5d19c45799e3abedef771430b5562f1b8764f
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-01-28 15:27:40 -0300

    linux: Add pidfd_open
    This was added on Linux 5.3 (32fcb426ec001cb6d5a4a195091a8486ea77e2df)
    as a way to retrieve a pid file descriptors for process that has not
    been created CLONE_PIDFD (by usual fork/clone).
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Tested-by: Carlos O'Donell <carlos@redhat.com>

---
+++ sysdeps/unix/sysv/linux/tst-pidfd-consts.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# Test that glibc's sys/pidfd.h constants match the kernel's.
+# Copyright (C) 2022 Free Software Foundation, Inc.
+# This file is part of the GNU C Library.
+#
+# The GNU C Library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# The GNU C Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with the GNU C Library; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import argparse
+import sys
+
+import glibcextract
+import glibcsyscalls
+
+
+def main():
+    """The main entry point."""
+    parser = argparse.ArgumentParser(
+        description="Test that glibc's sys/pidfd.h constants "
+        "match the kernel's.")
+    parser.add_argument('--cc', metavar='CC',
+                        help='C compiler (including options) to use')
+    args = parser.parse_args()
+
+    linux_version_headers = glibcsyscalls.linux_kernel_version(args.cc)
+    linux_version_glibc = (5, 16)
+    sys.exit(glibcextract.compare_macro_consts(
+                '#include <sys/pidfd.h>\n',
+                '#include <asm/fcntl.h>\n'
+                '#include <linux/pidfd.h>\n',
+                args.cc,
+                'PIDFD_.*',
+                None,
+                linux_version_glibc > linux_version_headers,
+                linux_version_headers > linux_version_glibc))
+
+if __name__ == '__main__':
+    main()
