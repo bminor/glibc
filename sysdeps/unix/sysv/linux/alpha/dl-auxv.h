150039ff07d19d2566c9d91b2a1368fb05451639
Florian Weimer <fweimer@redhat.com>
2022-02-03 10:58:59 +0100

    Linux: Consolidate auxiliary vector parsing
    And optimize it slightly.
    
    The large switch statement in _dl_sysdep_start can be replaced with
    a large array.  This reduces source code and binary size.  On
    i686-linux-gnu:
    
    Before:
    
       text	   data	    bss	    dec	    hex	filename
       7791	     12	      0	   7803	   1e7b	elf/dl-sysdep.os
    
    After:
    
       text	   data	    bss	    dec	    hex	filename
       7135	     12	      0	   7147	   1beb	elf/dl-sysdep.os
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>
    (cherry picked from commit 8c8510ab2790039e58995ef3a22309582413d3ff)

--- sysdeps/unix/sysv/linux/alpha/dl-auxv.h
+++ sysdeps/unix/sysv/linux/alpha/dl-auxv.h
@@ -20,16 +20,8 @@
 
 extern long __libc_alpha_cache_shape[4];
 
-#define DL_PLATFORM_AUXV				\
-      case AT_L1I_CACHESHAPE:				\
-	__libc_alpha_cache_shape[0] = av->a_un.a_val;	\
-	break;						\
-      case AT_L1D_CACHESHAPE:				\
-	__libc_alpha_cache_shape[1] = av->a_un.a_val;	\
-	break;						\
-      case AT_L2_CACHESHAPE:				\
-	__libc_alpha_cache_shape[2] = av->a_un.a_val;	\
-	break;						\
-      case AT_L3_CACHESHAPE:				\
-	__libc_alpha_cache_shape[3] = av->a_un.a_val;	\
-	break;
+#define DL_PLATFORM_AUXV					\
+  __libc_alpha_cache_shape[0] = auxv_values[AT_L1I_CACHESHAPE]; \
+  __libc_alpha_cache_shape[1] = auxv_values[AT_L1D_CACHESHAPE]; \
+  __libc_alpha_cache_shape[2] = auxv_values[AT_L2_CACHESHAPE];	\
+  __libc_alpha_cache_shape[3] = auxv_values[AT_L3_CACHESHAPE];
