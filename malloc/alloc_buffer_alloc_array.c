5ba5d609c15317cfe93f22201cd008200cab9c24
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-03-16 12:09:15 +0000

    malloc: Use uintptr_t in alloc_buffer
    The values represnt pointers and not sizes. The members of struct
    alloc_buffer are already uintptr_t.

--- malloc/alloc_buffer_alloc_array.c
+++ malloc/alloc_buffer_alloc_array.c
@@ -23,12 +23,12 @@ void *
 __libc_alloc_buffer_alloc_array (struct alloc_buffer *buf, size_t element_size,
                                  size_t align, size_t count)
 {
-  size_t current = buf->__alloc_buffer_current;
+  uintptr_t current = buf->__alloc_buffer_current;
   /* The caller asserts that align is a power of two.  */
-  size_t aligned = ALIGN_UP (current, align);
+  uintptr_t aligned = ALIGN_UP (current, align);
   size_t size;
   bool overflow = __builtin_mul_overflow (element_size, count, &size);
-  size_t new_current = aligned + size;
+  uintptr_t new_current = aligned + size;
   if (!overflow                /* Multiplication did not overflow.  */
       && aligned >= current    /* No overflow in align step.  */
       && new_current >= size   /* No overflow in size computation.  */
