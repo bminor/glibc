eca95865e37439888397d551d7465715158305b4
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-03-16 10:58:57 +0000

    aarch64: morello: string: dummy c strspn
    avoids out of bound access of the generic implementation.

---
+++ sysdeps/aarch64/morello/strspn.c
@@ -0,0 +1,40 @@
+/* Simple strspn.
+
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <string.h>
+
+size_t strspn (const char *str, const char *accept)
+{
+  if (accept[0] == 0)
+    return 0;
+  if (accept[1] == 0)
+    {
+      const char *p;
+      for (p = str; *p == accept[0]; p++);
+      return p - str;
+    }
+
+  unsigned char table[256] = {0};
+  unsigned char *us;
+  for (us = (unsigned char*) accept; *us != 0; table[*us++] = 1);
+
+  for (us = (unsigned char*) str; table[*us] != 0; us++);
+  return us - (unsigned char *) str;
+}
+libc_hidden_builtin_def (strspn)
