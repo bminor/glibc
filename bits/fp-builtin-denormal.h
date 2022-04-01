0c82fbfe68c7e1001626a7d2bd070f0e66f41849
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-04-01 16:54:55 -0300

    x86: Do not use __builtin_fpclassify on clang
    clang does not handle pseudo normal numbers, so disable the fpclassify
    and isnormal builtin optimization if clang is used.  This only affect
    x86, so add a new header, fp-builtin-denormal.h, which defines whether
    the architecture required to disable the optimization through a new
    glibc define (__FP_BUILTIN_DENORMAL).
    
    It fixes the regression on test-ldouble-fpclassify and test-float64x-fpclassify
    when built with clang:
    
    Failure: fpclassify (pseudo_zero): Exception "Invalid operation" set
    Failure: fpclassify (pseudo_inf): Exception "Invalid operation" set
    Failure: fpclassify (pseudo_qnan): Exception "Invalid operation" set
    Failure: fpclassify (pseudo_snan): Exception "Invalid operation" set
    Failure: fpclassify (pseudo_unnormal): Exception "Invalid operation" set
    Failure: fpclassify_downward (pseudo_zero): Exception "Invalid operation" set
    Failure: fpclassify_downward (pseudo_inf): Exception "Invalid operation" set
    Failure: fpclassify_downward (pseudo_qnan): Exception "Invalid operation" set
    Failure: fpclassify_downward (pseudo_snan): Exception "Invalid operation" set
    Failure: fpclassify_downward (pseudo_unnormal): Exception "Invalid operation" set
    Failure: fpclassify_towardzero (pseudo_zero): Exception "Invalid operation" set
    Failure: fpclassify_towardzero (pseudo_inf): Exception "Invalid operation" set
    Failure: fpclassify_towardzero (pseudo_qnan): Exception "Invalid operation" set
    Failure: fpclassify_towardzero (pseudo_snan): Exception "Invalid operation" set
    Failure: fpclassify_towardzero (pseudo_unnormal): Exception "Invalid operation" set
    Failure: fpclassify_upward (pseudo_zero): Exception "Invalid operation" set
    Failure: fpclassify_upward (pseudo_inf): Exception "Invalid operation" set
    Failure: fpclassify_upward (pseudo_qnan): Exception "Invalid operation" set
    Failure: fpclassify_upward (pseudo_snan): Exception "Invalid operation" set
    Failure: fpclassify_upward (pseudo_unnormal): Exception "Invalid operation" set

---
+++ bits/fp-builtin-denormal.h
@@ -0,0 +1,27 @@
+/* Define __FP_BUILTIN_DENORMAL.
+   Copyright (C) 2025 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _MATH_H
+# error "Never use <bits/fp-builtin-denormal.h> directly; include <math.h> instead."
+#endif
+
+/*  __FP_BUILTIN_DENORMAL is define to 1 if compiler supports handling
+    pseudo-normal numbers with builtins.  Pseudo-normal are defined onlyi
+    Intel double extended-precision floating point (long double), so
+    assume 1 to enable the math optimization.  */
+#define __FP_BUILTIN_DENORMAL 1
