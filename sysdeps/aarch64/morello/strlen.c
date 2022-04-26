538a80eaf442d4808b81729c21fb75f671df1600
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-04-26 08:19:02 +0100

    aarch64: morello: string: dummy c strlen

---
+++ sysdeps/aarch64/morello/strlen.c
@@ -0,0 +1,30 @@
+/* Simple strlen.
+
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If
+   not, see <https://www.gnu.org/licenses/>.  */
+
+#include <string.h>
+
+size_t __strlen (const char *s)
+{
+  size_t n;
+  for (n = 0; s[n] != 0; n++);
+  return n;
+}
+
+weak_alias (__strlen, strlen)
+libc_hidden_builtin_def (strlen)
