9638ef8594d14d1e6fa86229f335cac913348afa
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-04-26 08:18:43 +0100

    aarch64: morello: string: dummy c strchr

---
+++ sysdeps/aarch64/morello/strchr.c
@@ -0,0 +1,33 @@
+/* Simple strchr.
+
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If
+   not, see <https://www.gnu.org/licenses/>.  */
+
+#include <string.h>
+
+char *strchr (const char *s, int c)
+{
+  unsigned char uc = c;
+  const unsigned char *us = (const unsigned char *) s;
+  for (; *us != uc; us++)
+    if (*us == 0)
+      return NULL;
+  return (char *) us;
+}
+
+weak_alias (strchr, index)
+libc_hidden_builtin_def (strchr)
