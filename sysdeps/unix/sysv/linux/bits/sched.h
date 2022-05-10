04b30fe4f85d6e4c0390581a5e90db066a0b2f1f
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-05-10 13:59:48 -0300

    linux: Add CLONE_NEWTIME from Linux 5.6 to bits/sched.h
    It was added in commit 769071ac9f20b6a447410c7eaa55d1a5233ef40c.

--- sysdeps/unix/sysv/linux/bits/sched.h
+++ sysdeps/unix/sysv/linux/bits/sched.h
@@ -71,6 +71,10 @@
 # define CLONE_NEWPID	0x20000000	/* New pid namespace.  */
 # define CLONE_NEWNET	0x40000000	/* New network namespace.  */
 # define CLONE_IO	0x80000000	/* Clone I/O context.  */
+
+/* cloning flags intersect with CSIGNAL so can be used only with unshare and
+   clone3 syscalls.  */
+#define CLONE_NEWTIME	0x00000080      /* New time namespace */
 #endif
 
 #include <bits/types/struct_sched_param.h>
