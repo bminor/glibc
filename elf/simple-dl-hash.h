6fd435485f270a8c76e47bf7377c1b872885e683
Noah Goldstein <goldstein.w.n@gmail.com>
2022-05-19 17:17:59 -0500

    elf: Add tests for the dl hash funcs (_dl_new_hash and _dl_elf_hash)
    If we want to further optimize the functions tests are needed.
    Reviewed-by: Siddhesh Poyarekar <siddhesh@sourceware.org>

---
+++ elf/simple-dl-hash.h
@@ -0,0 +1,42 @@
+/* __simple_dl_elf_hash for testing true elf symbol lookup.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _SIMPLE_DL_ELF_HASH_H
+#define _SIMPLE_DL_ELF_HASH_H 1
+
+#include <stdint.h>
+
+/* For testing/benchmarking purposes.  Real implementation in
+   sysdeps/generic/dl-hash.h.  */
+static uint32_t
+__attribute__ ((unused))
+__simple_dl_elf_hash (const char *name_arg)
+{
+  unsigned long int hash = 0;
+  for (unsigned char c = *name_arg; c != '\0'; c = *(++name_arg))
+    {
+      unsigned long int hi;
+      hash = (hash << 4) + c;
+      hi = hash & 0xf0000000;
+      hash ^= hi >> 24;
+      hash &= 0x0fffffff;
+    }
+  return hash;
+}
+
+#endif /* simple-dl-hash.h */
---
+++ elf/tst-dl-hash.c
@@ -0,0 +1,115 @@
+/* Test dl-hash functions.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+
+#include <simple-dl-hash.h>
+#include <dl-hash.h>
+#include <dl-new-hash.h>
+#include <support/support.h>
+#include <support/check.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef unsigned int (*hash_f) (const char *);
+
+
+
+static int
+do_fill_test (size_t len, int fill, const char *name, hash_f testf,
+	      hash_f expecf)
+{
+  uint32_t expec, res;
+  char buf[len + 1];
+  memset (buf, fill, len);
+  buf[len] = '\0';
+
+  expec = expecf (buf);
+  res = testf (buf);
+  if (expec != res)
+    FAIL_EXIT1 ("FAIL: fill(%d) %s(%zu), %x != %x\n", fill, name, len, expec,
+		res);
+
+  return 0;
+}
+
+static int
+do_fill_tests (size_t len, int fill)
+{
+  if (do_fill_test (len, fill, "dl_new_hash", &_dl_new_hash,
+		    &__simple_dl_new_hash))
+    return 1;
+
+  return do_fill_test (len, fill, "dl_elf_hash", &_dl_elf_hash,
+		       &__simple_dl_elf_hash);
+}
+
+static int
+do_rand_test (size_t len, const char *name, hash_f testf, hash_f expecf)
+{
+  uint32_t expec, res;
+  size_t i;
+  char buf[len + 1];
+  char v;
+  for (i = 0; i < len; ++i)
+    {
+      v = random ();
+      if (v == 0)
+	v = 1;
+
+      buf[i] = v;
+    }
+  buf[len] = '\0';
+
+  expec = expecf (buf);
+  res = testf (buf);
+  if (expec != res)
+    FAIL_EXIT1 ("FAIL: random %s(%zu), %x != %x\n", name, len, expec, res);
+
+  return 0;
+}
+
+static int
+do_rand_tests (size_t len)
+{
+  if (do_rand_test (len, "dl_new_hash", &_dl_new_hash, &__simple_dl_new_hash))
+    return 1;
+
+  return do_rand_test (len, "dl_elf_hash", &_dl_elf_hash, &__simple_dl_elf_hash);
+}
+
+static int
+do_test (void)
+{
+  size_t i, j;
+  for (i = 0; i < 100; ++i)
+    {
+      for (j = 0; j < 8192; ++j)
+	{
+	  if (do_rand_tests (i))
+	    return 1;
+
+	  if (do_fill_tests (i, -1) || do_fill_tests (i, 1)
+	      || do_fill_tests (i, 0x80) || do_fill_tests (i, 0x88))
+	    return 1;
+	}
+    }
+  return 0;
+}
+
+#include <support/test-driver.c>
