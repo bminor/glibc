6488f4d00653b489e7969c0a489dc665c26514a8
Joseph Myers <joseph@codesourcery.com>
2022-05-31 13:49:53 +0000

    Add SOL_SMC from Linux 5.18 to bits/socket.h
    Linux 5.18 adds a constant SOL_SMC to the getsockopt / setsockopt
    levels; add this constant to bits/socket.h.
    
    Tested for x86_64.

--- sysdeps/unix/sysv/linux/bits/socket.h
+++ sysdeps/unix/sysv/linux/bits/socket.h
@@ -171,6 +171,7 @@ typedef __socklen_t socklen_t;
 #define SOL_XDP		283
 #define SOL_MPTCP	284
 #define SOL_MCTP	285
+#define SOL_SMC		286
 
 /* Maximum queue length specifiable by listen.  */
 #define SOMAXCONN	4096
