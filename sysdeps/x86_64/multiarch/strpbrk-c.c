4654e7fd5a129c4507257dc962540b53c7cf42ab
Noah Goldstein <goldstein.w.n@gmail.com>
2022-06-08 17:27:59 -0700

    x86: Add copyright to strpbrk-c.c

--- sysdeps/x86_64/multiarch/strpbrk-c.c
+++ sysdeps/x86_64/multiarch/strpbrk-c.c
@@ -1,3 +1,21 @@
+/* strpbrk with SSE4.2 intrinsics
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
 #define USE_AS_STRPBRK
 #define STRCSPN_SSE2 __strpbrk_sse2
 #define STRCSPN_SSE42 __strpbrk_sse42
