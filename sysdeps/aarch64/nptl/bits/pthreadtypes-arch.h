1ffce991bd8c38d39d2acc69ee91436079efef53
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-07-13 12:22:10 +0100

    aarch64: morello: nptl: fix pthread types for 128 bit pointers

--- sysdeps/aarch64/nptl/bits/pthreadtypes-arch.h
+++ sysdeps/aarch64/nptl/bits/pthreadtypes-arch.h
@@ -29,6 +29,16 @@
 # define __SIZEOF_PTHREAD_RWLOCK_T      48
 # define __SIZEOF_PTHREAD_BARRIER_T     20
 # define __SIZEOF_PTHREAD_BARRIERATTR_T  4
+# define __LOCK_ALIGNMENT
+#elif defined __CHERI_PURE_CAPABILITY__
+# define __SIZEOF_PTHREAD_ATTR_T       112
+# define __SIZEOF_PTHREAD_MUTEX_T       64
+# define __SIZEOF_PTHREAD_MUTEXATTR_T    8
+# define __SIZEOF_PTHREAD_CONDATTR_T     8
+# define __SIZEOF_PTHREAD_RWLOCK_T      56
+# define __SIZEOF_PTHREAD_BARRIER_T     32
+# define __SIZEOF_PTHREAD_BARRIERATTR_T  8
+# define __LOCK_ALIGNMENT __attribute__ ((__aligned__(16)))
 #else
 # define __SIZEOF_PTHREAD_ATTR_T        64
 # define __SIZEOF_PTHREAD_MUTEX_T       48
@@ -37,11 +47,11 @@
 # define __SIZEOF_PTHREAD_RWLOCK_T      56
 # define __SIZEOF_PTHREAD_BARRIER_T     32
 # define __SIZEOF_PTHREAD_BARRIERATTR_T  8
+# define __LOCK_ALIGNMENT
 #endif
 #define __SIZEOF_PTHREAD_COND_T         48
 #define __SIZEOF_PTHREAD_RWLOCKATTR_T	8
 
-#define __LOCK_ALIGNMENT
 #define __ONCE_ALIGNMENT
 
 #endif	/* bits/pthreadtypes.h */
