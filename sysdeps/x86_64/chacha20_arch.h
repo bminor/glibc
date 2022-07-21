84cfc6479bdabcca2e0b8099459dae1b51d3a4a3
Adhemerval Zanella Netto <adhemerval.zanella@linaro.org>
2022-07-21 10:05:04 -0300

    x86: Add AVX2 optimized chacha20
    It adds vectorized ChaCha20 implementation based on libgcrypt
    cipher/chacha20-amd64-avx2.S.  It is used only if AVX2 is supported
    and enabled by the architecture.
    
    As for generic implementation, the last step that XOR with the
    input is omited.  The final state register clearing is also
    omitted.
    
    On a Ryzen 9 5900X it shows the following improvements (using
    formatted bench-arc4random data):
    
    SSE                                        MB/s
    -----------------------------------------------
    arc4random [single-thread]               704.25
    arc4random_buf(16) [single-thread]      1018.17
    arc4random_buf(32) [single-thread]      1315.27
    arc4random_buf(48) [single-thread]      1449.36
    arc4random_buf(64) [single-thread]      1511.16
    arc4random_buf(80) [single-thread]      1539.48
    arc4random_buf(96) [single-thread]      1571.06
    arc4random_buf(112) [single-thread]     1596.16
    arc4random_buf(128) [single-thread]     1613.48
    -----------------------------------------------
    
    AVX2                                       MB/s
    -----------------------------------------------
    arc4random [single-thread]               922.61
    arc4random_buf(16) [single-thread]      1478.70
    arc4random_buf(32) [single-thread]      2241.80
    arc4random_buf(48) [single-thread]      2681.28
    arc4random_buf(64) [single-thread]      2913.43
    arc4random_buf(80) [single-thread]      3009.73
    arc4random_buf(96) [single-thread]      3141.16
    arc4random_buf(112) [single-thread]     3254.46
    arc4random_buf(128) [single-thread]     3305.02
    -----------------------------------------------
    
    Checked on x86_64-linux-gnu.

--- sysdeps/x86_64/chacha20_arch.h
+++ sysdeps/x86_64/chacha20_arch.h
@@ -16,6 +16,7 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
+#include <isa-level.h>
 #include <ldsodefs.h>
 #include <cpu-features.h>
 #include <sys/param.h>
@@ -23,16 +24,32 @@
 unsigned int __chacha20_sse2_blocks4 (uint32_t *state, uint8_t *dst,
 				      const uint8_t *src, size_t nblks)
      attribute_hidden;
+unsigned int __chacha20_avx2_blocks8 (uint32_t *state, uint8_t *dst,
+				      const uint8_t *src, size_t nblks)
+     attribute_hidden;
 
 static inline void
 chacha20_crypt (uint32_t *state, uint8_t *dst, const uint8_t *src,
 		size_t bytes)
 {
-  _Static_assert (CHACHA20_BUFSIZE % 4 == 0,
-		  "CHACHA20_BUFSIZE not multiple of 4");
-  _Static_assert (CHACHA20_BUFSIZE >= CHACHA20_BLOCK_SIZE * 4,
-		  "CHACHA20_BUFSIZE <= CHACHA20_BLOCK_SIZE * 4");
+  _Static_assert (CHACHA20_BUFSIZE % 4 == 0 && CHACHA20_BUFSIZE % 8 == 0,
+		  "CHACHA20_BUFSIZE not multiple of 4 or 8");
+  _Static_assert (CHACHA20_BUFSIZE >= CHACHA20_BLOCK_SIZE * 8,
+		  "CHACHA20_BUFSIZE < CHACHA20_BLOCK_SIZE * 8");
 
-  __chacha20_sse2_blocks4 (state, dst, src,
+#if MINIMUM_X86_ISA_LEVEL > 2
+  __chacha20_avx2_blocks8 (state, dst, src,
 			   CHACHA20_BUFSIZE / CHACHA20_BLOCK_SIZE);
+#else
+  const struct cpu_features* cpu_features = __get_cpu_features ();
+
+  /* AVX2 version uses vzeroupper, so disable it if RTM is enabled.  */
+  if (X86_ISA_CPU_FEATURE_USABLE_P (cpu_features, AVX2)
+      && X86_ISA_CPU_FEATURES_ARCH_P (cpu_features, Prefer_No_VZEROUPPER, !))
+    __chacha20_avx2_blocks8 (state, dst, src,
+			     CHACHA20_BUFSIZE / CHACHA20_BLOCK_SIZE);
+  else
+    __chacha20_sse2_blocks4 (state, dst, src,
+			     CHACHA20_BUFSIZE / CHACHA20_BLOCK_SIZE);
+#endif
 }
