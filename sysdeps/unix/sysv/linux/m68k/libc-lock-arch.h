aeb4d2e9815d459e2640a31f5abb8ef803830107
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-08-30 10:33:15 -0300

    m68k: Enforce 4-byte alignment on internal locks (BZ #29537)
    A new internal definition, __LIBC_LOCK_ALIGNMENT, is used to force
    the 4-byte alignment only for m68k, other architecture keep the
    natural alignment of the type used internally (and hppa does not
    require 16-byte alignment for kernel-assisted CAS).
    
    Reviewed-by: Florian Weimer <fweimer@redhat.com>

---
+++ sysdeps/unix/sysv/linux/m68k/libc-lock-arch.h
@@ -0,0 +1,25 @@
+/* Private libc-internal arch-specific definitions.  m68k version.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If
+   not, see <https://www.gnu.org/licenses/>.  */
+
+#ifndef _LIBC_LOCK_ARCH_H
+#define _LIBC_LOCK_ARCH_H
+
+/* Linux enforces 4-bytes alignment on futex inputs.  */
+#define __LIBC_LOCK_ALIGNMENT __attribute__ ((__aligned__ (4)))
+
+#endif
