456f4dd6e5311b020cda926e6da4f3a820516339
Szabolcs Nagy <szabolcs.nagy@arm.com>
2022-08-31 11:55:45 +0100

    TODO: cheri: elf: Add new AT_* auxv types
    Used for purecap ABI.
    
    TODO: squash into
    091bcf0e1c cheri: elf: Add new a_type entries

--- elf/elf.h
+++ elf/elf.h
@@ -1252,11 +1252,17 @@ typedef struct
 
 #define AT_MINSIGSTKSZ		51 /* Stack needed for signal delivery  */
 
-# define AT_CHERI_EXEC_RW_CAP	60
-# define AT_CHERI_EXEC_RX_CAP	61
-# define AT_CHERI_INTERP_RW_CAP	62
-# define AT_CHERI_INTERP_RX_CAP	63
-# define AT_CHERI_SEAL_CAP	64
+#define AT_CHERI_EXEC_RW_CAP	60
+#define AT_CHERI_EXEC_RX_CAP	61
+#define AT_CHERI_INTERP_RW_CAP	62
+#define AT_CHERI_INTERP_RX_CAP	63
+#define AT_CHERI_STACK_CAP	64
+#define AT_CHERI_SEAL_CAP	65
+#define AT_CHERI_CID_CAP	66
+#define AT_ARGC			80
+#define AT_ARGV			81
+#define AT_ENVC			82
+#define AT_ENVP			83
 
 /* Note section contents.  Each entry in the note section begins with
    a header of a fixed form.  */
