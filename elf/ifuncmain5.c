ba7a5e3e0f413e590104a3b26cf2f359eed4ec02
Florian Weimer <fweimer@redhat.com>
2022-11-04 18:37:16 +0100

    elf: Disable some subtests of ifuncmain1, ifuncmain5 for !PIE
    (cherry picked from commit 9cc9d61ee12f2f8620d8e0ea3c42af02bf07fe1e)

--- elf/ifuncmain5.c
+++ elf/ifuncmain5.c
@@ -14,12 +14,19 @@ get_foo (void)
   return foo;
 }
 
+
+/* Address-significant access to protected symbols is not supported in
+   position-dependent mode on several architectures because GCC
+   generates relocations that assume that the address is local to the
+   main program.  */
+#ifdef __PIE__
 foo_p
 __attribute__ ((noinline))
 get_foo_protected (void)
 {
   return foo_protected;
 }
+#endif
 
 int
 main (void)
@@ -30,9 +37,11 @@ main (void)
   if ((*p) () != -1)
     abort ();
 
+#ifdef __PIE__
   p = get_foo_protected ();
   if ((*p) () != 0)
     abort ();
+#endif
 
   return 0;
 }
