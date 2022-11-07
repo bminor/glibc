a10f9fb3ff37b98f0adc5bdc0b4c32920c0c540a
Xiaolin Tang <tangxiaolin@loongson.cn>
2022-11-07 22:08:20 +0800

    LoongArch: Hard Float Support for fmaximum_mag{f/ }, fminimum_mag{f/ }.
    Use hardware Floating-point instruction f{maxa/mina}.{s/d}, fclass.{s/d}
    to implement fmaximum_mag{f/ }, fminimum_mag{f/ }.
    
           *  sysdeps/loongarch/fpu/s_fmaximum_mag.c: New file.
           *  sysdeps/loongarch/fpu/s_fmaximum_magf.c: Likewise.
           *  sysdeps/loongarch/fpu/s_fminimum_mag.c: Likewise.
           *  sysdeps/loongarch/fpu/s_fminimum_magf.c: Likewise.

---
+++ sysdeps/loongarch/fpu/s_fminimum_mag.c
@@ -0,0 +1,40 @@
+/* fminimum_mag().  LoongArch version.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#define NO_MATH_REDIRECT
+#include <math.h>
+#include <libm-alias-double.h>
+#include <fpu_control.h>
+
+double
+__fminimum_mag (double x, double y)
+{
+  int x_cond;
+  int y_cond;
+  asm volatile ("fclass.d \t%0, %1" : "=f" (x_cond) : "f" (x));
+  asm volatile ("fclass.d \t%0, %1" : "=f" (y_cond) : "f" (y));
+
+  if (__glibc_unlikely((x_cond | y_cond) & _FCLASS_NAN))
+      return x * y;
+  else
+    {
+      asm volatile ("fmina.d \t%0, %1, %2" : "=f" (x) : "f" (x), "f" (y));
+      return x;
+    }
+}
+libm_alias_double (__fminimum_mag, fminimum_mag)
