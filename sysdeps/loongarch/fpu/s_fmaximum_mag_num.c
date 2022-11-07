838b0da96a33e1200adb279c9cef30b1079b6709
Xiaolin Tang <tangxiaolin@loongson.cn>
2022-11-07 22:08:21 +0800

    LoongArch: Hard Float Support for fmaximum_mag_num{f/ }, fminimum_mag_num{f/ }.
    Use hardware Floating-point instruction f{maxa/mina}.{s/d}, fclass.{s/d}
    to implement fmaximum_mag_num{f/ }, fminimum_mag_num{f/ }.
    
           *  sysdeps/loongarch/fpu/s_fmaximum_mag_num.c: New file.
           *  sysdeps/loongarch/fpu/s_fmaximum_mag_numf.c: Likewise.
           *  sysdeps/loongarch/fpu/s_fminimum_mag_num.c: Likewise.
           *  sysdeps/loongarch/fpu/s_fminimum_mag_numf.c: Likewise.

---
+++ sysdeps/loongarch/fpu/s_fmaximum_mag_num.c
@@ -0,0 +1,48 @@
+/* fmaximum_mag_num().  LoongArch version.
+   Copyright (C) 2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#define NO_MATH_REDIRECT
+#include <math.h>
+#include <libm-alias-double.h>
+#include <fpu_control.h>
+
+double
+__fmaximum_mag_num (double x, double y)
+{
+  int x_cond;
+  int y_cond;
+  asm volatile ("fclass.d \t%0, %1" : "=f" (x_cond) : "f" (x));
+  asm volatile ("fclass.d \t%0, %1" : "=f" (y_cond) : "f" (y));
+
+  if (__glibc_unlikely((x_cond & _FCLASS_NAN) && !(y_cond & _FCLASS_NAN)))
+    {
+      asm volatile ("fmaxa.d \t%0, %1, %2" : "=f" (x) : "f" (x), "f" (y));
+      return y;
+    }
+  else if (__glibc_unlikely(!(x_cond & _FCLASS_NAN) && (y_cond & _FCLASS_NAN)))
+    {
+      asm volatile ("fmaxa.d \t%0, %1, %2" : "=f" (y) : "f" (x), "f" (y));
+      return x;
+    }
+  else
+    {
+      asm volatile ("fmaxa.d \t%0, %1, %2" : "=f" (x) : "f" (x), "f" (y));
+      return x;
+    }
+}
+libm_alias_double (__fmaximum_mag_num, fmaximum_mag_num)
