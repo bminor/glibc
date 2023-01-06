ae612c45efb5e34713859a5facf92368307efb6e
Joseph Myers <joseph@codesourcery.com>
2023-01-06 21:45:36 +0000

    Update copyright dates not handled by scripts/update-copyrights
    I've updated copyright dates in glibc for 2023.  This is the patch for
    the changes not generated by scripts/update-copyrights and subsequent
    build / regeneration of generated files.

--- elf/sprof.c
+++ elf/sprof.c
@@ -390,7 +390,7 @@ Copyright (C) %s Free Software Foundation, Inc.\n\
 This is free software; see the source for copying conditions.  There is NO\n\
 warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
 "),
-	   "2022");
+	   "2023");
   fprintf (stream, gettext ("Written by %s.\n"), "Ulrich Drepper");
 }
 
