506f7dbbabbad1f2d9f745636937b20c2670c29b
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-01-10 18:00:56 -0300

    string: Improve generic strchr
    New algorithm now calls strchrnul.
    
    Checked on x86_64-linux-gnu, i686-linux-gnu, powerpc-linux-gnu,
    and powerpc64-linux-gnu by removing the arch-specific assembly
    implementation and disabling multi-arch (it covers both LE and BE
    for 64 and 32 bits).
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Noah Goldstein <goldstein.w.n@gmail.com>

--- sysdeps/s390/strchr-c.c
+++ sysdeps/s390/strchr-c.c
@@ -21,13 +21,14 @@
 #if HAVE_STRCHR_C
 # if HAVE_STRCHR_IFUNC
 #  define STRCHR STRCHR_C
-#  undef weak_alias
-#  if defined SHARED && IS_IN (libc)
-#   undef libc_hidden_builtin_def
-#   define libc_hidden_builtin_def(name)			\
-     __hidden_ver1 (__strchr_c, __GI_strchr, __strchr_c);
-#  endif
 # endif
 
 # include <string/strchr.c>
+
+# if HAVE_STRCHR_IFUNC
+#  if defined SHARED && IS_IN (libc)
+__hidden_ver1 (__strchr_c, __GI_strchr, __strchr_c);
+#  endif
+# endif
+
 #endif
