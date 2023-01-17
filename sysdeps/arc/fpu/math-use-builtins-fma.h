dab63442791e334d592ce91827ffa9d14ca92ea9
Pavel Kozlov <pavel.kozlov@synopsys.com>
2023-01-17 16:12:23 +0400

    ARC:fpu: add extra capability check before use of sqrt and fma builtins
    Add extra check for compiler definitions to ensure that compiler provides
    sqrt and fma hw fpu instructions else use software implementation.
    
    As divide/sqrt and FMA hw support from CPU side is optional,
    the compiler can be configured by options to generate hw FPU instructions,
    but without use of FDDIV, FDSQRT, FSDIV, FSSQRT, FDMADD and FSMADD
    instructions. In this case __builtin_sqrt and __builtin_sqrtf provided by
    compiler can't be used inside the glibc code, as these builtins are used
    in implementations of sqrt() and sqrtf() functions but at the same time
    these builtins unfold to sqrt() and sqrtf(). So it is possible to receive
    code like that:
    
    0001c4b4 <__ieee754_sqrtf>:
       1c4b4:    0001 0000      b     0         ;1c4b4 <__ieee754_sqrtf>
    
    The same is also true for __builtin_fma and __builtin_fmaf.
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- sysdeps/arc/fpu/math-use-builtins-fma.h
+++ sysdeps/arc/fpu/math-use-builtins-fma.h
@@ -1,4 +1,14 @@
-#define USE_FMA_BUILTIN 1
-#define USE_FMAF_BUILTIN 1
+#if defined __ARC_FPU_DP_FMA__
+# define USE_FMA_BUILTIN 1
+#else
+# define USE_FMA_BUILTIN 0
+#endif
+
+#if defined __ARC_FPU_SP_FMA__
+# define USE_FMAF_BUILTIN 1
+#else
+# define USE_FMAF_BUILTIN 0
+#endif
+
 #define USE_FMAL_BUILTIN 0
 #define USE_FMAF128_BUILTIN 0
--- sysdeps/arc/fpu/math-use-builtins-sqrt.h
+++ sysdeps/arc/fpu/math-use-builtins-sqrt.h
@@ -1,4 +1,14 @@
-#define USE_SQRT_BUILTIN 1
-#define USE_SQRTF_BUILTIN 1
+#if defined __ARC_FPU_DP_DIV__
+# define USE_SQRT_BUILTIN 1
+#else
+# define USE_SQRT_BUILTIN 0
+#endif
+
+#if defined __ARC_FPU_SP_DIV__
+# define USE_SQRTF_BUILTIN 1
+#else
+# define USE_SQRTF_BUILTIN 0
+#endif
+
 #define USE_SQRTL_BUILTIN 0
 #define USE_SQRTF128_BUILTIN 0
