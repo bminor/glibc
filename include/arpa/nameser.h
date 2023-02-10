62d4c768a4df59e4381464385b3a7246e6df6661
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-02-10 11:24:15 -0300

    resolv: Remove _STRING_ARCH_unaligned usage
    GCC with default implementation already generates optimized code.
    
    Checked on x86_64-linux-gnu and i686-linux-gnu.
    
    Reviewed-by: Wilco Dijkstra  <Wilco.Dijkstra@arm.com>

--- include/arpa/nameser.h
+++ include/arpa/nameser.h
@@ -13,42 +13,6 @@
 
 extern const struct _ns_flagdata _ns_flagdata[] attribute_hidden;
 
-#if _STRING_ARCH_unaligned
-
-# undef NS_GET16
-# define NS_GET16(s, cp) \
-  do {									      \
-    const uint16_t *t_cp = (const uint16_t *) (cp);			      \
-    (s) = ntohs (*t_cp);						      \
-    (cp) += NS_INT16SZ;							      \
-  } while (0)
-
-# undef NS_GET32
-# define NS_GET32(l, cp) \
-  do {									      \
-    const uint32_t *t_cp = (const uint32_t *) (cp);			      \
-    (l) = ntohl (*t_cp);						      \
-    (cp) += NS_INT32SZ;							      \
-  } while (0)
-
-# undef NS_PUT16
-# define NS_PUT16(s, cp) \
-  do {									      \
-    uint16_t *t_cp = (uint16_t *) (cp);					      \
-    *t_cp = htons (s);							      \
-    (cp) += NS_INT16SZ;							      \
-  } while (0)
-
-# undef NS_PUT32
-# define NS_PUT32(l, cp) \
-  do {									      \
-    uint32_t *t_cp = (uint32_t *) (cp);					      \
-    *t_cp = htonl (l);							      \
-    (cp) += NS_INT32SZ;							      \
-  } while (0)
-
-#endif
-
 extern unsigned int	__ns_get16 (const unsigned char *) __THROW;
 extern unsigned long	__ns_get32 (const unsigned char *) __THROW;
 int __ns_name_ntop (const unsigned char *, char *, size_t) __THROW;
