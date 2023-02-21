47e61ba7720c1c646077318e079dcb75224b1001
Szabolcs Nagy <szabolcs.nagy@arm.com>
2023-02-21 16:47:37 +0000

    TODO(vdso): aarch64: morello: disable VDSO loading
    Once the kernel starts providing a VDSO, old binaries should continue
    to work (without using the new VDSO).  For this we set the vdso to 0
    independently of auxv, otherwise glibc attempts to load it even if the
    target has no vdso symbols specified and the load can crash since the
    setup code is not purecap compatible.

---
+++ sysdeps/unix/sysv/linux/aarch64/morello/dl-auxv.h
@@ -0,0 +1,21 @@
+/* Auxiliary vector processing.  Morello version.
+   Copyright (C) 2023 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+/* Disable VDSO loading even if Linux provides one as it may crash.  */
+#define DL_PLATFORM_AUXV \
+  GLRO(dl_sysinfo_dso) = 0;
