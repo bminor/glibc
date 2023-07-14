01f52b11ded15a1c0214692c707f1ddee1f4179f
Szabolcs Nagy <szabolcs.nagy@arm.com>
2023-07-14 15:49:11 +0100

    aarch64: Enable GCS in dynamic linked exe
    Use the dynamic linker start code to enable GCS in the dynamic linked
    case after _dl_start returns and before _dl_start_user which marks
    the point after which user code may run.
    
    Like in the static linked case this ensures that GCS is enabled on a
    top level stack frame.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

---
+++ sysdeps/aarch64/dl-gcs.c
@@ -0,0 +1,25 @@
+/* AArch64 GCS functions.
+   Copyright (C) 2024-2025 Free Software Foundation, Inc.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <ldsodefs.h>
+
+/* Used to report error when prctl system call to enabled GCS fails.  */
+
+void _dl_gcs_enable_failed (int code)
+{
+  _dl_fatal_printf ("failed to enable GCS: %d\n", -code);
+}
