85ac7edcdf67010c223541936243427213ac87a6
Adhemerval Zanella Netto <adhemerval.zanella@linaro.org>
2023-07-21 09:18:16 -0300

    string: Fix bug-strncat1 with fortify enabled
    If fortify is enabled, the truncated output warning is issued by
    the wrapper itself:
    
    bug-strncat1.c: In function ‘main’:
    bug-strncat1.c:14:3: error: ‘__builtin___strncat_chk’ output truncated
    copying 1 byte from a string of length 2 [-Werror=stringop-truncation]
       14 |   strncat (d, "\5\6", 1);
          |   ^
    
    Checked on x86_64-linux-gnu.
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>

--- string/bug-strncat1.c
+++ string/bug-strncat1.c
@@ -1,9 +1,16 @@
 #undef __USE_STRING_INLINES
 #define __USE_STRING_INLINES
+#include <sys/cdefs.h>
+#include <libc-diag.h>
+#if __GNUC_PREREQ (8, 0)
+/* GCC warns about strncat truncating output; this is deliberately
+   tested here.  If fortify is enabled, it is also triggered by the
+   wrappers. */
+DIAG_IGNORE_NEEDS_COMMENT (8, "-Wstringop-truncation");
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <libc-diag.h>
 
 char d[3] = "\0\1\2";
 
@@ -11,11 +18,6 @@ int
 main (void)
 {
   DIAG_PUSH_NEEDS_COMMENT;
-#if __GNUC_PREREQ (8, 0)
-  /* GCC 8 warns about strncat truncating output; this is deliberately
-     tested here.  */
-  DIAG_IGNORE_NEEDS_COMMENT (8, "-Wstringop-truncation");
-#endif
   strncat (d, "\5\6", 1);
   DIAG_POP_NEEDS_COMMENT;
   if (d[0] != '\5')
