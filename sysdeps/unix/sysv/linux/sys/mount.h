803f4073cc8901ee89d73a90eed0ebfd50113f4f
Joseph Myers <joseph@codesourcery.com>
2023-09-14 14:58:15 +0000

    Add MOVE_MOUNT_BENEATH from Linux 6.5 to sys/mount.h
    This patch adds the MOVE_MOUNT_BENEATH constant from Linux 6.5 to
    glibc's sys/mount.h and updates tst-mount-consts.py to reflect these
    constants being up to date with that Linux kernel version.
    
    Tested with build-many-glibcs.py.

--- sysdeps/unix/sysv/linux/sys/mount.h
+++ sysdeps/unix/sysv/linux/sys/mount.h
@@ -226,6 +226,7 @@ struct mount_attr
 #define MOVE_MOUNT_T_AUTOMOUNTS 0x00000020 /* Follow automounts on to path */
 #define MOVE_MOUNT_T_EMPTY_PATH 0x00000040 /* Empty to path permitted */
 #define MOVE_MOUNT_SET_GROUP    0x00000100 /* Set sharing group instead */
+#define MOVE_MOUNT_BENEATH      0x00000200 /* Mount beneath top mount */
 
 
 /* fspick flags.  */
