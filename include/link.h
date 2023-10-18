ad96b5532570a6782f784b4f2c6aa2a52359144d
Florian Weimer <fweimer@redhat.com>
2023-10-18 14:30:00 +0200

    Revert "elf: Remove unused l_text_end field from struct link_map"
    This reverts commit 9f0d3bb2e325dc30ae20e347cccbe10fa0b4ce9b.
    
    Reason for revert: Preserve ABI after revert of commit 5d83a52a4.

--- include/link.h
+++ include/link.h
@@ -253,6 +253,8 @@ struct link_map
     /* Start and finish of memory map for this object.  l_map_start
        need not be the same as l_addr.  */
     ElfW(Addr) l_map_start, l_map_end;
+    /* End of the executable part of the mapping.  */
+    ElfW(Addr) l_text_end;
 
     /* Linked list of objects in reverse ELF constructor execution
        order.  Head of list is stored in _dl_init_called_list.  */
