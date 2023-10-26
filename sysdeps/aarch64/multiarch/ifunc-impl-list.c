f9ae26cbbec008eb572a2af8a743ea7d3945e78e
Wilco Dijkstra <wilco.dijkstra@arm.com>
2023-10-26 16:34:47 +0100

    AArch64: Cleanup emag memset
    Cleanup emag memset - merge the memset_base64.S file, remove
    the unused ZVA code (since it is disabled on emag).
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>
    (cherry picked from commit 9627ab99b50d250c6dd3001a3355aa03692f7fe5)

--- sysdeps/aarch64/multiarch/ifunc-impl-list.c
+++ sysdeps/aarch64/multiarch/ifunc-impl-list.c
@@ -57,7 +57,7 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 	      /* Enable this on non-falkor processors too so that other cores
 		 can do a comparative analysis with __memset_generic.  */
 	      IFUNC_IMPL_ADD (array, i, memset, (zva_size == 64), __memset_falkor)
-	      IFUNC_IMPL_ADD (array, i, memset, (zva_size == 64), __memset_emag)
+	      IFUNC_IMPL_ADD (array, i, memset, 1, __memset_emag)
 	      IFUNC_IMPL_ADD (array, i, memset, 1, __memset_kunpeng)
 #if HAVE_AARCH64_SVE_ASM
 	      IFUNC_IMPL_ADD (array, i, memset, sve && zva_size == 256, __memset_a64fx)
