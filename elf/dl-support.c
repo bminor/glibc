3be3728df2f1912c80abd3288bc6e3a25ad679e4
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-11-06 17:25:49 -0300

    elf: Ignore LD_LIBRARY_PATH and debug env var for setuid for static
    It mimics the ld.so behavior.
    
    Checked on x86_64-linux-gnu.
    Reviewed-by: Siddhesh Poyarekar <siddhesh@sourceware.org>
    
    (cherry picked from commit 5451fa962cd0a90a0e2ec1d8910a559ace02bba0)
    
    Changes:
    
    	git/elf/dl-support.c
    	  (missing commit 55f41ef8de4a4d0c5762d78659e11202d3c765d4
    	   ("elf: Remove LD_PROFILE for static binaries"))

--- elf/dl-support.c
+++ elf/dl-support.c
@@ -276,14 +276,28 @@ _dl_non_dynamic_init (void)
   _dl_main_map.l_phdr = GL(dl_phdr);
   _dl_main_map.l_phnum = GL(dl_phnum);
 
-  _dl_verbose = *(getenv ("LD_WARN") ?: "") == '\0' ? 0 : 1;
-
   /* Set up the data structures for the system-supplied DSO early,
      so they can influence _dl_init_paths.  */
   setup_vdso (NULL, NULL);
 
   /* With vDSO setup we can initialize the function pointers.  */
   setup_vdso_pointers ();
+
+  if (__libc_enable_secure)
+    {
+      static const char unsecure_envvars[] =
+	UNSECURE_ENVVARS
+	;
+      const char *cp = unsecure_envvars;
+
+      while (cp < unsecure_envvars + sizeof (unsecure_envvars))
+	{
+	  __unsetenv (cp);
+	  cp = strchr (cp, '\0') + 1;
+	}
+    }
+
+  _dl_verbose = *(getenv ("LD_WARN") ?: "") == '\0' ? 0 : 1;
 
   /* Initialize the data structures for the search paths for shared
      objects.  */
@@ -305,20 +319,6 @@ _dl_non_dynamic_init (void)
   if (_dl_profile_output == NULL || _dl_profile_output[0] == '\0')
     _dl_profile_output
       = &"/var/tmp\0/var/profile"[__libc_enable_secure ? 9 : 0];
-
-  if (__libc_enable_secure)
-    {
-      static const char unsecure_envvars[] =
-	UNSECURE_ENVVARS
-	;
-      const char *cp = unsecure_envvars;
-
-      while (cp < unsecure_envvars + sizeof (unsecure_envvars))
-	{
-	  __unsetenv (cp);
-	  cp = strchr (cp, '\0') + 1;
-	}
-    }
 
 #ifdef DL_PLATFORM_INIT
   DL_PLATFORM_INIT;
