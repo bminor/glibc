f258b3fa5abb35c1a9252c1676d5c1980affc509
H.J. Lu <hjl.tools@gmail.com>
2023-12-16 08:53:12 -0800

    x86/cet: Don't assume that SHSTK implies IBT
    Since shadow stack (SHSTK) is enabled in the Linux kernel without
    enabling indirect branch tracking (IBT), don't assume that SHSTK
    implies IBT.  Use "CPU_FEATURE_ACTIVE (IBT)" to check if IBT is active
    and "CPU_FEATURE_ACTIVE (SHSTK)" to check if SHSTK is active.
    
    (cherry picked from commit 442983319ba70de801fc856e8dd4748fba8f7f1b)

--- sysdeps/x86/tst-cet-legacy-10.c
+++ sysdeps/x86/tst-cet-legacy-10.c
@@ -21,19 +21,19 @@
 #include <support/test-driver.h>
 #include <support/xunistd.h>
 
-/* Check that CPU_FEATURE_ACTIVE on IBT and SHSTK matches _get_ssp.  */
+/* Check that CPU_FEATURE_ACTIVE on SHSTK matches _get_ssp.  */
 
 static int
 do_test (void)
 {
   if (_get_ssp () != 0)
     {
-      if (CPU_FEATURE_ACTIVE (IBT) && CPU_FEATURE_ACTIVE (SHSTK))
+      if (CPU_FEATURE_ACTIVE (SHSTK))
 	return EXIT_SUCCESS;
     }
   else
     {
-      if (!CPU_FEATURE_ACTIVE (IBT) && !CPU_FEATURE_ACTIVE (SHSTK))
+      if (!CPU_FEATURE_ACTIVE (SHSTK))
 	return EXIT_SUCCESS;
     }
 
