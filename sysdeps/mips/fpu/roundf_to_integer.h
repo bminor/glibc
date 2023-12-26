ac1ef8bd961d5225e890741f15c10966172cbdf4
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-12-26 19:25:41 -0300

    mips: Implement ceilf with hardware floating-point rounding instruction

---
+++ sysdeps/mips/fpu/roundf_to_integer.h
@@ -0,0 +1,68 @@
+/* Round to integer generic implementation.
+   Copyright (C) 2023 Free Software .
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _ROUND_TO_INTEGER_H
+#define _ROUND_TO_INTEGER_H
+
+#include <fenv_private.h>
+#include <sysdeps/ieee754/flt-32/math_config.h>
+#include <stdio.h>
+
+enum round_mode
+{
+  CEIL,
+};
+
+static inline float
+round_to_integer_float (enum round_mode mode, float x)
+{
+  uint32_t hx = asuint (x);
+  int ex = (hx & ~SIGN_MASK) >> MANTISSA_WIDTH;
+
+  float r = x;
+
+  fenv_t fe;
+  libc_feholdexceptf (&fe);
+  switch (mode)
+  {
+  case CEIL:
+    asm ("ceil.l.s %0, %0" : "+f" (r));
+    break;
+  }
+  libc_fesetenvf (&fe);
+
+  /* Inf or NaN.  */
+  if (__glibc_unlikely (ex == 0xff))
+    return x + x;
+
+  /* Number input and no fraction, return the number itself.  */
+  if (ex >= 127 + 25)
+    return x;
+
+  asm ("cvt.s.l %0, %1" : "+f" (r));
+  /* The copysign seems to generate better code.  */
+#if 1
+  return copysignf (r, x);
+#else
+  if (ex >= 127)
+    return r;
+  return hx & SIGN_MASK ? -r : r;
+#endif
+}
+
+#endif
