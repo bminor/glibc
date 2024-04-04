294154a9b39259b65638ddcadc984cc497657354
chiichen <chiichen@qq.com>
2024-04-04 17:52:35 +0800

    feat: added support for automatically insatll dependencies and build

---
+++ install_deps.sh
@@ -0,0 +1,17 @@
+apt-get install -y gcc make gdb
+apt-get install -y texinfo gawk bison sed
+apt-get install -y python3-dev python3-pip python-is-python3
+pip install pexpect
+PWD = $(pwd)
+mkdir build
+mkdir install
+echo "target install dir is ${PWD}/install"
+cd build
+../glibc/configure \
+  --prefix=${PWD}/install \
+  --host=x86_64-linux-gnu \
+  --build=x86_64-linux-gnu \
+  CC="gcc -m64" \
+  CXX="g++ -m64" \
+  CFLAGS="-O2" \
+  CXXFLAGS="-O2"
