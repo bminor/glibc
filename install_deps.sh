33c9e89d67f776a55890fa3efaec985410a2a1d6
chiichen <chiichen@qq.com>
2024-04-04 20:07:41 +0800

    chore: update workflow

--- install_deps.sh
+++ install_deps.sh
@@ -2,13 +2,11 @@ sudo apt-get install -y gcc make gdb
 sudo apt-get install -y texinfo gawk bison sed
 sudo apt-get install -y python3-dev python3-pip python-is-python3
 pip install pexpect
-PWD=$(pwd)
-mkdir build
-mkdir install
-echo "target install dir is ${PWD}/install"
+mkdir -p build
+mkdir -p install
 cd build
 ../configure \
-  --prefix=${PWD}/install \
+  --prefix=/usr \
   --host=x86_64-linux-gnu \
   --build=x86_64-linux-gnu \
   CC="gcc -m64" \
