13f421d68021887b648a3f70287e2c119afe8838
chiichen <chiichen@qq.com>
2024-04-05 17:41:53 +0800

    chore: fix cross compile error

--- default_configure.sh
+++ default_configure.sh
@@ -4,15 +4,13 @@ if [ -z "$ARCH" ] || [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "riscv" ] &&  [ "$A
     echo "No ARCH specified, use x86_64 as default"
     export ARCH="x86_64"
 fi
-echo "TARGET ARCH is: $ARCH"
-export HOST=`uname -m`
-echo "HOST ARCH is: $HOST"
+if [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "riscv" ] ||  [ "$ARCH" == "aarch64" ]; then
+    export TRIPLET=${ARCH}-linux-gnu
+fi
+export BUILD=`uname -m`
 cd build
 ../configure \
   --prefix=/usr \
-  --host=${HOST}-linux-gnu \
-  --build=${ARCH}-linux-gnu \
-  CC="gcc -m64" \
-  CXX="g++ -m64" \
-  CFLAGS="-O2" \
-  CXXFLAGS="-O2"
+  --host=${TRIPLET} \
+  --build=${BUILD}-linux-gnu \
+  --CC=${TRIPLET}
--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,13 @@
-sudo apt-get install -y gcc make gdb
+sudo apt-get install -y make gdb
 sudo apt-get install -y texinfo gawk bison sed
 sudo apt-get install -y python3-dev python-is-python3
+if [ -z "$ARCH" ] || [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "riscv" ] &&  [ "$ARCH" != "aarch64" ]; then
+    echo "No ARCH specified, use x86_64 as default"
+    export ARCH="x86_64"
+fi
+
+if [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "riscv" ] ||  [ "$ARCH" == "aarch64" ]; then
+    export TRIPLET=${ARCH}-linux-gnu
+fi
+
+sudo apt-get install -y gcc-${TRIPLET}
