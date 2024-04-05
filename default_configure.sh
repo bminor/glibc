6966386e42907b5c5a6b3e81ca905ddbc5497dca
chiichen <chiichen@qq.com>
2024-04-05 11:59:08 +0800

    chore: add seperated configure script for cross compile

---
+++ default_configure.sh
@@ -0,0 +1,18 @@
+mkdir -p build
+mkdir -p install
+if [ -z "$ARCH" ] || [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "riscv" ]; then
+    echo "No ARCH specified, use x86_64 as default"
+    export ARCH="x86_64"
+fi
+echo "TARGET ARCH is: $ARCH"
+export HOST=`uname -m`
+echo "HOST ARCH is: $HOST"
+cd build
+../configure \
+  --prefix=/usr \
+  --host=${HOST}-linux-gnu \
+  --build=${ARCH}-linux-gnu \
+  CC="gcc -m64" \
+  CXX="g++ -m64" \
+  CFLAGS="-O2" \
+  CXXFLAGS="-O2"
--- install_deps.sh
+++ install_deps.sh
@@ -1,14 +1,3 @@
 sudo apt-get install -y gcc make gdb
 sudo apt-get install -y texinfo gawk bison sed
 sudo apt-get install -y python3-dev python-is-python3
-mkdir -p build
-mkdir -p install
-cd build
-../configure \
-  --prefix=/usr \
-  --host=x86_64-linux-gnu \
-  --build=x86_64-linux-gnu \
-  CC="gcc -m64" \
-  CXX="g++ -m64" \
-  CFLAGS="-O2" \
-  CXXFLAGS="-O2"
