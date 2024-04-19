5361ad3910c257bc327567be76fde532ed238e42
Florian Weimer <fweimer@redhat.com>
2024-04-19 14:38:17 +0200

    login: Use unsigned 32-bit types for seconds-since-epoch
    These fields store timestamps when the system was running.  No Linux
    systems existed before 1970, so these values are unused.  Switching
    to unsigned types allows continued use of the existing struct layouts
    beyond the year 2038.
    
    The intent is to give distributions more time to switch to improved
    interfaces that also avoid locking/data corruption issues.
    
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

---
+++ login/tst-utmp-unsigned-64.c
@@ -0,0 +1,1 @@
+#include "tst-utmp-unsigned.c"
---
+++ login/tst-utmp-unsigned.c
@@ -0,0 +1,40 @@
+/* Check that struct utmp, struct utmpx, struct lastlog use unsigned epoch.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <utmp.h>
+#include <utmpx.h>
+#include <utmp-size.h>
+
+/* Undefined.  Used to check that the conditions below are optimized away.  */
+void link_failure_utmp (void);
+void link_failure_utmpx (void);
+void link_failure_lastlog (void);
+
+static int
+do_test (void)
+{
+  if ((struct utmp) { .ut_tv = { 0x80000000U, }, }.ut_tv.tv_sec <= 0)
+    link_failure_utmp ();
+  if ((struct utmpx) { .ut_tv = { 0x80000000U, }, }.ut_tv.tv_sec <= 0)
+    link_failure_utmpx ();
+  if ((struct lastlog) { .ll_time = 0x80000000U, }.ll_time <= 0)
+    link_failure_lastlog ();
+  return 0;
+}
+
+#include <support/test-driver.c>
