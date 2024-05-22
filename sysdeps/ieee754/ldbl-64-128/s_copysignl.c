eaa8113bf0eb599025e3efdbe1bb214ee8dc645a
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2024-05-22 15:07:12 -0300

    math: Provide missing math symbols on libc.a (BZ 31781)
    The libc.a for alpha, s390, and sparcv9 does not provide
    copysignf64x, copysignf128, frexpf64x, frexpf128, modff64x, and
    modff128.
    
    Checked with a static build for the affected ABIs.
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>

--- sysdeps/ieee754/ldbl-64-128/s_copysignl.c
+++ sysdeps/ieee754/ldbl-64-128/s_copysignl.c
@@ -1,10 +1,10 @@
 #include <math_ldbl_opt.h>
 #include <libm-alias-ldouble.h>
-#if IS_IN (libc)
+#if IS_IN (libc) && defined SHARED
 # undef libm_alias_ldouble
 # define libm_alias_ldouble(from, to)
 #endif
 #include <sysdeps/ieee754/ldbl-128/s_copysignl.c>
-#if IS_IN (libc)
+#if IS_IN (libc) && defined SHARED
 long_double_symbol (libc, __copysignl, copysignl);
 #endif
