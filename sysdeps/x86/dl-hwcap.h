f14b6dfc87bc237bb9a673f1b74d52d434f75b7c
Stefan Liebler <stli@linux.ibm.com>
2024-06-07 13:42:36 +0200

    x86: Remove HWCAP_START and HWCAP_COUNT
    Both defines are not used anymore.  Those were only used for
    _dl_string_hwcap(), which itself was removed with commit
    ab40f20364f4a417a63dd51fdd943742070bfe96
    "elf: Remove _dl_string_hwcap"
    
    Just clean up.
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- sysdeps/x86/dl-hwcap.h
+++ sysdeps/x86/dl-hwcap.h
@@ -23,23 +23,17 @@
    to cover all platforms and hardware capabilities.  */
 # define HWCAP_PLATFORMS_START	0
 # define HWCAP_PLATFORMS_COUNT	4
-# define HWCAP_START		0
-# define HWCAP_COUNT		3
 # define HWCAP_IMPORTANT \
   (HWCAP_X86_SSE2 | HWCAP_X86_64 | HWCAP_X86_AVX512_1)
 #elif defined __x86_64__
 /* For 64 bit, only cover x86-64 platforms and capabilities.  */
 # define HWCAP_PLATFORMS_START	2
 # define HWCAP_PLATFORMS_COUNT	4
-# define HWCAP_START		1
-# define HWCAP_COUNT		3
 # define HWCAP_IMPORTANT	(HWCAP_X86_64 | HWCAP_X86_AVX512_1)
 #else
 /* For 32 bit, only cover i586, i686 and SSE2.  */
 # define HWCAP_PLATFORMS_START	0
 # define HWCAP_PLATFORMS_COUNT	2
-# define HWCAP_START		0
-# define HWCAP_COUNT		1
 # define HWCAP_IMPORTANT	(HWCAP_X86_SSE2)
 #endif
 
