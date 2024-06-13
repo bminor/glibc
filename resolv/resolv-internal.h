708f507e30163d764e8df7ba7cb6b346d2f7bace
Florian Weimer <fweimer@redhat.com>
2024-06-13 18:56:30 +0200

    resolv: Track single-request fallback via _res._flags (bug 31476)
    This avoids changing _res.options, which inteferes with change
    detection as part of automatic reloading of /etc/resolv.conf.
    
    Reviewed-by: DJ Delorie <dj@redhat.com>
    (cherry picked from commit 868ab8923a2ec977faafec97ecafac0c3159c1b2)

--- resolv/resolv-internal.h
+++ resolv/resolv-internal.h
@@ -26,6 +26,8 @@
 #define RES_F_VC        0x00000001 /* Socket is TCP.  */
 #define RES_F_CONN      0x00000002 /* Socket is connected.  */
 #define RES_F_EDNS0ERR  0x00000004 /* EDNS0 caused errors.  */
+#define RES_F_SNGLKUP	0x00200000 /* Private version of RES_SNGLKUP.  */
+#define RES_F_SNGLKUPREOP 0x00400000 /* Private version of RES_SNGLKUPREOP.  */
 
 /* The structure HEADER is normally aligned on a word boundary.  In
    some code, we need to access this structure when it may be aligned
