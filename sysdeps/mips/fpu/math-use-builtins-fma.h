9d0e9c8a138fe25e58fdfc21c2e000425579d9a8
YunQiang Su <syq@gcc.gnu.org>
2024-06-21 00:07:56 +0800

    MIPSr6/math: Use builtin fma and fmaf
    MIPSr6 has MADDF.s/MADDF.d instructions, which are fused.
    
    In MIPS ISA, double support can be subsetted.  Only FMAF is enabled
    for this case.
    
    	* sysdeps/mips/fpu/math-use-builtins-fma.h
    
    Signed-off-by: YunQiang Su <syq@gcc.gnu.org>
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

---
+++ sysdeps/mips/fpu/math-use-builtins-fma.h
@@ -0,0 +1,36 @@
+/* Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+/* MIPSr6 has MADDF.s/MADDF.d instructions, which are fused.  In MIPS
+   ISA, double support can be subsetted.  Only FMAF is enabled for this
+   case.  */
+
+#include <sysdep.h>
+
+#if __mips_isa_rev >= 6
+# ifdef __mips_single_float
+#  define USE_FMA_BUILTIN 0
+# else
+#  define USE_FMA_BUILTIN 1
+# endif
+# define USE_FMAF_BUILTIN 1
+#else
+# define USE_FMA_BUILTIN 0
+# define USE_FMAF_BUILTIN 0
+#endif
+#define USE_FMAL_BUILTIN 0
+#define USE_FMAF128_BUILTIN 0
