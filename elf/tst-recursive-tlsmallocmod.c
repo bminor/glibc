7485d6cfe9b4ced5bca2f07bb9011ce5f61428b7
Florian Weimer <fweimer@redhat.com>
2024-07-01 17:42:04 +0200

    elf: Support recursive use of dynamic TLS in interposed malloc
    It turns out that quite a few applications use bundled mallocs that
    have been built to use global-dynamic TLS (instead of the recommended
    initial-exec TLS).  The previous workaround from
    commit afe42e935b3ee97bac9a7064157587777259c60e ("elf: Avoid some
    free (NULL) calls in _dl_update_slotinfo") does not fix all
    encountered cases unfortunatelly.
    
    This change avoids the TLS generation update for recursive use
    of TLS from a malloc that was called during a TLS update.  This
    is possible because an interposed malloc has a fixed module ID and
    TLS slot.  (It cannot be unloaded.)  If an initially-loaded module ID
    is encountered in __tls_get_addr and the dynamic linker is already
    in the middle of a TLS update, use the outdated DTV, thus avoiding
    another call into malloc.  It's still necessary to update the
    DTV to the most recent generation, to get out of the slow path,
    which is why the check for recursion is needed.
    
    The bookkeeping is done using a global counter instead of per-thread
    flag because TLS access in the dynamic linker is tricky.
    
    All this will go away once the dynamic linker stops using malloc
    for TLS, likely as part of a change that pre-allocates all TLS
    during pthread_create/dlopen.
    
    Fixes commit d2123d68275acc0f061e73d5f86ca504e0d5a344 ("elf: Fix slow
    tls access after dlopen [BZ #19924]").
    
    Reviewed-by: Szabolcs Nagy <szabolcs.nagy@arm.com>
    (cherry picked from commit 018f0fc3b818d4d1460a4e2384c24802504b1d20)

---
+++ elf/tst-recursive-tlsmallocmod.c
@@ -0,0 +1,64 @@
+/* Interposed malloc with dynamic TLS.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdlib.h>
+#include <dlfcn.h>
+
+__thread unsigned int malloc_subsytem_counter;
+
+static __typeof (malloc) *malloc_fptr;
+static __typeof (free) *free_fptr;
+static __typeof (calloc) *calloc_fptr;
+static __typeof (realloc) *realloc_fptr;
+
+static void __attribute__ ((constructor))
+init (void)
+{
+  malloc_fptr = dlsym (RTLD_NEXT, "malloc");
+  free_fptr = dlsym (RTLD_NEXT, "free");
+  calloc_fptr = dlsym (RTLD_NEXT, "calloc");
+  realloc_fptr = dlsym (RTLD_NEXT, "realloc");
+}
+
+void *
+malloc (size_t size)
+{
+  ++malloc_subsytem_counter;
+  return malloc_fptr (size);
+}
+
+void
+free (void *ptr)
+{
+  ++malloc_subsytem_counter;
+  return free_fptr (ptr);
+}
+
+void *
+calloc (size_t a, size_t b)
+{
+  ++malloc_subsytem_counter;
+  return calloc_fptr (a, b);
+}
+
+void *
+realloc (void *ptr, size_t size)
+{
+  ++malloc_subsytem_counter;
+  return realloc_fptr (ptr, size);
+}
