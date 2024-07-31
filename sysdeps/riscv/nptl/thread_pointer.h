8dd15887940e9a6a4304fe360c7bd7569fe9ba21
Michael Jeanson <mjeanson@efficios.com>
2024-07-31 11:20:36 -0400

    nptl: Add <thread_pointer.h> for RISC-V
    This will be required by the rseq extensible ABI implementation on all
    Linux architectures exposing the '__rseq_size' and '__rseq_offset'
    symbols to set the initial value of the 'cpu_id' field which can be used
    by applications to test if rseq is available and registered. As long as
    the symbols are exposed it is valid for an application to perform this
    test even if rseq is not yet implemented in libc for this architecture.
    
    Both code paths tested on a Visionfive 2 with Debian sid.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Reviewed-by: Palmer Dabbelt <palmer@rivosinc.com>
    Acked-by: Palmer Dabbelt <palmer@rivosinc.com>

---
+++ sysdeps/riscv/nptl/thread_pointer.h
@@ -0,0 +1,40 @@
+/* __thread_pointer definition.  riscv version.
+   Copyright (C) 2021-2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _SYS_THREAD_POINTER_H
+#define _SYS_THREAD_POINTER_H
+
+#include <features.h>
+
+#if __GNUC_PREREQ (10, 3) || __glibc_clang_prereq (11, 0)
+static inline void *
+__thread_pointer (void)
+{
+  return __builtin_thread_pointer ();
+}
+#else
+static inline void *
+__thread_pointer (void)
+{
+  void *__thread_register;
+  __asm__ ("mv %0, tp" : "=r" (__thread_register));
+  return __thread_register;
+}
+#endif
+
+#endif /* _SYS_THREAD_POINTER_H */
