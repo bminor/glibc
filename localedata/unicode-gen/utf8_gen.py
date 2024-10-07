cae9944a6c436baa9f7a86df3d6436d30c7c8d3a
Carlos O'Donell <carlos@redhat.com>
2024-10-07 18:04:22 -0400

    Fix whitespace related license issues.
    Several copies of the licenses in files contained whitespace related
    problems.  Two cases are addressed here, the first is two spaces
    after a period which appears between "PURPOSE." and "See". The other
    is a space after the last forward slash in the URL. Both issues are
    corrected and the licenses now match the official textual description
    of the license (and the other license in the sources).
    
    Since these whitespaces changes do not alter the paragraph structure of
    the license, nor create new sentences, they do not change the license.

--- localedata/unicode-gen/utf8_gen.py
+++ localedata/unicode-gen/utf8_gen.py
@@ -11,7 +11,7 @@
 #
 # The GNU C Library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
--- sysdeps/powerpc/powerpc64/multiarch/strcat-power8.c
+++ sysdeps/powerpc/powerpc64/multiarch/strcat-power8.c
@@ -8,12 +8,12 @@
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
-   <https://www.gnu.org/licenses/ >.  */
+   <https://www.gnu.org/licenses/>.  */
 
 #include <string.h>
 
