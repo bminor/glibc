43db5e2c0672cae7edea7c9685b22317eae25471
Florian Weimer <fweimer@redhat.com>
2024-10-25 16:50:10 +0200

    elf: Signal RT_CONSISTENT after relocation processing in dlopen (bug 31986)
    Previously, a la_activity audit event was generated before
    relocation processing completed.  This does did not match what
    happened during initial startup in elf/rtld.c (towards the end
    of dl_main).  It also caused various problems if an auditor
    tried to open the same shared object again using dlmopen:
    If it was the directly loaded object, it had a search scope
    associated with it, so the early exit in dl_open_worker_begin
    was taken even though the object was unrelocated.  This caused
    the r_state == RT_CONSISTENT assert to fail.  Avoidance of the
    assert also depends on reversing the order of r_state update
    and auditor event (already implemented in a previous commit).
    
    At the later point, args->map can be NULL due to failure,
    so use the assigned namespace ID instead if that is available.
    
    Reviewed-by: Adhemerval Zanella <adhemerval.zanella@linaro.org>

---
+++ elf/tst-dlopen-auditdupmod.c
@@ -0,0 +1,48 @@
+/* Directly opened test module that gets reopened from the auditor.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <support/xdlfcn.h>
+
+int auditdupmod_status;
+
+/* Used to check for successful relocation processing.  */
+int *auditdupmod_status_address = &auditdupmod_status;
+
+static void __attribute__ ((constructor))
+init (void)
+{
+  ++auditdupmod_status;
+  printf ("info: tst-dlopen-auditdupmod.so constructor called (status %d)\n",
+          auditdupmod_status);
+}
+
+static void __attribute__ ((destructor))
+fini (void)
+{
+  /* The tst-dlopen-auditdup-auditmod.so auditor incremented
+     auditdupmod_status.  */
+  printf ("info: tst-dlopen-auditdupmod.so destructor called (status %d)\n",
+          auditdupmod_status);
+  if (auditdupmod_status != 2)
+    {
+      puts ("error: auditdupmod_status == 2 expected");
+      exit (1);
+    }
+}
