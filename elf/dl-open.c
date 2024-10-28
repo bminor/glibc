2b89de7c91e5e71732a32efef94075d9edbff95e
Florian Weimer <fweimer@redhat.com>
2024-10-28 14:45:30 +0100

    Revert "elf: Run constructors on cyclic recursive dlopen (bug 31986)"
    This reverts commit 9897ced8e78db5d813166a7ccccfd5a42c69ef20.
    
    Adjust the test expectations in elf/tst-dlopen-auditdup-auditmod.c
    accordingly.
    
    (cherry picked from commit 95129e6b8fabdaa8cd8a4a5cc20be0f4cb0ba59f)

--- elf/dl-open.c
+++ elf/dl-open.c
@@ -605,14 +605,6 @@ dl_open_worker_begin (void *a)
       if ((mode & RTLD_GLOBAL) && new->l_global == 0)
 	add_to_global_update (new);
 
-      /* Do not return without calling the (supposedly new) map's
-	 constructor.  This case occurs if a dependency of a directly
-	 opened map has a constructor that calls dlopen again on the
-	 initially opened map.  The new map is initialized last, so
-	 checking only it is enough.  */
-      if (!new->l_init_called)
-	_dl_catch_exception (NULL, call_dl_init, args);
-
       return;
     }
 
