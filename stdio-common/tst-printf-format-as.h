b350a60b6ecd77b7ec30c7969de1df8b73642e55
Maciej W. Rozycki <macro@redhat.com>
2024-11-07 06:14:24 +0000

    stdio-common: Add tests for formatted asprintf output specifiers
    Wire asprintf into test infrastructure for formatted printf output
    specifiers.
    
    Owing to mtrace logging of lots of memory allocation calls these tests
    take a considerable amount of time to complete, except for the character
    conversion, taking from 00m20s for 'tst-printf-format-as-s --direct s',
    through 01m10s and 03m53s for 'tst-printf-format-as-char --direct i' and
    'tst-printf-format-as-double --direct f' respectively, to 19m24s for
    'tst-printf-format-as-ldouble --direct f', all in standalone execution
    from NFS on a RISC-V FU740@1.2GHz system and with output redirected over
    100Mbps network via SSH.  It is with the skeleton's stub implementation
    of dladdr(3); execution times with regular dladdr(3) are up to over
    twice longer.
    
    Set timeouts for the tests accordingly then, with a global default for
    all the asprintf tests, and then individual higher settings for double
    and long double tests each.
    
    Reviewed-by: DJ Delorie <dj@redhat.com>

---
+++ stdio-common/tst-printf-format-as.h
@@ -0,0 +1,46 @@
+/* Test feature wrapper for formatted 'asprintf' output.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define printf_under_test(...)						\
+({									\
+  __label__ out;							\
+  int result;								\
+  char *str;								\
+									\
+  result = asprintf (&str, __VA_ARGS__);				\
+  if (result < 0)							\
+    {									\
+      perror ("asprintf");						\
+      goto out;								\
+    }									\
+  if (fwrite (str, sizeof (*str), result, stdout) != result)		\
+    {									\
+      perror ("fwrite");						\
+      result = -1;							\
+    }									\
+  free (str);								\
+out:									\
+  result;								\
+})
+
+#ifndef TIMEOUT
+# define TIMEOUT (DEFAULT_TIMEOUT * 12)
+#endif
