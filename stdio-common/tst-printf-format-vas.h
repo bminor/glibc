349670f8093d920d4d683472c88029f6901f7ae7
Maciej W. Rozycki <macro@redhat.com>
2024-11-07 06:14:24 +0000

    stdio-common: Add tests for formatted vasprintf output specifiers
    Wire vasprintf into test infrastructure for formatted printf output
    specifiers.
    
    Owing to mtrace logging these tests take amounts of time to complete
    similar to those of corresponding asprintf tests, so set timeouts for
    the tests accordingly, with a global default for all the vasprintf
    tests, and then individual higher settings for double and long double
    tests each.
    
    Reviewed-by: DJ Delorie <dj@redhat.com>

---
+++ stdio-common/tst-printf-format-vas.h
@@ -0,0 +1,50 @@
+/* Test feature wrapper for formatted 'vasprintf' output.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int
+printf_under_test (const char *restrict fmt, ...)
+{
+  va_list ap;
+  int result;
+  char *str;
+
+  va_start (ap, fmt);
+  result = vasprintf (&str, fmt, ap);
+  va_end (ap);
+  if (result < 0)
+    {
+      perror ("vasprintf");
+      goto out;
+    }
+  if (fwrite (str, sizeof (*str), result, stdout) != result)
+    {
+      perror ("fwrite");
+      result = -1;
+    }
+  free (str);
+out:
+  return result;
+}
+
+#ifndef TIMEOUT
+# define TIMEOUT (DEFAULT_TIMEOUT * 12)
+#endif
