a357d6273f796b384b7e36d58a8de947ebde9668
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2024-11-18 08:45:16 -0300

    math: Use atanf from CORE-MATH
    The CORE-MATH implementation is correctly rounded (for any rounding mode)
    and shows slight better performance to the generic atanf.
    
    The code was adapted to glibc style and to use the definition of
    math_config.h (to handle errno, overflow, and underflow).
    
    Benchtest on x64_64 (Ryzen 9 5900X, gcc 14.2.1), aarch64 (Neoverse-N1,
    gcc 13.3.1), and powerpc (POWER10, gcc 13.2.1):
    
    Latency                      master        patched   improvement
    x86_64                      56.8265        53.6842         5.53%
    x86_64v2                    54.8177        53.6842         2.07%
    x86_64v3                    46.2915        48.7034        -5.21%
    i686                       158.3760        108.9560       31.20%
    aarch64 (Neoverse)           21.687        20.5893         5.06%
    power10                     13.1903        13.5012        -2.36%
    
    reciprocal-throughput        master        patched   improvement
    x86_64                      16.6787        16.7601        -0.49%
    x86_64v2                    16.6983        16.7601        -0.37%
    x86_64v3                    16.2268        12.1391        25.19%
    i686                       138.6840        36.0640        74.00%
    aarch64 (Neoverse)          11.8012        10.3565        12.24%
    power10                      5.3212         4.2894        19.39%
    
    Signed-off-by: Alexei Sibidanov <sibid@uvic.ca>
    Signed-off-by: Paul Zimmermann <Paul.Zimmermann@inria.fr>
    Signed-off-by: Adhemerval Zanella <adhemerval.zanella@linaro.org>
    Reviewed-by: DJ Delorie <dj@redhat.com>

--- sysdeps/ieee754/flt-32/s_atanf.c
+++ sysdeps/ieee754/flt-32/s_atanf.c
@@ -1,102 +1,113 @@
-/* s_atanf.c -- float version of s_atan.c.
- */
+/* Correctly-rounded arc-tangent of binary32 value.
 
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
+Copyright (c) 2022-2024 Alexei Sibidanov.
 
-#if defined(LIBM_SCCS) && !defined(lint)
-static char rcsid[] = "$NetBSD: s_atanf.c,v 1.4 1995/05/10 20:46:47 jtc Exp $";
-#endif
+The original version of this file was copied from the CORE-MATH
+project (file src/binary32/atan/atanf.c, revision a8066a5).
 
-#include <float.h>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <math.h>
-#include <math_private.h>
-#include <math-underflow.h>
+#include <stdint.h>
 #include <libm-alias-float.h>
+#include "math_config.h"
 
-static const float atanhi[] = {
-  4.6364760399e-01, /* atan(0.5)hi 0x3eed6338 */
-  7.8539812565e-01, /* atan(1.0)hi 0x3f490fda */
-  9.8279368877e-01, /* atan(1.5)hi 0x3f7b985e */
-  1.5707962513e+00, /* atan(inf)hi 0x3fc90fda */
-};
-
-static const float atanlo[] = {
-  5.0121582440e-09, /* atan(0.5)lo 0x31ac3769 */
-  3.7748947079e-08, /* atan(1.0)lo 0x33222168 */
-  3.4473217170e-08, /* atan(1.5)lo 0x33140fb4 */
-  7.5497894159e-08, /* atan(inf)lo 0x33a22168 */
-};
-
-static const float aT[] = {
-  3.3333334327e-01, /* 0x3eaaaaaa */
- -2.0000000298e-01, /* 0xbe4ccccd */
-  1.4285714924e-01, /* 0x3e124925 */
- -1.1111110449e-01, /* 0xbde38e38 */
-  9.0908870101e-02, /* 0x3dba2e6e */
- -7.6918758452e-02, /* 0xbd9d8795 */
-  6.6610731184e-02, /* 0x3d886b35 */
- -5.8335702866e-02, /* 0xbd6ef16b */
-  4.9768779427e-02, /* 0x3d4bda59 */
- -3.6531571299e-02, /* 0xbd15a221 */
-  1.6285819933e-02, /* 0x3c8569d7 */
-};
-
-static const float
-one   = 1.0,
-huge   = 1.0e30;
-
-float __atanf(float x)
+float
+__atanf (float x)
 {
-	float w,s1,s2,z;
-	int32_t ix,hx,id;
-
-	GET_FLOAT_WORD(hx,x);
-	ix = hx&0x7fffffff;
-	if(ix>=0x4c000000) {	/* if |x| >= 2^25 */
-	    if(ix>0x7f800000)
-		return x+x;		/* NaN */
-	    if(hx>0) return  atanhi[3]+atanlo[3];
-	    else     return -atanhi[3]-atanlo[3];
-	} if (ix < 0x3ee00000) {	/* |x| < 0.4375 */
-	    if (ix < 0x31000000) {	/* |x| < 2^-29 */
-		math_check_force_underflow (x);
-		if(huge+x>one) return x;	/* raise inexact */
-	    }
-	    id = -1;
-	} else {
-	x = fabsf(x);
-	if (ix < 0x3f980000) {		/* |x| < 1.1875 */
-	    if (ix < 0x3f300000) {	/* 7/16 <=|x|<11/16 */
-		id = 0; x = ((float)2.0*x-one)/((float)2.0+x);
-	    } else {			/* 11/16<=|x|< 19/16 */
-		id = 1; x  = (x-one)/(x+one);
-	    }
-	} else {
-	    if (ix < 0x401c0000) {	/* |x| < 2.4375 */
-		id = 2; x  = (x-(float)1.5)/(one+(float)1.5*x);
-	    } else {			/* 2.4375 <= |x| < 2^66 */
-		id = 3; x  = -(float)1.0/x;
-	    }
-	}}
-    /* end of argument reduction */
-	z = x*x;
-	w = z*z;
-    /* break sum from i=0 to 10 aT[i]z**(i+1) into odd and even poly */
-	s1 = z*(aT[0]+w*(aT[2]+w*(aT[4]+w*(aT[6]+w*(aT[8]+w*aT[10])))));
-	s2 = w*(aT[1]+w*(aT[3]+w*(aT[5]+w*(aT[7]+w*aT[9]))));
-	if (id<0) return x - x*(s1+s2);
-	else {
-	    z = atanhi[id] - ((x*(s1+s2) - atanlo[id]) - x);
-	    return (hx<0)? -z:z;
+  const double pi2 = 0x1.921fb54442d18p+0;
+  uint32_t t = asuint (x);
+  int e = (t >> 23) & 0xff;
+  bool gt = e >= 127;
+  uint32_t ta = t & 0x7fffffff;
+  if (__glibc_unlikely (ta >= 0x4c700518u)) /* |x| > 0x1.e00a3p+25 */
+    {
+      if (ta > 0x7f800000u)
+	return x + x; /* nan */
+      return copysign (pi2, (double) x);
+    }
+  if (__glibc_unlikely (e < 127 - 13))
+    {
+      if (__glibc_unlikely (e < 127 - 25))
+	{
+	  if (!(t << 1))
+	    return x;
+	  return fmaf (-x, fabsf (x), x);
 	}
+      return fmaf (-0x1.5555555555555p-2f * x, x * x, x);
+    }
+  /* now |x| >= 0x1p-13 */
+  double z = x;
+  if (gt)
+    z = 1 / z; /* gt is non-zero for |x| >= 1 */
+  double z2 = z * z;
+  double z4 = z2 * z2;
+  double z8 = z4 * z4;
+  /* polynomials generated using rminimax
+     (https://gitlab.inria.fr/sfilip/rminimax) with the following command:
+     ./ratapprox --function="atan(x)" --dom=[0.000122070,1]
+     --num=[x,x^3,x^5,x^7,x^9,x^11,x^13] --den=[1,x^2,x^4,x^6,x^8,x^10,x^12]
+     --output=atanf.sollya --log (see output atanf.sollya) The coefficient
+     cd[0] was slightly reduced from the original value 0x1.51eccde075d67p-2 to
+     avoid an exceptional case for |x| = 0x1.1ad646p-4 and rounding to nearest.
+  */
+  static const double cn[] =
+    {
+      0x1.51eccde075d67p-2, 0x1.a76bb5637f2f2p-1, 0x1.81e0eed20de88p-1,
+      0x1.376c8ca67d11dp-2, 0x1.aec7b69202ac6p-5, 0x1.9561899acc73ep-9,
+      0x1.bf9fa5b67e6p-16
+    };
+  static const double cd[] =
+    {
+      0x1.51eccde075d66p-2, 0x1.dfbdd7b392d28p-1, 0x1p+0,
+      0x1.fd22bf0e89b54p-2, 0x1.d91ff8b576282p-4, 0x1.653ea99fc9bbp-7,
+      0x1.1e7fcc202340ap-12
+    };
+  double cn0 = cn[0] + z2 * cn[1];
+  double cn2 = cn[2] + z2 * cn[3];
+  double cn4 = cn[4] + z2 * cn[5];
+  double cn6 = cn[6];
+  cn0 += z4 * cn2;
+  cn4 += z4 * cn6;
+  cn0 += z8 * cn4;
+  cn0 *= z;
+  double cd0 = cd[0] + z2 * cd[1];
+  double cd2 = cd[2] + z2 * cd[3];
+  double cd4 = cd[4] + z2 * cd[5];
+  double cd6 = cd[6];
+  cd0 += z4 * cd2;
+  cd4 += z4 * cd6;
+  cd0 += z8 * cd4;
+  double r = cn0 / cd0;
+  if (!gt)
+    return r; /* for |x| < 1, (float) r is correctly rounded */
+
+#define PI_OVER2_H 0x1.9p0
+#define PI_OVER2_L 0x1.0fdaa22168c23p-7
+  /* now r approximates atan(1/x), we use atan(x) + atan(1/x) = sign(x)*pi/2,
+     where PI_OVER2_H + PI_OVER2_L approximates pi/2.
+     With sign(z)*L + (-r + sign(z)*H), it fails for x=0x1.98c252p+12 and
+     rounding upward.
+     With sign(z)*PI - r, where PI is a double approximation of pi to nearest,
+     it fails for x=0x1.ddf9f6p+0 and rounding upward. */
+  r = copysign (PI_OVER2_L, z) - r + copysign (PI_OVER2_H, z);
+  return r;
 }
 libm_alias_float (__atan, atan)
