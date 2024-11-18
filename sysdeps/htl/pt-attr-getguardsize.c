6caf24c972ccbc8584f9892c825ab18057cbf626
gfleury <gfleury@disroot.org>
2024-11-18 13:21:35 +0200

    htl move pthread_attr_getguardsize into libc.
    Signed-off-by: gfleury <gfleury@disroot.org>

--- sysdeps/htl/pt-attr-getguardsize.c
+++ sysdeps/htl/pt-attr-getguardsize.c
@@ -17,11 +17,17 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include <pthread.h>
+#include <shlib-compat.h>
 #include <pt-internal.h>
 
 int
-pthread_attr_getguardsize (const pthread_attr_t *attr, size_t * guardsize)
+__pthread_attr_getguardsize (const pthread_attr_t *attr, size_t * guardsize)
 {
   *guardsize = attr->__guardsize;
   return 0;
 }
+versioned_symbol (libpthread, __pthread_attr_getguardsize, pthread_attr_getguardsize, GLIBC_2_41);
+
+#if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_12, GLIBC_2_41)
+compat_symbol (libpthread, __pthread_attr_getguardsize, pthread_attr_getguardsize, GLIBC_2_12);
+#endif
