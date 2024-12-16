0cc88d232757d4cbd771fed695d30af79674eb45
H.J. Lu <hjl.tools@gmail.com>
2024-12-17 05:56:53 +0800

    Silence Clang #include_next error
    Use "#include <...>" to silence Clang #include_next error:
    
    In file included from ../sysdeps/x86_64/fpu/test-double-vlen4-wrappers.c:19:
    ../sysdeps/x86_64/fpu/test-double-vlen4.h:19:2: error: #include_next in file found relative to primary source file or found by absolute path; will search from start of include path [-Werror,-Winclude-next-absolute-path]
       19 | #include_next <test-double-vlen4.h>
          |  ^
    1 error generated.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>
    Reviewed-by: Sam James <sam@gentoo.org>

--- sysdeps/x86_64/fpu/test-double-vlen8-wrappers.c
+++ sysdeps/x86_64/fpu/test-double-vlen8-wrappers.c
@@ -16,7 +16,7 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include "test-double-vlen8.h"
+#include <test-double-vlen8.h>
 #include "test-math-vector-sincos.h"
 #include <immintrin.h>
 
--- sysdeps/x86_64/fpu/test-float-vlen8-avx2-wrappers.c
+++ sysdeps/x86_64/fpu/test-float-vlen8-avx2-wrappers.c
@@ -16,7 +16,7 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include "test-float-vlen8.h"
+#include <test-float-vlen8.h>
 #include "test-math-vector-sincos.h"
 #include <immintrin.h>
 
