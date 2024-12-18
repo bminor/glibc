034cd675286db07110a773fdb6a857285485d2d4
H.J. Lu <hjl.tools@gmail.com>
2024-12-18 18:58:34 +0800

    Don't use glibc <tgmath.h> when testing with Clang
    Clang has its own <tgmath.h> and doesn't use <tgmath.h> from glibc.  Pass
    "-I." to compiler only if $($(<F)-no-include-dot) are undefined.  Define
    it to yes for tgmath tests when testing with Clang.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>
    Reviewed-by: Sam James <sam@gentoo.org>

--- include/tgmath.h
+++ include/tgmath.h
@@ -1,1 +1,7 @@
-#include <math/tgmath.h>
+/* NB: Clang has its own <tgmath.h> and doesn't use <tgmath.h> from
+   glibc.  */
+#if defined _ISOMAC && defined __clang__
+# include_next <tgmath.h>
+#else
+# include <math/tgmath.h>
+#endif
