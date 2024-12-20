322e9d4e443084336ea41a8e7bf72456ab273b73
Joseph Myers <josmyers@redhat.com>
2024-12-20 11:47:33 +0000

    Add F_CREATED_QUERY from Linux 6.12 to bits/fcntl-linux.h
    Linux 6.12 adds a new constant F_CREATED_QUERY.  Add it to glibc's
    bits/fcntl-linux.h.
    
    Tested for x86_64.

--- sysdeps/unix/sysv/linux/bits/fcntl-linux.h
+++ sysdeps/unix/sysv/linux/bits/fcntl-linux.h
@@ -201,6 +201,7 @@
 # define F_GETLEASE	1025	/* Enquire what lease is active.  */
 # define F_NOTIFY	1026	/* Request notifications on a directory.  */
 # define F_DUPFD_QUERY  1027    /* Compare two file descriptors for sameness.  */
+# define F_CREATED_QUERY 1028   /* Was the file just created?  */
 # define F_SETPIPE_SZ	1031	/* Set pipe page size array.  */
 # define F_GETPIPE_SZ	1032	/* Set pipe page size array.  */
 # define F_ADD_SEALS	1033	/* Add seals to file.  */
