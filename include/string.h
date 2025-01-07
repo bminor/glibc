a4c414796a4b7464b24f5e13f35042f3b7a2444b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2025-01-07 02:36:55 +0100

    include/string.h: Also redirect calls if not inlined in libpthread
    htl's pt-alloc.c calls __mempcpy, which is #defined to
    __builtin_mempcpy, but which does not happen to get inlined (the size is
    dynamic), and then gcc emits a reference to mempcpy, thus violating
    symbol exposition standard. We thus also have to redirect such
    references to __mempcpy too.

--- include/string.h
+++ include/string.h
@@ -175,7 +175,7 @@ extern __typeof (strnlen) strnlen attribute_hidden;
 extern __typeof (strsep) strsep attribute_hidden;
 #endif
 
-#if IS_IN (libc) && !defined SHARED \
+#if (IS_IN (libc) || IS_IN (libpthread)) && !defined SHARED \
   && !defined NO_MEMPCPY_STPCPY_REDIRECT
 /* Redirect calls to __builtin_mempcpy and __builtin_stpcpy to call
    __mempcpy and __stpcpy if not inlined.  */
