8d55f2fb17f7069b3b10231f9673eba1a0d272eb
Sam James <sam@gentoo.org>
2025-01-25 05:34:20 +0000

    elf: fix 'valgrind' typo in comment

--- elf/valgrind-test.c
+++ elf/valgrind-test.c
@@ -1,5 +1,5 @@
 /* This is the simple test intended to be called by
-   tst-valgrind-smoke to perform vagrind smoke test.
+   tst-valgrind-smoke to perform valgrind smoke test.
    Copyright (C) 2022-2025 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
