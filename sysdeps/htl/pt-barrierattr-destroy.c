40cbd3c361f8460a53484d6803b96618119d078b
gfleury <gfleury@disroot.org>
2025-02-09 22:01:02 +0200

    htl: move pthread_barrierattr_destroy into libc.
    Message-ID: <20250209200108.865599-3-gfleury@disroot.org>

--- sysdeps/htl/pt-barrierattr-destroy.c
+++ sysdeps/htl/pt-barrierattr-destroy.c
@@ -16,11 +16,18 @@
    License along with the GNU C Library;  if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <pthread.h>
+#include <pthreadP.h>
 #include <pt-internal.h>
+#include <shlib-compat.h>
 
 int
-pthread_barrierattr_destroy (pthread_barrierattr_t *attr)
+__pthread_barrierattr_destroy (pthread_barrierattr_t *attr)
 {
   return 0;
 }
+libc_hidden_def (__pthread_barrierattr_destroy)
+versioned_symbol (libc, __pthread_barrierattr_destroy, pthread_barrierattr_destroy, GLIBC_2_42);
+
+#if OTHER_SHLIB_COMPAT (libpthread, GLIBC_2_12, GLIBC_2_42)
+compat_symbol (libpthread, __pthread_barrierattr_destroy, pthread_barrierattr_destroy, GLIBC_2_12);
+#endif
