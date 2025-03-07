ee3b1d15da412be19583085f81c220653b270c1f
Joseph Myers <josmyers@redhat.com>
2025-03-07 17:38:38 +0000

    Use binutils 2.44 branch and Linux 6.13 in build-many-glibcs.py
    Tested with build-many-glibcs.py (host-libraries, compilers and glibcs
    builds).

--- scripts/build-many-glibcs.py
+++ scripts/build-many-glibcs.py
@@ -827,11 +827,11 @@ class Context(object):
 
     def checkout(self, versions):
         """Check out the desired component versions."""
-        default_versions = {'binutils': 'vcs-2.43',
+        default_versions = {'binutils': 'vcs-2.44',
                             'gcc': 'vcs-14',
                             'glibc': 'vcs-mainline',
                             'gmp': '6.3.0',
-                            'linux': '6.12',
+                            'linux': '6.13',
                             'mpc': '1.3.1',
                             'mpfr': '4.2.1',
                             'mig': 'vcs-mainline',
