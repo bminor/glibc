5c94ce98f77179c94264176525b001888d6664a8
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-04-21 17:27:54 -0300

    elf: Fix UB on _dl_map_object_from_fd
    On 32-bit architecture ubsan triggers:
    
    UBSAN: Undefined behaviour in dl-load.c:1345:54 pointer index expression with base 0x00612508 overflowed  to 0xf7c3a508
    
    Use explicit uintptr_t operation instead.

--- elf/dl-load.c
+++ elf/dl-load.c
@@ -1340,7 +1340,7 @@ cannot enable executable stack as shared object requires");
 
   /* Adjust the address of the TLS initialization image.  */
   if (l->l_tls_initimage != NULL)
-    l->l_tls_initimage = (char *) l->l_tls_initimage + l->l_addr;
+    l->l_tls_initimage = (void*)((uintptr_t)l->l_tls_initimage + l->l_addr);
 
   /* Process program headers again after load segments are mapped in
      case processing requires accessing those segments.  Scan program
