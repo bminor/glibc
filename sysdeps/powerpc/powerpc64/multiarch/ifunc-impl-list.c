84bdbf8a6f2fdafd3661489dbb7f79835a52da82
Carlos O'Donell <carlos@redhat.com>
2025-06-11 09:19:17 -0400

    ppc64le: Revert "powerpc: Optimized strncmp for power10" (CVE-2025-5745)
    This reverts commit 23f0d81608d0ca6379894ef81670cf30af7fd081
    
    Reason for revert: Power10 strncmp clobbers non-volatile vector
    registers (Bug 33060)
    
    Tested on ppc64le with no regressions.
    
    (cherry picked from commit 63c60101ce7c5eac42be90f698ba02099b41b965)

--- sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
@@ -164,9 +164,6 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
   /* Support sysdeps/powerpc/powerpc64/multiarch/strncmp.c.  */
   IFUNC_IMPL (i, name, strncmp,
 #ifdef __LITTLE_ENDIAN__
-	      IFUNC_IMPL_ADD (array, i, strncmp, hwcap2 & PPC_FEATURE2_ARCH_3_1
-			      && hwcap & PPC_FEATURE_HAS_VSX,
-			      __strncmp_power10)
 	      IFUNC_IMPL_ADD (array, i, strncmp, hwcap2 & PPC_FEATURE2_ARCH_3_00
 			      && hwcap & PPC_FEATURE_HAS_ALTIVEC,
 			      __strncmp_power9)
