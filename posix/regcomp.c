a66bc3941ff298e474d5f02d0c3303401951141f
Florian Weimer <fweimer@redhat.com>
2025-07-21 21:43:49 +0200

    posix: Fix double-free after allocation failure in regcomp (bug 33185)
    If a memory allocation failure occurs during bracket expression
    parsing in regcomp, a double-free error may result.
    
    Reported-by: Anastasia Belova <abelova@astralinux.ru>
    Co-authored-by: Paul Eggert <eggert@cs.ucla.edu>
    Reviewed-by: Andreas K. Huettel <dilfridge@gentoo.org>
    (cherry picked from commit 7ea06e994093fa0bcca0d0ee2c1db271d8d7885d)

--- posix/regcomp.c
+++ posix/regcomp.c
@@ -3383,6 +3383,7 @@ parse_bracket_exp (re_string_t *regexp, re_dfa_t *dfa, re_token_t *token,
     {
 #ifdef RE_ENABLE_I18N
       free_charset (mbcset);
+      mbcset = NULL;
 #endif
       /* Build a tree for simple bracket.  */
       br_token.type = SIMPLE_BRACKET;
@@ -3398,7 +3399,8 @@ parse_bracket_exp (re_string_t *regexp, re_dfa_t *dfa, re_token_t *token,
  parse_bracket_exp_free_return:
   re_free (sbcset);
 #ifdef RE_ENABLE_I18N
-  free_charset (mbcset);
+  if (__glibc_likely (mbcset != NULL))
+    free_charset (mbcset);
 #endif /* RE_ENABLE_I18N */
   return NULL;
 }
