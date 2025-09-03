47975914fb106b83c42bc0baf6435a0944a23d30
Peter Bergner <bergner@tenstorrent.com>
2025-09-03 14:26:03 -0500

    riscv: Add vector registers to __SYSCALL_CLOBBERS
    The Linux kernel ABI specifies that the vector registers are not preserved
    across system calls, but the __SYSCALL_CLOBBERS macro doesn't mention them.
    This could possibly lead to compilers trying to keep data in the vector
    registers across the syscall leading to corruption.  Add the vector registers
    to __SYSCALL_CLOBBERS when the vector extension is enabled.  If the vector
    extension is enabled, then require GCC 15 or later and RVV 1.0 or later.
    
    Fixes: 36960f0c76 ("RISC-V: Linux Syscall Interface")
    
    Signed-off-by: Peter Bergner <bergner@tenstorrent.com>

--- sysdeps/unix/sysv/linux/riscv/sysdep.h
+++ sysdeps/unix/sysv/linux/riscv/sysdep.h
@@ -355,7 +355,14 @@
 	_sys_result;							\
 })
 
+#ifdef __riscv_v
+# define __SYSCALL_CLOBBERS "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", \
+			     "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", \
+			     "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", \
+			     "v30", "v31", "vl", "vtype", "vxrm", "vxsat", "memory"
+#else
 # define __SYSCALL_CLOBBERS "memory"
+#endif
 
 extern long int __syscall_error (long int neg_errno);
 
