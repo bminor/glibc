7ebac6bf9438a4338497a2e80cdcdffdd91c2542
Samuel Thibault <samuel.thibault@ens-lyon.org>
2025-09-23 21:38:46 +0200

    hurd: Fix building on i686-gnu
    vm_size_t and rlim_t may not be the same type.
    
    Fixes commit 802b0eba519b1333c639fd0e1b55222099df9263
    ("hurd: implement RLIMIT_AS against Mach RPCs")

--- hurd/hurdrlimit.c
+++ hurd/hurdrlimit.c
@@ -38,8 +38,12 @@ init_rlimit (void)
   __mutex_init (&_hurd_rlimit_lock);
 
 #ifdef HAVE_MACH_VM_GET_SIZE_LIMIT
-  __vm_get_size_limit (__mach_task_self (),
-      &_hurd_rlimits[RLIMIT_AS].rlim_cur, &_hurd_rlimits[RLIMIT_AS].rlim_max);
+  vm_size_t current, max;
+  if (__vm_get_size_limit (__mach_task_self (), &current, &max) == KERN_SUCCESS)
+    {
+      _hurd_rlimits[RLIMIT_AS].rlim_cur = current;
+      _hurd_rlimits[RLIMIT_AS].rlim_max = max;
+    }
 #endif
 
   for (i = 0; i < RLIM_NLIMITS; ++i)
