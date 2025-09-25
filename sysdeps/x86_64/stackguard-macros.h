e47728a77c38926945d3759f8654106b18be9427
Uros Bizjak <ubizjak@gmail.com>
2025-09-25 09:50:13 +0200

    x86: Simplify stack and pointer guard macros
    Replace manual casts with a direct `(__tcbhead_t __seg_gs *)0`
    dereferences for `stack_guard` and `pointer_guard`.  This makes
    the macros more straightforward and removes the dependency on
    <stdint.h>.
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>

--- sysdeps/x86_64/stackguard-macros.h
+++ sysdeps/x86_64/stackguard-macros.h
@@ -1,7 +1,3 @@
-#include <stdint.h>
+#define STACK_CHK_GUARD (((tcbhead_t __seg_fs *)0)->stack_guard)
 
-#define STACK_CHK_GUARD \
-  (*(uintptr_t __seg_fs *) offsetof (tcbhead_t, stack_guard))
-
-#define POINTER_CHK_GUARD \
-  (*(uintptr_t __seg_fs *) offsetof (tcbhead_t, pointer_guard))
+#define POINTER_CHK_GUARD (((tcbhead_t __seg_fs *)0)->pointer_guard)
