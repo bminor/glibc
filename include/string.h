bd6b49e11dfe6cb3c9da5a50b79d9429f13b5d62
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-10-31 17:00:42 -0300

    string: Check if attribute can declared after function declaration
    Some symbols that might be auto-generated by the compiler are redefined
    to internal alias (for instance mempcpy to __mempcpy).  However, if fortify
    is enabled, the fortify wrapper is define before the alias re-defined and
    clang warns attribute declaration must precede definition.
    
    Use an asm alias if compiler does not support it, instead of an
    attribute.
    
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>

--- include/string.h
+++ include/string.h
@@ -188,8 +188,13 @@ extern __typeof (strsep) strsep attribute_hidden;
   && !defined NO_MEMPCPY_STPCPY_REDIRECT
 /* Redirect calls to __builtin_mempcpy and __builtin_stpcpy to call
    __mempcpy and __stpcpy if not inlined.  */
+# ifdef ATTR_AFTER_FUNC_DECL
 extern __typeof (mempcpy) mempcpy __asm__ ("__mempcpy");
 extern __typeof (stpcpy) stpcpy __asm__ ("__stpcpy");
+# else
+__asm__ ("mempcpy = __mempcpy");
+__asm__ ("stpcpy = __stpcpy");
+# endif
 #endif
 
 extern void *__memcpy_chk (void *__restrict __dest,
