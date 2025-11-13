2254e871f49b8844a77367318d8b9cd8ca839374
Florian Weimer <fweimer@redhat.com>
2025-11-13 09:46:15 +0100

    hppa: Consistently reference LGPL in copyright header
    The file was added with a GPL reference (but LGPL statement) in
    commit 0d6bed71502f053fa702ccbb7dd4fa6741b2a0ed ("hppa: Add
    ____longjmp_check C implementation.").
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Reviewed-by: Collin Funk <collin.funk1@gmail.com>

--- sysdeps/unix/sysv/linux/hppa/____longjmp_chk.c
+++ sysdeps/unix/sysv/linux/hppa/____longjmp_chk.c
@@ -11,10 +11,9 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with GCC; see the file COPYING.  If not, write to the Free
-   Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
 
 #include <stdio.h>
 #include <signal.h>
