c6f151839b0e2ac29f8f825a3f0a07d774e4f3a7
Florian Weimer <fweimer@redhat.com>
2025-11-17 11:15:13 +0100

    Reference COPYING.LIB in <sframe.h> copyright header
    Commit 3360913c37f5c3a0c8737b76321fd5ce2608fcd5 ("elf: Add SFrame
    stack tracing") added this file with an inconsistent copyright header.
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>

--- sysdeps/generic/sframe.h
+++ sysdeps/generic/sframe.h
@@ -12,9 +12,9 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; see the file COPYING.  If not see
-   <http://www.gnu.org/licenses/>.  */
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
 
 #ifndef	_SFRAME_H
 #define	_SFRAME_H
